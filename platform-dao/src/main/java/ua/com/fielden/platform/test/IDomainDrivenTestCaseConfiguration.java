ee4d544b1743eb0e808219b4d31eda13b281415f
01es <oles.hodych@gmail.com>
2011-08-31 11:49:21 +0000

    refs #311
    Implemented domain driven base test case class.
    
    git-svn-id: http://kid/svn/trident-genesis/trunk@3382 50ee8237-08a1-5d4c-b143-727193748607

---
+++ platform-dao/src/main/java/ua/com/fielden/platform/test/IDomainDrivenTestCaseConfiguration.java
@@ -0,0 +1,22 @@
+package ua.com.fielden.platform.test;
+
+import java.util.Properties;
+
+import ua.com.fielden.platform.entity.factory.EntityFactory;
+import ua.com.fielden.platform.entity.meta.DomainMetaPropertyConfig;
+import ua.com.fielden.platform.entity.validation.DomainValidationConfig;
+
+/**
+ * Contract for configuration used by db driven test cases such as {@link AbstractDomainDrivenTestCase}.
+ *
+ * @author TG Team
+ *
+ */
+public interface IDomainDrivenTestCaseConfiguration {
+    final static Properties hbc = new Properties();
+
+    EntityFactory getEntityFactory();
+    <T> T getInstance(Class<T> type);
+    DomainValidationConfig getDomainValidationConfig();
+    DomainMetaPropertyConfig getDomainMetaPropertyConfig();
+}
