0422fb1b1eeea5a8edba69b9bfefbd7f7b001ea0
01es <oles.hodych@gmail.com>
2011-10-06 14:42:28 +0000

    refs #308
    Implemented support for easy generation of the development databases in the TG-based applications.
    
    git-svn-id: http://kid/svn/trident-genesis/trunk@3438 50ee8237-08a1-5d4c-b143-727193748607

---
+++ platform-dao/src/main/java/ua/com/fielden/platform/devdb_support/DomainDrivenDataPopulation.java
@@ -0,0 +1,168 @@
+package ua.com.fielden.platform.devdb_support;
+
+import static java.lang.String.format;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import ua.com.fielden.platform.dao.IEntityDao;
+import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.entity.DynamicEntityKey;
+import ua.com.fielden.platform.entity.factory.EntityFactory;
+import ua.com.fielden.platform.entity.factory.IDefaultConrollerProvider;
+import ua.com.fielden.platform.persistence.DdlGenerator;
+import ua.com.fielden.platform.test.IDomainDrivenTestCaseConfiguration;
+
+
+/**
+ * This is a base class for implementing development data population in a domain driven manner.
+ * Reuses {@link IDomainDrivenTestCaseConfiguration} for configuration of application specific IoC modules.
+ *
+ * @author TG Team
+ *
+ */
+public abstract class DomainDrivenDataPopulation {
+
+    private final List<String> dataScript = new ArrayList<String>();
+    private final List<String> truncateScript = new ArrayList<String>();
+
+    public final IDomainDrivenTestCaseConfiguration config;
+
+    private final IDefaultConrollerProvider provider;
+    private final EntityFactory factory;
+    private final DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+
+    private boolean domainPopulated = false;
+
+    protected DomainDrivenDataPopulation(final IDomainDrivenTestCaseConfiguration config) {
+	try {
+	    this.config = config;
+	    provider = config.getInstance(IDefaultConrollerProvider.class);
+	    factory = config.getEntityFactory();
+	} catch (final Exception e) {
+	    throw new IllegalStateException(e);
+	}
+    }
+
+    /**
+     * Should be implemented in order to provide domain driven data population.
+     */
+    protected abstract void populateDomain();
+
+    /**
+     * Should return a complete list of domain entity types.
+     *
+     * @return
+     */
+    protected abstract List<Class<? extends AbstractEntity>> domainEntityTypes();
+
+    public final void removeDbSchema() {
+	domainPopulated = false;
+	dataScript.clear();
+	truncateScript.clear();
+    }
+
+    /**
+     * The entry point to trigger creation of the database and its population.
+     *
+     * @throws Exception
+     */
+    public final void createAndPopulate() throws Exception {
+	final Connection conn = createConnection();
+
+	if (domainPopulated) {
+	    // apply data population script
+	    exec(dataScript, conn);
+	} else {
+	    populateDomain();
+
+	    // record data population statements
+	    final Statement st = conn.createStatement();
+	    final ResultSet set = st.executeQuery("SCRIPT");
+	    while (set.next()) {
+		final String result = set.getString(1).toUpperCase().trim();
+		if (!result.startsWith("INSERT INTO PUBLIC.UNIQUE_ID") && (result.startsWith("INSERT") || result.startsWith("UPDATE") || result.startsWith("DELETE"))) {
+		    dataScript.add(result);
+		}
+	    }
+	    set.close();
+	    st.close();
+
+	    // create truncate statements
+	    final DdlGenerator ddlGen = new DdlGenerator();
+	    for (final Class<? extends AbstractEntity> entityType : domainEntityTypes()) {
+		final String tableName = ddlGen.getTableClause(entityType);
+		truncateScript.add(format("TRUNCATE TABLE %s;", tableName));
+	    }
+
+	    domainPopulated = true;
+	}
+
+	conn.close();
+    }
+
+    private void exec(final List<String> statements, final Connection conn) throws SQLException {
+	final Statement st = conn.createStatement();
+	for (final String stmt : statements) {
+	    st.execute(stmt);
+	}
+	st.close();
+    }
+
+    public final void cleanData() throws Exception {
+	final Connection conn = createConnection();
+	exec(truncateScript, conn);
+    }
+
+    private static Connection createConnection() {
+	final String url = IDomainDrivenTestCaseConfiguration.hbc.getProperty("hibernate.connection.url");
+	final String jdbcDriver = IDomainDrivenTestCaseConfiguration.hbc.getProperty("hibernate.connection.driver_class");
+	final String user = IDomainDrivenTestCaseConfiguration.hbc.getProperty("hibernate.connection.username");
+	final String passwd = IDomainDrivenTestCaseConfiguration.hbc.getProperty("hibernate.connection.password");
+
+	try {
+	    Class.forName(jdbcDriver);
+	    return DriverManager.getConnection(url, user, passwd);
+	} catch (final Exception e) {
+	    throw new IllegalStateException(e);
+	}
+    }
+
+    public final <T> T getInstance(final Class<T> type) {
+	return config.getInstance(type);
+    }
+
+    public final <T extends AbstractEntity> T save(final T instance) {
+	final IEntityDao<T> pp = provider.findController(instance.getType());
+	return pp.save(instance);
+    }
+
+    public final <T extends IEntityDao<E>, E extends AbstractEntity> T ao(final Class<E> type) {
+	return (T) provider.findController(type);
+    }
+
+    public final Date date(final String dateTime) {
+	return formatter.parseDateTime(dateTime).toDate();
+    }
+
+    public final <T extends AbstractEntity<K>, K extends Comparable> T new_(final Class<T> entityClass, final K key, final String desc) {
+	return factory.newEntity(entityClass, key, desc);
+    }
+
+    public final <T extends AbstractEntity<K>, K extends Comparable> T new_(final Class<T> entityClass, final K key) {
+	return factory.newByKey(entityClass, key);
+    }
+
+    public final <T extends AbstractEntity<DynamicEntityKey>> T new_(final Class<T> entityClass, final Object... keys) {
+	return factory.newByKey(entityClass, keys);
+    }
+}
---
+++ platform-dao/src/main/java/ua/com/fielden/platform/devdb_support/SecurityTokenAssociator.java
@@ -0,0 +1,37 @@
+package ua.com.fielden.platform.devdb_support;
+
+import ua.com.fielden.platform.algorithm.search.ITreeNodePredicate;
+import ua.com.fielden.platform.dao.IEntityDao;
+import ua.com.fielden.platform.entity.factory.EntityFactory;
+import ua.com.fielden.platform.security.ISecurityToken;
+import ua.com.fielden.platform.security.provider.SecurityTokenNode;
+import ua.com.fielden.platform.security.user.SecurityRoleAssociation;
+import ua.com.fielden.platform.security.user.UserRole;
+
+/**
+ * A predicate, which saves associations between nodes' state as security token and the specified role while traversing the tree.
+ * Its primary purpose is to create associations between the specified role and all security tokens.
+ *
+ * @author TG Team
+ *
+ */
+public class SecurityTokenAssociator implements ITreeNodePredicate<Class<? extends ISecurityToken>, SecurityTokenNode> {
+
+    private final UserRole role;
+    private final IEntityDao<SecurityRoleAssociation> controller;
+
+    public SecurityTokenAssociator(final UserRole role, final IEntityDao<SecurityRoleAssociation> controller) {
+	this.role = role;
+	this.controller = controller;
+    }
+
+    @Override
+    public boolean eval(final SecurityTokenNode node) {
+	final EntityFactory factory = role.getEntityFactory();
+	final SecurityRoleAssociation assoc = factory.newByKey(SecurityRoleAssociation.class, node.state(), role);
+	controller.save(assoc);
+
+	return false;
+    }
+
+}
