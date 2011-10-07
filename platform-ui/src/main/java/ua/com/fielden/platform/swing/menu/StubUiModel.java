daa7faeac2c62effa38d1b54393c93d52f80c940
01es <oles.hodych@gmail.com>
2011-10-07 12:54:19 +0000

    refs #308
    Moved stub UI model to the platform in order to be reused in other TG-base applications.
    
    git-svn-id: http://kid/svn/trident-genesis/trunk@3440 50ee8237-08a1-5d4c-b143-727193748607

---
+++ platform-ui/src/main/java/ua/com/fielden/platform/swing/menu/StubUiModel.java
@@ -0,0 +1,87 @@
+package ua.com.fielden.platform.swing.menu;
+
+import java.util.Map;
+
+import javax.swing.Action;
+
+import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.swing.components.blocking.BlockingIndefiniteProgressPane;
+import ua.com.fielden.platform.swing.ei.editors.ILightweightPropertyBinder;
+import ua.com.fielden.platform.swing.model.UModel;
+
+/**
+ * This is a stub UI model implementing UiModel, which should be used only when there is no other more appropriate model implementation.
+ *
+ * @author TG Team
+ *
+ */
+
+@SuppressWarnings("unchecked")
+public class StubUiModel extends UModel {
+
+    public StubUiModel(final boolean lazy) {
+	super(null, null, null, lazy);
+    }
+
+    @Override
+    protected void preInit(final BlockingIndefiniteProgressPane blockingPane) {
+	blockingPane.setText("Initialising...");
+    }
+
+    @Override
+    protected void doInit(final BlockingIndefiniteProgressPane blockingPane) {
+    }
+
+    @Override
+    protected void postInit(final BlockingIndefiniteProgressPane blockingPane) {
+	if (getView() != null) {
+	    getView().buildUi();
+	}
+	blockingPane.setText("Completed initialisation");
+    }
+
+    @Override
+    protected Map buildEditors(final AbstractEntity entity, final Object controller, final ILightweightPropertyBinder propertyBinder) {
+	return null;
+    }
+
+    @Override
+    protected Action createCancelAction() {
+	return null;
+    }
+
+    @Override
+    protected Action createEditAction() {
+	return null;
+    }
+
+    @Override
+    protected Action createNewAction() {
+	return null;
+    }
+
+    @Override
+    protected Action createRefreshAction() {
+	return null;
+    }
+
+    @Override
+    protected Action createSaveAction() {
+	return null;
+    }
+
+    @Override
+    protected AbstractEntity getManagedEntity() {
+	return null;
+    }
+
+    @Override
+    protected void notifyActionStageChange(final ActionStage actionState) {
+    }
+
+    @Override
+    protected Action createDeleteAction() {
+	return null;
+    }
+
+}
