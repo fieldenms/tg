c4dd31c91f6d2a7aed9c66a450a7d709d02185bb
01es <oles.hodych@gmail.com>
2011-11-10 11:25:40 +0000

    Implemented an abstract base class, which can be conveniently used for implemening double click action on cells of the EGI embedded into Entity Centres representing some calculated values for displaying details of the value clicked.
    git-svn-id: http://kid/svn/trident-genesis/trunk@3468 50ee8237-08a1-5d4c-b143-727193748607

---
+++ platform-ui/src/main/java/ua/com/fielden/platform/swing/egi/models/AbstractEntityCentreDetailsClickAction.java
@@ -0,0 +1,92 @@
+package ua.com.fielden.platform.swing.egi.models;
+
+import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+import ua.com.fielden.platform.dao.IEntityDao;
+import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.swing.actions.BlockingLayerCommand;
+import ua.com.fielden.platform.swing.analysis.DetailsFrame;
+import ua.com.fielden.platform.swing.components.blocking.BlockingIndefiniteProgressLayer;
+import ua.com.fielden.platform.swing.components.blocking.IBlockingLayerProvider;
+import ua.com.fielden.platform.swing.egi.models.builders.PropertyTableModelBuilder;
+import ua.com.fielden.platform.swing.review.DetailsDynamicEntityReview;
+import ua.com.fielden.platform.swing.view.ICloseHook;
+import ua.com.fielden.platform.swing.view.UvEntityCentre;
+
+/**
+ * A base class to be used for implementing double click action to view details of some calculated values represented as part of an entity centre EGI.
+ *
+ * @author TG Team
+ *
+ */
+public abstract class AbstractEntityCentreDetailsClickAction
+	<MASTER_TYPE extends AbstractEntity,
+	 DETAILS_TYPE extends AbstractEntity,
+	 MASTER_VIEW extends UvEntityCentre<MASTER_TYPE, ? extends IEntityDao<MASTER_TYPE>, ?, ?>> extends BlockingLayerCommand<MASTER_TYPE> {
+    private static final long serialVersionUID = 1L;
+
+    protected final String propertyName;
+    protected final MASTER_VIEW view;
+    protected final Map<String, DetailsFrame> detailsCache = new HashMap<String, DetailsFrame>();
+
+    public AbstractEntityCentreDetailsClickAction(//
+	    final String propertyName, // calculated property for which the details need to be displayed
+	    final MASTER_VIEW view) { // the view from which the details are invoked
+	super("Details action", new BlockingProvider<MASTER_TYPE, DETAILS_TYPE, MASTER_VIEW>(view));
+	this.propertyName = propertyName;
+	this.view = view;
+    }
+
+    @Override
+    protected final MASTER_TYPE action(final ActionEvent e) throws Exception {
+	return view.getEntityGridInspector().getActualModel().getSelectedEntity();
+    }
+
+    @Override
+    protected final void postAction(final MASTER_TYPE value) {
+	final String frameTitle =  frameTitle(value);
+	// make the key base on the report configuration title and the frame title in order to separate details frames for different configurations of the same report type
+	final String detailsFrameKey = view.getSelectedTabTitle() + frameTitle;
+	DetailsFrame detailsFrame = detailsCache.get(detailsFrameKey);
+	if (detailsFrame == null) {
+	    detailsFrame = new DetailsFrame(detailsFrameKey, frameTitle, getDetailsEntityReview(value), new ICloseHook<DetailsFrame>() {
+		@Override
+		public void closed(final DetailsFrame frame) {
+		    detailsCache.remove(frame.getAssociatedEntity());
+		}
+
+	    });
+	    detailsCache.put(detailsFrameKey, detailsFrame);
+	}
+	detailsFrame.setVisible(true);
+	super.postAction(value);
+    }
+
+
+    protected abstract String frameTitle(final MASTER_TYPE value);
+
+    protected abstract DetailsDynamicEntityReview<DETAILS_TYPE, ? extends IEntityDao<DETAILS_TYPE>> getDetailsEntityReview(final MASTER_TYPE value);
+
+    protected abstract PropertyTableModelBuilder<DETAILS_TYPE> createPropertyTableModelBuilder();
+
+
+    /** Wrapper class. */
+    private static class BlockingProvider<MASTER_TYPE extends AbstractEntity,
+	 DETAILS_TYPE extends AbstractEntity,
+	 MASTER_VIEW extends UvEntityCentre<MASTER_TYPE, ? extends IEntityDao<MASTER_TYPE>, ?, ?>> implements IBlockingLayerProvider {
+
+	private final MASTER_VIEW view;
+
+	public BlockingProvider(final MASTER_VIEW view) {
+	    this.view = view;
+	}
+
+	@Override
+	public BlockingIndefiniteProgressLayer getBlockingLayer() {
+	    return view != null ? view.getReviewContract().getBlockingLayer() : null;
+	}
+
+    }
+}
