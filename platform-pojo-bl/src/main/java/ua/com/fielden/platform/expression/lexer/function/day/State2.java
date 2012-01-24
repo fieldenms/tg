440091ac975dfbd39911b8ed02b4274ff97eb7fa
01es <oles.hodych@gmail.com>
2012-01-24 11:12:59 +0000

    refs #286
    Enhanced the Expression Language with a lexer to support function DAY (+tests).
    
    git-svn-id: http://kid/svn/trident-genesis/trunk@3563 50ee8237-08a1-5d4c-b143-727193748607

---
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/expression/lexer/function/day/State2.java
@@ -0,0 +1,26 @@
+package ua.com.fielden.platform.expression.lexer.function.day;
+
+import ua.com.fielden.platform.expression.automata.AbstractState;
+import ua.com.fielden.platform.expression.automata.NoTransitionAvailable;
+
+/**
+ * Final and white space handling state.
+ *
+ * @author TG Team
+ *
+ */
+public class State2 extends AbstractState {
+
+    public State2() {
+	super("S2", false);
+    }
+
+    @Override
+    protected AbstractState transition(final char symbol) throws NoTransitionAvailable {
+	if (symbol == 'y' || symbol == 'Y') {
+	    return getAutomata().getState("S3");
+	}
+	throw new NoTransitionAvailable("Invalid symbol '" + symbol + "'" , this, symbol);
+    }
+
+}
---
+++ platform-pojo-bl/src/test/java/ua/com/fielden/platform/expression/lexer/DayTokenAutomataTest.java
@@ -0,0 +1,72 @@
+package ua.com.fielden.platform.expression.lexer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import ua.com.fielden.platform.expression.automata.NoTransitionAvailable;
+import ua.com.fielden.platform.expression.automata.SequenceRecognitionFailed;
+import ua.com.fielden.platform.expression.lexer.function.day.DayTokenAutomata;
+
+public class DayTokenAutomataTest {
+    private DayTokenAutomata automata = new DayTokenAutomata();
+
+    @Test
+    public void test_full_recognition_of_correct_sequences() throws NoTransitionAvailable, SequenceRecognitionFailed {
+	assertEquals("Incorrect recognition result", "DAY", automata.recognisePartiallyFromStart("DAY(", 0));
+	assertEquals("Incorrect recognition result", "DAY", automata.recognisePartiallyFromStart("   DaY (", 0));
+	assertEquals("Incorrect recognition result", "DAY", automata.recognisePartiallyFromStart("\t\ndAy\t(", 0));
+    }
+
+    @Test
+    public void test_recognition_of_partially_correct_sequences() throws NoTransitionAvailable, SequenceRecognitionFailed {
+	assertEquals("Incorrect recognition result", "DAY", automata.recognisePartiallyFromStart(" day ( property )", 0));
+	assertEquals("Incorrect recognition result", "DAY", automata.recognisePartiallyFromStart("\tDAY\t (\"", 0));
+    }
+
+    @Test
+    public void test_recognition_of_incorrect_sequences() {
+	try {
+	    automata.recognisePartiallyFromStart("", 0);
+	    fail("Should have failed");
+	} catch (final SequenceRecognitionFailed e) {
+	}
+	try {
+	    automata.recognisePartiallyFromStart("+DAY(", 0);
+	    fail("Should have failed");
+	} catch (final SequenceRecognitionFailed e) {
+	}
+	try {
+	    automata.recognisePartiallyFromStart("DAY_(", 0);
+	    fail("Should have failed");
+	} catch (final SequenceRecognitionFailed e) {
+	}
+	try {
+	    automata.recognisePartiallyFromStart("DA Y(", 0);
+	    fail("Should have failed");
+	} catch (final SequenceRecognitionFailed e) {
+	}
+	try {
+	    automata.recognisePartiallyFromStart("D AY(", 0);
+	    fail("Should have failed");
+	} catch (final SequenceRecognitionFailed e) {
+	}
+	try {
+	    automata.recognisePartiallyFromStart("DAY  ", 0);
+	    fail("Should have failed");
+	} catch (final SequenceRecognitionFailed e) {
+	}
+	try {
+	    automata.recognisePartiallyFromStart("DAY  d(", 0);
+	    fail("Should have failed");
+	} catch (final SequenceRecognitionFailed e) {
+	}
+	try {
+	    automata.recognisePartiallyFromStart("DAY)", 0);
+	    fail("Should have failed");
+	} catch (final SequenceRecognitionFailed e) {
+	}
+    }
+
+}
