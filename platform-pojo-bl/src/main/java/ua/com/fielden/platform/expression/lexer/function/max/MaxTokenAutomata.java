b341e99a610b4baf1bbddce10eb61e5d68958a43
01es <oles.hodych@gmail.com>
2012-01-24 10:56:14 +0000

    refs #286
    Improved lexer support for functions MIN and MAX.
    
    git-svn-id: http://kid/svn/trident-genesis/trunk@3561 50ee8237-08a1-5d4c-b143-727193748607

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/expression/lexer/function/max/MaxTokenAutomata.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/expression/lexer/function/max/MaxTokenAutomata.java
@@ -5,7 +5,7 @@ import ua.com.fielden.platform.expression.automata.SequenceRecognitionFailed;
 import ua.com.fielden.platform.expression.lexer.BaseNonDeterministicAutomata;
 
 /**
- * NDA for recognising AVG token of the expression language.
+ * NDA for recognising MAX token of the expression language.
  *
  * @author TG Team
  *
@@ -13,19 +13,19 @@ import ua.com.fielden.platform.expression.lexer.BaseNonDeterministicAutomata;
 public class MaxTokenAutomata extends BaseNonDeterministicAutomata {
 
     public MaxTokenAutomata() {
-	super(EgTokenCategory.AVG, TEXT_POST_PROCESSING.REMOVE_WS, new State0(), new State1(), new State2(), new State3(), new State4());
+	super(EgTokenCategory.MAX, TEXT_POST_PROCESSING.REMOVE_WS, new State0(), new State1(), new State2(), new State3(), new State4());
     }
 
     @Override
     public String recognisePartiallyFromStart(final String input, final Integer posInOriginalSequence) throws SequenceRecognitionFailed {
 	final String result = super.recognisePartiallyFromStart(input, posInOriginalSequence);
-	setCharsRecognised(getCharsRecognised() - 1); // need to decrease by one as the LPAREN after SUM was read, but should not be recognised as part of the token
+	setCharsRecognised(getCharsRecognised() - 1); // need to decrease by one as the LPAREN after MAX was read, but should not be recognised as part of the token
         return result.substring(0, result.length() - 1).toUpperCase(); // need to remove the left parenthesis
     }
 
     @Override
     public String toString() {
-        return "AVG token automata";
+        return "MAX token automata";
     }
 
 }
--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/expression/lexer/function/min/MinTokenAutomata.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/expression/lexer/function/min/MinTokenAutomata.java
@@ -5,7 +5,7 @@ import ua.com.fielden.platform.expression.automata.SequenceRecognitionFailed;
 import ua.com.fielden.platform.expression.lexer.BaseNonDeterministicAutomata;
 
 /**
- * NDA for recognising AVG token of the expression language.
+ * NDA for recognising MIN token of the expression language.
  *
  * @author TG Team
  *
@@ -13,19 +13,19 @@ import ua.com.fielden.platform.expression.lexer.BaseNonDeterministicAutomata;
 public class MinTokenAutomata extends BaseNonDeterministicAutomata {
 
     public MinTokenAutomata() {
-	super(EgTokenCategory.AVG, TEXT_POST_PROCESSING.REMOVE_WS, new State0(), new State1(), new State2(), new State3(), new State4());
+	super(EgTokenCategory.MIN, TEXT_POST_PROCESSING.REMOVE_WS, new State0(), new State1(), new State2(), new State3(), new State4());
     }
 
     @Override
     public String recognisePartiallyFromStart(final String input, final Integer posInOriginalSequence) throws SequenceRecognitionFailed {
 	final String result = super.recognisePartiallyFromStart(input, posInOriginalSequence);
-	setCharsRecognised(getCharsRecognised() - 1); // need to decrease by one as the LPAREN after SUM was read, but should not be recognised as part of the token
+	setCharsRecognised(getCharsRecognised() - 1); // need to decrease by one as the LPAREN after MIN was read, but should not be recognised as part of the token
         return result.substring(0, result.length() - 1).toUpperCase(); // need to remove the left parenthesis
     }
 
     @Override
     public String toString() {
-        return "AVG token automata";
+        return "MIN token automata";
     }
 
 }
