76c03c5c49458f0b301503287e2cf5954ab1366b
nazchk <nazar@fielden.com.au>
2012-01-25 13:09:44 +0000

    refs#324 Enhanced test.
    git-svn-id: http://kid/svn/trident-genesis/trunk@3581 50ee8237-08a1-5d4c-b143-727193748607

--- platform-pojo-bl/src/test/java/ua/com/fielden/platform/entity/query/model/elements/QueryYieldsEnhancementTest.java
+++ platform-pojo-bl/src/test/java/ua/com/fielden/platform/entity/query/model/elements/QueryYieldsEnhancementTest.java
@@ -41,4 +41,27 @@ public class QueryYieldsEnhancementTest extends BaseEntQueryTCase {
 
 	assertEquals("Incorrect yields enhancement", entValidQuery(explicitQry), entValidQuery(shortcutQry));
     }
+
+    @Test
+    public void test_model_with_no_explicit_yields_and_explicit_joins() {
+	final EntityResultQueryModel<TgVehicle> shortcutQry = select(VEHICLE). //
+		join(MODEL).as("model").on().prop("model").eq().prop("model.id"). //
+		model();
+
+	final EntityResultQueryModel<TgVehicle> explicitQry = select(VEHICLE). //
+		join(MODEL).as("model").on().prop("model").eq().prop("model.id"). //
+		yield().prop("id").as("id"). //
+		yield().prop("version").as("version"). //
+		yield().prop("key").as("key"). //
+		yield().prop("desc").as("desc"). //
+		yield().prop("replacedBy").as("replacedBy"). //
+		yield().prop("initDate").as("initDate"). //
+		yield().prop("model").as("model"). //
+		yield().prop("station").as("station"). //
+		yield().prop("price").as("price"). //
+		yield().prop("purchasePrice").as("purchasePrice"). //
+		modelAsEntity(VEHICLE);
+
+	assertEquals("Incorrect yields enhancement", entValidQuery(explicitQry), entValidQuery(shortcutQry));
+    }
 }
