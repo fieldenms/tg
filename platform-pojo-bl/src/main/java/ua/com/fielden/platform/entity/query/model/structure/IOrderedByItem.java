87ccf0545b0fbe6856fac555f8769fade1e75035
nazchk <nazar@fielden.com.au>
2012-02-03 20:12:09 +0000

    Removed obsolete classes
    git-svn-id: http://kid/svn/trident-genesis/trunk@3621 50ee8237-08a1-5d4c-b143-727193748607

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/query/model/structure/IOrderedByItem.java
+++
@@ -1,5 +0,0 @@
-package ua.com.fielden.platform.entity.query.model.structure;
-
-public interface IOrderedByItem extends IQueryItem  {
-
-}
--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/query/model/structure/IQueryItem.java
+++
@@ -1,8 +0,0 @@
-package ua.com.fielden.platform.entity.query.model.structure;
-
-import ua.com.fielden.platform.entity.query.model.transformation.IQuerySourceItem;
-
-public interface IQueryItem {
-    IQuerySourceItem getSource();
-
-}
