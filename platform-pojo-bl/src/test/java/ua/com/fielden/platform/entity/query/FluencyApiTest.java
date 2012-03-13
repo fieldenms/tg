b9866a243794a3cdeaec7d465529db98d09987e8
nazchk <nazar@fielden.com.au>
2012-03-13 15:06:47 +0000

    refs#282 Improved test readability.
    git-svn-id: http://kid/svn/trident-genesis/trunk@3760 50ee8237-08a1-5d4c-b143-727193748607

--- platform-pojo-bl/src/test/java/ua/com/fielden/platform/entity/query/FluencyApiTest.java
+++ platform-pojo-bl/src/test/java/ua/com/fielden/platform/entity/query/FluencyApiTest.java
@@ -8,10 +8,6 @@ import java.util.List;
 import java.util.Set;
 
 import junit.framework.TestCase;
-import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces;
-import ua.com.fielden.platform.entity.query.model.AggregatedResultQueryModel;
-import ua.com.fielden.platform.entity.query.model.EntityResultQueryModel;
-import ua.com.fielden.platform.entity.query.model.PrimitiveResultQueryModel;
 import ua.com.fielden.platform.sample.domain.TgVehicle;
 import static ua.com.fielden.platform.entity.query.fluent.query.select;
 
@@ -36,7 +32,27 @@ public class FluencyApiTest extends TestCase {
 
     private static String[] exists = new String[]{"exists", "notExists", "existsAnyOf", "notExistsAnyOf", "existsAllOf", "notExistsAllOf"};
 
-    private Set<String> getMethods(final Class<?> type) {
+    private static String model = "model";
+    private static String modelAsAggregate = "modelAsAggregate";
+    private static String prop = "prop";
+    private static String param = "param";
+    private static String iParam = "iParam";
+    private static String val = "val";
+    private static String iVal = "iVal";
+    private static String begin = "begin";
+    private static String notBegin = "notBegin";
+    private static String beginExpr = "beginExpr";
+    private static String expr = "expr";
+    private static String join = "join";
+    private static String as = "as";
+    private static String modelAsEntity = "modelAsEntity";
+    private static String modelAsPrimitive = "modelAsPrimitive";
+    private static String where = "where";
+    private static String groupBy = "groupBy";
+    private static String yield = "yield";
+    private static String leftJoin = "leftJoin";
+
+    private static Set<String> getMethods(final Class<?> type) {
 	final Set<String> result = new HashSet<String>();
 	for (final Method method : type.getMethods()) {
 	    if (!objectMethods.contains(method) && !"toString".equals(method.getName()) && !"getTokens".equals(method.getName())) {
@@ -46,7 +62,11 @@ public class FluencyApiTest extends TestCase {
 	return result;
     }
 
-    private String compare(final Set<String> set, final String[]...methods) {
+    private static String[] array(final String ... strings) {
+	return strings;
+    }
+
+    private static String compare(final Set<String> set, final String[]...methods) {
 	final List<String> methodList = new ArrayList<String>();
 	for (final String[] method : methods) {
 	    methodList.addAll(Arrays.asList(method));
@@ -70,107 +90,105 @@ public class FluencyApiTest extends TestCase {
 	return (missingMethods.size() + obsoleteMethods.size()) == 0 ? null : ((missingMethods.size() > 0 ? "Missing: " + missingMethods : "") + (obsoleteMethods.size() > 0 ? (" Obsolete: " + obsoleteMethods) : ""));
     }
 
-    public void test_IPlainJoin(){
-	final EntityQueryProgressiveInterfaces.IPlainJoin qry = select(TgVehicle.class);
-	final String result = compare(getMethods(qry.getClass()), new String[]{"model", "modelAsAggregate", "modelAsEntity" ,"where", "groupBy", "yield", "as", "join", "leftJoin"});
+    public static void checkFluency(final Object queryInterface, final String[]...methods) {
+	final String result = compare(getMethods(queryInterface.getClass()), methods);
 	assertNull(result, result);
+    }
+
+    public void test_IFromAlias(){
+	checkFluency( //
+		select(TgVehicle.class), //
+		array(model, modelAsAggregate, modelAsEntity, where, groupBy, yield, as, join, leftJoin));
+    }
+
+    @SuppressWarnings("unchecked")
+    public void test_IFromAlias2(){
+	checkFluency( //
+		select(select(TgVehicle.class).model()), //
+		array(model, modelAsAggregate, modelAsEntity, where, groupBy, yield, as, join, leftJoin));
+    }
+
+    public void test_IFromAlias3(){
+	checkFluency( //
+		select(select(TgVehicle.class).yield().countAll().as("count").modelAsAggregate()), //
+		array(model, modelAsAggregate, modelAsEntity, where, groupBy, yield, as, join, leftJoin));
     }
 
     public void test_IWhere0(){
-	final EntityQueryProgressiveInterfaces.IWhere0 qry = select(TgVehicle.class).where();
-	final String result = compare(getMethods(qry.getClass()), new String[]{"model", "prop", "param", "iParam", "val", "iVal", "begin", "notBegin", "beginExpr", "expr"}, exists, functions, anyOfs, allOfs);
-	assertNull(result, result);
+	checkFluency( //
+		select(TgVehicle.class).where(), //
+		array(model, prop, param, iParam, val, iVal, begin, notBegin, beginExpr, expr), exists, functions, anyOfs, allOfs);
     }
 
-    public void test_IFunctionLastArgument_1_ICompleted_1_(){
-	final EntityQueryProgressiveInterfaces.IFunctionLastArgument<EntityQueryProgressiveInterfaces.ICompleted> qry = select(TgVehicle.class).groupBy();
-	final String result = compare(getMethods(qry.getClass()), new String[]{"model", "prop", "param" , "iParam", "val", "iVal", "beginExpr", "expr"}, functions);
-	assertNull(result, result);
+    public void test_IWhere1(){
+	checkFluency( //
+		select(TgVehicle.class).where().begin(), //
+		array(model, prop, param, iParam, val, iVal, begin, notBegin, beginExpr, expr), exists, functions, anyOfs, allOfs);
     }
 
-//    public void test_IFunctionLastArgument_1_IOrder_2_ICompletedAndOrdered_2__1_(){
-//	final EntityQueryProgressiveInterfaces.IFunctionLastArgument<EntityQueryProgressiveInterfaces.IOrder<EntityQueryProgressiveInterfaces.ICompletedAndOrdered>> qry = select(TgVehicle.class).orderBy();
-//	final String result = compare(getMethods(qry.getClass()), new String[]{"model", "prop", "param", "iParam", "val", "iVal", "beginExpr", "expr"}, functions);
-//	assertNull(result, result);
-//    }
-
-    public void test_IFunctionYieldedLastArgument_1_IFirstYieldedItemAlias_2_ISubsequentCompletedAndYielded_2__1_(){
-	final EntityQueryProgressiveInterfaces.IFunctionYieldedLastArgument<EntityQueryProgressiveInterfaces.IFirstYieldedItemAlias<EntityQueryProgressiveInterfaces.ISubsequentCompletedAndYielded>> qry = select(TgVehicle.class).yield();
-	final String result = compare(getMethods(qry.getClass()), new String[]{"model", "prop", "param", "iParam", "val", "iVal", "beginExpr", "expr", "join"}, functions, aggregateFunctions);
-	assertNull(result, result);
+    public void test_IWhere2(){
+	checkFluency( //
+		select(TgVehicle.class).where().begin().begin(), //
+		array(model, prop, param, iParam, val, iVal, begin, notBegin, beginExpr, expr), exists, functions, anyOfs, allOfs);
     }
 
-    public void test_IFirstYieldedItemAlias_1_ISubsequentCompletedAndYielded_1_() {
-	final EntityQueryProgressiveInterfaces.IFirstYieldedItemAlias<EntityQueryProgressiveInterfaces.ISubsequentCompletedAndYielded> qry = select(TgVehicle.class).yield().prop("prop");
-	final String result = compare(getMethods(qry.getClass()), new String[]{"as", "modelAsEntity", "modelAsPrimitive", "modelAsPrimitive"});
-	assertNull(result, result);
+    public void test_IWhere3(){
+	checkFluency( //
+		select(TgVehicle.class).where().begin().begin().begin(), //
+		array(model, prop, param, iParam, val, iVal, beginExpr, expr), exists, functions, anyOfs, allOfs);
     }
 
-    public void test_(){
-	final EntityQueryProgressiveInterfaces.IFunctionLastArgument<EntityQueryProgressiveInterfaces.IFirstYieldedItemAlias<EntityQueryProgressiveInterfaces.ISubsequentCompletedAndYielded>> qry = select(TgVehicle.class).yield().secondOf();
-	final String result = compare(getMethods(qry.getClass()), new String[]{"model", "prop", "param", "iParam", "val", "iVal", "beginExpr", "expr"}, functions);
-	assertNull(result, result);
-	final EntityQueryProgressiveInterfaces.IFunctionLastArgument<EntityQueryProgressiveInterfaces.IComparisonOperator0> qry2 = select(TgVehicle.class).where().secondOf();
-	final String result2 = compare(getMethods(qry2.getClass()), new String[]{"model", "prop", "param", "iParam", "val", "iVal", "beginExpr", "expr"}, functions);
-	assertNull(result2, result2);
-
+    public void test_IFunctionLastArgument_with_ICompleted(){
+	checkFluency( //
+		select(TgVehicle.class).groupBy(), //
+		array(model, prop, param, iParam, val, iVal, beginExpr, expr), functions);
     }
 
-    public void test_2(){
-	final EntityQueryProgressiveInterfaces.IYieldExprItem0<EntityQueryProgressiveInterfaces.IFirstYieldedItemAlias<EntityQueryProgressiveInterfaces.ISubsequentCompletedAndYielded>> qry = select(TgVehicle.class).yield().beginExpr();
-	final String result = compare(getMethods(qry.getClass()), new String[]{"model", "prop", "param", "iParam", "val", "iVal", "beginExpr", "expr", "join"}, functions, aggregateFunctions);
-	assertNull(result, result);
-	final EntityQueryProgressiveInterfaces.IYieldExprItem1<EntityQueryProgressiveInterfaces.IFirstYieldedItemAlias<EntityQueryProgressiveInterfaces.ISubsequentCompletedAndYielded>> qry2 = select(TgVehicle.class).yield().beginExpr().beginExpr();
-	final String result2 = compare(getMethods(qry2.getClass()), new String[]{"model", "prop", "param", "iParam", "val", "iVal", "beginExpr", "expr", "join"}, functions, aggregateFunctions);
-	assertNull(result2, result2);
-
-	final EntityQueryProgressiveInterfaces.IWhere0 where = select(TgVehicle.class).where();
-	final EntityQueryProgressiveInterfaces.IWhere1 a = where.begin();
-	final EntityQueryProgressiveInterfaces.IWhere2 b = where.begin().begin();
-	final EntityQueryProgressiveInterfaces.IWhere3 c = where.begin().begin().begin();
-	final EntityQueryProgressiveInterfaces.ICompoundCondition2 d = where.begin().begin().begin().exists(null).end();
-	final EntityQueryProgressiveInterfaces.ICompoundCondition1 e = where.begin().begin().begin().exists(null).end().end();
-	final EntityQueryProgressiveInterfaces.ICompoundCondition0 e1 = where.begin().begin().begin().exists(null).end().end().end();
-
-	final EntityQueryProgressiveInterfaces.ISubsequentCompletedAndYielded qry11 = select(TgVehicle.class).yield().prop("a").as("a").yield().prop("b").as("b");
-	final EntityQueryProgressiveInterfaces.ISubsequentCompletedAndYielded qry22 = select(TgVehicle.class).yield().prop("a").as("a").yield().prop("b").as("b").yield().join("aa").as("bb");
+    public void test_IFunctionYieldedLastArgument_with_IFirstYieldedItemAlias_with_ISubsequentCompletedAndYielded(){
+	checkFluency( //
+		select(TgVehicle.class).yield(), //
+		array(model, prop, param, iParam, val, iVal, beginExpr, expr, join), functions, aggregateFunctions);
     }
 
-    public void test_yielding_aliases() {
-	//
-	final EntityResultQueryModel<TgVehicle> model1 = select(TgVehicle.class).yield().prop("prop1").modelAsEntity(TgVehicle.class/*Should be prop1 entity type*/);
-	//
-	final PrimitiveResultQueryModel model2a = select(TgVehicle.class).yield().beginExpr().prop("prop1").add().prop("prop2").endExpr().modelAsPrimitive();
-	//
-	final EntityResultQueryModel<TgVehicle> model3 = select(TgVehicle.class).yield().beginExpr().prop("prop1").add().prop("prop2").endExpr().as("sumOfProps").yield().model(null).as("newCalcProp").modelAsEntity(TgVehicle.class);
-	//
-	final AggregatedResultQueryModel model4 = select(TgVehicle.class).yield().beginExpr().prop("prop1").add().prop("prop2").endExpr().as("sumOfProps").yield().model(null).as("newCalcProp").modelAsAggregate();
-	//
-	//aliases are required of joining is used
-	select(TgVehicle.class).as("a").join(TgVehicle.class).as("b").on().prop("a.id").eq().prop("b.id").model();
-
+    public void test_IFirstYieldedItemAlias_with_ISubsequentCompletedAndYielded() {
+	checkFluency( //
+		select(TgVehicle.class).yield().prop("prop"), //
+		array(as, modelAsEntity, modelAsPrimitive));
     }
 
-//  public void test1(){
-//	final UnorderedQueryModel m1 = query.select(AbstractEntity.class).where().prop("p1").eq().val(1).model();
-//
-//	final EntityResultQueryModel<AbstractEntity> m2 = query.select(AbstractEntity.class).where().prop("p1").eq().val(1).yield().prop("entityProperty1").modelAsEntity(AbstractEntity.class);
-//
-//	final PrimitiveResultQueryModel m3 = query.select(AbstractEntity.class).where().prop("p1").eq().val(1).yield().sumOf().prop("decimalProperty1").modelAsPrimitive(BigDecimal.class);
-//
-//	final EntityResultQueryModel<AbstractEntity> m4 = query.select(AbstractEntity.class).where().prop("p1").eq().val(1).groupBy().prop("entityProperty1").yield().sumOf().prop("decimalProperty1").as("newProp1").modelAsEntity(AbstractEntity.class);
-//
-//	final AggregatedResultQueryModel m5 = query.select(AbstractEntity.class).where().prop("p1").eq().val(1).groupBy().prop("entityProperty1").yield().sumOf().prop("decimalProperty1").as("newProp1").yield().prop("entityProperty1").as("newProp2").modelAsAggregate();
-//
-//	final ua.com.fielden.platform.entity.query.model.QueryModel m1o = query.select(AbstractEntity.class).where().prop("p1").eq().val(1).orderBy().prop("p1").desc().modelAsEntity(AbstractEntity.class);
-//
-//	//final ua.com.fielden.platform.entity.query.model.QueryModel m2o = query.select(AbstractEntity.class).where().prop("p1").eq().val(1).yield().prop("entityProperty1").modelAsEntity(AbstractEntity.class);
-//
-//	//final ua.com.fielden.platform.entity.query.model.QueryModel m3o = query.select(AbstractEntity.class).where().prop("p1").eq().val(1).yield().sumOf().prop("decimalProperty1").modelAsPrimitive(BigDecimal.class);
-//
-//	final ua.com.fielden.platform.entity.query.model.QueryModel m4o = query.select(AbstractEntity.class).where().prop("p1").eq().val(1).groupBy().prop("entityProperty1").yield().sumOf().prop("decimalProperty1").as("newProp1").orderBy().prop("newProp1").desc().modelAsEntity(AbstractEntity.class);
-//
-//	final ua.com.fielden.platform.entity.query.model.QueryModel m5o = query.select(AbstractEntity.class).where().prop("p1").eq().val(1).groupBy().prop("entityProperty1").yield().sumOf().prop("decimalProperty1").as("newProp1").yield().prop("entityProperty1").as("newProp2").orderBy().prop("newProp1").desc().orderBy().prop("newProp2").asc().modelAsEntity(AbstractEntity.class);
-//
-//}
+    public void test_IFunctionLastArgument_with_IFirstYieldedItemAlias_with_ISubsequentCompletedAndYielded(){
+	checkFluency( //
+		select(TgVehicle.class).yield().secondOf(), //
+		array(model, prop, param, iParam, val, iVal, beginExpr, expr), functions);
+    }
+
+    public void test_IFunctionLastArgument_with_IComparisonOperator0(){
+	checkFluency( //
+		select(TgVehicle.class).where().secondOf(), //
+		array(model, prop, param, iParam, val, iVal, beginExpr, expr), functions);
+    }
+
+    public void test_IYieldExprItem0_with_IFirstYieldedItemAlias_with_ISubsequentCompletedAndYielded(){
+	checkFluency( //
+		select(TgVehicle.class).yield().beginExpr(), //
+		array(model, prop, param, iParam, val, iVal, beginExpr, expr, join), functions, aggregateFunctions);
+    }
+
+    public void test_IYieldExprItem1_with_IFirstYieldedItemAlias_with_ISubsequentCompletedAndYielded(){
+	checkFluency( //
+		select(TgVehicle.class).yield().beginExpr().beginExpr(), //
+		array(model, prop, param, iParam, val, iVal, beginExpr, expr, join), functions, aggregateFunctions);
+    }
+
+    public void test_IYieldExprItem2_with_IFirstYieldedItemAlias_with_ISubsequentCompletedAndYielded(){
+	checkFluency( //
+		select(TgVehicle.class).yield().beginExpr().beginExpr().beginExpr(), //
+		array(model, prop, param, iParam, val, iVal, beginExpr, expr, join), functions, aggregateFunctions);
+    }
+
+    public void test_IYieldExprItem3_with_IFirstYieldedItemAlias_with_ISubsequentCompletedAndYielded(){
+	checkFluency( //
+		select(TgVehicle.class).yield().beginExpr().beginExpr().beginExpr().beginExpr(), //
+		array(model, prop, param, iParam, val, iVal, expr, join), functions, aggregateFunctions);
+    }
 }
