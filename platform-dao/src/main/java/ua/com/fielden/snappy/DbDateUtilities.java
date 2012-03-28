00c9e4e92957ae7023bbaf4841c8b76774b29605
jhou-pro <jhou.pro@gmail.com>
2012-03-28 18:32:49 +0000

    refs #369
     * Removed ''rule-dao'' dependency from ''platform-dao''.
     * Migrated some classes (that are essential for both modules) to ''platform-dao''.
    
    git-svn-id: http://kid/svn/trident-genesis/trunk@3841 50ee8237-08a1-5d4c-b143-727193748607

---
+++ platform-dao/src/main/java/ua/com/fielden/snappy/DbDateUtilities.java
@@ -0,0 +1,36 @@
+package ua.com.fielden.snappy;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Query;
+
+public class DbDateUtilities extends DateUtilities {
+    /**
+     * This method initializes all date parameters that exist in "query" and are
+     * relative to previously retrieved "currentDbDate". </br> For instance :
+     * "BEG_PREV_MONTH", "MID_CURR_YEAR" or "END_NEXT_DAY" etc.
+     *
+     * @param query
+     * @param currentDbDate
+     * @return
+     */
+    public Query initializeSnappyDateParameters(final Query query, final Date currentDbDate) {
+	final List<String> namedParameters = Arrays.asList(query.getNamedParameters());
+	for (final String param : namedParameters) {
+	    final String[] paramParts = param.split("_");
+	    try {
+		final DateRangeSelectorEnum begMidEnd = DateRangeSelectorEnum.valueOf(paramParts[0]);
+		final DateRangePrefixEnum prevCurrNext = DateRangePrefixEnum.valueOf(paramParts[1]);
+		final MnemonicEnum rangeWidth = MnemonicEnum.valueOf(paramParts[2]);
+		final Date date = dateOfRangeThatIncludes(currentDbDate, begMidEnd, prevCurrNext, rangeWidth);
+		query.setDate(param, date);
+		System.err.println("param :" + param + " == " + date);
+	    } catch (final Exception e) {
+		throw new RuntimeException("Enumeration that represents date related stuff could not be retrived from date parameter  : " + param + "======= " + e.getMessage());
+	    }
+	}
+	return query;
+    }
+}
