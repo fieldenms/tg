f93442a36ffcb89bb3e34ecf6f59e1b05886dbae
01es <oles.hodych@gmail.com>
2012-04-19 15:07:14 +0000

    refs #383
    Removed accidentally committed test.
    
    git-svn-id: http://kid/svn/trident-genesis/trunk@3933 50ee8237-08a1-5d4c-b143-727193748607

--- platform-dao/src/test/java/ua/com/fielden/platform/dao/dynamic/DynamicQueryBuilderSqlTest.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/dao/dynamic/DynamicQueryBuilderSqlTest.java
@@ -23,8 +23,6 @@ import ua.com.fielden.platform.dao.HibernateMappingsGenerator;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.ICompleted;
 import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.IJoin;
-import ua.com.fielden.platform.entity.query.model.ExpressionModel;
-import ua.com.fielden.platform.expression.ExpressionText2ModelConverter;
 import ua.com.fielden.platform.persistence.types.DateTimeType;
 import ua.com.fielden.platform.persistence.types.SimpleMoneyType;
 import ua.com.fielden.platform.reflection.EntityDescriptor;
@@ -170,33 +168,6 @@ public class DynamicQueryBuilderSqlTest {
     ////////////////////////////////////////////////////////////////////////////////////////////
 
     ////////////////////////////////// 1.1. Range type /////////////////////////////////////////
-
-    @Test
-    public void test_atomic_query_composition_with_simple_expression() throws Exception {
-	set_up();
-	final QueryProperty property = queryProperties.get("integerCalculatedProp");
-	property.setValue(3);
-	property.setValue2(7);
-
-	final String cbn = property.getConditionBuildingName();
-
-	final ExpressionModel exprModel = new ExpressionText2ModelConverter(MasterEntity.class, "integerProp * 2").convert().getModel();
-	final ICompleted expected = //
-	/**/iJoin.where().begin() //
-	/*  */.begin().expr(exprModel).isNotNull().and() //
-	/*    */.begin().expr(exprModel).ge().iVal(3).and().expr(exprModel).le().iVal(7).end() //
-	/*  */.end() //
-	/**/.end(); //
-	final ICompleted actual = buildConditions(iJoin, new ArrayList<QueryProperty>(queryProperties.values()), alias);
-
-
-	assertEquals("Incorrect query sql has been built.", expected.model(), actual.model());
-	//assertEquals("Incorrect query parameter values has been built.", expected.model().getFinalModelResult(mappingExtractor).getParamValues(), actual.model().getFinalModelResult(mappingExtractor).getParamValues());
-
-
-	test_atomic_query_composition_for_range_type("integerProp");
-	test_atomic_query_composition_for_range_type("entityProp.integerProp");
-    }
 
     @Test
     public void test_atomic_query_composition_for_integer_range_type() {
