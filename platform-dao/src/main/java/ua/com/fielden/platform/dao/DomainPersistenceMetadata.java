77af9511ce32f02dd12af2a34301b30174b436aa
01es <oles.hodych@gmail.com>
2012-05-15 10:10:00 +0000

    refs #389
     Modified to skip processing of calculated properties.
     
    
    
    git-svn-id: http://kid/svn/trident-genesis/trunk@3997 50ee8237-08a1-5d4c-b143-727193748607

--- platform-dao/src/main/java/ua/com/fielden/platform/dao/DomainPersistenceMetadata.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/dao/DomainPersistenceMetadata.java
@@ -1,5 +1,17 @@
 package ua.com.fielden.platform.dao;
 
+import static org.apache.commons.lang.StringUtils.isEmpty;
+import static org.apache.commons.lang.StringUtils.isNotEmpty;
+import static ua.com.fielden.platform.reflection.AnnotationReflector.getAnnotation;
+import static ua.com.fielden.platform.reflection.AnnotationReflector.getKeyType;
+import static ua.com.fielden.platform.reflection.AnnotationReflector.getPropertyAnnotation;
+import static ua.com.fielden.platform.reflection.PropertyTypeDeterminator.determinePropertyType;
+import static ua.com.fielden.platform.utils.EntityUtils.getCalculatedProperties;
+import static ua.com.fielden.platform.utils.EntityUtils.getPersistedProperties;
+import static ua.com.fielden.platform.utils.EntityUtils.isPersistedEntityType;
+import static ua.com.fielden.platform.utils.EntityUtils.isPropertyPartOfKey;
+import static ua.com.fielden.platform.utils.EntityUtils.isPropertyRequired;
+
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,18 +42,6 @@ import ua.com.fielden.platform.reflection.AnnotationReflector;
 import ua.com.fielden.platform.utils.EntityUtils;
 
 import com.google.inject.Injector;
-
-import static org.apache.commons.lang.StringUtils.isEmpty;
-import static org.apache.commons.lang.StringUtils.isNotEmpty;
-import static ua.com.fielden.platform.reflection.AnnotationReflector.getAnnotation;
-import static ua.com.fielden.platform.reflection.AnnotationReflector.getKeyType;
-import static ua.com.fielden.platform.reflection.AnnotationReflector.getPropertyAnnotation;
-import static ua.com.fielden.platform.reflection.PropertyTypeDeterminator.determinePropertyType;
-import static ua.com.fielden.platform.utils.EntityUtils.getCalculatedProperties;
-import static ua.com.fielden.platform.utils.EntityUtils.getPersistedProperties;
-import static ua.com.fielden.platform.utils.EntityUtils.isPersistedEntityType;
-import static ua.com.fielden.platform.utils.EntityUtils.isPropertyPartOfKey;
-import static ua.com.fielden.platform.utils.EntityUtils.isPropertyRequired;
 
 
 /**
@@ -89,7 +89,9 @@ public class DomainPersistenceMetadata {
     private Set<PropertyPersistenceInfo> generatePPIsForCompositeTypeProps(final Set<PropertyPersistenceInfo> ppis) {
 	final Set<PropertyPersistenceInfo> result = new HashSet<PropertyPersistenceInfo>();
 	for (final PropertyPersistenceInfo ppi : ppis) {
-	    result.addAll(ppi.getCompositeTypeSubprops());
+	    if (!ppi.isCalculated()) {
+		result.addAll(ppi.getCompositeTypeSubprops());
+	    }
 	}
 	return result;
     }
