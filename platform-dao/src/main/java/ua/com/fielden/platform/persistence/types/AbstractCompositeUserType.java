6496bfac80f998cc32b6c28b7e4edbe9af80c3a0
01es <oles.hodych@gmail.com>
2012-05-18 13:59:49 +0000

    Provided '''Override''' annotations.
    git-svn-id: http://kid/svn/trident-genesis/trunk@4021 50ee8237-08a1-5d4c-b143-727193748607

--- platform-dao/src/main/java/ua/com/fielden/platform/persistence/types/AbstractCompositeUserType.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/persistence/types/AbstractCompositeUserType.java
@@ -8,26 +8,32 @@ import org.hibernate.usertype.CompositeUserType;
 
 public abstract class AbstractCompositeUserType implements CompositeUserType {
 
+    @Override
     public boolean isMutable() {
 	return false;
     }
 
+    @Override
     public Object deepCopy(final Object value) {
 	return value;
     }
 
+    @Override
     public Serializable disassemble(final Object value, final SessionImplementor session) {
 	return (Serializable) value;
     }
 
+    @Override
     public Object assemble(final Serializable cached, final SessionImplementor session, final Object owner) {
 	return cached;
     }
 
+    @Override
     public Object replace(final Object original, final Object target, final SessionImplementor session, final Object owner) {
 	return original;
     }
 
+    @Override
     public boolean equals(final Object x, final Object y) {
 	if (x == y) {
 	    return true;
@@ -38,10 +44,12 @@ public abstract class AbstractCompositeUserType implements CompositeUserType {
 	return x.equals(y);
     }
 
+    @Override
     public int hashCode(final Object x) {
 	return x.hashCode();
     }
 
+    @Override
     public void setPropertyValue(final Object component, final int property, final Object value) {
 	throw new UnsupportedOperationException("This type is immutable");
     }
