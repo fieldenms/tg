8892ab03db3deb8101e51acfb0cf1dba55e03e18
01es <oles.hodych@gmail.com>
2012-08-28 11:55:03 +0000

    refs #422
    Minor lexer improvement for keyword END.
    
    git-svn-id: http://kid/svn/trident-genesis/trunk@4257 50ee8237-08a1-5d4c-b143-727193748607

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/expression/lexer/case_when/end/State3.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/expression/lexer/case_when/end/State3.java
@@ -12,28 +12,20 @@ import ua.com.fielden.platform.expression.automata.NoTransitionAvailable;
  */
 public class State3 extends AbstractState {
 
-    private int wsCount = 0;
-
     public State3() {
 	super("S3", true);
     }
 
     @Override
     protected AbstractState transition(final char symbol) throws NoTransitionAvailable {
-	try {
-	    if (isWhiteSpace(symbol)) {
-		wsCount++;
-		return this;
-	    } else if (symbol == ExpressionLexer.EOF) {
-		throw new NoTransitionAvailable("Invalid symbol '" + symbol + "'", this, symbol);
-	    } else if (wsCount == 0) {
-		// this would happen only if string END would continua with some other characters e.g. ENDed, making it a valid name
-		// rather than keyword END, thus move to non-final state representing a dead-end for this automata
-		return getAutomata().getState("S4");
-	    }
+	if (isWhiteSpace(symbol)) {
+	    return this;
+	} else if (symbol == ExpressionLexer.EOF) {
 	    throw new NoTransitionAvailable("Invalid symbol '" + symbol + "'", this, symbol);
-	} finally {
-	    wsCount = 0;
+	} else {
+	    // this would happen only if string END would continua with some other characters e.g. ENDed, making it a valid name
+	    // rather than keyword END, thus move to non-final state representing a dead-end for this automata
+	    return getAutomata().getState("S4");
 	}
     }
 
