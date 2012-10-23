a2b38da107abd97f3211fdd4434a363bbed024d2
nazchk <nazar@fielden.com.au>
2012-10-23 10:50:28 +0000

    Corrected type related annotations.
    git-svn-id: http://kid/svn/trident-genesis/trunk@4395 50ee8237-08a1-5d4c-b143-727193748607

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/annotation/KeyType.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/annotation/KeyType.java
@@ -11,13 +11,13 @@ import java.lang.annotation.Target;
  * Java does not store type parameters at runtime, however quite often it is a requirement to know the exact type of an entity key when only entity class is known available (only
  * an instance). This annotation is a compromise between the need to know key type at runtime and having a developer to do an extra little to specify this annotation on every
  * concrete entity class.
- * 
+ *
  * @author 01es
- * 
+ *
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE })
 public @interface KeyType {
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings("rawtypes")
     Class<? extends Comparable> value();
 }
