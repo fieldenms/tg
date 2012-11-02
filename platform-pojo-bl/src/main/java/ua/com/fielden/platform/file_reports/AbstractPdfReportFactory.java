382cb29c52bd9292940b9af5fdfd663718cf26ac
nazchk <nazar@fielden.com.au>
2012-11-02 19:16:43 +0000

    Ensured UTF8 encoding and style adjustment upon export to PDF.
    git-svn-id: http://kid/svn/trident-genesis/trunk@4412 50ee8237-08a1-5d4c-b143-727193748607

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/file_reports/AbstractPdfReportFactory.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/file_reports/AbstractPdfReportFactory.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JRParameter;
 import net.sf.jasperreports.engine.JRRewindableDataSource;
+import net.sf.jasperreports.engine.JRStyle;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -21,12 +22,12 @@ import ua.com.fielden.platform.utils.ResourceLoader;
 
 public abstract class AbstractPdfReportFactory implements IReportFactory {
 
-
     /**
      * Provides default implementation of report generation in the form of pdf file binary representation
      */
     @Override
-    public byte[] createReport(final QueryExecutionModel<EntityAggregates, AggregatedResultQueryModel> query, final IEntityAggregatesDao aggregatesDao, final Map<String, Object> allParams) throws Exception {
+    public byte[] createReport(final QueryExecutionModel<EntityAggregates, AggregatedResultQueryModel> query, final IEntityAggregatesDao aggregatesDao, final Map<String, Object> allParams)
+	    throws Exception {
 
 	// obtaining necessary for report data
 	final List<EntityAggregates> reportData = aggregatesDao.getAllEntities(query);
@@ -40,19 +41,38 @@ public abstract class AbstractPdfReportFactory implements IReportFactory {
 	}
 
 	final JasperReport report = JasperCompileManager.compileReport(reportTemplatePath);
-
+	addPdfFontsToStyles(report.getStyles());
 	// filling report template with data
 	final JRRewindableDataSource jrs = new EntityAggregatesReportSource(reportData.toArray(new EntityAggregates[] {}), reportProps());
 	final JasperPrint print = JasperFillManager.fillReport(report, prepareReportParamsValues(allParams, report.getParameters()), jrs);
 
 	// exporting to PDF file as byte array
 	final JRPdfExporter exporter = new JRPdfExporter();
+	exporter.setParameter(JRExporterParameter.CHARACTER_ENCODING, "UTF-8");
+
 	final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 	exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
 	exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, outputStream);
 	exporter.exportReport();
 
 	return outputStream.toByteArray();
+    }
+
+    private void addPdfFontsToStyles(final JRStyle[] styles) {
+	if (styles != null) {
+	    for (final JRStyle style : styles) {
+		if (style.getName().equals("reportStyle")) {
+		    style.setPdfFontName("fonts/times.ttf");
+		    style.setBlankWhenNull(true);
+		}
+
+		if (style.getName().equals("reportBoldStyle")) {
+		    style.setPdfFontName("fonts/timesbd.ttf");
+		    style.setBlankWhenNull(true);
+		}
+
+	    }
+	}
     }
 
     /**
@@ -74,12 +94,14 @@ public abstract class AbstractPdfReportFactory implements IReportFactory {
 
     /**
      * Should provide report template path.
+     *
      * @return
      */
     protected abstract String reportTemplatePath();
 
     /**
      * Should provide report data source properties and their types.
+     *
      * @return
      */
     protected abstract Map<String, Class> reportProps();
