2c2e27d0f2fcd5aee8255ab14fcf0023dc01508a
01es <oles.hodych@gmail.com>
2012-11-23 17:48:45 +0000

    Sync with platform changes.
    git-svn-id: http://kid/svn/trident-genesis/trunk@4452 50ee8237-08a1-5d4c-b143-727193748607

--- platform-launcher/src/main/java/ua/com/fielden/platform/example/dynamiccriteria/BaseUserProvider.java
+++ platform-launcher/src/main/java/ua/com/fielden/platform/example/dynamiccriteria/BaseUserProvider.java
@@ -1,11 +1,11 @@
 package ua.com.fielden.platform.example.dynamiccriteria;
 
+import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.fetch;
+import ua.com.fielden.platform.security.provider.IUserController;
 import ua.com.fielden.platform.security.user.IUserDao;
 import ua.com.fielden.platform.security.user.User;
 
 import com.google.inject.Inject;
-
-import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.fetch;
 
 public class BaseUserProvider extends EntitycentreUserProvider {
 
@@ -17,6 +17,10 @@ public class BaseUserProvider extends EntitycentreUserProvider {
     @Override
     protected User initUser() {
 	return userDao.findByKeyAndFetch(fetch(User.class).with("basedOnUser"), User.system_users.SU.name());
+    }
+
+    @Override
+    public void setUsername(final String username, final IUserController controller) {
     }
 
 }
