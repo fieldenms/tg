353f19c8f7df2915b112da8c3559b37f92b4c733
nazchk <nazar@fielden.com.au>
2012-11-28 08:53:59 +0000

    Enhanced parameter values obtaining logic to handle the case of CRIT_ONLY.RANGE entity property.
    git-svn-id: http://kid/svn/trident-genesis/trunk@4465 50ee8237-08a1-5d4c-b143-727193748607

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/swing/review/DynamicParamBuilder.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/swing/review/DynamicParamBuilder.java
@@ -1,11 +1,7 @@
 package ua.com.fielden.platform.swing.review;
 
-import static ua.com.fielden.platform.criteria.generator.impl.CriteriaReflector.from;
-import static ua.com.fielden.platform.criteria.generator.impl.CriteriaReflector.is;
-import static ua.com.fielden.platform.criteria.generator.impl.CriteriaReflector.not;
-import static ua.com.fielden.platform.criteria.generator.impl.CriteriaReflector.to;
-
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -15,6 +11,11 @@ import ua.com.fielden.platform.swing.review.DynamicQueryBuilder.QueryProperty;
 import ua.com.fielden.platform.swing.review.development.EntityQueryCriteriaUtils;
 import ua.com.fielden.platform.utils.EntityUtils;
 import ua.com.fielden.platform.utils.Pair;
+import static ua.com.fielden.platform.criteria.generator.impl.CriteriaReflector.from;
+import static ua.com.fielden.platform.criteria.generator.impl.CriteriaReflector.is;
+import static ua.com.fielden.platform.criteria.generator.impl.CriteriaReflector.not;
+import static ua.com.fielden.platform.criteria.generator.impl.CriteriaReflector.to;
+import static ua.com.fielden.platform.swing.review.DynamicQueryBuilder.prepare;
 
 /**
  * The utility class that is a responsible for creating the map between property names and it's values.
@@ -48,15 +49,17 @@ public class DynamicParamBuilder {
      */
     private static Map<String, Object> getPropertyValues(final QueryProperty qp, final Entry<String, Pair<Object, Object>> propValEntry) {
 	final Map<String, Object> pairVals = new HashMap<String, Object>();
-	if(qp.isCritOnly()){
-	    if(qp.isSingle()){
+	if (qp.isCritOnly()) {
+	    if (qp.isSingle()) {
 		pairVals.put(propValEntry.getKey(), propValEntry.getValue().getKey());
-	    }else if(EntityUtils.isRangeType(qp.getType())){
+	    } else if (EntityUtils.isRangeType(qp.getType())) {
 		pairVals.put(from(propValEntry.getKey()), propValEntry.getValue().getKey());
 		pairVals.put(to(propValEntry.getKey()), propValEntry.getValue().getValue());
-	    }else if(EntityUtils.isBoolean(qp.getType())){
+	    } else if (EntityUtils.isBoolean(qp.getType())) {
 		pairVals.put(is(propValEntry.getKey()), propValEntry.getValue().getKey());
 		pairVals.put(not(propValEntry.getKey()), propValEntry.getValue().getValue());
+	    } else if (!qp.isSingle() && EntityUtils.isEntityType(qp.getType())) { // It is assumed that not SINGLE means RANGE
+		pairVals.put(propValEntry.getKey(), prepare((List<String>) propValEntry.getValue().getKey()));
 	    }
 	}
 	return pairVals;
