85ebbdfa24a496057911a99aeca7a99542d221a4
nazchk <nazar@fielden.com.au>
2013-01-24 11:34:26 +0000

    Implemented support for non-string arguments for UPPER/LOWER EQL functions.
    git-svn-id: http://kid/svn/trident-genesis/trunk@4541 50ee8237-08a1-5d4c-b143-727193748607

--- platform-dao/src/main/java/ua/com/fielden/platform/entity/query/generation/elements/LowerCaseOf.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/entity/query/generation/elements/LowerCaseOf.java
@@ -1,5 +1,7 @@
 package ua.com.fielden.platform.entity.query.generation.elements;
 
+import java.util.Date;
+
 import ua.com.fielden.platform.entity.query.generation.DbVersion;
 
 public class LowerCaseOf extends SingleOperandFunction {
@@ -9,6 +11,17 @@ public class LowerCaseOf extends SingleOperandFunction {
 
     @Override
     public String sql() {
-	return "LOWER(" + getOperand().sql() + ")";
+	final StringBuffer sb = new StringBuffer();
+	sb.append("LOWER(");
+	if (Date.class.equals(getOperand().type())) {
+	    sb.append("TO_CHAR(" + getOperand().sql() + ", 'YYYY-MM-DD HH24:MI:SS')");
+	} else if (String.class.equals(getOperand().type())) {
+	    sb.append(getOperand().sql());
+	} else {
+	    sb.append("CAST(" + getOperand().sql() + " AS VARCHAR(255))");
+	}
+	sb.append(")");
+
+	return sb.toString();
     }
 }
--- platform-dao/src/main/java/ua/com/fielden/platform/entity/query/generation/elements/UpperCaseOf.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/entity/query/generation/elements/UpperCaseOf.java
@@ -1,5 +1,7 @@
 package ua.com.fielden.platform.entity.query.generation.elements;
 
+import java.util.Date;
+
 import ua.com.fielden.platform.entity.query.generation.DbVersion;
 
 public class UpperCaseOf extends SingleOperandFunction {
@@ -7,8 +9,18 @@ public class UpperCaseOf extends SingleOperandFunction {
 	super(dbVersion, operand);
     }
 
-    @Override
     public String sql() {
-	return "UPPER(" + getOperand().sql() + ")";
+	final StringBuffer sb = new StringBuffer();
+	sb.append("UPPER(");
+	if (Date.class.equals(getOperand().type())) {
+	    sb.append("TO_CHAR(" + getOperand().sql() + ", 'YYYY-MM-DD HH24:MI:SS')");
+	} else if (String.class.equals(getOperand().type())) {
+	    sb.append(getOperand().sql());
+	} else {
+	    sb.append("CAST(" + getOperand().sql() + " AS VARCHAR(255))");
+	}
+	sb.append(")");
+
+	return sb.toString();
     }
 }
