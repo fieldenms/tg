e547934a48edb0076ae345e01363b536895aa5b7
01es <oles.hodych@gmail.com>
2013-02-08 14:08:48 +0000

    refs #275
    Added deletion support.
    
    git-svn-id: http://kid/svn/trident-genesis/trunk@4567 50ee8237-08a1-5d4c-b143-727193748607

--- platform-dao/src/main/java/ua/com/fielden/platform/dao/AttachmentDao.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/dao/AttachmentDao.java
@@ -1,8 +1,11 @@
 package ua.com.fielden.platform.dao;
 
+import java.util.Map;
+
 import ua.com.fielden.platform.attachment.Attachment;
 import ua.com.fielden.platform.attachment.IAttachmentController;
 import ua.com.fielden.platform.entity.query.IFilter;
+import ua.com.fielden.platform.entity.query.model.EntityResultQueryModel;
 import ua.com.fielden.platform.swing.review.annotations.EntityType;
 
 import com.google.inject.Inject;
@@ -23,6 +26,16 @@ public class AttachmentDao extends CommonEntityDao<Attachment> implements IAttac
     @Override
     public void delete(final Attachment entity) {
         defaultDelete(entity);
+    }
+
+    @Override
+    public void delete(final EntityResultQueryModel<Attachment> model) {
+        defaultDelete(model);
+    }
+
+    @Override
+    public void delete(final EntityResultQueryModel<Attachment> model, final Map<String, Object> paramValues) {
+        defaultDelete(model, paramValues);
     }
 
 }
