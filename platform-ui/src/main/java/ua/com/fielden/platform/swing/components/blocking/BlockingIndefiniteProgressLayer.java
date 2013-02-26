5b60cc9e9c15bd4d65a2162f24a804aad5570fee
01es <oles.hodych@gmail.com>
2013-02-26 17:36:03 +0000

    Updated Apache POI dependency to version 3.9.
    git-svn-id: http://kid/svn/trident-genesis/trunk@4613 50ee8237-08a1-5d4c-b143-727193748607

--- platform-ui/src/main/java/ua/com/fielden/platform/swing/components/blocking/BlockingIndefiniteProgressLayer.java
+++ platform-ui/src/main/java/ua/com/fielden/platform/swing/components/blocking/BlockingIndefiniteProgressLayer.java
@@ -9,7 +9,6 @@ import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
 import java.awt.geom.Point2D;
