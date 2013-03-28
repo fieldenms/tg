fa37f92bf7b1ee0ce2a8711ae8f438a8ea952a1c
nazchk <nazar@fielden.com.au>
2013-03-28 12:44:21 +0000

    Corrected.
    git-svn-id: http://kid/svn/trident-genesis/trunk@4643 50ee8237-08a1-5d4c-b143-727193748607

--- platform-dao/src/main/java/ua/com/fielden/platform/entity/query/generation/elements/DateOf.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/entity/query/generation/elements/DateOf.java
@@ -13,9 +13,11 @@ public class DateOf extends SingleOperandFunction {
     public String sql() {
 	switch (getDbVersion()) {
 	case H2:
+	    return "CAST(" + getOperand().sql() + " AS DATE)";
 	case MSSQL:
+	    return "DATEADD(dd, DATEDIFF(dd, 0, " + getOperand().sql() + "), 0)";
 	case POSTGRESQL:
-	    return "DATE_PART('day', " + getOperand().sql() + ")";
+	    return "DATE_TRUNC('day', " + getOperand().sql() + ")";
 	default:
 	    throw new IllegalStateException("Function [" + getClass().getSimpleName() +"] is not yet implemented for RDBMS [" + getDbVersion() + "]!");
 	}
