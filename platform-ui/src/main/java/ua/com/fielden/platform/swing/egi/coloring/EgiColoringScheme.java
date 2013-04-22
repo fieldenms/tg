6af016d886fd2d6b66480bc1491abd3bb9a92cd2
01es <oles.hodych@gmail.com>
2013-04-22 05:45:58 +0000

    Adjusted entity handling until the time sypertype manipulations are properly supported as part of the type enhancer.
    Until than, please use AbstractEntity instead of the actual type when creating custom colouring schemes for EGI.
    
    git-svn-id: http://kid/svn/trident-genesis/trunk@4672 50ee8237-08a1-5d4c-b143-727193748607

--- platform-ui/src/main/java/ua/com/fielden/platform/swing/egi/coloring/EgiColoringScheme.java
+++ platform-ui/src/main/java/ua/com/fielden/platform/swing/egi/coloring/EgiColoringScheme.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
-import ua.com.fielden.platform.reflection.asm.impl.DynamicEntityClassLoader;
 import ua.com.fielden.platform.swing.egi.EntityGridInspector;
 import ua.com.fielden.platform.swing.utils.DummyBuilder;
 
@@ -58,11 +57,18 @@ public class EgiColoringScheme<T extends AbstractEntity> implements IEgiColoring
     @Override
     public Color getBgColour(final T entity, final String propertyName) {
 	final IColouringScheme<T> propColouringScheme = bgPropertyColouringSchemes.get(propertyName) != null ? //
-	bgPropertyColouringSchemes.get(propertyName): bgRowColouringScheme;
+	bgPropertyColouringSchemes.get(propertyName)
+		: bgRowColouringScheme;
 	if (propColouringScheme == null) {
 	    return null;
 	} else {
-	    final T ent = ensureCorrectEntityType(entity);
+	    T ent;
+	    try {
+		ent = ensureCorrectEntityType(entity);
+	    } catch (final ClassNotFoundException e) {
+		e.printStackTrace();
+		ent = entity;
+	    }
 	    final Color propertyColor = propColouringScheme.getColor(ent);
 	    if (propertyColor != null) {
 		return propertyColor;
@@ -74,11 +80,18 @@ public class EgiColoringScheme<T extends AbstractEntity> implements IEgiColoring
 
     public Color getFgColour(final T entity, final String propertyName) {
 	final IColouringScheme<T> propColouringScheme = fgPropertyColouringSchemes.get(propertyName) != null ? //
-		fgPropertyColouringSchemes.get(propertyName): fgRowColouringScheme;
+	fgPropertyColouringSchemes.get(propertyName)
+		: fgRowColouringScheme;
 	if (propColouringScheme == null) {
 	    return null;
 	} else {
-	    final T ent = ensureCorrectEntityType(entity);
+	    T ent;
+	    try {
+		ent = ensureCorrectEntityType(entity);
+	    } catch (final ClassNotFoundException e) {
+		e.printStackTrace();
+		ent = entity;
+	    }
 	    final Color propertyColour = propColouringScheme.getColor(ent);
 	    if (propertyColour != null) {
 		return propertyColour;
@@ -88,15 +101,38 @@ public class EgiColoringScheme<T extends AbstractEntity> implements IEgiColoring
 	}
     }
 
-    private T ensureCorrectEntityType(final T entity) {
+    private T ensureCorrectEntityType(final T entity) throws ClassNotFoundException {
 	// TODO this conversion from enhanced entity to original is placed here to support execution of type-parameterised colouring schemes
 	// that would otherwise throw a runtime exception.
 	// However, this type compatibility problem exists only due to incompatibility of the original and enhanced types.
 	// Such situations may occur elsewhere, and thus needs to be resolved at the type level during generation of new types.
 	// This place is just a quick and dirty patch.
-	return !DynamicEntityClassLoader.isEnhanced(entity.getClass()) ? (T) entity :
-		(T) entity.copy(DynamicEntityClassLoader.getOriginalType(entity.getClass()));
-    }
 
+//	if (!DynamicEntityClassLoader.isEnhanced(entity.getClass())) {
+//	    return entity;
+//	} else {
+//	    final String originalType = DynamicEntityClassLoader.getOriginalType(entity.getClass()).getName();
+//	    final String genType = entity.getType().getName();
+//	    final String suggestedName = genType + "_adapted";
+//
+//	    Class<? extends AbstractEntity> newType;
+//	    try {
+//		newType = (Class<? extends AbstractEntity>) ClassLoader.getSystemClassLoader().loadClass(suggestedName);
+//	    } catch (final ClassNotFoundException e) {
+//		final DynamicEntityClassLoader cl = (DynamicEntityClassLoader) entity.getClass().getClassLoader();
+//		newType = (Class<? extends AbstractEntity>)//
+//			    cl.startModification(genType).//
+//			    modifyTypeName(suggestedName).//
+//			    modifySupertypeName(originalType).//
+//			    endModification();
+//	    }
+//
+//	    return (T) entity.copy(newType);
+//	}
+
+	//	return !DynamicEntityClassLoader.isEnhanced(entity.getClass()) ? (T) entity :
+	//		(T) entity.copy(DynamicEntityClassLoader.getOriginalType(entity.getClass()));
+	return entity;
+    }
 
 }
