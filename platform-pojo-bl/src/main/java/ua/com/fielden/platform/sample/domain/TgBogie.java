5e78ae3c1117c5071d0ad97c948c00c5894576ec
elvin-fms <elvin@fielden.com.au>
2013-05-16 07:33:40 +0000

    Provided annotation KeyTitle.
    git-svn-id: http://kid/svn/trident-genesis/trunk@4707 50ee8237-08a1-5d4c-b143-727193748607

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/TgBogie.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/TgBogie.java
@@ -5,6 +5,7 @@ import org.junit.Ignore;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.annotation.DescTitle;
 import ua.com.fielden.platform.entity.annotation.IsProperty;
+import ua.com.fielden.platform.entity.annotation.KeyTitle;
 import ua.com.fielden.platform.entity.annotation.KeyType;
 import ua.com.fielden.platform.entity.annotation.MapEntityTo;
 import ua.com.fielden.platform.entity.annotation.MapTo;
@@ -15,6 +16,7 @@ import ua.com.fielden.platform.entity.validation.annotation.CompanionObject;
 @KeyType(String.class)
 @MapEntityTo
 @DescTitle("Description")
+@KeyTitle("Bogie")
 @Ignore
 @CompanionObject(ITgBogie.class)
 public class TgBogie extends AbstractEntity<String> {
