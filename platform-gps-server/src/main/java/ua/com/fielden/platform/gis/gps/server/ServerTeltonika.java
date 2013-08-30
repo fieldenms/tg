22acd2ecebc0e41279196e1f62a1529e3c43d0bb
01es <oles.hodych@gmail.com>
2013-08-30 12:27:06 +0000

    Introduced ChannelGroup.
    git-svn-id: http://kid/svn/trident-genesis/trunk@4792 50ee8237-08a1-5d4c-b143-727193748607

--- platform-gps-server/src/main/java/ua/com/fielden/platform/gis/gps/server/ServerTeltonika.java
+++ platform-gps-server/src/main/java/ua/com/fielden/platform/gis/gps/server/ServerTeltonika.java
@@ -5,9 +5,12 @@ import java.util.concurrent.Executors;
 
 import org.apache.log4j.Logger;
 import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.group.ChannelGroup;
+import org.jboss.netty.channel.group.DefaultChannelGroup;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
 import ua.com.fielden.platform.gis.gps.factory.IGpsHandlerFactory;
@@ -18,6 +21,7 @@ public class ServerTeltonika implements Runnable {
 	private final String host;
 	private final int port;
 	private final IGpsHandlerFactory handlerFactory;
+	public static final ChannelGroup allChannels = new DefaultChannelGroup("gps-server");
 
 	public ServerTeltonika(final String host, final int port, final IGpsHandlerFactory handlerFactory) {
 		this.host = host;
@@ -47,7 +51,8 @@ public class ServerTeltonika implements Runnable {
 		});
 
 		// Bind and start to accept incoming connections.
-		bootstrap.bind(new InetSocketAddress(host, port));
+		final Channel channel = bootstrap.bind(new InetSocketAddress(host, port));
+		allChannels.add(channel);
 		log.info("Server started on " + host + ":" + port);
 	}
 }
