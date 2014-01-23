7ab514e88673a4faa6664aaa429003e4902058f3
01es <oles.hodych@gmail.com>
2014-01-23 16:07:15 +0000

    Introduced annotation WhenNullMessage.
    This annotation should be used to annotate either an entity object or its properties where some custom message should be provided when either key or the annotated property has value null (i.e. has no value).
    
    Such custom message can then be used as required whenever necessary...
    For example, it is used as part of property editors to display a custom message when property is null.
    
    git-svn-id: http://kid/svn/trident-genesis/trunk@4953 50ee8237-08a1-5d4c-b143-727193748607

---
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/annotation/WhenNullMessage.java
@@ -0,0 +1,22 @@
+package ua.com.fielden.platform.entity.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation should be used to annotate either an entity object or
+ * its properties where some custom message should be provided when either key or the annotated property has value <code>null</code>.
+ *
+ * Such custom message can then be used as required whenever necessary...
+ * For example, it is used as part of property editors.
+ *
+ * @author TG Team
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.FIELD })
+public @interface WhenNullMessage {
+    String value();
+}
