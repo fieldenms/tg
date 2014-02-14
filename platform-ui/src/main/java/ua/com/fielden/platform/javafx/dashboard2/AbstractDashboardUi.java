c737d23d018b283c0cae3bb45061858a13f9895e
jhou-pro <jhou.pro@gmail.com>
2014-02-14 09:48:22 +0000

    Enhanced dashboard UI to contain dashboard model.
    git-svn-id: http://kid/svn/trident-genesis/trunk@4981 50ee8237-08a1-5d4c-b143-727193748607

--- platform-ui/src/main/java/ua/com/fielden/platform/javafx/dashboard2/AbstractDashboardUi.java
+++ platform-ui/src/main/java/ua/com/fielden/platform/javafx/dashboard2/AbstractDashboardUi.java
@@ -13,14 +13,16 @@ import ua.com.fielden.platform.swing.view.BaseNotifPanel;
  * @author TG Team
  *
  */
-public abstract class AbstractDashboardUi extends BaseNotifPanel<StubUiModel> {
+public abstract class AbstractDashboardUi<DASHBOARD_MODEL extends AbstractDashboard<? extends AbstractDashboardUi>> extends BaseNotifPanel<StubUiModel> {
     private static final long serialVersionUID = 7623224149991897675L;
     private final JComponent paramsGetterUi;
     private final List<IDashboardItemUi> dashboardItemUis;
+    private final DASHBOARD_MODEL dashboardModel;
 
-    public AbstractDashboardUi(final JComponent paramsGetterUi, final List<IDashboardItemUi> dashboardItemUis) {
+    public AbstractDashboardUi(final JComponent paramsGetterUi, final List<IDashboardItemUi> dashboardItemUis, final DASHBOARD_MODEL dashboardModel) {
 	super("Dashboard", new StubUiModel(true));
 
+	this.dashboardModel = dashboardModel;
 	this.paramsGetterUi = paramsGetterUi;
 	this.dashboardItemUis = dashboardItemUis;
     }
@@ -37,5 +39,9 @@ public abstract class AbstractDashboardUi extends BaseNotifPanel<StubUiModel> {
 
     protected List<IDashboardItemUi> getDashboardItemUis() {
 	return dashboardItemUis;
+    }
+
+    public DASHBOARD_MODEL getDashboardModel() {
+	return dashboardModel;
     }
 }
