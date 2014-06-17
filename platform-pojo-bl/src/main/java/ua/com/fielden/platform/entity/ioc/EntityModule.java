953a1ac4529d4856d505d8be57b280a6d8edfb34
01es <oles.hodych@gmail.com>
2014-06-17 15:55:23 +0300

    Improved method interception logic by filtering out synthtic method (aka bridge methods) produced by the Java compiler.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/ioc/EntityModule.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/ioc/EntityModule.java
@@ -3,6 +3,11 @@ package ua.com.fielden.platform.entity.ioc;
 import static com.google.inject.matcher.Matchers.annotatedWith;
 import static com.google.inject.matcher.Matchers.any;
 import static com.google.inject.matcher.Matchers.subclassesOf;
+
+import java.lang.reflect.Method;
+
+import org.aopalliance.intercept.MethodInterceptor;
+
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.annotation.Observable;
 import ua.com.fielden.platform.security.AuthorisationInterceptor;
@@ -10,15 +15,27 @@ import ua.com.fielden.platform.security.Authorise;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Injector;
+import com.google.inject.matcher.AbstractMatcher;
+import com.google.inject.matcher.Matcher;
 
 /**
  * This Guice module ensures that properties for all {@link AbstractEntity} descendants are provided with an intercepter handling validation and observation.
- * 
+ *
  * @author TG Team
  */
 public abstract class EntityModule extends AbstractModule implements IModuleWithInjector {
 
     private final AuthorisationInterceptor ai = new AuthorisationInterceptor();
+
+    /**
+     * Synthetic methods should not be intercepted.
+     */
+    private final AbstractMatcher<Method> noSyntheticMethodMatcher = new AbstractMatcher<Method>() {
+        @Override
+        public boolean matches(final Method method) {
+            return !method.isSynthetic();
+        }
+    };
 
     /**
      * Binds intercepter for observable property mutators to ensure property change observation and validation. Only descendants of {@link AbstractEntity} are processed.
@@ -36,7 +53,13 @@ public abstract class EntityModule extends AbstractModule implements IModuleWith
     }
 
     @Override
+    protected void bindInterceptor(final Matcher<? super Class<?>> classMatcher, final Matcher<? super Method> methodMatcher, final MethodInterceptor... interceptors) {
+        super.bindInterceptor(classMatcher, noSyntheticMethodMatcher.and(methodMatcher), interceptors);
+    }
+
+    @Override
     public void setInjector(final Injector injector) {
         ai.setInjector(injector);
     }
+
 }
