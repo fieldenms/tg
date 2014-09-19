879b969616f3155018252d38cd3ecb1fa355447d
Jhou (work) <jhou.pro@gmail.com>
2014-09-19 18:02:39 +0300

    #47 Moved some common logic from specific GIS centres to base implementation.

--- platform-ui/src/main/web/ua/com/fielden/platform/web/gis/js/GisModule.js
+++ platform-ui/src/main/web/ua/com/fielden/platform/web/gis/js/GisModule.js
@@ -10,10 +10,14 @@ define(['angular', 'log'], function(angular, log) {
 				dependency: '@'
 			},
 			link: function(scope, iElement, iAttrs) {
-				require([scope.dependency], function(gisCreator){
-					gisCreator(iElement[0].lastElementChild, /*map*/
-					iElement[0].firstElementChild, /*progress*/
-					iElement[0].firstElementChild.firstElementChild /*progressBar*/ );
+				require([scope.dependency], function(GisComponentClass) {
+					var _gisComponent = new GisComponentClass(iElement[0].lastElementChild, /*map*/
+															iElement[0].firstElementChild, /*progress*/
+															iElement[0].firstElementChild.firstElementChild /*progressBar*/);
+					if (typeof java !== 'undefined') {
+						window.gisComponent = _gisComponent;
+					}
+					return _gisComponent;
 				});
 			}
 		};
