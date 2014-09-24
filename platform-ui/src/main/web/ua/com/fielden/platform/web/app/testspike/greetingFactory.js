efc75e7980f9768c2dafd278ab9b38ca00d82e10
oleh <oleh@fielden.com.au>
2014-09-24 14:47:34 +0300

    #52 Tested angularjs module and factory. Configured jasmine maven plugin to use phantomjs web driver

--- platform-ui/src/main/web/ua/com/fielden/platform/web/app/testspike/greetingFactory.js
+++ platform-ui/src/main/web/ua/com/fielden/platform/web/app/testspike/greetingFactory.js
@@ -1,4 +1,4 @@
-define(['angular', 'greeter'], function(angular, greeter) {
+define(['angular', 'ua/com/fielden/platform/web/app/testspike/greeter'], function(angular, greeter) {
     return angular.module("greeter", []).factory("greeterService", function() {
         return greeter;
     });
