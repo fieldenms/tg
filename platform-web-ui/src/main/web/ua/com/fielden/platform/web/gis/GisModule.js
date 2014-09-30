407fdf4e50a2f35337bb85173935260c9416c681
Jhou (work) <jhou.pro@gmail.com>
2014-09-30 14:54:53 +0300

    #58 Moved GIS-component-specific configuration closer to the main entry point for GIS-component 'require' call.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/gis/GisModule.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/gis/GisModule.js
@@ -10,10 +10,78 @@ define(['angular', 'log'], function(angular, log) {
 				dependency: '@'
 			},
 			link: function(scope, iElement, iAttrs) {
+				require.config({
+					paths: {
+						// 'gis' feature-specific library modules:
+						'leaflet': 'leaflet/leaflet', // AMD-compliant
+
+						'googlemaps': 'leaflet/providers/googlemaps', // an 'async' requirejs plugin wrapper for google maps loading -- AMD-compliant
+						'leaflet.googlemaps': 'leaflet/providers/Google', // non-AMD module
+						'yandexmaps': "http://api-maps.yandex.ru/2.0/?load=package.map&lang=ru-RU", // non-AMD module // TODO: lang RU??		
+						'leaflet.yandexmaps': 'leaflet/providers/Yandex', // non-AMD module
+
+						'leaflet.draw': 'leaflet/draw/leaflet.draw', // non-AMD module
+						'leaflet.markercluster': 'leaflet/markercluster/leaflet.markercluster', // non-AMD module
+						'leaflet.loadingcontrol': 'leaflet/controlloading/Control.Loading',
+						'leaflet.easybutton': 'leaflet/easybutton/easy-button', // non-AMD module
+						'leaflet.markerrotation': 'leaflet/markerrotation/Marker.Rotate' // non-AMD module
+					},
+
+					shim: {
+						// MAP PROVIDERS:
+						'leaflet.googlemaps': {
+							deps: ['leaflet', 'googlemaps!'],
+							exports: 'LeafletGoogleMaps'
+						},
+						'yandexmaps': {
+							deps: [],
+							exports: 'YandexMaps'
+						},
+						'leaflet.yandexmaps': {
+							deps: ['leaflet', 'yandexmaps'],
+							exports: 'LeafletYandexMaps'
+						},
+
+						// MAP DRAWING:
+						'leaflet.draw': {
+							deps: ['leaflet'],
+							exports: 'LeafletDraw'
+						},
+
+						// MAP CLUSTERING:
+						'leaflet.markercluster': {
+							deps: ['leaflet'],
+							exports: 'LeafletMarkercluster'
+						},
+
+						'leaflet.easybutton': {
+							deps: ['leaflet'],
+							exports: 'LeafletEasybutton'
+						},
+
+						'leaflet.markerrotation': {
+							deps: ['leaflet'],
+							exports: 'LeafletMarkerrotation'
+						}
+					},
+
+					googlemaps: { // configuration for 'async' requirejs plugin wrapper (google maps loading)
+						url: 'http://maps.google.com/maps/api/js',
+						// url: 'https://maps.googleapis.com/maps/api/js',
+						params: {
+							'v': '3.2',
+							// key: 'abcd1234',
+							// libraries: 'geometry',
+							sensor: false // Defaults to false
+						}
+						// async: asyncLoaderPlugin // Primarly for providing test stubs.
+					}
+				});
+
 				require([scope.dependency], function(GisComponentClass) {
 					var _gisComponent = new GisComponentClass(iElement[0].lastElementChild, /*map*/
-															iElement[0].firstElementChild, /*progress*/
-															iElement[0].firstElementChild.firstElementChild /*progressBar*/);
+						iElement[0].firstElementChild, /*progress*/
+						iElement[0].firstElementChild.firstElementChild /*progressBar*/ );
 					if (typeof java !== 'undefined') {
 						window.gisComponent = _gisComponent;
 					}
