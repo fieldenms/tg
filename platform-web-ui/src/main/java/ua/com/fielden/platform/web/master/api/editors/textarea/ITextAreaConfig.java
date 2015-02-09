cd218f8c829655faeaf18b370f690ddae1bf05e9
01es <oles.hodych@gmail.com>
2015-02-09 11:05:15 +1100

    #50 Adjusted the textarea related API and the flex-layout-example to demonstarte the use of textarea with flexbox.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/master/api/editors/textarea/ITextAreaConfig.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/master/api/editors/textarea/ITextAreaConfig.java
@@ -3,7 +3,7 @@ package ua.com.fielden.platform.web.master.api.editors.textarea;
 import ua.com.fielden.platform.entity.AbstractEntity;
 
 public interface ITextAreaConfig<T extends AbstractEntity<?>> extends ITextAreaConfig0<T> {
-    /** Indicates whether the hard word wrapping should be enforced.
-     * Hard wrapping requires the number of cols to be specified. */
-    ITextAreaConfig0<T> hardWrap(final int cols);
+    /** Indicates whether the textarea should be resizable or not.
+     * IMPLEMENTATION HINT: should add value <code>resize: none;</code> to the element's CSS. */
+    ITextAreaConfig0<T> resizable();
 }
