537291d0b81c25c9c6345fac05402945edea314c
01es <oles.hodych@gmail.com>
2015-02-14 18:40:44 +1100

    #50 Minor refactoring of Action API to be in line with Main Menu API names.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/master/api/ISimpleMaster.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/master/api/ISimpleMaster.java
@@ -62,7 +62,7 @@ public interface ISimpleMaster {
                     .postActionSuccess(new ToastUserWithMessage("Completed successfully")) // there is no need to imperatively state "stop infinite blocking pane", as this should be automatically understood from the context
                     .postActionError(new ShowMessageDlg("The action has completed with error: {{error}}"))
                     .enabledWhen(EnabledState.EDIT)
-                    .useIcon("icon name").shortDesc("could be used as title").longDesc("this description appeares in as a ")
+                    .icon("icon name").shortDesc("could be used as title").longDesc("this description appeares in as a ")
                 .also()
                     .addHtmlLabel("<p>This is some long text, which might spanned into several lines if necessary and may reference master entity properties. "
                         + "The main idea is to provder application developers with the abilit to add arbitrary, not boundn to any property text, to a view.</p>")
@@ -74,7 +74,7 @@ public interface ISimpleMaster {
                     .addDivider().atLevel2() // a subsection with no title
                 .also()
                     .addProp("desc").asMultilineText().resizable()
-                    .withAction(TgWorkOrder.class).enabledWhen(EnabledState.ANY).useIcon("my cool icon")
+                    .withAction(TgWorkOrder.class).enabledWhen(EnabledState.ANY).icon("my cool icon")
                 .also()
                     .addAction(TgWorkOrder.class).enabledWhen(EnabledState.VIEW).shortDesc("&New") // & defines keyboard binding
                     .addAction(TgWorkOrder.class).enabledWhen(EnabledState.VIEW).shortDesc("&Edit")
--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/master/api/actions/property/IPropertyActionConfig4.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/master/api/actions/property/IPropertyActionConfig4.java
@@ -3,5 +3,5 @@ package ua.com.fielden.platform.web.master.api.actions.property;
 import ua.com.fielden.platform.entity.AbstractEntity;
 
 public interface IPropertyActionConfig4<T extends AbstractEntity<?>> extends IPropertyActionConfig5<T> {
-    IPropertyActionConfig5<T> useIcon(final String iconName);
+    IPropertyActionConfig5<T> icon(final String iconName);
 }
