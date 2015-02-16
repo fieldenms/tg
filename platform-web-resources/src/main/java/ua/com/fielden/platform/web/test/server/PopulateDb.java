45c59a167f108a6fdf6d386b5af30bfcc97403a1
Jhou (work) <jhou.pro@gmail.com>
2015-02-16 20:52:35 +0200

    #116 #123 Implemented base 'tg-editor' component and reused it in 'tg-string-editor', 'tg-integer-editor' and 'tg-float-editor'. a) All binding logic has been incapsulated inside base editor component, for simple editors only two conversion methods should be implemented. b) Used 'entity+propertyName' attributes instead of 'bindTo' to better reuse JS entity logic. c) Added 'propTitle/Desc', 'msg/msgType' attributes and provided automatic binding for them. d) Used 'tg-flex-layout' as layouting mechanism in simple master example. e) Improved some methods ('_isEntity' etc.).

--- platform-web-resources/src/main/java/ua/com/fielden/platform/web/test/server/PopulateDb.java
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/test/server/PopulateDb.java
@@ -118,6 +118,9 @@ public class PopulateDb extends DomainDrivenDataPopulation {
         final TgPersistentEntityWithProperties staleEnt1New = save(staleEnt1.setConflictingProp("persistently modified"));
         System.out.println("staleEnt1New.getVersion() == " + staleEnt1New.getVersion());
 
+        final TgPersistentEntityWithProperties exampleEnt1 = save(new_(TgPersistentEntityWithProperties.class, "KEY11").setStringProp("ok").setIntegerProp(43).setEntityProp(defaultEnt).setBigDecimalProp(new BigDecimal(23.0)));
+        System.out.println("exampleEnt1.getId() == " + exampleEnt1.getId());
+
         try {
             final IApplicationSettings settings = config.getInstance(IApplicationSettings.class);
             final SecurityTokenProvider provider = new SecurityTokenProvider(settings.pathToSecurityTokens(), settings.securityTokensPackageName()); //  IDomainDrivenTestCaseConfiguration.hbc.getProperty("tokens.path"), IDomainDrivenTestCaseConfiguration.hbc.getProperty("tokens.package")
