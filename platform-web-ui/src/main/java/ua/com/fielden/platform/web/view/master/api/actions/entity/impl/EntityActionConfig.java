913126ff9a6e716f7e3d23dd9152410f0d4c3858
Jhou (work) <jhou.pro@gmail.com>
2015-02-25 13:59:08 +0200

    #123 Implemented entity action config and layout config.

---
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/api/actions/entity/impl/EntityActionConfig.java
@@ -0,0 +1,85 @@
+package ua.com.fielden.platform.web.view.master.api.actions.entity.impl;
+
+import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.web.interfaces.ILayout.Device;
+import ua.com.fielden.platform.web.interfaces.ILayout.Orientation;
+import ua.com.fielden.platform.web.view.master.api.actions.EnabledState;
+import ua.com.fielden.platform.web.view.master.api.actions.entity.IEntityActionConfig0;
+import ua.com.fielden.platform.web.view.master.api.actions.entity.IEntityActionConfig1;
+import ua.com.fielden.platform.web.view.master.api.actions.entity.IEntityActionConfig2;
+import ua.com.fielden.platform.web.view.master.api.actions.entity.IEntityActionConfig3;
+import ua.com.fielden.platform.web.view.master.api.actions.entity.IEntityActionConfig4;
+import ua.com.fielden.platform.web.view.master.api.actions.entity.IEntityActionConfig5;
+import ua.com.fielden.platform.web.view.master.api.actions.entity.IEntityActionConfig6;
+import ua.com.fielden.platform.web.view.master.api.actions.entity.IEntityActionConfig7;
+import ua.com.fielden.platform.web.view.master.api.actions.post.IPostAction;
+import ua.com.fielden.platform.web.view.master.api.actions.pre.IPreAction;
+import ua.com.fielden.platform.web.view.master.api.helpers.ILayoutConfigWithDone;
+import ua.com.fielden.platform.web.view.master.api.impl.SimpleMaster;
+
+public class EntityActionConfig<T extends AbstractEntity<?>> implements IEntityActionConfig0<T>, IEntityActionConfig1<T>, IEntityActionConfig2<T>, IEntityActionConfig3<T>, IEntityActionConfig4<T>, IEntityActionConfig5<T>, IEntityActionConfig6<T>, IEntityActionConfig7<T> {
+
+    private final EntityAction action;
+    private final SimpleMaster<T> simpleMaster;
+
+    public EntityActionConfig(final EntityAction action, final SimpleMaster<T> simpleMaster) {
+        this.action = action;
+        this.simpleMaster = simpleMaster;
+    }
+
+    @Override
+    public IEntityActionConfig0<T> addAction(final String name, final Class<? extends AbstractEntity<?>> functionalEntity) {
+        return simpleMaster.addAction(name, functionalEntity);
+    }
+
+    @Override
+    public ILayoutConfigWithDone setLayoutFor(final Device device, final Orientation orientation, final String flexString) {
+        return simpleMaster.setLayoutFor(device, orientation, flexString);
+    }
+
+    @Override
+    public IEntityActionConfig7<T> longDesc(final String longDesc) {
+        action.setLongDesc(longDesc);
+        return this;
+    }
+
+    @Override
+    public IEntityActionConfig6<T> shortDesc(final String shortDesc) {
+        action.setShortDesc(shortDesc);
+        return this;
+    }
+
+    @Override
+    public IEntityActionConfig5<T> icon(final String iconName) {
+        action.setIcon(iconName);
+        return this;
+    }
+
+    @Override
+    public IEntityActionConfig4<T> enabledWhen(final EnabledState state) {
+        action.setEnabledState(state);
+        return this;
+    }
+
+    @Override
+    public IEntityActionConfig2<T> postActionSuccess(final IPostAction postActionSuccess) {
+        action.setPostActionSuccess(postActionSuccess);
+        return this;
+    }
+
+    @Override
+    public IEntityActionConfig3<T> postActionError(final IPostAction postActionError) {
+        action.setPostActionError(postActionError);
+        return this;
+    }
+
+    @Override
+    public IEntityActionConfig1<T> preAction(final IPreAction preAction) {
+        action.setPreAction(preAction);
+        return this;
+    }
+
+    public EntityAction action() {
+        return action;
+    }
+}
--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/api/impl/SimpleMaster.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/api/impl/SimpleMaster.java
@@ -4,16 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.web.interfaces.ILayout.Device;
+import ua.com.fielden.platform.web.interfaces.ILayout.Orientation;
+import ua.com.fielden.platform.web.interfaces.IRenderable;
+import ua.com.fielden.platform.web.layout.FlexLayout;
 import ua.com.fielden.platform.web.view.master.api.actions.entity.IEntityActionConfig0;
+import ua.com.fielden.platform.web.view.master.api.actions.entity.impl.EntityAction;
+import ua.com.fielden.platform.web.view.master.api.actions.entity.impl.EntityActionConfig;
+import ua.com.fielden.platform.web.view.master.api.helpers.ILayoutConfig;
+import ua.com.fielden.platform.web.view.master.api.helpers.ILayoutConfigWithDone;
 import ua.com.fielden.platform.web.view.master.api.helpers.IPropertySelector;
 import ua.com.fielden.platform.web.view.master.api.helpers.IWidgetSelector;
 import ua.com.fielden.platform.web.view.master.api.helpers.impl.WidgetSelector;
 import ua.com.fielden.platform.web.view.master.api.widgets.IDividerConfig;
 import ua.com.fielden.platform.web.view.master.api.widgets.IHtmlText;
 
-public class SimpleMaster<T extends AbstractEntity<?>> implements IPropertySelector<T> {
+public class SimpleMaster<T extends AbstractEntity<?>> implements IPropertySelector<T>, ILayoutConfig, ILayoutConfigWithDone {
 
-    private final List<IWidgetSelector<T>> widgets = new ArrayList<>();
+    private final List<WidgetSelector<T>> widgets = new ArrayList<>();
+    private final List<EntityActionConfig<T>> entityActions = new ArrayList<>();
+    private final FlexLayout layout = new FlexLayout();
 
     public final Class<T> entityType;
 
@@ -23,8 +33,9 @@ public class SimpleMaster<T extends AbstractEntity<?>> implements IPropertySelec
 
     @Override
     public IEntityActionConfig0<T> addAction(final String name, final Class<? extends AbstractEntity<?>> functionalEntity) {
-        // TODO Auto-generated method stub
-        return null;
+        final EntityActionConfig<T> entityAction = new EntityActionConfig<>(new EntityAction(name, functionalEntity), this);
+        entityActions.add(entityAction);
+        return entityAction;
     }
 
     @Override
@@ -42,6 +53,18 @@ public class SimpleMaster<T extends AbstractEntity<?>> implements IPropertySelec
 
     @Override
     public IHtmlText<T> addHtmlLabel(final String htmlText) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ILayoutConfigWithDone setLayoutFor(final Device device, final Orientation orientation, final String flexString) {
+        layout.whenMedia(device, orientation).set(flexString);
+        return this;
+    }
+
+    @Override
+    public IRenderable done() {
         // TODO Auto-generated method stub
         return null;
     }
