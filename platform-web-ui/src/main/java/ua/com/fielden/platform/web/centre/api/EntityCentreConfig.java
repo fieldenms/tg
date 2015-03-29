2c53148de96e0d67efa5d428103dd311cdeb3d22
01es <oles.hodych@gmail.com>
2015-03-29 12:41:41 +1100

    #140 Minor refactorign and added TODO to define data structures for the selection criteria configs.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/EntityCentreConfig.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/EntityCentreConfig.java
@@ -9,6 +9,7 @@ import ua.com.fielden.platform.basic.IValueMatcherWithCentreContext;
 import ua.com.fielden.platform.basic.autocompleter.FallbackValueMatcherWithCentreContext;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.web.centre.api.actions.EntityActionConfig;
+import ua.com.fielden.platform.web.centre.api.resultset.PropDef;
 
 /**
  *
@@ -20,12 +21,34 @@ import ua.com.fielden.platform.web.centre.api.actions.EntityActionConfig;
 public class EntityCentreConfig {
     // TODO the actual structure still needs to be determined
 
+    //////////////////////////////////////////
+    ///////////// SELECTION CRIT /////////////
+    //////////////////////////////////////////
+
+
+    // TODO need to add structure suitable for capturing selection criteria configurations.
+    //      Could incorporate the valueMatchers map below
+
+    /**
+     * A map between selection criteria properties and their custom value matchers.
+     * If a matcher for a some criterion is not provided then a default instance of type {@link FallbackValueMatcherWithCentreContext} should be used.
+     */
+    private final Map<String, IValueMatcherWithCentreContext<? extends AbstractEntity<?>>> valueMatchers = new HashMap<>();
+
+
+    //////////////////////////////////////////
+    //////////////// RESULT SET //////////////
+    //////////////////////////////////////////
+
     /**
      * Contains action configurations for actions that are associated with individual properties of retrieved entities,
      * which are represented in the result set.
-     * This map can be empty is there is no need to provide custom actions specific for represented in the result set properties.
+     * This map can be empty if there is no need to provide custom actions specific for represented in the result set properties.
      * In this case, the default actions would still get associated with all not listed in this map, but added to the result set properties.
      * In order to skip the creation even of default actions, <code>no action</code> configuration needs to be provided for a property.
+     * <p>
+     * Custom properties that are added as instances of {@link PropDef} are represented in this map by their titles.
+     * This means that titles should be unique, which is only a natural requirement and is not a restrictive constraint.
      */
     private final Map<String, EntityActionConfig> resultSetPropActions = new HashMap<>();
 
@@ -41,9 +64,4 @@ public class EntityCentreConfig {
      */
     private List<EntityActionConfig> secondaryEntityActions = new ArrayList<>();
 
-    /**
-     * A map between selection criteria properties and their custom value matchers.
-     * If a matcher for a some criterion is not provided then a default instance of type {@link FallbackValueMatcherWithCentreContext} should be used.
-     */
-    private final Map<String, IValueMatcherWithCentreContext<? extends AbstractEntity<?>>> valueMatchers = new HashMap<>();
 }
