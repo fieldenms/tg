aebcf636b7ed76bc0b3ddfdec2dd8a3127a8b1e9
01es <oles.hodych@gmail.com>
2015-03-31 17:32:09 +1100

    #140 Introduced a contract for customisation of generated by Entity Centre query by means of enhancing it with additional, potentially context dependent, where conditions.

---
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/query_enhancer/IQueryEnhancer.java
@@ -0,0 +1,24 @@
+package ua.com.fielden.platform.web.centre.api.query_enhancer;
+
+import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.entity.annotation.CritOnly;
+import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.ICompleted;
+import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.IWhere0;
+import ua.com.fielden.platform.web.centre.CentreContext;
+
+/**
+ *
+ * A contract for enhancing an entity centre with custom conditions.
+ * It could be conveniently used to make an entity centre context dependent, where the nature of dependency can be expressed by providing additional
+ * EQL <code>where</code> clauses.
+ * <p>
+ * This contract can also be used to specify conditions based on invisible to users {@link CritOnly} parameters.
+ *
+ * @author TG Team
+ *
+ * @param <T>
+ */
+public interface IQueryEnhancer<T extends AbstractEntity<?>> {
+
+    ICompleted<T> enhanceQuery(final IWhere0<T> where, final CentreContext<T, ?> context);
+}
