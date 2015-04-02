ad7e217416fbf3e8c4e57663384e02f6169c867a
Jhou (work) <jhou.pro@gmail.com>
2015-04-02 17:51:38 +0300

    #131 Implemented first version of generated centre without criteria and without egi.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/EntityCentre.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/EntityCentre.java
@@ -1,11 +1,16 @@
 package ua.com.fielden.platform.web.centre;
 
+import java.util.LinkedHashSet;
+
 import org.apache.log4j.Logger;
 
+import ua.com.fielden.platform.dom.DomElement;
+import ua.com.fielden.platform.dom.InnerTextElement;
 import ua.com.fielden.platform.domaintree.IGlobalDomainTreeManager;
 import ua.com.fielden.platform.domaintree.centre.ICentreDomainTreeManager.ICentreDomainTreeManagerAndEnhancer;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.swing.menu.MiWithConfigurationSupport;
+import ua.com.fielden.platform.utils.ResourceLoader;
 import ua.com.fielden.platform.web.centre.api.EntityCentreConfig;
 import ua.com.fielden.platform.web.centre.api.ICentre;
 import ua.com.fielden.platform.web.interfaces.IRenderable;
@@ -24,22 +29,26 @@ public class EntityCentre<T extends AbstractEntity<?>> implements ICentre<T> {
     private final String name;
     private final EntityCentreConfig dslDefaultConfig;
     private final Injector injector;
+    private final Class<T> entityType;
+    private final Class<? extends MiWithConfigurationSupport<?>> miType;
 
     /**
      * Creates new {@link EntityCentre} instance for the menu item type and with specified name.
      *
-     * @param menuItemType
+     * @param miType
      *            - the menu item type for which this entity centre is to be created.
      * @param name
      *            - the name for this entity centre.
      * @param dslDefaultConfig
      *            -- default configuration taken from Centre DSL
      */
-    public EntityCentre(final Class<? extends MiWithConfigurationSupport<?>> menuItemType, final String name, final EntityCentreConfig dslDefaultConfig, final Injector injector) {
-        this.menuItemType = menuItemType;
+    public EntityCentre(final Class<? extends MiWithConfigurationSupport<?>> miType, final String name, final EntityCentreConfig dslDefaultConfig, final Injector injector) {
+        this.menuItemType = miType;
         this.name = name;
         this.dslDefaultConfig = dslDefaultConfig;
         this.injector = injector;
+        this.miType = miType;
+        this.entityType = (Class<T>) CentreUtils.getEntityType(miType);
     }
 
     /**
@@ -78,8 +87,35 @@ public class EntityCentre<T extends AbstractEntity<?>> implements ICentre<T> {
         final ICentreDomainTreeManagerAndEnhancer centre = CentreUtils.getFreshCentre(getUserSpecificGdtm(), this.menuItemType);
         logger.error("Building renderable for cdtmae:" + centre);
 
-        // TODO complete implementation
-        return null;
+        final LinkedHashSet<String> importPaths = new LinkedHashSet<>();
+        importPaths.add("polymer/polymer/polymer");
+        importPaths.add("master/tg-entity-master");
+
+        //        final DomElement editorContainer = layout.render();
+        //
+        //        importPaths.add(layout.importPath());
+        //        widgets.forEach(widget -> {
+        //            importPaths.add(widget.widget().importPath());
+        //            editorContainer.add(widget.widget().render());
+        //            if (widget.widget().action() != null) {
+        //                propertyActionsStr.append(widget.widget().action().code().toString());
+        //            }
+        //        });
+
+        final String entityCentreStr = ResourceLoader.getText("ua/com/fielden/platform/web/centre/tg-entity-centre-template.html").
+                // replace("<!--@imports-->", createImports(importPaths)).
+                replace("@entity_type", entityType.getSimpleName()).
+                replace("@full_entity_type", entityType.getName()).
+                replace("@mi_type", miType.getName());
+        // replace("<!--@criteria_editors-->", editorContainer.toString());
+
+        final IRenderable representation = new IRenderable() {
+            @Override
+            public DomElement render() {
+                return new InnerTextElement(entityCentreStr);
+            }
+        };
+        return representation;
     }
 
     /**
