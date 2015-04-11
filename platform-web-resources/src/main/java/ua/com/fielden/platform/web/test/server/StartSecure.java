cdf2a4591530c2853d3e57b5fcc9b6326446ece2
01es <oles.hodych@gmail.com>
2015-04-11 15:54:04 +1000

    #150 Implemented HTTPS support for TG-based applications.

---
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/test/server/StartSecure.java
@@ -0,0 +1,70 @@
+package ua.com.fielden.platform.web.test.server;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.restlet.Component;
+import org.restlet.data.Parameter;
+import org.restlet.data.Protocol;
+import org.restlet.util.Series;
+
+import ua.com.fielden.platform.utils.ResourceLoader;
+
+/**
+ * Web UI Testing Server launching class for full web server with platform Web UI web application and domain-driven persistent storage.
+ *
+ * @author TG Team
+ *
+ */
+public class StartSecure {
+    private static final Logger logger = Logger.getLogger(StartSecure.class);
+
+    public static void main(final String[] args) {
+        final String fileName = "src/main/resources/application.properties";
+        InputStream st = null;
+        Properties props = null;
+        try {
+            st = new FileInputStream(fileName);
+            props = new Properties();
+            props.load(st);
+        } catch (final Exception e) {
+            System.out.println(String.format("Application property file %s could not be located or its values are not recognised.", fileName));
+            e.printStackTrace();
+            System.exit(1);
+        } finally {
+            try {
+                st.close();
+            } catch (final Exception e) {
+                e.printStackTrace(); // can be ignored
+            }
+        }
+
+        DOMConfigurator.configure(props.getProperty("log4j"));
+
+        logger.info("Starting...");
+        final Component component = new ApplicationConfiguration(props);
+        //component.getServers().add(Protocol.HTTP, Integer.parseInt(props.getProperty("port")));
+
+        final org.restlet.Server server = component.getServers().add(Protocol.HTTPS, 9999);
+        final Series<Parameter> parameters = server.getContext().getParameters();
+
+        parameters.add("sslContextFactory", "org.restlet.engine.ssl.DefaultSslContextFactory");
+        // I have created self signed certificate. reference is attached with parameter
+        parameters.add("keystorePath", ResourceLoader.getURL("tsl/localhost_keystore").getPath());
+
+        parameters.add("keystorePassword", "changeit");
+        parameters.add("keyPassword", "changeit");
+        parameters.add("keystoreType", "JKS");
+
+        try {
+            component.start();
+            logger.info("started");
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(100);
+        }
+    }
+}
---
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/test/server/StartSecureJetty.java
@@ -0,0 +1,81 @@
+package ua.com.fielden.platform.web.test.server;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
+import ua.com.fielden.platform.utils.ResourceLoader;
+
+/**
+ * Web UI Testing Server launching class for full web server with platform Web UI web application and domain-driven persistent storage.
+ *
+ * @author TG Team
+ *
+ */
+public class StartSecureJetty {
+    private static final Logger logger = Logger.getLogger(StartSecureJetty.class);
+
+    public static void main(final String[] args) {
+        final String fileName = "src/main/resources/application.properties";
+        InputStream st = null;
+        Properties props = null;
+        try {
+            st = new FileInputStream(fileName);
+            props = new Properties();
+            props.load(st);
+        } catch (final Exception e) {
+            System.out.println(String.format("Application property file %s could not be located or its values are not recognised.", fileName));
+            e.printStackTrace();
+            System.exit(1);
+        } finally {
+            try {
+                st.close();
+            } catch (final Exception e) {
+                e.printStackTrace(); // can be ignored
+            }
+        }
+
+        DOMConfigurator.configure(props.getProperty("log4j"));
+
+        logger.info("Starting...");
+
+        final Server server = new Server();
+
+        final ServerConnector connector = new ServerConnector(server);
+
+        final HttpConfiguration https = new HttpConfiguration();
+        https.addCustomizer(new SecureRequestCustomizer());
+
+        final SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setKeyStorePath(ResourceLoader.getURL("tsl/localhost_keystore").getPath());
+        sslContextFactory.setKeyStorePassword("changeit");
+        sslContextFactory.setKeyManagerPassword("changeit");
+
+        final ServerConnector sslConnector = new ServerConnector(server,
+                new SslConnectionFactory(sslContextFactory, "http/1.1"),
+                new HttpConnectionFactory(https));
+        sslConnector.setPort(9998);
+
+        server.setConnectors(new Connector[] { connector, sslConnector });
+
+        try {
+            server.start();
+            logger.info("Started");
+        } catch (final Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
