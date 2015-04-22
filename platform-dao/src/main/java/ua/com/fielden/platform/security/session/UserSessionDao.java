f7d5fe42be5f692b2c91113e7d0327b4ebc3017c
01es <oles.hodych@gmail.com>
2015-04-22 16:11:29 +1000

    #150 Provided UserSessio with relevant properties. Its uniqueness is determined by properties user and seriesId. The companion object has been enhaced with various session clearing methods.

--- platform-dao/src/main/java/ua/com/fielden/platform/security/session/UserSessionDao.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/security/session/UserSessionDao.java
@@ -1,7 +1,13 @@
 package ua.com.fielden.platform.security.session;
 
+import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.*;
 import ua.com.fielden.platform.dao.CommonEntityDao;
+import ua.com.fielden.platform.dao.QueryExecutionModel;
 import ua.com.fielden.platform.entity.query.IFilter;
+import ua.com.fielden.platform.entity.query.fluent.fetch;
+import ua.com.fielden.platform.entity.query.model.EntityResultQueryModel;
+import ua.com.fielden.platform.entity.query.model.OrderingModel;
+import ua.com.fielden.platform.security.user.User;
 import ua.com.fielden.platform.swing.review.annotations.EntityType;
 
 import com.google.inject.Inject;
@@ -18,6 +24,58 @@ public class UserSessionDao extends CommonEntityDao<UserSession> implements IUse
     @Inject
     public UserSessionDao(final IFilter filter) {
         super(filter);
+    }
 
+    @Override
+    public void clearAll(final User user) {
+        super.delete(select(UserSession.class).where().prop("user").eq().val(user).model());
+    }
+
+    @Override
+    public void clearUntrusted(final User user) {
+        final EntityResultQueryModel<UserSession> query =
+                select(UserSession.class)
+                .where()
+                    .prop("user").eq().val(user)
+                    .and().prop("trusted").eq().val(false)
+                .model();
+        super.delete(query);
+    }
+
+    @Override
+    public void clearAll() {
+        super.delete(select(UserSession.class).model());
+
+    }
+
+    @Override
+    public void clearUntrusted() {
+        final EntityResultQueryModel<UserSession> query =
+                select(UserSession.class)
+                .where()
+                    .prop("trusted").eq().val(false)
+                .model();
+        super.delete(query);
+    }
+
+    @Override
+    public void clearExpired(final User user) {
+        final EntityResultQueryModel<UserSession> query =
+                select(UserSession.class)
+                .where()
+                    .prop("user").eq().val(user)
+                    .and().prop("expiryTime").lt().now()
+                .model();
+        super.delete(query);
+   }
+
+    @Override
+    public void clearExpired() {
+        final EntityResultQueryModel<UserSession> query =
+                select(UserSession.class)
+                .where()
+                    .prop("expiryTime").lt().now()
+                .model();
+        super.delete(query);
     }
 }
--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/session/IUserSession.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/session/IUserSession.java
@@ -1,13 +1,54 @@
 package ua.com.fielden.platform.security.session;
 
 import ua.com.fielden.platform.dao.IEntityDao;
+import ua.com.fielden.platform.security.user.User;
 
-/** 
+/**
  * Companion object for entity {@link UserSession}.
- * 
+ *
  * @author Developers
  *
  */
 public interface IUserSession extends IEntityDao<UserSession> {
+
+    /**
+     * Delete all sessions associated with the specified uses.
+     * Having no current sessions, enforces user to login explicitly.
+     * Therefore, this method should really be used by the system only in the event when a security threat is suspected or user account needs to be disabled.
+     *
+     * @param user
+     */
+    void clearAll(final User user);
+
+    /**
+     * Clears all sessions that are associated with untrusted devices for a given user.
+     *
+     * @param user
+     */
+    void clearUntrusted(final User user);
+
+    /**
+     * Clears all expired sessions for a given user. Could be used to tiding things up.
+     *
+     * @param user
+     */
+    void clearExpired(final User user);
+
+    /**
+     * Removes all sessions for all users.
+     */
+    void clearAll();
+
+    /**
+     * Clears all sessions that are associated with untrusted devices for all users.
+     */
+    void clearUntrusted();
+
+    /**
+     * Clears all expired sessions for all users. Could be used to tiding things up.
+     *
+     * @param user
+     */
+    void clearExpired();
 
 }
