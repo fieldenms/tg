540e35f4fecffce3af877580a09ad1d10322d202
Jhou (work) <jhou.pro@gmail.com>
2015-05-01 22:05:02 +0300

    #131 Completed functional entity for changing status to DR with dao and producer.

---
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/TgStatusActivationFunctionalEntityProducer.java
@@ -0,0 +1,33 @@
+package ua.com.fielden.platform.sample.domain;
+
+import ua.com.fielden.platform.dao.DefaultEntityProducer;
+import ua.com.fielden.platform.dao.IEntityProducer;
+import ua.com.fielden.platform.entity.factory.EntityFactory;
+import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
+
+import com.google.inject.Inject;
+
+/**
+ * A producer for new instances of entity {@link TgStatusActivationFunctionalEntity}.
+ *
+ * @author TG Team
+ *
+ */
+public class TgStatusActivationFunctionalEntityProducer extends DefaultEntityProducer<TgStatusActivationFunctionalEntity> implements IEntityProducer<TgStatusActivationFunctionalEntity> {
+    private final ITgStatusActivationFunctionalEntity companion;
+
+    @Inject
+    public TgStatusActivationFunctionalEntityProducer(final EntityFactory factory, final ICompanionObjectFinder companionFinder, final ITgStatusActivationFunctionalEntity companion) {
+        super(factory, TgStatusActivationFunctionalEntity.class, companionFinder);
+        this.companion = companion;
+    }
+
+    @Override
+    protected TgStatusActivationFunctionalEntity provideDefaultValues(final TgStatusActivationFunctionalEntity entity) {
+        entity.setKey("ANY");
+        if (getCentreContext() != null) {
+            entity.setContext(getCentreContext());
+        }
+        return entity;
+    }
+}
