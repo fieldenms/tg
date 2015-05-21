21da6c4a94ec207f1533a37f65b17938fa5a3893
Jhou (work) <jhou.pro@gmail.com>
2015-05-21 12:30:16 +0300

    #175 Added example for defining 'editable' state for some property after the change of other property (to demonstrate UI consequences).

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/definers/RequirednessDefiner.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/definers/RequirednessDefiner.java
@@ -8,6 +8,8 @@ public class RequirednessDefiner implements IAfterChangeEventHandler<Integer> {
     @Override
     public void handle(final MetaProperty<Integer> property, final Integer entityPropertyValue) {
         property.getEntity().getProperty("entityProp").setRequired(property.getValue() != null && property.getValue() > 100);
+
+        property.getEntity().getProperty("entityProp").setEditable(property.getValue() == null || property.getValue() != 13);
     }
 
 }
