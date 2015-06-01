12bdf046182713119d649f12d9685134bf0ead0b
01es <oles.hodych@gmail.com>
2015-06-01 13:26:31 +1000

    #187 Introduced contracts for specifying card-based layout for the restulset on Entity Centres.

---
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/resultset/layout/ICollapsedCardLayoutConfig.java
@@ -0,0 +1,25 @@
+package ua.com.fielden.platform.web.centre.api.resultset.layout;
+
+import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.web.centre.api.resultset.IResultSetBuilder3PrimaryAction;
+import ua.com.fielden.platform.web.layout.AbstractLayout;
+
+/**
+ * A contract for specifying card-based layout for an Entity Centre resultset.
+ *
+ * @author TG Team
+ *
+ * @param <T>
+ */
+public interface ICollapsedCardLayoutConfig<T extends AbstractEntity<?>> extends IResultSetBuilder3PrimaryAction<T> {
+    /**
+     * A method to specify a card layout for its collapsed state.
+     * This layout would be used as a default card-based representation of each entity in the resultset for the specified device.
+     *
+     * @param device
+     * @param orientation
+     * @param flexString
+     * @return
+     */
+    IExpandedCardLayoutConfig<T> setCollapsedCardLayoutFor(final AbstractLayout.Device device, final AbstractLayout.Orientation orientation, final String flexString);
+}
