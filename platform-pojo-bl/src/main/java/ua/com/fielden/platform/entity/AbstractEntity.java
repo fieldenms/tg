dd8f3fac12d1716012ddf1c18699031068e6e0dd
Jhou (work) <jhou.pro@gmail.com>
2015-07-30 09:48:27 +0300

    Removed stack trace from debug logging message.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/AbstractEntity.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/AbstractEntity.java
@@ -988,7 +988,7 @@ public abstract class AbstractEntity<K extends Comparable> implements Serializab
             return annotations;
         } catch (final NoSuchMethodException e1) {
             // do nothing if setter does not exist
-            logger.debug(format("There is no setter for property [%s].", field.getName()), e1);
+            logger.debug(format("There is no setter for property [%s].", field.getName()));
         }
         return new HashSet<>();
     }
