cf06d2353867fb674cc6d960e962561eb1f26dca
oleh-maikovych <oleh@fielden.com.au>
2015-08-25 13:58:40 +0300

    #215 Added resource that builds simplified mobile view

--- platform-web-resources/src/main/java/ua/com/fielden/platform/web/factories/AppIndexResourceFactory.java
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/factories/AppIndexResourceFactory.java
@@ -6,7 +6,7 @@ import org.restlet.Restlet;
 import org.restlet.data.Method;
 
 import ua.com.fielden.platform.web.app.IWebUiConfig;
-import ua.com.fielden.platform.web.resources.AppIndexResource;
+import ua.com.fielden.platform.web.resources.MobileAppIndexResource;
 
 public class AppIndexResourceFactory extends Restlet {
 
@@ -21,7 +21,11 @@ public class AppIndexResourceFactory extends Restlet {
         super.handle(request, response);
 
         if (Method.GET.equals(request.getMethod())) {
-            new AppIndexResource(app, getContext(), request, response).handle();
+            //if (request.getClientInfo().getAgent().contains("Android")) {
+            new MobileAppIndexResource(app, getContext(), request, response).handle();
+            //} else {
+            //new AppIndexResource(app, getContext(), request, response).handle();
+            //}
         }
     }
 }
