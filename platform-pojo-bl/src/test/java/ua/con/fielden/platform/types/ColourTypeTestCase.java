fdabe2cf7abdf1ec2a6f70a30f962abbda330a7d
krvavych <natalia.krvavych@gmail.com>
2015-08-31 13:41:17 +0300

    #217 Tests for Color type.

---
+++ platform-pojo-bl/src/test/java/ua/con/fielden/platform/types/ColourTypeTestCase.java
@@ -0,0 +1,81 @@
+package ua.con.fielden.platform.types;
+
+import static org.junit.Assert.*;
+import groovy.ui.Console;
+
+import org.junit.Test;
+
+import ua.com.fielden.platform.types.Colour;
+
+public class ColourTypeTestCase {
+	@Test
+	public void color_hexCode_creation_test() {
+		final Colour color = new Colour("bbb");
+		assertEquals("BBB", color.getColourValue());
+	}
+
+	@Test
+	public void colour_hexCode_must_have_correct_length() {
+		try {
+			new Colour("a");
+			fail();
+		} catch (final Exception e) {
+		}
+		try {
+			new Colour("");
+			fail();
+		} catch (final Exception e) {
+		}
+		try {
+			new Colour("a2");
+			fail();
+		} catch (final Exception e) {
+		}
+		try {
+			new Colour("aaaa");
+			fail();
+		} catch (final Exception e) {
+		}
+		try {
+			new Colour("aaaaa");
+			fail();
+		} catch (final Exception e) {
+		}
+		try {
+			new Colour(null);
+			fail();
+		} catch (final Exception e) {
+		}
+
+	}
+
+	@Test
+	public void color_hexCode_must_be_build_by_correct_sumbols() {
+		try {
+			new Colour("ggg");
+			fail();
+		} catch (final Exception e) {
+
+		}
+		try {
+			new Colour("ffj");
+			fail();
+		} catch (final Exception e) {
+
+		}
+		try {
+			new Colour("gba999");
+			fail();
+		} catch (final Exception e) {
+
+		}
+		try {
+			new Colour("12345v");
+			fail();
+		} catch (final Exception e) {
+
+		}
+
+	}
+
+}
