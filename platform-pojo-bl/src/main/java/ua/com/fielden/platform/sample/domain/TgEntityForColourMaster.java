8f31e1a7d8886288298e58425148a4acb2535b03
krvavych <natalia.krvavych@gmail.com>
2015-09-24 16:34:13 +0300

    #234 Added entity for creating master for Colour type.

---
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/TgEntityForColourMaster.java
@@ -0,0 +1,23 @@
+package ua.com.fielden.platform.sample.domain;
+
+import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.entity.annotation.CompanionObject;
+import ua.com.fielden.platform.entity.annotation.DescTitle;
+import ua.com.fielden.platform.entity.annotation.KeyTitle;
+import ua.com.fielden.platform.entity.annotation.KeyType;
+import ua.com.fielden.platform.entity.annotation.MapEntityTo;
+
+/**
+ * Master entity object.
+ *
+ * @author Developers
+ *
+ */
+@KeyType(String.class)
+@KeyTitle(value = "Key", desc = "Some key description")
+@CompanionObject(ITgEntityForColourMaster.class)
+@MapEntityTo
+@DescTitle(value = "Desc", desc = "Some desc description")
+public class TgEntityForColourMaster extends AbstractEntity<String> {
+
+}
