9022ae9ae6eb906423c95c90c4fc21a4205514fc
krvavych <natalia.krvavych@gmail.com>
2015-09-25 09:39:07 +0300

    #234 Added properties to TgEntityForColourMaster.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/TgEntityForColourMaster.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/TgEntityForColourMaster.java
@@ -1,11 +1,18 @@
 package ua.com.fielden.platform.sample.domain;
 
+import java.util.Date;
+
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.annotation.CompanionObject;
 import ua.com.fielden.platform.entity.annotation.DescTitle;
+import ua.com.fielden.platform.entity.annotation.IsProperty;
 import ua.com.fielden.platform.entity.annotation.KeyTitle;
 import ua.com.fielden.platform.entity.annotation.KeyType;
 import ua.com.fielden.platform.entity.annotation.MapEntityTo;
+import ua.com.fielden.platform.entity.annotation.MapTo;
+import ua.com.fielden.platform.entity.annotation.Observable;
+import ua.com.fielden.platform.entity.annotation.Title;
+import ua.com.fielden.platform.types.Colour;
 
 /**
  * Master entity object.
@@ -19,5 +26,64 @@ import ua.com.fielden.platform.entity.annotation.MapEntityTo;
 @MapEntityTo
 @DescTitle(value = "Desc", desc = "Some desc description")
 public class TgEntityForColourMaster extends AbstractEntity<String> {
+    @IsProperty
+    @MapTo
+    @Title(value = "String prop", desc = "String prop description")
+    private String stringProp;
+
+    @IsProperty
+    @MapTo
+    @Title(value = "Boolean prop", desc = " Boolean prop description")
+    private Boolean booleanProp;
+
+    @IsProperty
+    @MapTo
+    @Title(value = "Date prop", desc = "Date prop desc")
+    private Date dateProp;
+
+    @IsProperty
+    @MapTo
+    @Title(value = "Colour prop", desc = "Colour prop description")
+    private Colour colourProp;
+
+    @Observable
+    public TgEntityForColourMaster setColourProp(final Colour colourProp) {
+        this.colourProp = colourProp;
+        return this;
+    }
+
+    public Colour getColourProp() {
+        return colourProp;
+    }
+
+    @Observable
+    public TgEntityForColourMaster setDateProp(final Date dateProp) {
+        this.dateProp = dateProp;
+        return this;
+    }
+
+    public Date getDateProp() {
+        return dateProp;
+    }
+
+    @Observable
+    public TgEntityForColourMaster setBooleanProp(final Boolean booleanProp) {
+        this.booleanProp = booleanProp;
+        return this;
+    }
+
+    public Boolean getBooleanProp() {
+        return booleanProp;
+    }
+
+    @Observable
+    public TgEntityForColourMaster setStringProp(final String stringProp) {
+        this.stringProp = stringProp;
+        return this;
+    }
+
+    public String getStringProp() {
+        return stringProp;
+    }
 
 }
