543583a3ac25dd054f50decc52d28f10427c743e
Jhou and 01es <oles@Neuromancer.local>
2015-10-16 10:27:09 +1100

    #265 Reused the default implementation for method enhanceQueryParams in IQueryEnhancer.

--- platform-web-resources/src/main/java/ua/com/fielden/platform/web/test/server/WebUiConfig.java
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/test/server/WebUiConfig.java
@@ -661,11 +661,6 @@ public class WebUiConfig extends AbstractWebUiConfig {
             }
             return where.val(1).eq().val(1);
         }
-
-        @Override
-        public Map<String, Object> enhanceQueryParams(final Map<String, Object> queryParams, final Optional<CentreContext<TgPersistentEntityWithProperties, ?>> context) {
-            return queryParams;
-        }
     }
 
     private static class TgPersistentEntityWithPropertiesQueryEnhancer implements IQueryEnhancer<TgPersistentEntityWithProperties> {
@@ -688,11 +683,6 @@ public class WebUiConfig extends AbstractWebUiConfig {
             }
 
             return where.prop("status").eq().val(coStatus.findByKey("IS"));
-        }
-
-        @Override
-        public Map<String, Object> enhanceQueryParams(final Map<String, Object> queryParams, final Optional<CentreContext<TgPersistentEntityWithProperties, ?>> context) {
-            return queryParams;
         }
     }
 
