41c673b2058ac0dbccdb65f40f2e26ecd0e33e69
01es <oles.hodych@gmail.com>
2015-11-27 13:28:31 +1100

    #311 Introduced a 3 sec delay for TgDummyAction in order to demonstrate the current problem with the action spinner using style attribute display.

--- platform-dao/src/main/java/ua/com/fielden/platform/sample/domain/TgDummyActionDao.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/sample/domain/TgDummyActionDao.java
@@ -1,12 +1,10 @@
 package ua.com.fielden.platform.sample.domain;
 
-import ua.com.fielden.platform.dao.CommonEntityDao;
-import ua.com.fielden.platform.swing.review.annotations.EntityType;
-import ua.com.fielden.platform.entity.query.IFilter;
-import ua.com.fielden.platform.entity.query.model.EntityResultQueryModel;
-import java.util.Map;
-import ua.com.fielden.platform.dao.annotations.SessionRequired;
 import com.google.inject.Inject;
+
+import ua.com.fielden.platform.dao.CommonEntityDao;
+import ua.com.fielden.platform.entity.query.IFilter;
+import ua.com.fielden.platform.swing.review.annotations.EntityType;
 
 /** 
  * DAO implementation for companion object {@link ITgDummyAction}.
@@ -16,9 +14,21 @@ import com.google.inject.Inject;
  */
 @EntityType(TgDummyAction.class)
 public class TgDummyActionDao extends CommonEntityDao<TgDummyAction> implements ITgDummyAction {
+    
     @Inject
     public TgDummyActionDao(final IFilter filter) {
         super(filter);
+    }
+    
+    @Override
+    public TgDummyAction save(TgDummyAction entity) {
+        // let's introduce some delay to demonstrate action in progress spinner
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+        }
+
+        return super.save(entity);
     }
 
 }
