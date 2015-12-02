59a80ba71a27a1659510d56716e305ad7119cdba
krvavych <natalia.krvavych@gmail.com>
2015-12-02 10:37:46 +0200

    #395 Made functions static.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/svg/combining/SvgIconsetUtility.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/svg/combining/SvgIconsetUtility.java
@@ -11,22 +11,7 @@ import com.google.common.base.Charsets;
 
 public class SvgIconsetUtility {
 
-//    public static void main(final String[] args) throws IOException {
-//        final String[] srcFiles = new String[args.length - 3];
-//        final String iconsetId = args[0];
-//        final String svgWidth = args[1];
-//        final String outputFile = args[2];
-//
-//        for (int i = 0; i < args.length-3; i++) {
-//            srcFiles[i] = args[i+3];
-//        }
-//        final SvgIconsetUtility combiningUtility = new SvgIconsetUtility();
-//        combiningUtility.createSvgIconset(srcFiles, outputFile, iconsetId, svgWidth);
-//    }
-
     public static void createSvgIconset(final String[] srcFiles, final String outputFile, final String iconsetId, final String svgWidth) throws IOException {
-
-        final SvgIconsetUtility iconsetUtility = new SvgIconsetUtility();
         final Validator validator = new Validator();
         for (final String file : srcFiles) {
             validator.validate(file);
@@ -34,14 +19,14 @@ public class SvgIconsetUtility {
         validator.validate(outputFile);
         validator.validateInt(svgWidth);
         try (OutputStream outputStream = new FileOutputStream(outputFile)) {
-           iconsetUtility.writeAllFilesContent(outputStream, iconsetUtility.getAndJoinContentOfFiles(srcFiles) + "\n", iconsetId, svgWidth);
+           writeAllFilesContent(outputStream, getAndJoinContentOfFiles(srcFiles) + "\n", iconsetId, svgWidth);
             System.out.println("Iron-iconset-svg creation is complete!");
         } catch (final IOException e) {
             throw new IOException("Something is wrong! Iron-iconset-svg was not created!");
         }
     }
 
-    private OutputStream writeAllFilesContent(final OutputStream outputStream, final String string, final String name, final String size) throws IOException {
+    private static OutputStream writeAllFilesContent(final OutputStream outputStream, final String string, final String name, final String size) throws IOException {
         final String fileBegin = new FileBegin(name, size).getFileBegin();
         final String fileEnd = new FileEnd().getFileEnd();
         outputStream.write(fileBegin.getBytes(Charsets.UTF_8));
@@ -51,11 +36,11 @@ public class SvgIconsetUtility {
 
     }
 
-    private String getAndJoinContentOfFiles(final String[] srcFile) throws IOException {
+    private static String getAndJoinContentOfFiles(final String[] srcFile) throws IOException {
         return joinFilesContent(0, srcFile, "");
     }
 
-    private String joinFilesContent(final int fileToCopy, final String[] files, final String joinedFilesConent) throws IOException {
+    private static String joinFilesContent(final int fileToCopy, final String[] files, final String joinedFilesConent) throws IOException {
         if (fileToCopy == files.length) {
             return joinedFilesConent;
         } else {
