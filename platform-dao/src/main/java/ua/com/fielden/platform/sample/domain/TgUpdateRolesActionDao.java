6af07b0b73237cfe9731a38f29ed48cc7529b4c2
Jhou <jhou.pro@gmail.com>
2016-01-18 18:24:41 +0200

    #363 Extracted generic validation logic for functional action dao and reused in example TgUpdateRolesAction entity. Added additional check for removal of master entity. Improved messaging in case of validation errors.

--- platform-dao/src/main/java/ua/com/fielden/platform/sample/domain/TgUpdateRolesActionDao.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/sample/domain/TgUpdateRolesActionDao.java
@@ -1,7 +1,5 @@
 package ua.com.fielden.platform.sample.domain;
 
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
@@ -14,9 +12,8 @@ import ua.com.fielden.platform.dao.AbstractFunctionalEntityProducerForCollection
 import ua.com.fielden.platform.dao.CommonEntityDao;
 import ua.com.fielden.platform.dao.IUserRoleDao;
 import ua.com.fielden.platform.dao.annotations.SessionRequired;
-import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.entity.query.IFilter;
-import ua.com.fielden.platform.error.Result;
 import ua.com.fielden.platform.security.IUserAndRoleAssociationBatchAction;
 import ua.com.fielden.platform.security.UserAndRoleAssociationBatchAction;
 import ua.com.fielden.platform.security.user.User;
@@ -33,87 +30,44 @@ import ua.com.fielden.platform.swing.review.annotations.EntityType;
 @EntityType(TgUpdateRolesAction.class)
 public class TgUpdateRolesActionDao extends CommonEntityDao<TgUpdateRolesAction> implements ITgUpdateRolesAction {
     private final Logger logger = Logger.getLogger(getClass());
-    private final IUserRoleDao coUserRole;
     private final IUserAndRoleAssociationBatchAction coUserAndRoleAssociationBatchAction;
+    private final EntityFactory factory;
     
     @Inject
-    public TgUpdateRolesActionDao(final IFilter filter, final IUserRoleDao coUserRole, final IUserAndRoleAssociationBatchAction coUserAndRoleAssociationBatchAction) {
+    public TgUpdateRolesActionDao(final IFilter filter, final IUserAndRoleAssociationBatchAction coUserAndRoleAssociationBatchAction, final EntityFactory factory) {
         super(filter);
-        this.coUserRole = coUserRole;
         this.coUserAndRoleAssociationBatchAction = coUserAndRoleAssociationBatchAction;
+        this.factory = factory;
     }
     
     @Override
     @SessionRequired
-    public TgUpdateRolesAction save(final TgUpdateRolesAction entity) {
-        final Result res = entity.isValid();
-        if (!res.isSuccessful()) {
-            throw res;
-        }
+    public TgUpdateRolesAction save(final TgUpdateRolesAction action) {
+        final TgUpdateRolesAction actionToSave = AbstractFunctionalEntityProducerForCollectionModification.validateAction(action, a -> a.getRoles(), this, factory);
         
-        logger.error("surrogate version after returning to the server == " + entity.getSurrogateVersion());
-        
-        // logger.error("entity.getRoles() = " + entity.getRoles());
-        // logger.error("entity.getContext().getMasterEntity() = " + entity.getContext().getMasterEntity());
-        // logger.error("((User) entity.getContext().getMasterEntity()).getRoles() = " + ((User) entity.getContext().getMasterEntity()).getRoles() );
-        
-        logger.error("entity.getChosenIds() = " + entity.getChosenIds());
-        logger.error("entity.getAddedIds() = " + entity.getAddedIds());
-        logger.error("entity.getRemovedIds() = " + entity.getRemovedIds());
-        
-        final User userBeingUpdated = entity.getKey();
-        final Map<Long, UserRole> roles = mapById(coUserRole.findAll());
+        // after all validations have passed -- the association changes could be saved:
+        final User userBeingUpdated = action.getKey();
+        final Map<Long, UserRole> availableRoles = AbstractFunctionalEntityProducerForCollectionModification.mapById(action.getRoles());
         
         final Set<UserAndRoleAssociation> addedAssociations = new LinkedHashSet<>();
-        for (final Long addedId : entity.getAddedIds()) {
-            if (!roles.containsKey(addedId)) {
-                throw Result.failure(String.format("Another user has deleted the role with id = %s.", addedId)); // TODO need to have a description of non-existent entity?
-            }
-            addedAssociations.add(new UserAndRoleAssociation(userBeingUpdated, roles.get(addedId)));
+        for (final Long addedId : action.getAddedIds()) {
+            addedAssociations.add(new UserAndRoleAssociation(userBeingUpdated, availableRoles.get(addedId)));
         }
 
         final Set<UserAndRoleAssociation> removedAssociations = new LinkedHashSet<>();
-        for (final Long removedId : entity.getRemovedIds()) {
-            if (!roles.containsKey(removedId)) {
-                throw Result.failure(String.format("Another user has deleted the role with id = %s.", removedId)); // TODO need to have a description of non-existent entity?
-            }
-            removedAssociations.add(new UserAndRoleAssociation(userBeingUpdated, roles.get(removedId)));
+        for (final Long removedId : action.getRemovedIds()) {
+            removedAssociations.add(new UserAndRoleAssociation(userBeingUpdated, availableRoles.get(removedId)));
         }
 
-        final UserAndRoleAssociationBatchAction action = new UserAndRoleAssociationBatchAction();
-        action.setSaveEntities(addedAssociations);
-        action.setRemoveEntities(removedAssociations);
+        final UserAndRoleAssociationBatchAction batchAction = new UserAndRoleAssociationBatchAction();
+        batchAction.setSaveEntities(addedAssociations);
+        batchAction.setRemoveEntities(removedAssociations);
+        coUserAndRoleAssociationBatchAction.save(batchAction);
         
-        final TgUpdateRolesAction persistedEntity = AbstractFunctionalEntityProducerForCollectionModification.retrieveActionFor(userBeingUpdated, this, TgUpdateRolesAction.class);
-        final TgUpdateRolesAction entityToSave = persistedEntity == null ? new TgUpdateRolesAction().setKey(userBeingUpdated) : persistedEntity;
-        
-        if (AbstractFunctionalEntityProducerForCollectionModification.surrogateVersion(persistedEntity) > entity.getSurrogateVersion()) {
-            throw Result.failure(String.format("Another user has changed 'roles' collection of [%s]. surrogateVersion(persistedEntity) = %s > entity.getSurrogateVersion() = %s", userBeingUpdated, TgUpdateRolesActionProducer.surrogateVersion(persistedEntity), entity.getSurrogateVersion()));
-        }
-        
-        // the next block of code is intended to mark entityToSave as 'dirty' to be properly saved and to increase its db-related version. New entity (persistedEntity == null) is always dirty - no need to do anything.
-        if (persistedEntity != null) {
-            entityToSave.setSurrogateVersion(persistedEntity.getVersion() + 1L);
-        }
-        
-        coUserAndRoleAssociationBatchAction.save(action);
-        final TgUpdateRolesAction saved = super.save(entityToSave);
+        // after the association changes were successfully saved, the action should also be saved:
+        final TgUpdateRolesAction saved = super.save(actionToSave);
         logger.error("saved.getVersion() = " + saved.getVersion());
         logger.error("saved.getSurrogateVersion() = " + saved.getSurrogateVersion());
         return saved;
-    }
-    
-    /**
-     * Returns the map between id and the entity with that id.
-     * 
-     * @param entities
-     * @return
-     */
-    private <T extends AbstractEntity<?>> Map<Long, T> mapById(final Collection<T> entities) {
-        final Map<Long, T> map = new HashMap<>();
-        for (final T entity : entities) {
-            map.put(entity.getId(), entity);
-        }
-        return map;
     }
 }
--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/dao/AbstractFunctionalEntityProducerForCollectionModification.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/dao/AbstractFunctionalEntityProducerForCollectionModification.java
@@ -4,6 +4,11 @@ import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.fetch
 import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.from;
 import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.select;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 import org.apache.log4j.Logger;
 
 import com.google.inject.Inject;
@@ -31,10 +36,11 @@ import ua.com.fielden.platform.web.centre.CentreContext;
  *
  */
 public abstract class AbstractFunctionalEntityProducerForCollectionModification<MASTER_TYPE extends AbstractEntity<?>, T extends AbstractFunctionalEntityForCollectionModification<MASTER_TYPE>> extends DefaultEntityProducerWithContext<T, T> implements IEntityProducer<T> {
-    private final Logger logger = Logger.getLogger(getClass());
+    private final static Logger logger = Logger.getLogger(AbstractFunctionalEntityProducerForCollectionModification.class);
     private final IEntityDao<T> companion;
     private final ICompanionObjectFinder companionFinder;
     private final Class<MASTER_TYPE> masterEntityType;
+    private static final String TRY_AGAIN_MSG = "Please cancel this action and try again!";
     
     @Inject
     public AbstractFunctionalEntityProducerForCollectionModification(final EntityFactory factory, final Class<T> actionType, final ICompanionObjectFinder companionFinder) {
@@ -70,6 +76,9 @@ public abstract class AbstractFunctionalEntityProducerForCollectionModification<
             throw Result.failure("This action is applicable only to a saved entity! Please save entity and try again!");
         }
         final MASTER_TYPE masterEntity = companionFinder.find(masterEntityType).findById(masterEntityFromContext.getId(), fetchModelForMasterEntity());
+        if (masterEntity == null) {
+            throw Result.failure("The master entity has been deleted. " + TRY_AGAIN_MSG);
+        }
         
         // IMPORTANT: it is necessary to reset state for "key" property after its change.
         //   This is necessary to make the property marked as 'not changed from original' (origVal == val == 'DEMO') to be able not to re-apply afterwards
@@ -103,14 +112,84 @@ public abstract class AbstractFunctionalEntityProducerForCollectionModification<
      */
     protected abstract T provideCurrentlyAssociatedValues(final T entity, final MASTER_TYPE masterEntity);
     
-    public static <T extends AbstractEntity<?>> Long surrogateVersion(final T persistedEntity) {
+    private static <T extends AbstractEntity<?>> Long surrogateVersion(final T persistedEntity) {
         return persistedEntity == null ? 99L : (persistedEntity.getVersion() + 100L);
     }
     
-    public static <MASTER_TYPE extends AbstractEntity<?>, T extends AbstractFunctionalEntityForCollectionModification<MASTER_TYPE>> T retrieveActionFor(final MASTER_TYPE masterEntity, final IEntityDao<T> companion, final Class<T> actionType) {
+    private static <MASTER_TYPE extends AbstractEntity<?>, T extends AbstractFunctionalEntityForCollectionModification<MASTER_TYPE>> T retrieveActionFor(final MASTER_TYPE masterEntity, final IEntityDao<T> companion, final Class<T> actionType) {
         return companion.getEntity(
                 from(select(actionType).where().prop(AbstractEntity.KEY).eq().val(masterEntity).model())
                 .with(fetch(actionType).with(AbstractEntity.KEY)).model()
         );
+    }
+    
+    /**
+     * Validates restored action (it was "produced" by corresponding producer) on subject of 1) removal of available collectional items 2) concurrent modification of the same collection for the same master entity.
+     * After the validation has 
+     * 
+     * @param action
+     * @param availableEntitiesGetter
+     * @param companion
+     * @param factory
+     * @return
+     */
+    public static <MASTER_TYPE extends AbstractEntity<?>, ITEM extends AbstractEntity<?>, T extends AbstractFunctionalEntityForCollectionModification<MASTER_TYPE>> T validateAction(final T action, final Function<T, Collection<ITEM>> availableEntitiesGetter, final IEntityDao<T> companion, final EntityFactory factory) {
+        final Result res = action.isValid();
+        // throw validation result of the action if it is not successful:
+        if (!res.isSuccessful()) {
+            throw res;
+        }
+        
+        logger.error("action's surrogate version after returning to the server == " + action.getSurrogateVersion());
+        
+        logger.error("action's chosenIds = " + action.getChosenIds());
+        logger.error("action's addedIds = " + action.getAddedIds());
+        logger.error("action's removedIds = " + action.getRemovedIds());
+        
+        final MASTER_TYPE masterEntityBeingUpdated = action.getKey(); // existence of master entity is checked during "producing" of functional action
+        final Map<Long, ITEM> availableEntities = mapById(availableEntitiesGetter.apply(action));
+        
+        for (final Long addedId : action.getAddedIds()) {
+            if (!availableEntities.containsKey(addedId)) {
+                throw Result.failure("Another user has deleted the item, that you're trying to choose. " + TRY_AGAIN_MSG);
+            }
+        }
+        for (final Long removedId : action.getRemovedIds()) {
+            if (!availableEntities.containsKey(removedId)) {
+                throw Result.failure("Another user has deleted the item, that you're trying to un-tick. " + TRY_AGAIN_MSG);
+            }
+        }
+        
+        final Class<T> actionType = (Class<T>) action.getType();
+        final T persistedEntity = retrieveActionFor(masterEntityBeingUpdated, companion, actionType);
+        
+        if (surrogateVersion(persistedEntity) > action.getSurrogateVersion()) {
+            throw Result.failure("Another user has changed the chosen items. " + TRY_AGAIN_MSG);
+        }
+        
+        final T entityToSave;
+        // the next block of code is intended to mark entityToSave as 'dirty' to be properly saved and to increase its db-related version. New entity (persistedEntity == null) is always dirty - no need to do anything.
+        if (persistedEntity != null) {
+            entityToSave = persistedEntity;
+            entityToSave.setSurrogateVersion(persistedEntity.getVersion() + 1L);
+        } else {
+            entityToSave = factory.newPlainEntity(actionType, null);
+            entityToSave.setKey(masterEntityBeingUpdated);
+        }
+        return entityToSave;
+    }
+    
+    /**
+     * Returns the map between id and the entity with that id.
+     * 
+     * @param entities
+     * @return
+     */
+    public static <T extends AbstractEntity<?>> Map<Long, T> mapById(final Collection<T> entities) {
+        final Map<Long, T> map = new HashMap<>();
+        for (final T entity : entities) {
+            map.put(entity.getId(), entity);
+        }
+        return map;
     }
 }
