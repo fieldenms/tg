d96acabd55d48aa6c708cb2ab19a8a7f16781c63
Jhou <jhou.pro@gmail.com>
2016-01-21 11:18:31 +0200

    #363 Started implementation of saving added / removed token associations.

--- platform-dao/src/main/java/ua/com/fielden/platform/sample/domain/TgUpdateTokensActionDao.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/sample/domain/TgUpdateTokensActionDao.java
@@ -13,9 +13,11 @@ import ua.com.fielden.platform.dao.CommonEntityDao;
 import ua.com.fielden.platform.dao.annotations.SessionRequired;
 import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.entity.query.IFilter;
+import ua.com.fielden.platform.error.Result;
+import ua.com.fielden.platform.security.ISecurityToken;
 import ua.com.fielden.platform.security.IUserAndRoleAssociationBatchAction;
 import ua.com.fielden.platform.security.UserAndRoleAssociationBatchAction;
-import ua.com.fielden.platform.security.user.User;
+import ua.com.fielden.platform.security.user.SecurityRoleAssociation;
 import ua.com.fielden.platform.security.user.UserAndRoleAssociation;
 import ua.com.fielden.platform.security.user.UserRole;
 import ua.com.fielden.platform.swing.review.annotations.EntityType;
@@ -45,30 +47,43 @@ public class TgUpdateTokensActionDao extends CommonEntityDao<TgUpdateTokensActio
         final TgUpdateTokensAction actionToSave = AbstractFunctionalEntityProducerForCollectionModification.validateAction(action, a -> a.getTokens(), this, factory, String.class);
 
         // TODO implement
-//        // after all validations have passed -- the association changes could be saved:
-//        final User userBeingUpdated = action.getKey();
-//        final Map<Long, UserRole> availableRoles = AbstractFunctionalEntityProducerForCollectionModification.mapById(action.getRoles());
-//        
-//        final Set<UserAndRoleAssociation> addedAssociations = new LinkedHashSet<>();
-//        for (final Long addedId : action.getAddedIds()) {
-//            addedAssociations.add(new UserAndRoleAssociation(userBeingUpdated, availableRoles.get(addedId)));
-//        }
-//
-//        final Set<UserAndRoleAssociation> removedAssociations = new LinkedHashSet<>();
-//        for (final Long removedId : action.getRemovedIds()) {
-//            removedAssociations.add(new UserAndRoleAssociation(userBeingUpdated, availableRoles.get(removedId)));
-//        }
-//
+        // after all validations have passed -- the association changes could be saved:
+        final UserRole userRoleBeingUpdated = action.getKey();
+        final Map<Object, TgSecurityToken> availableTokens = AbstractFunctionalEntityProducerForCollectionModification.mapById(action.getTokens(), String.class);
+        
+        final Set<SecurityRoleAssociation> addedAssociations = new LinkedHashSet<>();
+        for (final String addedId : action.getAddedIds()) {
+            final Class<? extends ISecurityToken> token = loadToken(availableTokens.get(addedId).getKey());
+            addedAssociations.add(factory.newPlainEntity(SecurityRoleAssociation.class, null).setSecurityToken(token).setRole(userRoleBeingUpdated));
+        }
+
+        final Set<SecurityRoleAssociation> removedAssociations = new LinkedHashSet<>();
+        for (final String removedId : action.getRemovedIds()) {
+            final Class<? extends ISecurityToken> token = loadToken(availableTokens.get(removedId).getKey());
+            removedAssociations.add(factory.newPlainEntity(SecurityRoleAssociation.class, null).setSecurityToken(token).setRole(userRoleBeingUpdated));
+        }
+        
+        logger.error("addedAssociations == " + addedAssociations + " removedAssociations == " + removedAssociations);
+
 //        final UserAndRoleAssociationBatchAction batchAction = new UserAndRoleAssociationBatchAction();
 //        batchAction.setSaveEntities(addedAssociations);
 //        batchAction.setRemoveEntities(removedAssociations);
 //        coUserAndRoleAssociationBatchAction.save(batchAction);
-//        
-//        // after the association changes were successfully saved, the action should also be saved:
-//        final TgUpdateRolesAction saved = super.save(actionToSave);
-//        logger.error("saved.getVersion() = " + saved.getVersion());
-//        logger.error("saved.getSurrogateVersion() = " + saved.getSurrogateVersion());
-//        return saved;
-        return actionToSave;
+        
+        // after the association changes were successfully saved, the action should also be saved:
+        final TgUpdateTokensAction saved = super.save(actionToSave);
+        logger.error("saved.getVersion() = " + saved.getVersion());
+        logger.error("saved.getSurrogateVersion() = " + saved.getSurrogateVersion());
+        return saved;
+    }
+
+    private Class<? extends ISecurityToken> loadToken(final String name) {
+        final Class<? extends ISecurityToken> token;
+        try {
+            token = (Class<? extends ISecurityToken>) Class.forName(name);
+        } catch (final ClassNotFoundException e) {
+            throw Result.failure(new IllegalStateException(String.format("Security token [%s] could not be found.", name)));
+        }
+        return token;
     }
 }
