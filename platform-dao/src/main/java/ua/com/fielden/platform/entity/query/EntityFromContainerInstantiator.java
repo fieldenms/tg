c5a8df178173ab88b525effe20efa9c1d41515dd
Oleh <oleh@fielden.com.au>
2016-02-01 20:17:03 +0200

    #385 Removed comment.

--- platform-dao/src/main/java/ua/com/fielden/platform/entity/query/EntityFromContainerInstantiator.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/entity/query/EntityFromContainerInstantiator.java
@@ -78,7 +78,7 @@ public class EntityFromContainerInstantiator {
             setPropertyValue(justAddedEntity, entityEntry.getKey(), instantiate(entityEntry.getValue().getContainers()), entityContainer.getResultType());
         }
 
-        if (!lightweight/* && !isGenerated(entityContainer.getResultType())*/) {
+        if (!lightweight) {
             EntityUtils.handleMetaProperties(justAddedEntity, proxiedProps);
         }
 
