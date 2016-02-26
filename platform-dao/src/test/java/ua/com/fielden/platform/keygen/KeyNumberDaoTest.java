faa3781f5496e503afcd77d640fbf3f18031783e
01es <oles.hodych@gmail.com>
2016-02-26 11:15:02 +1100

    #392 Adjusted KeyNumber to adhere to a more strict entity contract (+tests).

--- platform-dao/src/test/java/ua/com/fielden/platform/keygen/KeyNumberDaoTest.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/keygen/KeyNumberDaoTest.java
@@ -1,23 +1,45 @@
 package ua.com.fielden.platform.keygen;
 
+import org.junit.Test;
+
+import ua.com.fielden.platform.dao.exceptions.EntityCompanionException;
 import ua.com.fielden.platform.test.DbDrivenTestCase;
 
 /**
- * Ensures correct generation of work order numbers.
+ * Ensures correct generation of named numbers that are used for automatic entity key generations such as 
+ * in case of work orders and purchase orders.
  * 
- * @author 01es
+ * @author TG Team
  * 
  */
 public class KeyNumberDaoTest extends DbDrivenTestCase {
-    private final IKeyNumber keyNumberGen = injector.getInstance(IKeyNumber.class);
+    private final IKeyNumber coKeyNumber = injector.getInstance(IKeyNumber.class);
 
-    public void testThatCurrentWoNoIsRetrievedCorrectly() {
-        assertEquals("Incorrect current WONO.", new Integer("500"), keyNumberGen.currNumber("WO"));
+    @Test
+    public void test_new_keynumber_can_be_created_and_persisted_adhoc() {
+        final String newKey = "YET-NON-EXISTING";
+        try {
+            coKeyNumber.currNumber(newKey);
+            fail("Getting the next number for non existing key should fail.");
+        } catch (final EntityCompanionException ex) {
+            assertEquals(String.format("No number associated with key [%s].", newKey), ex.getMessage());
+        }
+        
+        final Integer expected = 1;
+        assertEquals("Incorrect initial number.", expected, coKeyNumber.nextNumber(newKey));
+        assertEquals("Incorrect current number.", expected, coKeyNumber.currNumber(newKey));
+    }
+    
+    @Test
+    public void test_existing_keynumber_is_retrievable_by_key() {
+        assertEquals("Incorrect current WO number.", new Integer("500"), coKeyNumber.currNumber("WO"));
     }
 
-    public void testThatNextWoNoIsGenereatedCorrectly() {
-        assertEquals("Incorrectly generated next WONO.", new Integer("501"), keyNumberGen.nextNumber("WO"));
-        assertEquals("Incorrect current WONO after generation.", new Integer("501"), keyNumberGen.currNumber("WO"));
+    @Test
+    public void test_the_next_keynumber_gets_generated_and_simultaneously_persisted() {
+        final Integer nextNumber = 501;
+        assertEquals("Incorrectly generated next WO number.", nextNumber, coKeyNumber.nextNumber("WO"));
+        assertEquals("Incorrect current WO number after generating the next number.", nextNumber, coKeyNumber.currNumber("WO"));
     }
 
     @Override
