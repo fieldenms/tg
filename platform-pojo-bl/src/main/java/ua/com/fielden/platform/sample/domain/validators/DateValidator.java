3930453bf186b75ce32cd03ff9fa9d8fdad2ecf9
Jhou <jhou.pro@gmail.com>
2016-03-21 19:45:27 +0200

    #474 Added the tests that check rebinding of entity when exceptions / results are thrown for new / existing entity.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/validators/DateValidator.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/validators/DateValidator.java
@@ -8,6 +8,7 @@ import org.joda.time.DateTime;
 
 import com.google.inject.Inject;
 
+import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.meta.MetaProperty;
 import ua.com.fielden.platform.entity.validation.IBeforeChangeEventHandler;
 import ua.com.fielden.platform.error.Result;
@@ -21,8 +22,15 @@ public class DateValidator implements IBeforeChangeEventHandler<Date> {
 
     @Override
     public Result handle(final MetaProperty<Date> property, final Date newValue, final Date oldValue, final Set<Annotation> mutatorAnnotations) {
-        if (newValue != null && new DateTime(2003, 2, 1, 6, 19).equals(new DateTime(newValue))) {
-            return Result.failure(newValue, "[1/2/3 6:19] is not acceptable.");
+        final AbstractEntity<?> entity = property.getEntity();
+        if (!entity.isPersisted()) {
+            if (newValue != null && new DateTime(2003, 2, 1, 6, 19).equals(new DateTime(newValue))) {
+                return Result.failure(newValue, "[1/2/3 6:19] is not acceptable.");
+            }
+        } else {
+            if (newValue != null && new DateTime(2003, 2, 1, 6, 22).equals(new DateTime(newValue))) {
+                return Result.failure(newValue, "[1/2/3 6:22] is not acceptable for persisted entity.");
+            }
         }
         return Result.successful(newValue);
     }
