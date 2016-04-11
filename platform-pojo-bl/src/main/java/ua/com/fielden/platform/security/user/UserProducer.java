86dc5e50e2cfed03dd746277992363ce0938427b
01es <oles.hodych@gmail.com>
2016-04-11 11:46:50 +1000

    #495 Added new and delete User actions. Refactored User edit and add/remove roles actions.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/user/UserProducer.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/user/UserProducer.java
@@ -2,8 +2,7 @@ package ua.com.fielden.platform.security.user;
 
 import com.google.inject.Inject;
 
-import ua.com.fielden.platform.dao.DefaultEntityProducerWithContext;
-import ua.com.fielden.platform.dao.IEntityProducer;
+import ua.com.fielden.platform.dao.EntityProducerWithNewEditActions;
 import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
 
@@ -13,14 +12,10 @@ import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
  * @author TG Team
  *
  */
-public class UserProducer extends DefaultEntityProducerWithContext<User, User> implements IEntityProducer<User> {
+public class UserProducer extends EntityProducerWithNewEditActions<User, User> {
+    
     @Inject
     public UserProducer(final EntityFactory factory, final ICompanionObjectFinder companionFinder) {
         super(factory, User.class, companionFinder);
-    }
-
-    @Override
-    protected User provideDefaultValues(final User entity) {
-        return entity;
     }
 }
