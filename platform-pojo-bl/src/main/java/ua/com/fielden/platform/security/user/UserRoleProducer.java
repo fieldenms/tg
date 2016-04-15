2bd415fa4422829f8bef66a45d991d1a8263077b
01es <oles.hodych@gmail.com>
2016-04-15 15:14:23 +1000

    #499 Implemented creation of new and deletion of existing user roles.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/user/UserRoleProducer.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/user/UserRoleProducer.java
@@ -2,10 +2,11 @@ package ua.com.fielden.platform.security.user;
 
 import com.google.inject.Inject;
 
-import ua.com.fielden.platform.dao.DefaultEntityProducerWithContext;
-import ua.com.fielden.platform.dao.IEntityProducer;
+import ua.com.fielden.platform.dao.EntityProducerWithNewEditActions;
 import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
+import ua.com.fielden.platform.security.Authorise;
+import ua.com.fielden.platform.security.tokens.user.UserRoleSaveToken;
 
 /**
  * A producer for new instances of entity {@link UserRole}.
@@ -13,14 +14,23 @@ import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
  * @author TG Team
  *
  */
-public class UserRoleProducer extends DefaultEntityProducerWithContext<UserRole, UserRole> implements IEntityProducer<UserRole> {
+public class UserRoleProducer extends EntityProducerWithNewEditActions<UserRole, UserRole> {
+    
     @Inject
     public UserRoleProducer(final EntityFactory factory, final ICompanionObjectFinder companionFinder) {
         super(factory, UserRole.class, companionFinder);
     }
 
     @Override
-    protected UserRole provideDefaultValues(final UserRole entity) {
-        return entity;
+    @Authorise(UserRoleSaveToken.class)
+    protected UserRole provideDefaultValuesForNewEntity(final UserRole entity) {
+        return super.provideDefaultValuesForNewEntity(entity);
     }
+    
+    @Override
+    @Authorise(UserRoleSaveToken.class)
+    protected UserRole provideDefaultValues(final UserRole entity) {
+        return super.provideDefaultValues(entity);
+    }
+
 }
--- platform-web-resources/src/main/java/ua/com/fielden/platform/web/resources/webui/UserRoleWebUiConfig.java
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/resources/webui/UserRoleWebUiConfig.java
@@ -1,6 +1,7 @@
 package ua.com.fielden.platform.web.resources.webui;
 
 import static java.lang.String.format;
+import static ua.com.fielden.platform.web.centre.api.actions.ConfirmationPreAction.okCancel;
 import static ua.com.fielden.platform.web.centre.api.actions.impl.EntityActionBuilder.action;
 import static ua.com.fielden.platform.web.centre.api.context.impl.EntityCentreContextSelector.context;
 
@@ -8,6 +9,9 @@ import java.util.Optional;
 
 import com.google.inject.Injector;
 
+import ua.com.fielden.platform.entity.EntityDeleteAction;
+import ua.com.fielden.platform.entity.EntityEditAction;
+import ua.com.fielden.platform.entity.EntityNewAction;
 import ua.com.fielden.platform.sample.domain.MiUserRole;
 import ua.com.fielden.platform.security.user.UserRole;
 import ua.com.fielden.platform.security.user.UserRoleProducer;
@@ -51,28 +55,19 @@ public class UserRoleWebUiConfig {
         final EntityCentre<UserRole> userRoleCentre = new EntityCentre<>(MiUserRole.class, "User Roles",
                 EntityCentreBuilder.centreFor(UserRole.class)
                 .runAutomatically()
+                .addTopAction(UserRoleActions.NEW_ACTION.mkAction()).also()                
+                .addTopAction(UserRoleActions.DELETE_ACTION.mkAction())
                 .addCrit("this").asMulti().autocompleter(UserRole.class).also()
                 .addCrit("desc").asMulti().text()
                 .setLayoutFor(Device.DESKTOP, Optional.empty(), "[['center-justified', 'start', ['margin-right: 40px', 'flex'], ['flex']]]")
-
-                .addProp("this").also()
-                .addProp("desc")
-                .addPrimaryAction(EntityActionConfig.createMasterInDialogInvocationActionConfig())
+                .addProp("this")
+                    .order(1).asc()
+                    .width(200)
                 .also()
-                .addSecondaryAction(
-                    action(UserRoleTokensUpdater.class)
-                    .withContext(context().withCurrentEntity().build())
-                    .icon("add-circle")
-                    .shortDesc("Add / Remove tokens")
-                    .longDesc("Add / Remove tokens for this user role")
-                                        //.prefDimForView(mkDim(600, 750))
-                    .build())
-                .build(), injector, (centre) -> {
-                    // ... please implement some additional hooks if necessary -- for e.g. centre.getFirstTick().setWidth(...), add calculated properties through domain tree API, etc.
-                    centre.getSecondTick().setWidth(UserRole.class, "", 120);
-                    centre.getSecondTick().setWidth(UserRole.class, "desc", 360);
-                    return centre;
-                });
+                .addProp("desc").minWidth(200)
+                .addPrimaryAction(UserRoleActions.EDIT_ACTION.mkAction()).also()
+                .addSecondaryAction(UserRoleActions.MANAGE_SECURITY_TOKEN_ACTION.mkAction())
+                .build(), injector, null);
         return userRoleCentre;
     }
 
@@ -131,5 +126,64 @@ public class UserRoleWebUiConfig {
                 UserRoleTokensUpdaterProducer.class,
                 masterConfig,
                 injector);
+    }
+    
+    private static enum UserRoleActions {
+        
+        NEW_ACTION {
+            @Override
+            public EntityActionConfig mkAction() {
+                return action(EntityNewAction.class)
+                        .withContext(context().withSelectionCrit().build())
+                        .icon("add-circle-outline")
+                        .shortDesc("Add new User Role")
+                        .longDesc("Initiates creation of a new User Role.")
+                        .build();
+            }
+
+        },
+        
+        EDIT_ACTION {
+            @Override
+            public EntityActionConfig mkAction() {
+                return action(EntityEditAction.class)
+                        .withContext(context().withCurrentEntity().withSelectionCrit().build())
+                        .icon("editor:mode-edit")
+                        .shortDesc("Edit User Role")
+                        .longDesc("Opens master for User Role editing.")
+                        .build();
+            }
+
+        },
+        
+        DELETE_ACTION {
+            @Override
+            public EntityActionConfig mkAction() {
+                final String desc = "Delete selected user role(s).";
+                return action(EntityDeleteAction.class)
+                        .withContext(context().withSelectedEntities().build())
+                        .preAction(okCancel("Please confirm whether the selected user role(s) should be deleted?"))
+                        .icon("remove-circle-outline")
+                        .shortDesc(desc)
+                        .longDesc(desc)
+                        .build();
+            }
+
+        },
+        
+        MANAGE_SECURITY_TOKEN_ACTION {
+            @Override
+            public EntityActionConfig mkAction() {
+                return action(UserRoleTokensUpdater.class)
+                        .withContext(context().withCurrentEntity().build())
+                        .icon("av:playlist-add-check")
+                        .shortDesc("Add/Remove Security Tokens")
+                        .longDesc("Add/remove security token associations for the current user role.")
+                        .build();
+            }
+            
+        };
+
+        public abstract EntityActionConfig mkAction();
     }
 }
