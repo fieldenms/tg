c383b9a5051b8e5dd2df8764a5ab7203d328ed71
Nazar Chaykivskyy <nazar@fielden.com.au>
2016-04-18 22:08:27 +0300

    #507 Extended export API to support multiple sets of different entities

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/file_reports/WorkbookExporter.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/file_reports/WorkbookExporter.java
@@ -2,6 +2,7 @@ package ua.com.fielden.platform.file_reports;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.zip.Deflater;
@@ -17,66 +18,20 @@ import org.joda.time.DateTime;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.serialisation.xstream.GZipOutputStreamEx;
+import ua.com.fielden.platform.utils.Pair;
 
 public class WorkbookExporter {
 
     public static <M extends AbstractEntity<?>> HSSFWorkbook export(final List<M> entities, final String[] propertyNames, final String[] propertyTitles) {
-        final HSSFWorkbook wb = new HSSFWorkbook();
-        final HSSFSheet sheet = wb.createSheet("Exported Data");
-        // Create a header row.
-        final HSSFRow headerRow = sheet.createRow(0);
-        // Create a new font and alter it
-        final HSSFFont font = wb.createFont();
-        font.setFontHeightInPoints((short) 12);
-        font.setFontName("Courier New");
-        font.setBoldweight((short) 1000);
-        // Fonts are set into a style so create a new one to use
-        final HSSFCellStyle headerCellStyle = wb.createCellStyle();
-        headerCellStyle.setFont(font);
-        headerCellStyle.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-        final HSSFCellStyle headerInnerCellStyle = wb.createCellStyle();
-        headerInnerCellStyle.setFont(font);
-        headerInnerCellStyle.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-        headerInnerCellStyle.setBorderRight(HSSFCellStyle.BORDER_HAIR);
-        // Create cells and put column names there
-        for (int index = 0; index < propertyTitles.length; index++) {
-            final HSSFCell cell = headerRow.createCell(index);
-            cell.setCellValue(propertyTitles[index]);
-            cell.setCellStyle(index < propertyTitles.length - 1 ? headerInnerCellStyle : headerCellStyle);
+        List<Pair<String, String>> propNamesAndTitles = new ArrayList<>();
+        
+        for (int index = 0; index < propertyNames.length && index < propertyTitles.length; index++) {
+            propNamesAndTitles.add(new Pair<String, String>(propertyNames[index], propertyTitles[index]));
         }
-
-        // let's make cell style to handle borders
-        final HSSFCellStyle dataCellStyle = wb.createCellStyle();
-        dataCellStyle.setBorderRight(HSSFCellStyle.BORDER_HAIR);
-        for (int index = 0; index < entities.size(); index++) {
-            final HSSFRow row = sheet.createRow(index + 1); // new row starting with 1
-            // iterate through values in the current table row and populate the sheet row
-            for (int propIndex = 0; propIndex < propertyNames.length; propIndex++) {
-                final HSSFCell cell = row.createCell(propIndex); // create new cell
-                if (propIndex < propertyNames.length - 1) { // the last column should not have right border
-                    cell.setCellStyle(dataCellStyle);
-                }
-                final Object value = entities.get(index).get(propertyNames[propIndex]); // get the value
-                // need to try to do the best job with types
-                if (value instanceof Date) {
-                    cell.setCellValue(DateTimeDateFormat.getDateTimeInstance().format(value));
-                } else if (value instanceof DateTime) {
-                    cell.setCellValue(DateTimeDateFormat.getDateTimeInstance().format(value));
-                } else if (value instanceof Number) {
-                    cell.setCellType(HSSFCell.CELL_TYPE_NUMERIC);
-                    cell.setCellValue(((Number) value).doubleValue());
-                } else if (value instanceof Boolean) {
-                    cell.setCellType(HSSFCell.CELL_TYPE_BOOLEAN);
-                    cell.setCellValue((Boolean) value);
-                } else if (value == null) { // if null then leave call blank
-                    cell.setCellType(HSSFCell.CELL_TYPE_BLANK);
-                } else { // otherwise treat value as String
-                    cell.setCellType(HSSFCell.CELL_TYPE_STRING);
-                    cell.setCellValue(value.toString());
-                }
-            }
-        }
-        return wb;
+        DataForWorkbookSheet<M> dataForWorkbookSheet = new DataForWorkbookSheet<M>("Exported data", entities, propNamesAndTitles);
+        List<DataForWorkbookSheet<? extends AbstractEntity<?>>> sheetsData = new ArrayList<>();
+        sheetsData.add(dataForWorkbookSheet);
+        return export(sheetsData);
     }
 
     public static byte[] convertToByteArray(final HSSFWorkbook workbook) throws IOException {
@@ -93,4 +48,66 @@ public class WorkbookExporter {
         return oStream.toByteArray();
     }
 
+    public static HSSFWorkbook export(List<DataForWorkbookSheet<? extends AbstractEntity<?>>> sheetsData) {
+        final HSSFWorkbook wb = new HSSFWorkbook();
+        for (DataForWorkbookSheet<? extends AbstractEntity<?>> sheetData : sheetsData) {
+            addSheetWithData(wb, sheetData);
+        }
+        return wb;
+    }
+    
+    private static <M extends AbstractEntity<?>> void addSheetWithData(HSSFWorkbook wb, DataForWorkbookSheet<M> sheetData) {
+        final HSSFSheet sheet = wb.createSheet(sheetData.getSheetTitle());
+        // Create a header row.
+        final HSSFRow headerRow = sheet.createRow(0);
+        // Create a new font and alter it
+        final HSSFFont font = wb.createFont();
+        font.setFontHeightInPoints((short) 12);
+        font.setFontName("Courier New");
+        font.setBoldweight((short) 1000);
+        // Fonts are set into a style so create a new one to use
+        final HSSFCellStyle headerCellStyle = wb.createCellStyle();
+        headerCellStyle.setFont(font);
+        headerCellStyle.setBorderBottom(HSSFCellStyle.BORDER_THIN);
+        final HSSFCellStyle headerInnerCellStyle = wb.createCellStyle();
+        headerInnerCellStyle.setFont(font);
+        headerInnerCellStyle.setBorderBottom(HSSFCellStyle.BORDER_THIN);
+        headerInnerCellStyle.setBorderRight(HSSFCellStyle.BORDER_HAIR);
+        // Create cells and put column names there
+        for (int index = 0; index < sheetData.getPropTitles().size(); index++) {
+            final HSSFCell cell = headerRow.createCell(index);
+            cell.setCellValue(sheetData.getPropTitles().get(index));
+            cell.setCellStyle(index < sheetData.getPropTitles().size() - 1 ? headerInnerCellStyle : headerCellStyle);
+        }
+
+        // let's make cell style to handle borders
+        final HSSFCellStyle dataCellStyle = wb.createCellStyle();
+        dataCellStyle.setBorderRight(HSSFCellStyle.BORDER_HAIR);
+        for (int index = 0; index < sheetData.getEntities().size(); index++) {
+            final HSSFRow row = sheet.createRow(index + 1); // new row starting with 1
+            // iterate through values in the current table row and populate the sheet row
+            for (int propIndex = 0; propIndex < sheetData.getPropNames().size(); propIndex++) {
+                final HSSFCell cell = row.createCell(propIndex); // create new cell
+                if (propIndex < sheetData.getPropNames().size() - 1) { // the last column should not have right border
+                    cell.setCellStyle(dataCellStyle);
+                }
+                final Object value = sheetData.getEntities().get(index).get(sheetData.getPropNames().get(propIndex)); // get the value
+                // need to try to do the best job with types
+                if (value instanceof Date || value instanceof DateTime) {
+                    cell.setCellValue(DateTimeDateFormat.getDateTimeInstance().format(value));
+                } else if (value instanceof Number) {
+                    cell.setCellType(HSSFCell.CELL_TYPE_NUMERIC);
+                    cell.setCellValue(((Number) value).doubleValue());
+                } else if (value instanceof Boolean) {
+                    cell.setCellType(HSSFCell.CELL_TYPE_BOOLEAN);
+                    cell.setCellValue((Boolean) value);
+                } else if (value == null) { // if null then leave call blank
+                    cell.setCellType(HSSFCell.CELL_TYPE_BLANK);
+                } else { // otherwise treat value as String
+                    cell.setCellType(HSSFCell.CELL_TYPE_STRING);
+                    cell.setCellValue(value.toString());
+                }
+            }
+        }
+    }
 }
