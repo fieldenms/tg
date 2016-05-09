65c55bb14e42651b7ed8ce51d70c2bfe76718d46
Jhou <jhou.pro@gmail.com>
2016-05-09 23:16:59 +0300

    #206 Added utility methods to construct master with centres.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/api/compound/Compound.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/api/compound/Compound.java
@@ -6,14 +6,17 @@ import static ua.com.fielden.platform.web.centre.api.context.impl.EntityCentreCo
 import com.google.inject.Injector;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.entity.AbstractFunctionalEntityForCompoundMenuItem;
 import ua.com.fielden.platform.entity.AbstractFunctionalEntityWithCentreContext;
+import ua.com.fielden.platform.web.centre.EntityCentre;
 import ua.com.fielden.platform.web.centre.api.actions.EntityActionConfig;
 import ua.com.fielden.platform.web.view.master.EntityMaster;
+import ua.com.fielden.platform.web.view.master.api.with_centre.impl.MasterWithCentreBuilder;
 import ua.com.fielden.platform.web.view.master.api.with_master.impl.MasterWithMasterBuilder;
 
 public class Compound {
     
-    public static <K extends Comparable<?>, MENU_ITEM extends AbstractFunctionalEntityWithCentreContext<K>> EntityMaster<MENU_ITEM> miMaster(
+    public static <K extends Comparable<?>, MENU_ITEM extends AbstractFunctionalEntityForCompoundMenuItem<K>> EntityMaster<MENU_ITEM> miMaster(
             final Class<MENU_ITEM> menuItemType, 
             final EntityMaster<? extends AbstractEntity<?>> embeddedMaster,
             final Injector injector) {
@@ -26,7 +29,33 @@ public class Compound {
                 injector);
     }
     
-    public static <K extends Comparable<?>, MENU_ITEM extends AbstractFunctionalEntityWithCentreContext<K>> EntityActionConfig miAction(
+    public static <K extends Comparable<?>, MENU_ITEM extends AbstractFunctionalEntityForCompoundMenuItem<K>> EntityMaster<MENU_ITEM> miCentre(
+            final Class<MENU_ITEM> menuItemType,
+            final EntityCentre<? extends AbstractEntity<?>> embeddedCentre,
+            final Injector injector) {
+        return new EntityMaster<MENU_ITEM>(
+                menuItemType,
+                new MasterWithCentreBuilder<MENU_ITEM>()
+                    .forEntityWithSaveOnActivate(menuItemType)
+                    .withCentre(embeddedCentre)
+                    .done(),
+                injector);
+    }
+    
+    public static <DETAILS_ACTION extends AbstractFunctionalEntityWithCentreContext<?>> EntityMaster<DETAILS_ACTION> detailsCentre(
+            final Class<DETAILS_ACTION> menuItemType,
+            final EntityCentre<? extends AbstractEntity<?>> embeddedCentre,
+            final Injector injector) {
+        return new EntityMaster<DETAILS_ACTION>(
+                menuItemType,
+                new MasterWithCentreBuilder<DETAILS_ACTION>()
+                    .forEntityWithSaveOnActivate(menuItemType)
+                    .withCentre(embeddedCentre)
+                    .done(),
+                injector);
+    }
+    
+    public static <K extends Comparable<?>, MENU_ITEM extends AbstractFunctionalEntityForCompoundMenuItem<K>> EntityActionConfig miAction(
             final Class<MENU_ITEM> menuItemType, 
             final String icon, 
             final String shortDesc, 
