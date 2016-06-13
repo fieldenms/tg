5214774376f0a3ae3971ae45a4d154b2637a449e
01es <oles.hodych@gmail.com>
2016-06-13 16:53:20 +1000

    #571 Removed the old web-resources for handling entity life-cycle changes.

--- platform-web-resources/src/main/java/ua/com/fielden/platform/web/resources/RouterHelper.java
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/resources/RouterHelper.java
@@ -1,15 +1,11 @@
 package ua.com.fielden.platform.web.resources;
 
-import org.restlet.Restlet;
 import org.restlet.routing.Router;
 
 import com.google.inject.Injector;
 
-import ua.com.fielden.platform.dao.IEntityDao;
-import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.file_reports.IReportDaoFactory;
-import ua.com.fielden.platform.web.factories.EntityLifecycleResourceFactory;
 import ua.com.fielden.platform.web.factories.ReportResourceFactory;
 
 /**
@@ -25,16 +21,6 @@ public final class RouterHelper {
     public RouterHelper(final Injector injector, final EntityFactory factory) {
         this.injector = injector;
         this.factory = factory;
-    }
-
-    public <T extends AbstractEntity<?>, DAO extends IEntityDao<T>> void register(final Router router, final Class<DAO> daoType) {
-        registerLifecycleResource(router, daoType);
-    }
-
-    public <T extends AbstractEntity<?>, DAO extends IEntityDao<T>> void registerLifecycleResource(final Router router, final Class<DAO> daoType) {
-        final DAO dao = injector.getInstance(daoType); // needed just to get entity type... might need to optimize it
-        final Restlet lifecycleResource = new EntityLifecycleResourceFactory<T, DAO>(daoType, injector);
-        router.attach("/users/{username}/lifecycle/" + dao.getEntityType().getSimpleName(), lifecycleResource);
     }
 
     public void registerReportResource(final Router router, final RestServerUtil rsu) {
