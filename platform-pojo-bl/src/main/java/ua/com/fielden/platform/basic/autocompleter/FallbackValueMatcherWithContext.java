dfbbd8c43c06cebd49e0a381ec181dd0ec8b1210
01es <oles.hodych@gmail.com>
2016-06-29 14:17:52 +1000

    #589 Modified the default matching logic to include both 'key' and 'desc' for entities that have property 'desc'. Matching by 'desc' is made less strict than by 'key',  whereby the seach pattern includes search anywhere in the text.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/basic/autocompleter/FallbackValueMatcherWithContext.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/basic/autocompleter/FallbackValueMatcherWithContext.java
@@ -5,6 +5,7 @@ import ua.com.fielden.platform.dao.IEntityDao;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.ICompoundCondition0;
 import ua.com.fielden.platform.entity.query.model.EntityResultQueryModel;
+import ua.com.fielden.platform.utils.EntityUtils;
 
 /**
  * This is a fall back implementation for {@link IValueMatcherWithContext}, which does not support context. It simply performs the search by key operation.
@@ -16,8 +17,12 @@ import ua.com.fielden.platform.entity.query.model.EntityResultQueryModel;
  */
 public class FallbackValueMatcherWithContext<CONTEXT extends AbstractEntity<?>, T extends AbstractEntity<?>> extends AbstractSearchEntityByKeyWithContext<CONTEXT, T> {
 
-    public FallbackValueMatcherWithContext(final IEntityDao<T> dao) {
-        super(dao);
+    private final Class<T> entityType;
+    
+    public FallbackValueMatcherWithContext(final IEntityDao<T> co) {
+        super(co);
+        
+        entityType = co.getEntityType();
     }
 
     @Override
@@ -32,7 +37,7 @@ public class FallbackValueMatcherWithContext<CONTEXT extends AbstractEntity<?>, 
 
     @Override
     public Integer getPageSize() {
-        return 10;
+        return IEntityDao.DEFAULT_PAGE_CAPACITY;
     }
 
     @Override
@@ -40,7 +45,11 @@ public class FallbackValueMatcherWithContext<CONTEXT extends AbstractEntity<?>, 
             final CONTEXT context,
             final String searchString,
             final ICompoundCondition0<T> incompleteEql) {
-        return incompleteEql.model();
+        if (EntityUtils.hasDescProperty(entityType)) {
+            return incompleteEql.or().upperCase().prop(AbstractEntity.DESC).iLike().val("%" + searchString).model();
+        } else {
+            return incompleteEql.model();
+        }
     }
 
 }
