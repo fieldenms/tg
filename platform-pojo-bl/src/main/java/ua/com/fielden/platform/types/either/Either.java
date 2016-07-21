1d740d0a59f10069d446c5f1943e62d394e60a54
01es <oles.hodych@gmail.com>
2016-07-22 09:38:15 +1000

    #602 Provided initial implementation for type 'Either' and 'TryWarpper'. Refer to test case 'TryTestCase' to the use.

---
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/types/either/Either.java
@@ -0,0 +1,15 @@
+package ua.com.fielden.platform.types.either;
+
+/**
+ * A container type that represent one of two possible values of some computed.
+ * One of the values that is often referred to as the <code>Right</code> value has the semantics of being correct.
+ * Another value, which is often referred to as the <code>Left</code> value, has the semantics of being invalid or alternative to the correct value. 
+ * 
+ * @author TG Team
+ *
+ * @param <E>
+ * @param <V>
+ */
+public abstract class Either<L, R> {
+
+}
