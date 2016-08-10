4b47375637d21f0f8cb92af61b16dcb6b3d6606e
Oleh <oleh@fielden.com.au>
2016-08-10 11:29:22 +0300

    #598 Fixed layout for acknowladge master

--- platform-web-resources/src/main/java/ua/com/fielden/platform/web/resources/webui/AcknowledgeWarningsWebUiConfig.java
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/resources/webui/AcknowledgeWarningsWebUiConfig.java
@@ -2,8 +2,6 @@ package ua.com.fielden.platform.web.resources.webui;
 
 import java.util.Optional;
 
-import com.google.inject.Injector;
-
 import ua.com.fielden.platform.entity.functional.master.AcknowledgeWarnings;
 import ua.com.fielden.platform.entity.functional.master.AcknowledgeWarningsProducer;
 import ua.com.fielden.platform.web.app.config.IWebUiBuilder;
@@ -13,9 +11,11 @@ import ua.com.fielden.platform.web.view.master.EntityMaster;
 import ua.com.fielden.platform.web.view.master.api.IMaster;
 import ua.com.fielden.platform.web.view.master.api.actions.MasterActions;
 import ua.com.fielden.platform.web.view.master.api.impl.SimpleMasterBuilder;
-/** 
+
+import com.google.inject.Injector;
+/**
  * {@link AcknowledgeWarnings} Web UI configuration.
- * 
+ *
  * @author Developers
  *
  */
