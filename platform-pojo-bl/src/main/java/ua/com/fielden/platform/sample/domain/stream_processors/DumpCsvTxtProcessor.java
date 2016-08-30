7ca8f548ff7d3645e6b9cd507c2ef94bf7c03344
01es <oles.hodych@gmail.com>
2016-08-30 15:23:03 +1000

    #539 A spike on computing a file checksum as a decorator for the input stream reading.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/stream_processors/DumpCsvTxtProcessor.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/stream_processors/DumpCsvTxtProcessor.java
@@ -1,5 +1,15 @@
 package ua.com.fielden.platform.sample.domain.stream_processors;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+
+import ua.com.fielden.platform.cypher.HexString;
 import ua.com.fielden.platform.entity.AbstractEntityWithInputStream;
 import ua.com.fielden.platform.entity.annotation.CompanionObject;
 import ua.com.fielden.platform.entity.annotation.IsProperty;
@@ -8,6 +18,7 @@ import ua.com.fielden.platform.entity.annotation.KeyType;
 import ua.com.fielden.platform.entity.annotation.MapTo;
 import ua.com.fielden.platform.entity.annotation.Observable;
 import ua.com.fielden.platform.entity.annotation.Title;
+import ua.com.fielden.platform.error.Result;
 
 /** 
  * An demo entity that both provides an input stream for processing and represent the result of that processing as implemented in its companion's method <code>save</code>.
@@ -38,4 +49,27 @@ public class DumpCsvTxtProcessor extends AbstractEntityWithInputStream<String> {
         return noOfProcessedLines;
     }
     
+    public static void main(String[] args) throws Exception {
+        final MessageDigest md = MessageDigest.getInstance("SHA1");
+        
+        try (final InputStream is = Files.newInputStream(Paths.get("../pom.xml"));
+             final DigestInputStream dis = new DigestInputStream(is, md); // decorator
+             final BufferedReader br = new BufferedReader(new InputStreamReader(dis));) 
+        {
+          /* Read decorated stream (dis) to EOF as normal... */
+            String line = null;
+            try {
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                throw Result.failure(e);
+            }
+        }
+        byte[] digest = md.digest();
+        final String hash = HexString.bufferToHex(digest, 0, digest.length);
+        System.out.println(hash);
+        //AD35A51B8C8658E0ACB1DFCF5A11923BE8B05DD4
+        //95B3FF9137053279C8B1ECE96F817BA0129F614C
+    }
 }
