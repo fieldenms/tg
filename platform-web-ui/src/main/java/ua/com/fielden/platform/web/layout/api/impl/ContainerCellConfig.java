97af45a76d82c1ec378c76dfade4eb7ac46ecc96
Oleh <oleh@fielden.com.au>
2016-09-01 09:32:41 +0300

    #637 Removed unnecessary layout exception and check whther cells are empty before repeat.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/layout/api/impl/ContainerCellConfig.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/layout/api/impl/ContainerCellConfig.java
@@ -123,9 +123,6 @@ public class ContainerCellConfig extends ContainerConfig implements ILayoutCell,
 
     @Override
     public ContainerCellConfig repeat(final int times) {
-        if (cells.isEmpty()) {
-            throw new LayoutException("There are no cells to copy.");
-        }
         final CellConfig lastCell = cells.get(cells.size() - 1);
         for (int time = 0; time < times - 1; time++) {
             cells.add(lastCell);
