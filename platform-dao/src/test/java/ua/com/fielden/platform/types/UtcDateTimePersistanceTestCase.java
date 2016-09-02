62cef977d6647fff5be0a2bb1e99ec7ae857716d
01es <oles.hodych@gmail.com>
2016-09-02 17:27:47 +1000

    #642 Adjusted the unit test to set the apropriate time zone.

--- platform-dao/src/test/java/ua/com/fielden/platform/types/UtcDateTimePersistanceTestCase.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/types/UtcDateTimePersistanceTestCase.java
@@ -1,6 +1,5 @@
 package ua.com.fielden.platform.types;
 
-import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
@@ -9,7 +8,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
-import org.joda.time.DateTimeConstants;
 import org.junit.Test;
 
 import ua.com.fielden.platform.sample.domain.TgAuthor;
@@ -32,39 +30,25 @@ public class UtcDateTimePersistanceTestCase extends AbstractDaoTestCase {
     
     @Test
     public void utc_date_is_stored_and_retrieved_correctly_with_respect_to_daylightsaving() throws Exception {
+        TimeZone.setDefault(australia);
+        
         final TgPersonName chris = save(new_(TgPersonName.class, "Chris", "Chris"));
         
-        final String dateTime1 = "2013-04-07 2:30:00";
-        final Date dob = date(dateTime1);
-        final Date utcDob = new Date(dob.getTime() - DateTimeConstants.MILLIS_PER_HOUR);
-        
-        System.out.println("             Local DOB: " + dob);
-        System.out.println("formated     Local DOB: " + utcFormat.format(dob));
-        System.out.println("             UTC DOB: " + utcDob);
-        System.out.println("formated     UTC DOB: " + utcFormat.format(utcDob));
+        final Date localDate = localFormat.parse("2016-01-01 11:00:00");
+        final Date utcDate = utcFormat.parse("2016-01-01 00:00:00");
+
         
         final TgAuthor date = save(new_composite(TgAuthor.class, chris, "Date", null)
-                .setDob(dob).setUtcDob(utcDob));
+                .setDob(localDate).setUtcDob(utcDate));
         
-        assertEquals(dob, date.getDob());
-        assertEquals(utcDob, date.getUtcDob());
+        assertEquals(localDate, date.getDob());
+        assertEquals(utcDate, date.getUtcDob());
         
-        System.out.println("----------------- loaded from db --------------");
-        System.out.println("             Local DOB: " + date.getDob());
-        System.out.println("formated     Local DOB: " + utcFormat.format(date.getDob()));
-        System.out.println("             UTC DOB: " + date.getUtcDob());
-        System.out.println("formated     UTC DOB: " + utcFormat.format(date.getUtcDob()));
+        TimeZone.setDefault(null);
     }
     
     @Test
     public void date_conversions_between_local_and_UTC_timezone_using_string_representation_works_as_expected() throws Exception {
-        final DateFormat utcFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        utcFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-        
-        final DateFormat localFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        localFormat.setTimeZone(TimeZone.getTimeZone("Australia/Melbourne"));
-        
-        
         final Date localDate = localFormat.parse("2016-01-01 11:00:00");
         final Date utcDate = utcFormat.parse("2016-01-01 00:00:00");
         
