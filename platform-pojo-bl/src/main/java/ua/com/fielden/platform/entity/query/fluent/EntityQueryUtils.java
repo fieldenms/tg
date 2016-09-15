3a7b0708a318cb1f127ada6e15bc0ade213af6da
Jhou <jhou.pro@gmail.com>
2016-09-15 22:38:33 +0300

    #671 Externalised attaching of key member conditions from findByKeyAndFetch-like method implementation. Moved method for query by entitiesWithKeys to common companion object implementation (named createQueryByKeyFor). Reused externalised method for attaching key conditions inside createQueryByKeyFor method. Fixed building of key conditions for the properties of type Class -- for now query condition is based on the name of Class-typed key value.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/query/fluent/EntityQueryUtils.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/query/fluent/EntityQueryUtils.java
@@ -1,25 +1,16 @@
 package ua.com.fielden.platform.entity.query.fluent;
 
-import java.lang.reflect.Field;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
-
 import ua.com.fielden.platform.dao.QueryExecutionModel;
 import ua.com.fielden.platform.dao.QueryExecutionModel.Builder;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.query.EntityAggregates;
-import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.ICompoundCondition0;
 import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.IFromAlias;
 import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.IOrderingItem;
 import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.IStandAloneConditionOperand;
 import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.IStandAloneExprOperand;
-import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.IWhere0;
 import ua.com.fielden.platform.entity.query.fluent.fetch.FetchCategory;
 import ua.com.fielden.platform.entity.query.model.AggregatedResultQueryModel;
 import ua.com.fielden.platform.entity.query.model.EntityResultQueryModel;
-import ua.com.fielden.platform.reflection.Finder;
 
 public class EntityQueryUtils {
     public static <T extends AbstractEntity<?>> IFromAlias<T> select(final Class<T> entityType) {
@@ -105,41 +96,4 @@ public class EntityQueryUtils {
     public static fetch<EntityAggregates> fetchAggregates() {
         return new fetch<EntityAggregates>(EntityAggregates.class, FetchCategory.ID_AND_VERSTION);
     }
-    
-    /**
-     * Creates a query for entities by their keys (simple or composite). If <code>entitiesWithKeys</code> are empty -- returns empty optional. 
-     * 
-     * @param entityType -- the entity type
-     * @param entitiesWithKeys -- the entities with <b>all</b> key values correctly fetched / assigned
-     * @return
-     */
-    public static <T extends AbstractEntity<?>> Optional<EntityResultQueryModel<T>> queryByKeys(final Class<T> entityType, final Collection<T> entitiesWithKeys) {
-        IWhere0<T> partQ = select(entityType).where();
-        final List<Field> keyMembers = Finder.getKeyMembers(entityType);
-        for (final Iterator<T> iter = entitiesWithKeys.iterator(); iter.hasNext();) {
-            final T entityWithKey = iter.next();
-            
-            ICompoundCondition0<T> and = null;
-            for (final Iterator<Field> keyMemberIter = keyMembers.iterator(); keyMemberIter.hasNext();) {
-                final String keyName = keyMemberIter.next().getName();
-                
-                and = partQ.prop(keyName).eq().val(entityWithKey.get(keyName));
-                
-                if (keyMemberIter.hasNext()) {
-                    partQ = and.and();
-                }
-            }
-            
-            final ICompoundCondition0<T> or = and;
-
-            if (iter.hasNext()) {
-                partQ = or.or();
-            } else {
-                return Optional.of(or.model());
-            }
-        }
-
-        return Optional.empty();
-    }
-
 }
