daf1e2ab56690d71337cf641bf2fdfd07504caf6
Jhou <jhou.pro@gmail.com>
2016-10-27 13:08:23 +0300

    #690 First steps in implementing immediate changing of save button disablement on each user editing activity.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/api/widgets/impl/AbstractWidget.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/api/widgets/impl/AbstractWidget.java
@@ -85,7 +85,7 @@ public abstract class AbstractWidget implements IRenderable, IImportable {
             attrs.put("debug", "true");
         }
         attrs.put("id", "editor_4_"+ this.propertyName);
-        attrs.put("entity", "[[_currBindingEntity]]");
+        attrs.put("entity", "{{_currBindingEntity}}");
         attrs.put("property-name", this.propertyName);
         attrs.put("validation-callback", this.skipValidation ? "[[doNotValidate]]" : "[[validate]]");
         attrs.put("prop-title", this.title);
