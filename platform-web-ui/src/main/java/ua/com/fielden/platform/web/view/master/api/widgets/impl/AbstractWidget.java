bd4a4836789aa5fa8b98cc0b2c4f9f18b9cd0b48
Jhou <jhou.pro@gmail.com>
2016-10-28 20:45:01 +0300

    #690 Reimplemented edited properties comparison to be based on original value not previous.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/api/widgets/impl/AbstractWidget.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/api/widgets/impl/AbstractWidget.java
@@ -86,6 +86,7 @@ public abstract class AbstractWidget implements IRenderable, IImportable {
         }
         attrs.put("id", "editor_4_"+ this.propertyName);
         attrs.put("entity", "{{_currBindingEntity}}");
+        attrs.put("original-entity", "[[_originalBindingEntity]]");
         attrs.put("property-name", this.propertyName);
         attrs.put("validation-callback", this.skipValidation ? "[[doNotValidate]]" : "[[validate]]");
         attrs.put("prop-title", this.title);
