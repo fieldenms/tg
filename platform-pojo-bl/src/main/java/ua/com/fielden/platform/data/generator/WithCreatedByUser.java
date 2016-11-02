76d8cdabf15d3c133e0344eaa04874d13457d45b
Jhou <jhou.pro@gmail.com>
2016-11-02 16:52:44 +0200

    #703 Removed demo generator, another one remains (TgGeneratedEntityGenerator). Added explanation of how to implement WithCreatedByUser contract for entities that are generated (TgGeneratedEntity).

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/data/generator/WithCreatedByUser.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/data/generator/WithCreatedByUser.java
@@ -4,7 +4,7 @@ import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.security.user.User;
 
 /**
- * This is just a type-safety marker that highlights the need to have property <code>createBy:User</code> for entities that are to be used for data generation.
+ * This is just a type-safety marker that highlights the need to have property <code>createdBy:User</code> for entities that are to be used for data generation.
  * 
  * @author TG Team
  *
