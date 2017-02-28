b36b3152ca61749ff62b4dfb4ac9f3a35d5eeff2
01es <oles.hodych@gmail.com>
2017-02-28 18:39:58 +1100

    #721 Provided initial API and skeleton implementation for data streaming.

--- platform-dao/src/main/java/ua/com/fielden/platform/entity/query/EntityContainerFetcher.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/entity/query/EntityContainerFetcher.java
@@ -7,6 +7,7 @@ import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.selec
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.stream.Stream;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
@@ -44,6 +45,23 @@ public class EntityContainerFetcher {
         return new EntityContainerEnhancer<E>(this, domainMetadataAnalyser, executionContext.getIdOnlyProxiedEntityTypeCache()).enhance(result, modelResult.getFetchModel());
     }
     
+    public <E extends AbstractEntity<?>> Stream<EntityContainer<E>> streamAndEnhanceContainers(final QueryExecutionModel<E, ?> queryModel)
+            throws Exception {
+        final DomainMetadataAnalyser domainMetadataAnalyser = new DomainMetadataAnalyser(executionContext.getDomainMetadata());
+        final QueryModelResult<E> modelResult = getModelResult(queryModel, domainMetadataAnalyser, executionContext.getFilter(), executionContext.getUsername());
+
+        if (modelResult.idOnlyQuery()) {
+            return streamContainersForIdOnlyQuery(queryModel, modelResult.getResultType());
+        }
+        
+        final Stream<EntityContainer<E>> result = streamContainersAsIs(modelResult);
+        logger.debug("Fetch model:\n" + modelResult.getFetchModel());
+        
+        // TODO implement streaming
+        return result;//new EntityContainerEnhancer<E>(this, domainMetadataAnalyser, executionContext.getIdOnlyProxiedEntityTypeCache()).enhance(result, modelResult.getFetchModel());
+    }
+
+    
     private <E extends AbstractEntity<?>> List<EntityContainer<E>> listContainersForIdOnlyQuery(final QueryExecutionModel<E, ?> queryModel, final Class<E> resultType, final Integer pageNumber, final Integer pageCapacity) throws Exception {
         final EntityResultQueryModel<E> idOnlyModel = select(resultType).where().prop("id").in().model((SingleResultQueryModel) queryModel.getQueryModel()).model();
         
@@ -60,7 +78,7 @@ public class EntityContainerFetcher {
             throws Exception {
         final EntityTree<E> resultTree = new EntityResultTreeBuilder().buildEntityTree(modelResult.getResultType(), modelResult.getYieldedPropsInfo());
 
-        final EntityHibernateRetrievalQueryProducer queryProducer = new EntityHibernateRetrievalQueryProducer(modelResult.getSql(), resultTree.getScalarFromEntityTree(), modelResult.getParamValues(), pageNumber, pageCapacity);
+        final EntityHibernateRetrievalQueryProducer queryProducer = EntityHibernateRetrievalQueryProducer.mkQueryProducerWithPagination(modelResult.getSql(), resultTree.getScalarFromEntityTree(), modelResult.getParamValues(), pageNumber, pageCapacity);
         
         final Query query = queryProducer.produceHibernateQuery(executionContext.getSession());
 
@@ -68,7 +86,33 @@ public class EntityContainerFetcher {
 
         return entityRawResultConverter.transformFromNativeResult(resultTree, query.list());
     }
+
+    private <E extends AbstractEntity<?>> Stream<EntityContainer<E>> streamContainersForIdOnlyQuery(final QueryExecutionModel<E, ?> queryModel, final Class<E> resultType) throws Exception {
+        final EntityResultQueryModel<E> idOnlyModel = select(resultType).where().prop("id").in().model((SingleResultQueryModel) queryModel.getQueryModel()).model();
+        
+        final QueryExecutionModel<E,EntityResultQueryModel<E>> idOnlyQem = from(idOnlyModel)
+        .with(queryModel.getOrderModel())
+        .with(queryModel.getFetchModel())
+        .with(queryModel.getParamValues())
+        .lightweight().model();
+        
+        return streamAndEnhanceContainers(idOnlyQem);
+    }
+
     
+    private <E extends AbstractEntity<?>> Stream<EntityContainer<E>> streamContainersAsIs(final QueryModelResult<E> modelResult) throws Exception {
+        final EntityTree<E> resultTree = new EntityResultTreeBuilder().buildEntityTree(modelResult.getResultType(), modelResult.getYieldedPropsInfo());
+
+        final EntityHibernateRetrievalQueryProducer queryProducer = EntityHibernateRetrievalQueryProducer.mkQueryProducerWithoutPagination(modelResult.getSql(), resultTree.getScalarFromEntityTree(), modelResult.getParamValues());
+        
+        final Query query = queryProducer.produceHibernateQuery(executionContext.getSession());
+
+        final EntityRawResultConverter<E> entityRawResultConverter = new EntityRawResultConverter<>(executionContext.getEntityFactory());
+
+        // TODO implement streaming
+        return null;//entityRawResultConverter.transformFromNativeResult(resultTree, query.list());
+    }
+
     private <E extends AbstractEntity<?>> QueryModelResult<E> getModelResult(final QueryExecutionModel<E, ?> qem, final DomainMetadataAnalyser domainMetadataAnalyser, final IFilter filter, final String username) {
         final EntQueryGenerator gen = new EntQueryGenerator(domainMetadataAnalyser, filter, username, executionContext.getUniversalConstants());
         final IRetrievalModel<E> fm = qem.getFetchModel() == null ? //
