9750acdb5d2a33811852988170daf46b1a517fa2
01es <oles.hodych@gmail.com>
2017-03-28 12:50:30 +1100

    Optimised imports.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/reflection/Finder.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/reflection/Finder.java
@@ -1,22 +1,20 @@
 package ua.com.fielden.platform.reflection;
 
-import static ua.com.fielden.platform.types.try_wrapper.TryWrapper.Try;
 import static java.lang.String.format;
 import static ua.com.fielden.platform.entity.AbstractEntity.COMMON_PROPS;
 import static ua.com.fielden.platform.entity.AbstractEntity.DESC;
 import static ua.com.fielden.platform.entity.AbstractEntity.ID;
 import static ua.com.fielden.platform.entity.AbstractEntity.KEY;
+import static ua.com.fielden.platform.types.try_wrapper.TryWrapper.Try;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.text.Collator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
