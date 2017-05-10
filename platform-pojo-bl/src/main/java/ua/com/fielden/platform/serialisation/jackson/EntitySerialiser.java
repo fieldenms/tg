89ee61ec3c7d3ead28e44d0fb1d8e315b5b3d3b2
01es <oles.hodych@gmail.com>
2017-05-10 18:19:21 +1000

    #791 Improved serialisation related error repoting by using a specialied error type. Adjusted type parameterisation.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/serialisation/jackson/EntitySerialiser.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/serialisation/jackson/EntitySerialiser.java
@@ -91,8 +91,8 @@ public class EntitySerialiser<T extends AbstractEntity<?>> {
         properties = createCachedProperties(type);
         this.entityTypeInfo = createEntityTypeInfo(entityTypeInfoGetter, serialisationTypeEncoder);
 
-        final EntityJsonSerialiser<T> serialiser = new EntityJsonSerialiser<T>(type, properties, this.entityTypeInfo, excludeNulls, propertyDescriptorType);
-        final EntityJsonDeserialiser<T> deserialiser = new EntityJsonDeserialiser<T>(mapper, factory, type, properties, serialisationTypeEncoder, idOnlyProxiedEntityTypeCache, propertyDescriptorType);
+        final EntityJsonSerialiser<T> serialiser = new EntityJsonSerialiser<>(type, properties, this.entityTypeInfo, excludeNulls, propertyDescriptorType);
+        final EntityJsonDeserialiser<T> deserialiser = new EntityJsonDeserialiser<>(mapper, factory, type, properties, serialisationTypeEncoder, idOnlyProxiedEntityTypeCache, propertyDescriptorType);
 
         // register serialiser and deserialiser
         module.addSerializer(type, serialiser);
@@ -226,7 +226,7 @@ public class EntitySerialiser<T extends AbstractEntity<?>> {
         return newId.toString();
     }
 
-    static private ThreadLocal<JacksonContext> contextThreadLocal = new ThreadLocal<JacksonContext>() {
+    private static ThreadLocal<JacksonContext> contextThreadLocal = new ThreadLocal<JacksonContext>() {
         @Override
         protected JacksonContext initialValue() {
             return new JacksonContext();
@@ -271,7 +271,7 @@ public class EntitySerialiser<T extends AbstractEntity<?>> {
      * @author TG Team
      *
      */
-    public final static class CachedProperty {
+    public static final class CachedProperty {
         private final Field field;
         private Class<?> propertyType;
         private boolean entityTyped = false;
@@ -303,7 +303,7 @@ public class EntitySerialiser<T extends AbstractEntity<?>> {
      *
      * @see Context
      */
-    static public JacksonContext getContext() {
+    public static JacksonContext getContext() {
         return contextThreadLocal.get();
     }
 }
