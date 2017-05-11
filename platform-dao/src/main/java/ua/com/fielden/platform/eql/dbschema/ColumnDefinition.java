b4c1a2b8086ca0a8181cd1a4f0f072ffb7d0cd48
Nazar Chaykivskyy <nazar@fielden.com.au>
2017-05-11 03:48:42 +0300

    #313 Implemented a data structure for column DDL generation.

---
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/dbschema/ColumnDefinition.java
@@ -0,0 +1,80 @@
+package ua.com.fielden.platform.eql.dbschema;
+
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.dialect.Dialect;
+
+import ua.com.fielden.platform.eql.dbschema.exceptions.DbSchemaException;
+
+/**
+ * A data structure to capture the information required to generate column DDL statement. 
+ * 
+ * @author TG Team
+ *
+ */
+public class ColumnDefinition {
+    public final boolean nullable;
+    public final String name;
+    public final Class<?> javaType; // could be useful for determining if the FK constraint is applicable
+    public final int sqlType;
+    public final int length;
+    public final int scale;
+    public final int precision;
+    public final String defaultValue;
+
+    public ColumnDefinition(final boolean nullable, final String name, final Class<?> javaType, final int sqlType, final int length, final int scale, final int precision, final String defaultValue) {
+        if (StringUtils.isEmpty(name)) {
+            throw new DbSchemaException("Column name can not be empty!");
+        }
+        this.nullable = nullable;
+        this.name = name;
+        this.javaType = javaType;
+        this.sqlType = sqlType;
+        this.length = length;
+        this.scale = scale;
+        this.precision = precision;
+        this.defaultValue = defaultValue;
+    }
+
+    /**
+     * Generates DDL statement for a column based on provided RDBMS dialect.
+     * 
+     * @param dialect
+     * @return
+     */
+    public String schemaString(final Dialect dialect) {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(name);
+        sb.append(" ");
+        sb.append(dialect.getTypeName(sqlType, length, precision, scale));
+        
+        if (!nullable) {
+            sb.append(" NOT NULL");
+        }
+
+        if (!defaultValue.isEmpty()) {
+            sb.append(" DEFAULT ");
+            sb.append(defaultValue);
+        }
+        
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * name.hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof ColumnDefinition)) {
+            return false;
+        }
+        final ColumnDefinition other = (ColumnDefinition) obj;
+
+        return StringUtils.equals(name, other.name);
+    }
+}
