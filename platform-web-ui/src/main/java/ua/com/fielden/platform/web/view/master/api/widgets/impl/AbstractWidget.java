a46db245f6e414bddd5f0cdfd45ae2cc879f5ae2
Oleh <oleh@fielden.com.au>
2017-06-02 15:34:10 +0300

    #815 added exclusiveness visual cues for range editors.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/api/widgets/impl/AbstractWidget.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/api/widgets/impl/AbstractWidget.java
@@ -30,7 +30,6 @@ public abstract class AbstractWidget implements IRenderable, IImportable {
     private Optional<EntityActionConfig> action = Optional.empty();
     private boolean skipValidation = false;
     private boolean debug = false;
-    private boolean criterionEditor = false;
 
     /**
      * Creates {@link AbstractWidget} from <code>entityType</code> type and <code>propertyName</code> and the name&path of widget.
@@ -93,9 +92,6 @@ public abstract class AbstractWidget implements IRenderable, IImportable {
         attrs.put("prop-title", this.title);
         attrs.put("prop-desc", this.desc);
         attrs.put("current-state", "[[currentState]]");
-        if (criterionEditor) {
-            attrs.put("class", "criterion-editors");
-        }
         return attrs;
     }
 
@@ -154,10 +150,5 @@ public abstract class AbstractWidget implements IRenderable, IImportable {
 
     public void setDebug(final boolean debug) {
         this.debug = debug;
-    }
-
-    public AbstractWidget markAsCriterionEditor() {
-        this.criterionEditor = true;
-        return this;
     }
 }
