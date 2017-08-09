130c0a0ab3ebc2550621a15d18272151269f0fcf
Oleh <oleh@fielden.com.au>
2017-08-09 09:32:45 +0300

    Added entity title to centre seelction criteria entity to be displayed in error messages.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/criteria/enhanced/CentreEntityQueryCriteriaToEnhance.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/criteria/enhanced/CentreEntityQueryCriteriaToEnhance.java
@@ -1,14 +1,15 @@
 package ua.com.fielden.platform.criteria.enhanced;
 
+import com.google.inject.Inject;
+
 import ua.com.fielden.platform.dao.IEntityDao;
 import ua.com.fielden.platform.dao.IGeneratedEntityController;
 import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.entity.annotation.EntityTitle;
 import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
 import ua.com.fielden.platform.entity.matcher.IValueMatcherFactory;
 import ua.com.fielden.platform.entity_centre.review.criteria.EnhancedCentreEntityQueryCriteria;
 import ua.com.fielden.platform.serialisation.api.ISerialiser;
-
-import com.google.inject.Inject;
 
 //TODO must finish implementation in order to provide correct ordering, fetch model etc. Consider to provide reference on to the ICriteriaDomainTreeManager.
 /**
@@ -19,6 +20,7 @@ import com.google.inject.Inject;
  * @param <T>
  * @param <DAO>
  */
+@EntityTitle("Centre Selection Criteria")
 public class CentreEntityQueryCriteriaToEnhance<T extends AbstractEntity<?>, DAO extends IEntityDao<T>> extends EnhancedCentreEntityQueryCriteria<T, DAO> {
 
     private static final long serialVersionUID = -5189571197523084383L;
