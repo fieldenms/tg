72d5624facb844b1168be5be53c21566224947d9
Nazar Chaykivskyy <nazar@fielden.com.au>
2017-08-15 04:24:23 +0300

    #892 Removed temporalily added exceptions to determine places in code with incorrect implementation.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/query/fluent/AbstractQueryLink.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/query/fluent/AbstractQueryLink.java
@@ -2,34 +2,23 @@ package ua.com.fielden.platform.entity.query.fluent;
 
 abstract class AbstractQueryLink {
 
-    private Tokens tokens;
+	private Tokens tokens;
 
-    static protected <T> T copy(final T parent, final Tokens tokens) {
-        if (tokens == null) {
-        	throw new RuntimeException("NO TOKENS WHILE SET!");
-        }
-    	((AbstractQueryLink) parent).setTokens(tokens);
-    	return parent;        
-    }
+	static protected <T> T copy(final T parent, final Tokens tokens) {
+		((AbstractQueryLink) parent).setTokens(tokens);
+		return parent;
+	}
 
-    public Tokens getTokens() {
-        if (tokens == null) {
-        	throw new RuntimeException("NO TOKENS WHILE GET!");
-        }
-        return tokens;
-    }
+	public Tokens getTokens() {
+		return tokens;
+	}
 
-    @Override
-    public String toString() {
-        return tokens.toString();
-    }
+	@Override
+	public String toString() {
+		return tokens.toString();
+	}
 
-    void setTokens(final Tokens tokens) {
-        if (this.tokens == null) {
-        	this.tokens = tokens;
-        } else {
-        	throw new RuntimeException("TRYING TO REPLACE ALREADY ASSIGNED TOKENS!");
-        }
-    	
-    }
+	void setTokens(final Tokens tokens) {
+		this.tokens = tokens;
+	}
 }
