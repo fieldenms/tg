bf11a36f7f7cd5e26df1268e8e1dfd094f075994
01es <oles.hodych@gmail.com>
2017-08-15 11:34:01 +1000

    #891 Extended contract 'ICompanionObjectFinder' with method 'findAsReader', which provides a convenient way to obtain an instance of a companion object that is narrawed down to the reader contract.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/factory/ICompanionObjectFinder.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/factory/ICompanionObjectFinder.java
@@ -1,5 +1,6 @@
 package ua.com.fielden.platform.entity.factory;
 
+import ua.com.fielden.platform.companion.IEntityReader;
 import ua.com.fielden.platform.dao.IEntityDao;
 import ua.com.fielden.platform.entity.AbstractEntity;
 
@@ -10,5 +11,22 @@ import ua.com.fielden.platform.entity.AbstractEntity;
  * 
  */
 public interface ICompanionObjectFinder {
+    
+    /**
+     * Returns a new instance of a fully fledged companion object as defined for the provided entity type.
+     * <p>
+     * Should not, buy may return <code>null</code>.
+     * 
+     * @param type
+     * @return
+     */
     <T extends IEntityDao<E>, E extends AbstractEntity<?>> T find(final Class<E> type);
+    
+    /**
+     * Returns a new instance of a companion object as defined for the provided entity type, but narrowed down to a reader contract, which is defined by {@link IEntityReader}. 
+     * 
+     * @param type
+     * @return
+     */
+    <T extends IEntityReader<E>, E extends AbstractEntity<?>> T findAsReader(final Class<E> type);
 }
