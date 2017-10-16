dc60615e0ed19c0300ea41972c150dfc2646e387
01es <oles.hodych@gmail.com>
2017-10-16 14:30:12 +1100

    #786 JavaDoc improvements.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/AbstractFunctionalEntityForCompoundOpener.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/AbstractFunctionalEntityForCompoundOpener.java
@@ -8,11 +8,11 @@ import ua.com.fielden.platform.entity.annotation.mutator.AfterChange;
 import ua.com.fielden.platform.entity.meta.impl.DefaultOpenCompoundMasterActionKeyDefiner;
 
 /**
- * A base class for functional entities that are intended to be used as compound master opener.
+ * A base class for functional entities that are intended to be used as actions to open some compound master.
  * <p>
- * Please, note that this implementation contains handling of 'sectionTitle' updates through default implementation of key definer.
- * This logic can be overridden in concrete descendants using the same approach of overriding key property as it is done here.
- * However, in most cases, such override is not needed. Please refer {@link DefaultOpenCompoundMasterActionKeyDefiner} for more details.
+ * Please, note that this implementation contains handling of changes to <code>sectionTitle</code> through the default key definer.
+ * This logic can be overridden in concrete descendants by redefining property <code>key</code> to provide an alternative definer.
+ * In most cases the default behaviour should suffice. Please refer {@link DefaultOpenCompoundMasterActionKeyDefiner} for more details.
  * 
  * @author TG Team
  *
