b2ff2c5e1ceaf69f83b9e2141ef6272f3ea22b88
01es <oles.hodych@gmail.com>
2017-11-30 11:50:43 +1100

    Enriched CollectionUtil with a convenient function 'tail' for generic arrays (i.e. no support for primitives).

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/utils/CollectionUtil.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/utils/CollectionUtil.java
@@ -9,6 +9,7 @@ import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.types.tuples.T2;
@@ -48,12 +49,25 @@ public final class CollectionUtil {
     }
     
     /**
+     * A convenient method to obtain a tail of an array. Returns an empty optional if the length of arrays is 0.
+     * 
+     * @param array
+     * @return
+     */
+    public static <T> Optional<T[]> tail(final T[] array) {
+        if (array.length == 0) {
+            return Optional.empty();
+        }
+
+        return Optional.of(Arrays.copyOfRange(array, 1, array.length));
+    }
+    /**
      * Converts collection to a string separating the elements with a provided separator.
      * <p>
      * No precaution is taken if toString representation of an element already contains a symbol equal to a separator.
      */
     public static <T> String toString(final Collection<T> collection, final String separator) {
-        final StringBuffer buffer = new StringBuffer();
+        final StringBuilder buffer = new StringBuilder();
         for (final Iterator<T> iter = collection.iterator(); iter.hasNext();) {
             buffer.append(iter.next() + (iter.hasNext() ? separator : ""));
         }
@@ -65,10 +79,10 @@ public final class CollectionUtil {
      * <p>
      * No precaution is taken if toString representation of property's value already contains a symbol equal to a separator.
      */
-    public static String toString(final Collection<? extends AbstractEntity> collection, final String propertyName, final String separator) {
-        final StringBuffer buffer = new StringBuffer();
-        for (final Iterator<? extends AbstractEntity> iter = collection.iterator(); iter.hasNext();) {
-            final AbstractEntity entity = iter.next();
+    public static String toString(final Collection<? extends AbstractEntity<?>> collection, final String propertyName, final String separator) {
+        final StringBuilder buffer = new StringBuilder();
+        for (final Iterator<? extends AbstractEntity<?>> iter = collection.iterator(); iter.hasNext();) {
+            final AbstractEntity<?> entity = iter.next();
             final Object value = entity != null ? entity.get(propertyName) : null;
             buffer.append(value + (iter.hasNext() ? separator : ""));
         }
