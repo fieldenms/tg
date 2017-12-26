7efb27bd8f608e03284a7a75641b4383579b6a46
jhou-pro <jhou.pro@gmail.com>
2017-12-26 18:13:31 +0200

    #967 Added simplistic API to support custom JavaScript-based cases for opening different master incarnations.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/action/pre/PolymorphicEditPreAction.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/action/pre/PolymorphicEditPreAction.java
@@ -1,5 +1,10 @@
 package ua.com.fielden.platform.web.action.pre;
 
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import ua.com.fielden.platform.web.minijs.JsCode;
 import ua.com.fielden.platform.web.view.master.api.actions.pre.IPreAction;
 
@@ -10,32 +15,51 @@ import ua.com.fielden.platform.web.view.master.api.actions.pre.IPreAction;
  *
  */
 public class PolymorphicEditPreAction implements IPreAction {
-    
-    
+    private final Map<String, String> predicateAndActions = new LinkedHashMap<>();
     
     @Override
     public JsCode build() {
-        return new JsCode(""
-            + "// console.error('XXX preAction: action and self', action, self);\n"
-            + "\n"
-            + "const parentSelector = '[parent-element-alias=\"' + action.elementAlias + '\"]';\n"
-            + "if (action.currentEntity.key === 'ST000001' || action.currentEntity.key === 'DC000001') {\n"
-            + "    const concreteAction = self.querySelector('tg-ui-action' + parentSelector + '[child-name=\"" + "editStAndDcPOInSimpleMaster" + "\"]');\n"
-            + "    concreteAction.currentEntity = action.currentEntity;\n"
-            + "    concreteAction.chosenProperty = action.chosenProperty;\n"
-            + "    concreteAction._run();\n"
-            + "    return false;\n"
-            + "} else if (action.currentEntity.dc === true) {\n"
-            + "    return true;\n"
-            + "} else if (action.currentEntity.st === true) {\n"
-            + "    const concreteAction = self.querySelector('tg-ui-action' + parentSelector + '[child-name=\"" + "editStPO" + "\"]');\n"
-            + "    concreteAction.currentEntity = action.currentEntity;\n"
-            + "    concreteAction.chosenProperty = action.chosenProperty;\n"
-            + "    concreteAction._run();\n"
-            + "    return false;\n"
-            + "}\n"
-            + "\n"
-            + "\n");
+        final StringBuilder codeBuilder = new StringBuilder();
+        codeBuilder.append("const parentSelector = '[parent-element-alias=\"' + action.elementAlias + '\"]';\n");
+        codeBuilder.append("const entity = action.currentEntity;\n");
+        final Iterator<Entry<String, String>> iterator = predicateAndActions.entrySet().iterator();
+        if (iterator.hasNext()) {
+            final Entry<String, String> firstPredicateAndAction = iterator.next();
+            codeBuilder.append(predicateAndActionCode(firstPredicateAndAction, true));
+            while (iterator.hasNext()) {
+                codeBuilder.append(predicateAndActionCode(iterator.next(), false));
+            }
+            codeBuilder.append("}\n");
+        }
+        codeBuilder.append("throw 'There is no suitable case for entity [' + entity + '] (polymorphic editing).';\n");
+        return new JsCode(codeBuilder.toString());
     }
     
+    private String predicateAndActionCode(final Entry<String, String> predicateAndAction, final boolean first) {
+        if ("this".equalsIgnoreCase(predicateAndAction.getValue())) {
+            return ""
+                + (first ? "" : "} else ") + "if (" + predicateAndAction.getKey() + ") {\n"
+                + "    return true;\n";
+        } else {
+            return ""
+                + (first ? "" : "} else ") + "if (" + predicateAndAction.getKey() + ") {\n"
+                + "    const concreteAction = self.querySelector('tg-ui-action' + parentSelector + '[child-name=\"" + predicateAndAction.getValue() + "\"]');\n"
+                + "    concreteAction.currentEntity = action.currentEntity;\n"
+                + "    concreteAction.chosenProperty = action.chosenProperty;\n"
+                + "    concreteAction._run();\n"
+                + "    return false;\n";
+        }
+    }
+
+    /**
+     * Adds sequential JS case for opening polymorphic master depending on currently clicked entity.
+     * 
+     * @param predicate -- JS predicate for JavaScript 'entity' constant
+     * @param actionNameOrThis -- action name (child key for parent action) or 'this' (for parent action itself)
+     * @return
+     */
+    public PolymorphicEditPreAction caseWhen(final String predicate, final String actionNameOrThis) {
+        predicateAndActions.put(predicate, actionNameOrThis);
+        return this;
+    }
 }
