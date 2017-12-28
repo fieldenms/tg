e8c364c4688bb72dac3ad307c0ea4f0214a487bd
Oleh <oleh@fielden.com.au>
2017-12-28 13:21:48 +0200

    #944 Fixed problem when entity centre doesn't get resizied when insertion point receives it's size.

--- platform-web-resources/src/main/java/ua/com/fielden/platform/web/test/server/WebUiConfig.java
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/test/server/WebUiConfig.java
@@ -1701,16 +1701,16 @@ public class WebUiConfig extends AbstractWebUiConfig {
 //                                    .withNoParentCentreRefresh()
 //                                    .build(),
 //                            InsertionPoints.RIGHT)
-                    .addInsertionPoint(
-                            action(TgCentreInvokerWithCentreContext.class)
-                            .withContext(context().withSelectionCrit().withSelectedEntities().build())
-                            .icon("assignment-ind")
-                            .shortDesc("Insertion Point")
-                            .longDesc("Functional context-dependent Insertion Point")
-                            .prefDimForView(mkDim("'350px'", "'500px'"))
-                            .withNoParentCentreRefresh()
-                            .build(),
-                    InsertionPoints.LEFT)
+//                    .addInsertionPoint(
+//                            action(TgCentreInvokerWithCentreContext.class)
+//                            .withContext(context().withSelectionCrit().withSelectedEntities().build())
+//                            .icon("assignment-ind")
+//                            .shortDesc("Insertion Point")
+//                            .longDesc("Functional context-dependent Insertion Point")
+//                            .prefDimForView(mkDim("'350px'", "'500px'"))
+//                            .withNoParentCentreRefresh()
+//                            .build(),
+//                    InsertionPoints.LEFT)
                     .build();
         }
         return scl.build();
