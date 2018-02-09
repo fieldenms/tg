5c7015cfee1ea6c84f98bae7da3f96b7f58a3b9f
jhou-pro <jhou.pro@gmail.com>
2018-02-09 16:34:54 +0200

    #979 Added support for crit-only single criteria validation in case where several properties were not added to the resultant centre configuration.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/criteria/generator/impl/SynchroniseCriteriaWithModelHandler.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/criteria/generator/impl/SynchroniseCriteriaWithModelHandler.java
@@ -108,33 +108,34 @@ public class SynchroniseCriteriaWithModelHandler<CDTME extends ICentreDomainTree
         criteriaEntity.beginInitialising();
         snapshot.forEach(metaProp -> {
             final String criteriaPropName = generateCriteriaPropertyName(metaProp.getEntity().getType(), metaProp.getName());
-            final MetaProperty<Object> criteriaMetaProp = criteriaEntity.getProperty(criteriaPropName);
-            
-            // the order of meta-info application is synced with EntityJsonDeserialiser; all properties are copied excluding prevValue, valueChangeCount and visible -- it is believed that these props are not relevant for critOnlySinglePrototype lifecycle 
-            final Result firstFailure = metaProp.getFirstFailure();
-            // TODO do we need to clear required validation result?
-            // TODO following logic is needed for 'clearing requiredness errors during validation' -- need to clarify whether such logic should be implemented
-            // TODO criteriaMetaProp.setRequiredValidationResult(successful(criteriaEntity));
-            criteriaMetaProp.setDomainValidationResult(firstFailure == null ? metaProp.getFirstWarning() : firstFailure);
-            criteriaMetaProp.setOriginalValue(metaProp.getOriginalValue());
-            criteriaMetaProp.setLastInvalidValue(metaProp.getLastInvalidValue());
-            criteriaMetaProp.setAssigned(metaProp.isAssigned());
-            criteriaMetaProp.setEditable(metaProp.isEditable());
-            criteriaMetaProp.setRequired(metaProp.isRequired());
-            if (firstFailure != null) {
-                // In case where property is invalid there is a pair of values: valid and attempted.
-                // Valid value usually represents the value, that was previously set.
-                // In some cases valid and attempted values could even be the same: this occurs in case of valid->invalid @dependent property revalidation.
-                // In both cases valid value will be different from 'null' value that appears in critOnlySinglePrototype entity during its creation (values in property definitions must be NOT set).
-                // This will confuse conflict resolution mechanism.
-                // We need to sync the value with initial values in critOnlySinglePrototype.
-                criteriaMetaProp.setValue(null);
-            } else {
-                criteriaMetaProp.setValue(metaProp.getValue());
-            }
-            // It is very important to copy 'lastAttemptedValue' into centre manager, not just 'metaProp.getValue()'.
-            // This is needed to correctly assign initial values (and perform their validation) in the next round of criteriaEntity creation (this includes, for example, next selection criteria validation round-trip).
-            updateTreeManagerProperty(criteriaEntity.getCentreDomainTreeMangerAndEnhancer().getFirstTick(), entityType, getCriteriaProperty(criteriaType, criteriaPropName), metaProp.getLastAttemptedValue(), criteriaType, criteriaPropName);
+            criteriaEntity.getPropertyOptionally(criteriaPropName).ifPresent(mp -> { // several crit-only single properties of original entity could be NOT added to selection criteria: need to ignore them
+                final MetaProperty<Object> criteriaMetaProp = (MetaProperty<Object>) mp;
+                // the order of meta-info application is synced with EntityJsonDeserialiser; all properties are copied excluding prevValue, valueChangeCount and visible -- it is believed that these props are not relevant for critOnlySinglePrototype lifecycle 
+                final Result firstFailure = metaProp.getFirstFailure();
+                // TODO do we need to clear required validation result?
+                // TODO following logic is needed for 'clearing requiredness errors during validation' -- need to clarify whether such logic should be implemented
+                // TODO criteriaMetaProp.setRequiredValidationResult(successful(criteriaEntity));
+                criteriaMetaProp.setDomainValidationResult(firstFailure == null ? metaProp.getFirstWarning() : firstFailure);
+                criteriaMetaProp.setOriginalValue(metaProp.getOriginalValue());
+                criteriaMetaProp.setLastInvalidValue(metaProp.getLastInvalidValue());
+                criteriaMetaProp.setAssigned(metaProp.isAssigned());
+                criteriaMetaProp.setEditable(metaProp.isEditable());
+                criteriaMetaProp.setRequired(metaProp.isRequired());
+                if (firstFailure != null) {
+                    // In case where property is invalid there is a pair of values: valid and attempted.
+                    // Valid value usually represents the value, that was previously set.
+                    // In some cases valid and attempted values could even be the same: this occurs in case of valid->invalid @dependent property revalidation.
+                    // In both cases valid value will be different from 'null' value that appears in critOnlySinglePrototype entity during its creation (values in property definitions must be NOT set).
+                    // This will confuse conflict resolution mechanism.
+                    // We need to sync the value with initial values in critOnlySinglePrototype.
+                    criteriaMetaProp.setValue(null);
+                } else {
+                    criteriaMetaProp.setValue(metaProp.getValue());
+                }
+                // It is very important to copy 'lastAttemptedValue' into centre manager, not just 'metaProp.getValue()'.
+                // This is needed to correctly assign initial values (and perform their validation) in the next round of criteriaEntity creation (this includes, for example, next selection criteria validation round-trip).
+                updateTreeManagerProperty(criteriaEntity.getCentreDomainTreeMangerAndEnhancer().getFirstTick(), entityType, getCriteriaProperty(criteriaType, criteriaPropName), metaProp.getLastAttemptedValue(), criteriaType, criteriaPropName);
+            });
         });
         // turn on all validations / definers again
         criteriaEntity.endInitialising();
