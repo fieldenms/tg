3f0ad5ef6ec86dfd8299e686034821cb20217fec
01es <oles.hodych@gmail.com>
2018-02-13 18:39:48 +1100

    #539 Adjusted domain metadata generation for db-mapping purposes to skip PURE entity types.

--- platform-dao/src/main/java/ua/com/fielden/platform/dao/PureEntityMetadata.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/dao/PureEntityMetadata.java
@@ -1,14 +1,12 @@
 package ua.com.fielden.platform.dao;
 
-import java.util.SortedMap;
-
-import org.apache.commons.lang.StringUtils;
+import java.util.TreeMap;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
 
 public class PureEntityMetadata<ET extends AbstractEntity<?>> extends AbstractEntityMetadata<ET>{
 
-        public PureEntityMetadata(final String table, final Class<ET> type, final SortedMap<String, PropertyMetadata> props) {
-        super(type, props);
+    public PureEntityMetadata(final String table, final Class<ET> type) {
+        super(type, new TreeMap<>());
     }
 }
