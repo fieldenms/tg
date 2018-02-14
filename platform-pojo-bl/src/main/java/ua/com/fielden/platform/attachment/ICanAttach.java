608641f5f218a7074e6b403f194a830cd98e8def
01es <oles.hodych@gmail.com>
2018-02-14 15:23:03 +1100

    #539 Minor modification to type parameterisation of ICanAttach.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/attachment/ICanAttach.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/attachment/ICanAttach.java
@@ -4,10 +4,12 @@ import ua.com.fielden.platform.entity.AbstractPersistentEntity;
 
 /**
  * A contract for associating entities of type <code>T</code> by means of creating association instances of type <code>A</code>.
+ *
+ * @param <T> -- entity to be associated with attachments
  * 
  * @author TG Team
  */
-public interface ICanAttach {
+public interface ICanAttach<T extends AbstractPersistentEntity<?>> {
     
     /**
      * Associates <code>attachment</code> with <code>entity</code> and returns the resultant association already persisted.
@@ -15,11 +17,10 @@ public interface ICanAttach {
      * This method should handle the situation where a corresponding association may already exists.
      * There is no need to throw any exceptions in such cases -- simply return existing association. 
      *
-     * @param <T> -- entity to be associated with attachments
      * @param <A> -- entity type representing attachment association
      * @param attachment
      * @param entity
      * @return persisted association instance
      */
-    <T extends AbstractPersistentEntity<?>, A extends AbstractPersistentEntity<?>> A attach(final Attachment attachment, final T entity);
+    <A extends AbstractPersistentEntity<?>> A attach(final Attachment attachment, final T entity);
 }
