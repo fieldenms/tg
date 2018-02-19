010a26a4f19a8eeb8af0ddcd16ff13919d4d6a44
01es <oles.hodych@gmail.com>
2018-02-20 09:14:32 +1100

    #539 Introduced 'error-page-template.html', and used it for reporting errors that may occur when attempting to download attachments.

--- platform-web-resources/src/main/java/ua/com/fielden/platform/web/resources/webui/AttachmentDownloadResource.java
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/resources/webui/AttachmentDownloadResource.java
@@ -1,19 +1,28 @@
 package ua.com.fielden.platform.web.resources.webui;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.util.Optional;
 
+import org.apache.log4j.Logger;
 import org.restlet.Context;
 import org.restlet.Request;
 import org.restlet.Response;
 import org.restlet.data.Disposition;
+import org.restlet.data.Encoding;
 import org.restlet.data.MediaType;
+import org.restlet.data.Status;
+import org.restlet.engine.application.EncodeRepresentation;
 import org.restlet.representation.FileRepresentation;
+import org.restlet.representation.InputRepresentation;
 import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
 import ua.com.fielden.platform.attachment.Attachment;
 import ua.com.fielden.platform.attachment.IAttachment;
+import ua.com.fielden.platform.utils.ResourceLoader;
 
 /**
  * This resource should be used for uploading files to be processed with the specified functional entity.
@@ -24,6 +33,7 @@ import ua.com.fielden.platform.attachment.IAttachment;
  * 
  */
 public class AttachmentDownloadResource extends ServerResource {
+    private static final Logger LOGGER = Logger.getLogger(AttachmentDownloadResource.class);
 
     private final IAttachment coAttachment;
     private final Long attachmentId;
@@ -40,19 +50,42 @@ public class AttachmentDownloadResource extends ServerResource {
 
     @Get
     public Representation download() {
-        final Attachment attachment = coAttachment.findById(attachmentId, coAttachment.getFetchProvider().fetchModel());
-        
-        final File file = coAttachment.asFile(attachment);
-        final MediaType fileType = MediaType.valueOf(attachment.getMime());
-        
-        final Disposition disposition = new Disposition();
-        disposition.setType(Disposition.TYPE_ATTACHMENT);
-        disposition.setFilename(attachment.getOrigFileName());
-        disposition.setModificationDate(attachment.getLastModified());
-        
-        final FileRepresentation repFile = new FileRepresentation(file, fileType);
-        repFile.setDisposition(disposition);
-        
-        return repFile;
+        final Optional<Attachment> opAttachment = coAttachment.findByIdOptional(attachmentId, coAttachment.getFetchProvider().fetchModel());
+        return opAttachment
+               .map(attachment -> {
+                    try {
+                        final File file = coAttachment.asFile(attachment);
+                        final MediaType fileType = MediaType.valueOf(attachment.getMime());
+
+                        final Disposition disposition = new Disposition();
+                        disposition.setType(Disposition.TYPE_ATTACHMENT);
+                        disposition.setFilename(attachment.getOrigFileName());
+                        disposition.setModificationDate(attachment.getLastModified());
+
+                        final Representation repFile = new FileRepresentation(file, fileType);
+                        repFile.setDisposition(disposition);
+
+                        return repFile;
+                    } catch (final Exception ex) {
+                        LOGGER.fatal(ex);
+                        return this.errorPage();
+                    }
+                })
+               .orElseGet(this::errorPage);
+    }
+    
+    private Representation errorPage() {
+        getResponse().setStatus(Status.CLIENT_ERROR_GONE);
+        try {
+            final String errorPageTemplate = ResourceLoader.getText("ua/com/fielden/platform/web/error-page-template.html")
+                    .replaceAll("@title", "Error")
+                    .replaceAll("@error-heading", "Error")
+                    .replaceAll("@error-subheading", "Attachment could not be found.");
+            
+            return new EncodeRepresentation(Encoding.GZIP, new StringRepresentation(errorPageTemplate, MediaType.TEXT_HTML));
+        } catch (final Exception ex) {
+            LOGGER.fatal(ex);
+            throw new IllegalStateException(ex);
+        }
     }
 }
