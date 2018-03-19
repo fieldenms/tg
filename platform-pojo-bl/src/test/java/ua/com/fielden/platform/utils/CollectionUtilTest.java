5c6f4c06efa24047c861e31afdac839e9ca37f22
01es <oles.hodych@gmail.com>
2018-03-19 11:23:55 +1100

    Added a test for CollectionUtil.listOf with no arguments.

--- platform-pojo-bl/src/test/java/ua/com/fielden/platform/utils/CollectionUtilTest.java
+++ platform-pojo-bl/src/test/java/ua/com/fielden/platform/utils/CollectionUtilTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static ua.com.fielden.platform.types.tuples.T2.t2;
+import static ua.com.fielden.platform.utils.CollectionUtil.listOf;
 import static ua.com.fielden.platform.utils.CollectionUtil.mapOf;
 import static ua.com.fielden.platform.utils.CollectionUtil.tail;
 
@@ -14,6 +15,11 @@ import java.util.Map;
 import org.junit.Test;
 
 public class CollectionUtilTest {
+
+    @Test
+    public void listOf_with_no_arguments_returns_empty_list() {
+        assertEquals(0, listOf().size());
+    }
 
     @Test
     public void mapOf_with_no_arguments_produces_empty_map() {
