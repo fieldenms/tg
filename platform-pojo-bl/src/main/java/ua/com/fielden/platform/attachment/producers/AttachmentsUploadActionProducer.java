02db79366bb3c7383fccc156f0497d824ba97146
Myroslav Loboda <elvin@fielden.com.au>
2018-04-12 16:16:53 +1000

    Close #1020 Implemented support for attachments as properties of functional entities

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/attachment/producers/AttachmentsUploadActionProducer.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/attachment/producers/AttachmentsUploadActionProducer.java
@@ -7,7 +7,7 @@ import com.google.inject.Inject;
 
 import ua.com.fielden.platform.attachment.Attachment;
 import ua.com.fielden.platform.attachment.AttachmentsUploadAction;
-import ua.com.fielden.platform.entity.AbstractPersistentEntity;
+import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.DefaultEntityProducerWithContext;
 import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
@@ -15,7 +15,7 @@ import ua.com.fielden.platform.entity.meta.MetaProperty;
 
 /**
  * Producer of {@link AttachmentsUploadAction} that populates its master entity if that entity is present in the context.
- * 
+ *
  * @author TG Team
  *
  */
@@ -28,11 +28,11 @@ public class AttachmentsUploadActionProducer extends DefaultEntityProducerWithCo
 
     @Override
     protected AttachmentsUploadAction provideDefaultValues(final AttachmentsUploadAction entity) {
-        if (masterEntityNotEmpty() && masterEntityInstanceOf(AbstractPersistentEntity.class)) {
+        if (masterEntityNotEmpty() && masterEntityInstanceOf(AbstractEntity.class)) {
             // first assign the master entity
-            final AbstractPersistentEntity<?> masterEntity = (AbstractPersistentEntity<?>) masterEntity();
+            final AbstractEntity<?> masterEntity = masterEntity();
             entity.setMasterEntity(masterEntity);
-            
+
             // now let's analyse the chosen property
             // if chosen property is specified and it is of type Attachment then it is assumed that this property is to be modified as the result of the attachment uploading
             if (chosenPropertyNotEmpty()) {
@@ -45,11 +45,11 @@ public class AttachmentsUploadActionProducer extends DefaultEntityProducerWithCo
                     }
                 }
             }
-            
-        } else if (keyOfMasterEntityInstanceOf(AbstractPersistentEntity.class)) {
-            entity.setMasterEntity(keyOfMasterEntity(AbstractPersistentEntity.class));
+
+        } else if (keyOfMasterEntityInstanceOf(AbstractEntity.class)) {
+            entity.setMasterEntity(keyOfMasterEntity(AbstractEntity.class));
         } else if (selectedEntitiesOnlyOne()) {
-            final AbstractPersistentEntity<?> selected = (AbstractPersistentEntity<?>) selectedEntities().get(0);
+            final AbstractEntity<?> selected = selectedEntities().get(0);
             entity.setMasterEntity(selected);
         }
         return entity;
