cfa05d0ad1dab841df7da157833ed1de0c5e97b3
Oleh <oleh@fielden.com.au>
2018-05-03 16:15:11 +0300

    #1016 Implemented client side save action and enhnced security matrix save action entity with properties those hold the information about new and remvoved associations.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/SecurityMatrixSaveAction.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/SecurityMatrixSaveAction.java
@@ -1,6 +1,16 @@
 package ua.com.fielden.platform.entity;
+import static ua.com.fielden.platform.entity.NoKey.NO_KEY;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import ua.com.fielden.platform.entity.annotation.CompanionObject;
+import ua.com.fielden.platform.entity.annotation.IsProperty;
 import ua.com.fielden.platform.entity.annotation.KeyType;
+import ua.com.fielden.platform.entity.annotation.Observable;
+import ua.com.fielden.platform.entity.annotation.Title;
 
 /**
  * Represents the security matrix save action.
@@ -12,4 +22,37 @@ import ua.com.fielden.platform.entity.annotation.KeyType;
 @CompanionObject(ISecurityMatrixSaveAction.class)
 public class SecurityMatrixSaveAction  extends AbstractFunctionalEntityWithCentreContext<NoKey> {
 
+    @IsProperty(Object.class)
+    @Title("Token Role Associations To Save")
+    private Map<String, List<Long>> associationsToSave = new HashMap<>();
+
+    @IsProperty(Object.class)
+    @Title("Token Role Associations To Remove")
+    private Map<String, List<Long>> associationsToRemove = new HashMap<>();
+
+    protected SecurityMatrixSaveAction() {
+        setKey(NO_KEY);
+    }
+
+    @Observable
+    protected SecurityMatrixSaveAction setAssociationsToRemove(final Map<String, List<Long>> associationsToRemove) {
+        this.associationsToRemove.clear();
+        this.associationsToRemove.putAll(associationsToRemove);
+        return this;
+    }
+
+    public Map<String, List<Long>> getAssociationsToRemove() {
+        return Collections.unmodifiableMap(associationsToRemove);
+    }
+
+    @Observable
+    protected SecurityMatrixSaveAction setAssociationsToSave(final Map<String, List<Long>> aasociationsToSave) {
+        this.associationsToSave.clear();
+        this.associationsToSave.putAll(aasociationsToSave);
+        return this;
+    }
+
+    public Map<String, List<Long>> getAssociationsToSave() {
+        return Collections.unmodifiableMap(associationsToSave);
+    }
 }
