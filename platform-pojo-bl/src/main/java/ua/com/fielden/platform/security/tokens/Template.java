a38d6c3c4546c1fb6018c6ba780df2f6266d78b7
01es <oles.hodych@gmail.com>
2018-05-08 10:30:44 +1000

    #1016 Refactored 'Template' for security tokens in order to make it easier for code generation based on JCodeModel.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/tokens/Template.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/tokens/Template.java
@@ -8,17 +8,31 @@ package ua.com.fielden.platform.security.tokens;
  */
 public enum Template {
     
-    SAVE("%s Can Save", "Authorises saving of new or modified data."),
-    DELETE("%s Can Delete", "Authorises deletion of data."),
-    MODIFY("%s Can Modify property [%s]", "Authorises modification of the specified property."),
-    EXECUTE("%s Can Execute", "Authorises action execution."),
-    MASTER_OPEN("%s Can Open", "Authorises opening of a compound master."),
-    MASTER_MENU_ITEM_ACCESS("%s Can Access", "Authorises access to a menu item of a compound master.");
+    SAVE("%s_CanSave", "%s Can Save", "Authorises saving of new or modified data."),
+    DELETE("%s_CanDelete", "%s Can Delete", "Authorises deletion of data."),
+    MODIFY("%s_CanModify_%s", "%s Can Modify property [%s]", "Authorises modification of the specified property."),
+    EXECUTE("%s_CanExecute", "%s Can Execute", "Authorises action execution."),
+    MASTER_OPEN("%s_CanOpen", "%s Can Open", "Authorises opening of a compound master."),
+    MASTER_MENU_ITEM_ACCESS("%s_CanAccess", "%s Can Access", "Authorises access to a menu item of a compound master.");
     
-    public final String forTitle;
-    public final String forDesc;
-    
-    private Template(final String forTitle, final String forDesc) {
+    private final String forClassName;
+    private final String forTitle;
+    private final String forDesc;
+
+    public final String forClassName() {
+        return forClassName;
+    }
+
+    public final String forTitle() {
+        return forTitle;
+    }
+
+    public final String forDesc() {
+        return forDesc;
+    }
+
+    private Template(final String forClassName, final String forTitle, final String forDesc) {
+        this.forClassName = forClassName;
         this.forTitle = forTitle;
         this.forDesc = forDesc;
     }
