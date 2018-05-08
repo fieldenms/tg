314f073d063236a029566d40f56feb4c923b3c76
Oleh <oleh@fielden.com.au>
2018-05-08 08:00:40 +0300

    #1016 Added master view for sub menu items.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/menu/item/IModuleMenuConfig4.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/menu/item/IModuleMenuConfig4.java
@@ -2,10 +2,13 @@ package ua.com.fielden.platform.web.menu.item;
 
 import ua.com.fielden.platform.web.centre.EntityCentre;
 import ua.com.fielden.platform.web.custom_view.AbstractCustomView;
+import ua.com.fielden.platform.web.view.master.EntityMaster;
 
 public interface IModuleMenuConfig4 {
 
     IModuleMenuConfig5 centre(EntityCentre<?> centre);
+
+    IModuleMenuConfig5 master(final EntityMaster<?> entityMaster);
 
     IModuleMenuConfig5 view(final AbstractCustomView view);
 }
