590a79469fda0956ca7f6900e2ae49ef535bd43a
jhou-pro <jhou.pro@gmail.com>
2018-05-19 14:16:40 +0300

    #918 Implemented validation of title in Edit / Copy actions for base / based users in regards of existing other configurations with the same titles.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/web/centre/CentreConfigEditActionTitleValidator.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/web/centre/CentreConfigEditActionTitleValidator.java
@@ -2,13 +2,23 @@ package ua.com.fielden.platform.web.centre;
 
 import static ua.com.fielden.platform.error.Result.failure;
 import static ua.com.fielden.platform.error.Result.successful;
+import static ua.com.fielden.platform.error.Result.warning;
+import static ua.com.fielden.platform.web.centre.CentreConfigEditAction.EditKind.EDIT;
+import static ua.com.fielden.platform.web.centre.CentreConfigEditAction.EditKind.valueOf;
 
 import java.lang.annotation.Annotation;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
+import com.google.inject.Inject;
+
+import ua.com.fielden.platform.domaintree.impl.GlobalDomainTreeManager;
 import ua.com.fielden.platform.entity.meta.MetaProperty;
 import ua.com.fielden.platform.entity.validation.IBeforeChangeEventHandler;
 import ua.com.fielden.platform.error.Result;
+import ua.com.fielden.platform.types.tuples.T2;
+import ua.com.fielden.platform.web.utils.ICriteriaEntityRestorer;
 
 /**
  * Validator for centre configuration title in {@link CentreConfigEditAction}.
@@ -17,6 +27,12 @@ import ua.com.fielden.platform.error.Result;
  *
  */
 public class CentreConfigEditActionTitleValidator implements IBeforeChangeEventHandler<String> {
+    private final ICriteriaEntityRestorer criteriaEntityRestorer;
+    
+    @Inject
+    public CentreConfigEditActionTitleValidator(final ICriteriaEntityRestorer criteriaEntityRestorer) {
+        this.criteriaEntityRestorer = criteriaEntityRestorer;
+    }
     
     @Override
     public Result handle(final MetaProperty<String> property, final String newValue, final Set<Annotation> mutatorAnnotations) {
@@ -24,6 +40,21 @@ public class CentreConfigEditActionTitleValidator implements IBeforeChangeEventH
             return failure("Brackets are not allowed.");
         } else if (newValue != null && (newValue.contains("{") || newValue.contains("}"))) {
             return failure("Curly braces are not allowed.");
+        } else {
+            final CentreConfigEditAction entity = property.getEntity();
+            final T2<List<LoadableCentreConfig>, Optional<String>> configsAndSaveAsName = 
+                criteriaEntityRestorer.restoreCriteriaEntity(entity.getCentreContextHolder())
+                .loadableCentresSupplier().get();
+            final Optional<String> saveAsName = configsAndSaveAsName._2;
+            final String currentTitle = saveAsName.map(name -> name).orElse(GlobalDomainTreeManager.DEFAULT_CONFIG_TITLE);
+            
+            final boolean titleCanBeCurrent = EDIT.equals(valueOf(entity.getEditKind()));
+            if (configsAndSaveAsName._1.stream()
+                .filter(lcc -> titleCanBeCurrent ? !lcc.getKey().equals(currentTitle) : true)
+                .anyMatch(lcc -> lcc.getKey().equals(newValue)))
+            {
+                return warning("Configuration with this title already exists.");
+            }
         }
         return successful("ok");
     }
