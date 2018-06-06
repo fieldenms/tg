5aad723a1e9837df20b9a539ea828cbcdd939c64
jhou-pro <jhou.pro@gmail.com>
2018-06-06 23:51:12 +0300

    #1038 Fixed unit test by syncing to data type extension.

--- platform-dao/src/test/java/ua/com/fielden/platform/test/mapping/MappingGenerationTest.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/test/mapping/MappingGenerationTest.java
@@ -30,6 +30,7 @@ public class MappingGenerationTest {
                 + "	<property name=\"desc\" column=\"DESC_\" type=\"org.hibernate.type.StringType\"/>\n"
                 + "	<many-to-one name=\"menuItem\" class=\"ua.com.fielden.platform.ui.config.MainMenuItem\" column=\"ID_MAIN_MENU\"/>\n"
                 + "	<many-to-one name=\"owner\" class=\"ua.com.fielden.platform.security.user.User\" column=\"ID_CRAFT\"/>\n"
+                + "	<property name=\"preferred\" column=\"PREFERRED_\" type=\"org.hibernate.type.YesNoType\"/>\n"
                 + "	<property name=\"principal\" column=\"IS_PRINCIPAL\" type=\"org.hibernate.type.YesNoType\"/>\n"
                 + "	<property name=\"title\" column=\"TITLE\" type=\"org.hibernate.type.StringType\"/>\n" + "</class>\n\n" + "</hibernate-mapping>";
         assertEquals("Incorrect mapping.", expectedMapping, tgModelMapping);
