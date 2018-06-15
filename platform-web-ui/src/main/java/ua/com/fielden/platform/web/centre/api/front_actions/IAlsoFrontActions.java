f782e813552b0210415b5c713f950cc4db38db08
Oleh <oleh@fielden.com.au>
2018-06-15 15:59:42 +0300

    #1065 Provide documentation for front actions API.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/front_actions/IAlsoFrontActions.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/front_actions/IAlsoFrontActions.java
@@ -3,7 +3,19 @@ package ua.com.fielden.platform.web.centre.api.front_actions;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.web.centre.api.top_level_actions.ICentreTopLevelActions;
 
+/**
+ * Contract that allows one to add another front actions otherwise switch to other configurations
+ *
+ * @author TG Team
+ *
+ * @param <T>
+ */
 public interface IAlsoFrontActions<T extends AbstractEntity<?>> extends ICentreTopLevelActions<T>{
 
+    /**
+     * Meaningful method call that allows to augment other actions.
+     *
+     * @return
+     */
     IFrontActions<T> also();
 }
