dde01ed153fdf5f8e57922592ec90716e8d775e0
01es <oles.hodych@gmail.com>
2018-07-04 17:43:03 +1000

    Introduced a cleanup HTTP filter to clear thred-local caches.

---
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/filters/CleanupFilter.java
@@ -0,0 +1,32 @@
+package ua.com.fielden.platform.web.filters;
+
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.routing.Filter;
+
+import com.esotericsoftware.kryo.Kryo;
+
+import ua.com.fielden.platform.serialisation.jackson.EntitySerialiser;
+
+/**
+ * A HTTP filter that should be used in order to clear various thread-local caches.
+ * <p>
+ * This filter needs to be wired in the application {@code main} method such as in application {@code Start} classes.
+ * <pre>
+ * final CleanupFilter cleanupFilter = new CleanupFilter();
+ * cleanupFilter.setNext(application);
+ * server.setNext(cleanupFilter);
+ * </pre>
+ *  
+ * @author TG Team 
+ *
+ */
+public class CleanupFilter extends Filter {
+
+    @Override
+    protected void afterHandle(Request request, Response response) {
+        Kryo.getContext().reset();
+        EntitySerialiser.getContext().reset();
+        super.afterHandle(request, response);
+    }
+}
