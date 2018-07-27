ef12e49a1b25920b78a1bb124dd10a889279172c
Nazar Chaykivskyy <nazar@fielden.com.au>
2018-07-27 15:58:57 +0300

    #1102 Removed obsolete stuff.

--- platform-dao/src/main/java/ua/com/fielden/platform/entity/query/generation/elements/NothingBasedSource.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/entity/query/generation/elements/NothingBasedSource.java
@@ -6,7 +6,6 @@ import java.util.List;
 import ua.com.fielden.platform.dao.DomainMetadataAnalyser;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.query.DbVersion;
-import ua.com.fielden.platform.entity.query.EntityAggregates;
 import ua.com.fielden.platform.utils.Pair;
 
 public class NothingBasedSource extends AbstractSource {
@@ -22,7 +21,7 @@ public class NothingBasedSource extends AbstractSource {
 
     @Override
     public Class<? extends AbstractEntity<?>> sourceType() {
-        return EntityAggregates.class;
+    	return null;
     }
 
     @Override
@@ -41,42 +40,15 @@ public class NothingBasedSource extends AbstractSource {
 
     @Override
     public String toString() {
-        if (dbVersion == DbVersion.ORACLE) {
-            return " DUAL ";
-        } else {
-            return "";
-        }
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = super.hashCode();
-        return result;
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!super.equals(obj)) {
-            return false;
-        }
-        if (!(obj instanceof NothingBasedSource)) {
-            return false;
-        }
-        return true;
+        return sql();
     }
 
     @Override
     public void populateSourceItems(boolean parentLeftJoinLegacy) {
-        // TODO Auto-generated method stub
     }
 
     @Override
     protected Pair<PurePropInfo, PurePropInfo> lookForProp(String dotNotatedPropName) {
-        // TODO Auto-generated method stub
         return null;
     }
 }
