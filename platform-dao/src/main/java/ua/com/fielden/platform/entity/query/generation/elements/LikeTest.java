df4b42aa35beb14b20a66005c50c002f249160f4
01es <oles.hodych@gmail.com>
2018-08-13 17:50:49 +1000

    #1109 Externalised DB specific case insensitive LIKE logic to DbVersion.

--- platform-dao/src/main/java/ua/com/fielden/platform/entity/query/generation/elements/LikeTest.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/entity/query/generation/elements/LikeTest.java
@@ -1,15 +1,16 @@
 package ua.com.fielden.platform.entity.query.generation.elements;
 
 import static java.lang.String.format;
-import static ua.com.fielden.platform.entity.query.DbVersion.H2;
 import static ua.com.fielden.platform.entity.query.DbVersion.MSSQL;
 import static ua.com.fielden.platform.entity.query.DbVersion.MYSQL;
 import static ua.com.fielden.platform.entity.query.DbVersion.POSTGRESQL;
 import static ua.com.fielden.platform.utils.CollectionUtil.listOf;
+import static ua.com.fielden.platform.utils.EntityUtils.equalsEx;
 
 import java.util.List;
 
 import ua.com.fielden.platform.entity.query.DbVersion;
+import ua.com.fielden.platform.entity.query.exceptions.EqlException;
 
 public class LikeTest extends AbstractCondition {
     private final ISingleOperand leftOperand;
@@ -18,12 +19,10 @@ public class LikeTest extends AbstractCondition {
     private final boolean caseInsensitive;
     private final DbVersion dbVersion;
 
-    @Override
-    public String sql() {
-        return format("%s %s %s", prepareOperandSql(leftOperand), prepareLikeOperand(), prepareOperandSql(rightOperand));
-    }
-
     public LikeTest(final ISingleOperand leftOperand, final ISingleOperand rightOperand, final boolean negated, final boolean caseInsensitive, final DbVersion dbVersion) {
+        if (dbVersion == null) {
+            throw new EqlException("The dabase version is missing.");
+        }
         this.leftOperand = leftOperand;
         this.rightOperand = rightOperand;
         this.negated = negated;
@@ -31,15 +30,11 @@ public class LikeTest extends AbstractCondition {
         this.dbVersion = dbVersion;
     }
 
-    private String prepareOperandSql(ISingleOperand operand) {
-        return /*dbVersion == H2 || */dbVersion == MSSQL || dbVersion == MYSQL || dbVersion == POSTGRESQL ? operand.sql() : format(" UPPER(%s) ", operand.sql()); 
+    @Override
+    public String sql() {
+        return dbVersion.likeSql(negated, leftOperand.sql(), rightOperand.sql(), caseInsensitive);// format("%s %s %s", prepareOperandSql(leftOperand), prepareLikeOperand(), prepareOperandSql(rightOperand));
     }
-    
-    private String prepareLikeOperand() {
-        String operator = caseInsensitive && (/*dbVersion == H2 || */dbVersion == POSTGRESQL) ? "ILIKE" : "LIKE"; 
-        return negated ? format("NOT %s", operator) : operator;
-    }
-    
+
     @Override
     public boolean ignore() {
         return leftOperand.ignore() || rightOperand.ignore();
@@ -55,10 +50,10 @@ public class LikeTest extends AbstractCondition {
         final int prime = 31;
         int result = 1;
         result = prime * result + (caseInsensitive ? 1231 : 1237);
-        result = prime * result + ((dbVersion == null) ? 0 : dbVersion.hashCode());
-        result = prime * result + ((leftOperand == null) ? 0 : leftOperand.hashCode());
+        result = prime * result + dbVersion.hashCode();
+        result = prime * result + (leftOperand == null ? 0 : leftOperand.hashCode());
         result = prime * result + (negated ? 1231 : 1237);
-        result = prime * result + ((rightOperand == null) ? 0 : rightOperand.hashCode());
+        result = prime * result + (rightOperand == null ? 0 : rightOperand.hashCode());
         return result;
     }
 
@@ -67,36 +62,15 @@ public class LikeTest extends AbstractCondition {
         if (this == obj) {
             return true;
         }
-        if (obj == null) {
-            return false;
-        }
         if (!(obj instanceof LikeTest)) {
             return false;
         }
-        LikeTest other = (LikeTest) obj;
-        if (caseInsensitive != other.caseInsensitive) {
-            return false;
-        }
-        if (dbVersion != other.dbVersion) {
-            return false;
-        }
-        if (leftOperand == null) {
-            if (other.leftOperand != null) {
-                return false;
-            }
-        } else if (!leftOperand.equals(other.leftOperand)) {
-            return false;
-        }
-        if (negated != other.negated) {
-            return false;
-        }
-        if (rightOperand == null) {
-            if (other.rightOperand != null) {
-                return false;
-            }
-        } else if (!rightOperand.equals(other.rightOperand)) {
-            return false;
-        }
-        return true;
+
+        final LikeTest other = (LikeTest) obj;
+        return caseInsensitive == other.caseInsensitive &&
+               negated == other.negated &&
+               dbVersion == other.dbVersion &&
+               equalsEx(leftOperand, other.leftOperand) && 
+               equalsEx(rightOperand, other.rightOperand);
     }
 }
