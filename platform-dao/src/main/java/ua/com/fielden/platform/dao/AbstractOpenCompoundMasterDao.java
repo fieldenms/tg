aaec4f0f9ec0be135c362e2532dbb63892b72838
01es <oles.hodych@gmail.com>
2018-08-24 19:28:03 +1000

    #1053 Provided extensive javadoc + some refactoring.

--- platform-dao/src/main/java/ua/com/fielden/platform/dao/AbstractOpenCompoundMasterDao.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/dao/AbstractOpenCompoundMasterDao.java
@@ -1,5 +1,6 @@
 package ua.com.fielden.platform.dao;
 
+import static java.util.stream.Collectors.toMap;
 import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.from;
 import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.select;
 import static ua.com.fielden.platform.types.tuples.T3.t3;
@@ -11,7 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import com.google.inject.Inject;
 
@@ -24,8 +24,59 @@ import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfa
 import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.IWhere0;
 import ua.com.fielden.platform.types.tuples.T2;
 import ua.com.fielden.platform.types.tuples.T3;
+import ua.com.fielden.platform.web.centre.IQueryEnhancer;
 
-public class CommonOpenCompoundMasterDao<T extends AbstractFunctionalEntityToOpenCompoundMaster<?>> extends CommonEntityDao<T>{
+/**
+ * This class should be extended by companion objects to various {@code Open<EntityName>MaserAction} entities that model an action for opening Compound Entity Masters.
+ * The main purpose of that would be to support indication for those master menu items that represent one-2-many associations (i.e. embedded centres) with actually some associations present.
+ * Such menu items would become highlighted upon opening a compound master.
+ * <p>
+ * Binding between the menu items and respective logic for determining the existence of the underlying data is achieved by using methods {@code addViewBinding} as part of the companion's constructor.
+ * For example, a companion to open master action of entity {@code Category} may have the following constructor:
+ * <pre>
+ * {@code
+ *   public OpenCategoryMasterActionDao(final IFilter filter, final IEntityAggregatesOperations coAggregates) {
+ *       super(filter, coAggregates);
+ *       addViewBinding(SYSTEMS, Systema.class, "category");
+ *   }
+ * }  
+ * </pre>
+ * In the above snippet, line {@code addViewBinding(SYSTEMS, Systema.class, "category");} binds the logic to check existence of entities {@code Systema} for a given {@code Category}.
+ * The Web UI configuration for the master would have a corresponding view defined. For example:
+ * <pre>
+ * {@code
+ *         compoundMaster = CompoundMasterBuilder.<Category, OpenCategoryMasterAction>create(injector, builder)
+            .forEntity(OpenCategoryMasterAction.class)
+            .withProducer(OpenCategoryMasterActionProducer.class)
+            // potentially many other menu items...
+            // but we're interested in SYSTEMS menu item
+            .addMenuItem(CategoryMaster_OpenSystema_MenuItem.class)
+                .icon("icons:view-module")
+                .shortDesc(SYSTEMS)
+                .longDesc("Systems in this category")
+                .withView(createSystemaCentre(editSystemaAction))
+            .done();
+ * }
+ * </code>
+ * Internally {@code addViewBindings} use on of the overloaded utility functions {@code enhanceEmbededCentreQuery}, which should also be used as part of the Web UI configuration of corresponding embedded centres for implementing {@link IQueryEnhancer}.
+ * In case of the already established example, we could have something like this:
+ * <pre>
+ * {@code
+    private static class CategoryMaster_SystemaCentre_QueryEnhancer implements IQueryEnhancer<Systema> {
+        @Override
+        public ICompleted<Systema> enhanceQuery(final IWhere0<Systema> where, final Optional<CentreContext<Systema, ?>> context) {
+            return enhanceEmbededCentreQuery(where, "category", context.get().getMasterEntity().getKey());
+        }
+    }  
+ * }
+ * </pre>
+ *
+ * @author TG Team
+ *
+ * @param <T>
+ */
+public abstract class AbstractOpenCompoundMasterDao<T extends AbstractFunctionalEntityToOpenCompoundMaster<?>> extends CommonEntityDao<T>{
+    private static final String THIS = "this";
 
     private final IEntityAggregatesOperations coAggregates;
     private final List<T3<String, Class<? extends AbstractEntity<?>>, BiFunction<IWhere0<? extends AbstractEntity<?>>, Object, ICompleted<? extends AbstractEntity<?>>>>> compoundMasterConfig = new ArrayList<>();
@@ -33,7 +84,7 @@ public class CommonOpenCompoundMasterDao<T extends AbstractFunctionalEntityToOpe
     private final Map<String, Function<Object, Object>> additionalParameters = new HashMap<>();
 
     @Inject
-    protected CommonOpenCompoundMasterDao(final IFilter filter, final IEntityAggregatesOperations coAggregates) {
+    protected AbstractOpenCompoundMasterDao(final IFilter filter, final IEntityAggregatesOperations coAggregates) {
         super(filter);
         this.coAggregates = coAggregates;
     }
@@ -56,17 +107,13 @@ public class CommonOpenCompoundMasterDao<T extends AbstractFunctionalEntityToOpe
         additionalParameters.entrySet().stream().forEach(entry -> queryParams.put(entry.getKey(), entry.getValue().apply(entity.getKey())));
         final EntityAggregates existEntity = coAggregates.getEntity(from(queryPart.modelAsAggregate()).with(queryParams).model());
         final Map<String, Integer> newPresence = new HashMap<>();
-        compoundMasterConfig.stream().forEach(pair -> {
-            newPresence.put(pair._1, existEntity.get(pair._1));
-        });
-        parameters.stream().forEach(pair -> {
-            newPresence.put(pair._1, existEntity.get(pair._1));
-        });
+        compoundMasterConfig.stream().forEach(pair -> newPresence.put(pair._1, existEntity.get(pair._1)));
+        parameters.stream().forEach(pair -> newPresence.put(pair._1, existEntity.get(pair._1)));
         entity.setEntityPresence(newPresence);
         entity.setCalculated(true);
         return super.save(entity);
     }
-
+    
     @SafeVarargs
     public final void addViewBinding(final String binding, final Class<? extends AbstractEntity<?>> type, final String propertyName, final T2<String, String>... paramConfig) {
         addViewBinding(binding, type, (where, value) -> enhanceEmbededCentreQuery(where, propertyName, value), paramConfig);
@@ -84,12 +131,12 @@ public class CommonOpenCompoundMasterDao<T extends AbstractFunctionalEntityToOpe
             final BiFunction<IWhere0<? extends AbstractEntity<?>>, Object, ICompleted<? extends AbstractEntity<?>>> queryEnhnacer,
             final T2<String, String>... paramConfig) {
         compoundMasterConfig.add(t3(binding, type, queryEnhnacer));
-        additionalParameters.putAll(Arrays.stream(paramConfig).collect(Collectors.toMap(entry -> entry._1, entry -> (value) -> getValue(value, entry._2))));
+        additionalParameters.putAll(Arrays.stream(paramConfig).collect(toMap(entry -> entry._1, entry -> value -> getValue(value, entry._2))));
     }
 
     @SafeVarargs
     public final void addViewBinding(final String binding, final Class<? extends AbstractEntity<?>> type, final T2<String, String>... parameters) {
-        this.parameters.add(t3(binding, type, Arrays.stream(parameters).collect(Collectors.toMap(parameter -> parameter._1, parameter -> (value) -> getValue(value, parameter._2)))));
+        this.parameters.add(t3(binding, type, Arrays.stream(parameters).collect(toMap(parameter -> parameter._1, parameter -> value -> getValue(value, parameter._2)))));
     }
 
     public static <K extends AbstractEntity<?>> ICompleted<K> enhanceEmbededCentreQuery(final IWhere0<K> where, final String prop, final Object value, final String relativeValuePropertyName) {
@@ -101,19 +148,15 @@ public class CommonOpenCompoundMasterDao<T extends AbstractFunctionalEntityToOpe
     }
 
     public static <K extends AbstractEntity<?>> Map<String, Object> enhanceParameters(final Map<String, String> parameters, final K entity) {
-        final Map<String, Function<Object, Object>> functionMap = parameters.entrySet().stream().collect(Collectors.toMap(entry -> entry.getKey(), entry -> (value) -> getValue(value, entry.getValue())));
+        final Map<String, Function<Object, Object>> functionMap = parameters.entrySet().stream().collect(toMap(Map.Entry::getKey, entry -> value -> getValue(value, entry.getValue())));
         return enhanceParametersWithCustomValue(functionMap, entity);
     }
 
     public static <K extends AbstractEntity<?>> Map<String, Object> enhanceParametersWithCustomValue(final Map<String, Function<Object, Object>> parameters, final K entity) {
-        final Map<String, Object> queryParams = new HashMap<>();
-        parameters.entrySet().stream().forEach(entry -> {
-            queryParams.put(entry.getKey(), entry.getValue().apply(entity));
-        });
-        return queryParams;
+        return parameters.entrySet().stream().collect(toMap(Map.Entry::getKey, entry -> entry.getValue().apply(entity)));
     }
 
     private static Object getValue(final Object value, final String propertyName) {
-        return "this".equals(propertyName) ? value : ((AbstractEntity<?>)value).get(propertyName);
+        return THIS.equals(propertyName) ? value : ((AbstractEntity<?>)value).get(propertyName);
     }
 }
