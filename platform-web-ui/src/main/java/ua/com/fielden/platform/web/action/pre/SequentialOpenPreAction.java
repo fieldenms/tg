9da5966d8d0446221683a20abac826a78f434efa
oleh-maikovych <oleh@fielden.com.au>
2018-08-31 17:14:00 +0300

    #1055 Added navigation acions to custom dialog. Enhancing work in progress for sequential edit action.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/action/pre/SequentialOpenPreAction.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/action/pre/SequentialOpenPreAction.java
@@ -11,9 +11,38 @@ import ua.com.fielden.platform.web.view.master.api.actions.pre.IPreAction;
  */
 public class SequentialOpenPreAction implements IPreAction {
 
+    private final String navigationType;
+
+    public SequentialOpenPreAction(final String navigationType) {
+        this.navigationType = navigationType;
+    }
+
     @Override
     public JsCode build() {
-        return new JsCode("\n"
+        return new JsCode(String.format("%n"
+                + "if (!action.supportsNavigation) {%n"
+                + "    action.supportsNavigation = true;%n"
+                + "    action.navigationType = %s;%n"
+                + "    action.firstEntry = fucntion() {%n"
+                + "    }.bind(self);%n"
+                + "    action.previousEntry = fucntion() {%n"
+                + "    }.bind(self);%n"
+                + "    action.nextEntry = fucntion() {%n"
+                + "    }.bind(self);%n"
+                + "    action.lastEntry = fucntion() {%n"
+                + "    }.bind(self);%n"
+                + "    action.hasPreviousEntry = fucntion() {%n"
+                + "        const entityIndex = this.$.egi.findEntityIndex(action.currentEntity);%n"
+                + "        return entityIndex > 0;%n"
+                + "    }.bind(self);%n"
+                + "    action.hasNextEntry = fucntion() {%n"
+                + "        const entityIndex = this.$.egi.findEntityIndex(action.currentEntity);%n"
+                + "        return entityIndex >= 0 && entityIndex === this.$.egi.entities.length - 1;%n"
+                + "    }.bind(self);%n"
+                + "}%n", navigationType)
+                + ""
+                + ""
+                + ""
                 + "if (!action.modifyFunctionalEntity) {\n"
                 + "    action.modifyFunctionalEntity = function (bindingEntity) {\n"
                 + "        if (!action.entitiesToEdit && this.$.egi.entities.length > 0) {\n"
