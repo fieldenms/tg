79379cb01739f4636524cfb00f57dbbfe0185d35
01es <oles.hodych@gmail.com>
2018-12-12 10:11:53 +1100

    #1209 Implemented both issue requirements.

--- platform-dao/src/main/java/ua/com/fielden/platform/entity/query/metadata/EntityTypeInfo.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/entity/query/metadata/EntityTypeInfo.java
@@ -1,5 +1,6 @@
 package ua.com.fielden.platform.entity.query.metadata;
 
+import static java.lang.String.format;
 import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang.StringUtils.isEmpty;
 import static ua.com.fielden.platform.entity.query.metadata.DomainMetadataUtils.produceUnionEntityModels;
@@ -9,7 +10,7 @@ import static ua.com.fielden.platform.entity.query.metadata.EntityCategory.QUERY
 import static ua.com.fielden.platform.entity.query.metadata.EntityCategory.UNION;
 import static ua.com.fielden.platform.reflection.AnnotationReflector.getAnnotation;
 import static ua.com.fielden.platform.reflection.Finder.getKeyMembers;
-import static ua.com.fielden.platform.reflection.asm.impl.DynamicEntityClassLoader.getOriginalType;
+import static ua.com.fielden.platform.reflection.asm.impl.DynamicEntityClassLoader.isGenerated;
 import static ua.com.fielden.platform.utils.EntityUtils.getEntityModelsOfQueryBasedEntityType;
 import static ua.com.fielden.platform.utils.EntityUtils.isCompositeEntity;
 import static ua.com.fielden.platform.utils.EntityUtils.isPersistedEntityType;
@@ -17,15 +18,24 @@ import static ua.com.fielden.platform.utils.EntityUtils.isSyntheticEntityType;
 import static ua.com.fielden.platform.utils.EntityUtils.isUnionEntityType;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.ImmutableList;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.annotation.MapEntityTo;
+import ua.com.fielden.platform.entity.query.exceptions.EqlException;
 import ua.com.fielden.platform.entity.query.model.EntityResultQueryModel;
+import ua.com.fielden.platform.reflection.asm.impl.DynamicTypeNamingService;
 import ua.com.fielden.platform.types.tuples.T2;
 
 public class EntityTypeInfo <ET extends AbstractEntity<?>> {
+
+    /** A cache of table names for entity class names, which includes both ordinary and generated entities. */
+    private static final Cache<String, String> ENTITY_NAME_TO_TABLE_NAME = CacheBuilder.newBuilder().initialCapacity(500).maximumSize(1000).concurrencyLevel(50).build();
+    
     public final Class<ET> entityType;
     public final EntityCategory category;
     public final String tableName;
@@ -35,20 +45,23 @@ public class EntityTypeInfo <ET extends AbstractEntity<?>> {
 
     public EntityTypeInfo(final Class<ET> entityType) {
         this.entityType = entityType;
-        tableName = getTableClause(entityType);
         if (isPersistedEntityType(entityType)) {
+            tableName = getTableClause(entityType);
             category = PERSISTED;
             entityModels = ImmutableList.of();
             unionEntityModels = ImmutableList.of();
         } else if (isSyntheticEntityType(entityType)) {
+            tableName = null;
             category = QUERY_BASED;
             entityModels = ImmutableList.copyOf(getEntityModelsOfQueryBasedEntityType(entityType));
             unionEntityModels = ImmutableList.of();
         } else if (isUnionEntityType(entityType)) {
+            tableName = null;
             category = UNION;
             entityModels = ImmutableList.of();
             unionEntityModels = ImmutableList.copyOf(produceUnionEntityModels(entityType));
         } else {
+            tableName = null;
             category = PURE;
             entityModels = ImmutableList.of();
             unionEntityModels = ImmutableList.of();
@@ -58,13 +71,37 @@ public class EntityTypeInfo <ET extends AbstractEntity<?>> {
     }
 
     private String getTableClause(final Class<ET> entityType) {
-        final MapEntityTo mapEntityToAnnotation = getAnnotation(entityType, MapEntityTo.class);
-        if (mapEntityToAnnotation == null) {
-            return null;
+        try {
+            return ENTITY_NAME_TO_TABLE_NAME.get(entityType.getSimpleName(), () -> {
+                    final MapEntityTo mapEntityToAnnotation = getAnnotation(entityType, MapEntityTo.class);
+                    if (mapEntityToAnnotation == null || isEmpty(mapEntityToAnnotation.value())) {
+                        return deriveTableName(entityType);
+                    } else {
+                        return mapEntityToAnnotation.value();
+                    }
+                }
+            );
+        } catch (final ExecutionException ex) {
+            throw new EqlException(format("Could not determine table name for entity [%s].", entityType.getSimpleName()), ex);
         }
+    }
 
-        final String providedTableName = mapEntityToAnnotation.value();
-        return !isEmpty(providedTableName) ? providedTableName : getOriginalType(entityType).getSimpleName().toUpperCase() + "_";
+    /**
+     * Derives a table name for a entity by its type.
+     * Uses a class name instead of a class loader to derive the table name.
+     *  
+     * @param type
+     * @return
+     */
+    private static String deriveTableName(final Class<? extends AbstractEntity<?>> type) {
+        final String template = "%s_";
+        final String typeName = type.getSimpleName();
+        if (isGenerated(type)) {
+            final String originalTypeName = typeName.substring(0, typeName.indexOf(DynamicTypeNamingService.APPENDIX));
+            return format(template, originalTypeName.toUpperCase());
+        } else {
+            return format(template, typeName.toUpperCase());
+        }
     }
     
     private List<T2<String, Class<?>>> getCompositeKeyMembers(final Class<ET> entityType) {
