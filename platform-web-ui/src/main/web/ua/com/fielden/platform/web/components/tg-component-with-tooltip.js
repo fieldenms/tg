61213f8b71b540364fc5cc89d5fd4a3bd2720a64
oleh-maikovych <oleh@fielden.com.au>
2019-01-18 15:33:48 +0200

    #1233 migrated tooltip example. Added web animation polyfill.

---
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/components/tg-component-with-tooltip.js
@@ -0,0 +1,53 @@
+import '/resources/polymer/@polymer/polymer/polymer-legacy.js';
+import '/resources/polymer/@polymer/iron-icon/iron-icon.js';
+import '/resources/polymer/@polymer/iron-icons/iron-icons.js';
+import '/resources/polymer/@polymer/iron-flex-layout/iron-flex-layout.js';
+
+import '/resources/layout/tg-flex-layout.js'
+
+import {Polymer} from '/resources/polymer/@polymer/polymer/lib/legacy/polymer-fn.js';
+import {html} from '/resources/polymer/@polymer/polymer/lib/utils/html-tag.js';
+
+import {TgTooltipBehavior} from '/resources/components/tg-tooltip-behavior.js'
+
+const template = html`
+    <style>
+        div, span {
+            cursor: default;
+            border: 1px dashed black;
+        }
+    </style>
+    <tg-flex-layout when-desktop="[[_whenDesktop]]" , when-tablet="[[_whenDesktop]]" , when-mobile="[[_whenDesktop]]">
+        <div id="div_text" tooltip-text="Tooltip text for div">
+            <span tooltip-text="Tooltip text for embeded span">Embeded component</span>
+            Component with activated tooltip
+        </div>
+        <template is="dom-repeat" items="[[_tooltipItems]]">
+            <div id="[[item.id]]" tooltip-text$="[[item.tooltip]]">[[item.text]]</div>
+        </template>
+    </tg-flex-layout>
+    <div>Component's tooltip goes here.</div>`;
+
+template.setAttribute('strip-whitespace', '');
+
+Polymer({
+    _template: template,
+    
+    is: 'tg-component-with-tooltip',
+
+    behaviors: [TgTooltipBehavior],
+
+    properties: {
+        _whenDesktop: Array,
+        _tooltipItems: Array
+    },
+    
+    hostAttributes: {
+        'tooltip-text': "The component's tooltip works!"
+    },
+
+    ready: function () {
+        this._whenDesktop = ['horizontal', ['flex'], ['flex']];
+        this._tooltipItems = [{id: "div_html_text", text: "Another componsnt with activated tooltips", tooltip: '<iron-icon icon="attachment"></iron-icon><i>Test italic</i> html tooltip text'}];
+    }
+});
