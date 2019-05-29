c2cf33ede0f43fc5861f9155ef2d3a164d99643b
01es <oles.hodych@gmail.com>
2019-05-29 16:10:23 +1000

    Files to aid in the generation of certificats for TLS infrastrucutre and HAProxy config. Script 'genkey.sh' generates haproxy.pem for 'localhost' and 'tgdev.com', which us used by 'haproxy.cfg' to establish HTTPS and HTTP2 communications. Configuration file 'haproxy.cfg' expects 'haproxy.pem' to be present in the same directory. File 'start_haproxy.sh' is a convenience script to start a docker container for HAProxy, using tag 1.9.8. It is expected that Docker is installed and image haproxy:1.9.8 pulled.

---
+++ dev-mics/local-cert-gen-scripts/genkey.sh
@@ -0,0 +1,24 @@
+echo "Generating keys..."
+openssl req \
+    -x509 -sha256 \
+    -newkey rsa:4096 \
+    -days 1024 \
+    -nodes \
+    -subj "/C=AU/ST=VIC/O=Fielden/CN=localhost" \
+    -extensions SAN \
+    -reqexts SAN \
+    -config <(cat /etc/ssl/openssl.cnf \
+            <(printf "\n[SAN]\nsubjectAltName=DNS:localhost,DNS:tgdev.com")) \
+    -keyout "localhost.key" \
+    -out "localhost.pem"
+
+openssl x509 -in ./localhost.pem -text -noout
+
+echo "Inspect the generated certificate, wich should have Subject Alternative Name afer the public key."
+read -p "Press enter to continue"
+
+echo "Concatenating keys into haproxy.pem..."
+cat ./localhost.pem localhost.key > haproxy.pem
+
+echo "File haproxy.pem should be moved into the HAProxy configuration directory."
+read -p "Press enter to exit"
