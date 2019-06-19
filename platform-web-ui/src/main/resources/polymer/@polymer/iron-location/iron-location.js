3ba40f9fd81978ec897201a29c1f282f4fc20847
oleh-maikovych <oleh@fielden.com.au>
2019-06-19 12:42:47 +0300

    #1341 Modified iron location so that location uris won't be encoded or decoded inside iron-location component.

--- platform-web-ui/src/main/resources/polymer/@polymer/iron-location/iron-location.js
+++ platform-web-ui/src/main/resources/polymer/@polymer/iron-location/iron-location.js
@@ -60,33 +60,30 @@ Polymer({
     path: {
       type: String,
       notify: true,
-      value: function () {
-        return window.decodeURIComponent(window.location.pathname);
+      value: function() {
+        return window.location.pathname;
       }
     },
-
     /**
      * The query string portion of the URL.
      */
     query: {
       type: String,
       notify: true,
-      value: function () {
+      value: function() {
         return window.location.search.slice(1);
       }
     },
-
     /**
      * The hash component of the URL.
      */
     hash: {
       type: String,
       notify: true,
-      value: function () {
-        return window.decodeURIComponent(window.location.hash.slice(1));
+      value: function() {
+        return window.location.hash.slice(1);
       }
     },
-
     /**
      * If the user was on a URL for less than `dwellTime` milliseconds, it
      * won't be added to the browser's history, but instead will be replaced
@@ -99,7 +96,6 @@ Polymer({
       type: Number,
       value: 2000
     },
-
     /**
      * A regexp that defines the set of URLs that should be considered part
      * of this web app.
@@ -117,20 +113,6 @@ Polymer({
       type: String,
       value: ''
     },
-
-    /**
-     * A flag that specifies whether the spaces in query that would normally be
-     * encoded as %20 should be encoded as +.
-     *
-     * Given an example text "hello world", it is encoded in query as
-     * - "hello%20world" without the parameter
-     * - "hello+world" with the parameter
-     */
-    encodeSpaceAsPlusInQuery: {
-      type: Boolean,
-      value: false
-    },
-
     /**
      * urlSpaceRegex, but coerced into a regexp.
      *
@@ -139,8 +121,9 @@ Polymer({
     _urlSpaceRegExp: {
       computed: '_makeRegExp(urlSpaceRegex)'
     },
-    _lastChangedAt: {
-      type: Number
+    _lastChangedAtAt: {
+      type: Number,
+      value: -Infinity
     },
     _initialized: {
       type: Boolean,
@@ -151,249 +134,170 @@ Polymer({
     hidden: true
   },
   observers: ['_updateUrl(path, query, hash)'],
-  created: function () {
-    this.__location = window.location;
-  },
-  attached: function () {
+  attached: function() {
     this.listen(window, 'hashchange', '_hashChanged');
     this.listen(window, 'location-changed', '_urlChanged');
     this.listen(window, 'popstate', '_urlChanged');
-    this.listen(
-    /** @type {!HTMLBodyElement} */
-    document.body, 'click', '_globalOnClick'); // Give a 200ms grace period to make initial redirects without any
-    // additions to the user's history.
-
-    this._lastChangedAt = window.performance.now() - (this.dwellTime - 200);
+    this.listen(/** @type {!HTMLBodyElement} */(document.body), 'click', '_globalOnClick');
     this._initialized = true;
-
     this._urlChanged();
   },
-  detached: function () {
+  detached: function() {
     this.unlisten(window, 'hashchange', '_hashChanged');
     this.unlisten(window, 'location-changed', '_urlChanged');
     this.unlisten(window, 'popstate', '_urlChanged');
-    this.unlisten(
-    /** @type {!HTMLBodyElement} */
-    document.body, 'click', '_globalOnClick');
+    this.unlisten(/** @type {!HTMLBodyElement} */(document.body), 'click', '_globalOnClick');
     this._initialized = false;
   },
-  _hashChanged: function () {
-    this.hash = window.decodeURIComponent(this.__location.hash.substring(1));
+  /**
+   * @return {number} the number of milliseconds since some point in the
+   *     past. Only useful for comparing against other results from this
+   *     function.
+   */
+  _now: function() {
+    if (window.performance && window.performance.now) {
+      return window.performance.now();
+    }
+    return new Date().getTime();
   },
-  _urlChanged: function () {
+  _hashChanged: function() {
+    this.hash = window.location.hash.substring(1);
+  },
+  _urlChanged: function() {
     // We want to extract all info out of the updated URL before we
     // try to write anything back into it.
     //
     // i.e. without _dontUpdateUrl we'd overwrite the new path with the old
     // one when we set this.hash. Likewise for query.
     this._dontUpdateUrl = true;
-
     this._hashChanged();
-
-    this.path = window.decodeURIComponent(this.__location.pathname);
-    this.query = this.__location.search.substring(1);
+    this.path = window.location.pathname;
+    this.query = window.location.search.substring(1);
     this._dontUpdateUrl = false;
-
     this._updateUrl();
   },
-  _getUrl: function () {
-    var partiallyEncodedPath = window.encodeURI(this.path).replace(/\#/g, '%23').replace(/\?/g, '%3F');
-    var partiallyEncodedQuery = '';
-
-    if (this.query) {
-      partiallyEncodedQuery = '?' + this.query.replace(/\#/g, '%23');
-
-      if (this.encodeSpaceAsPlusInQuery) {
-        partiallyEncodedQuery = partiallyEncodedQuery.replace(/\+/g, '%2B').replace(/ /g, '+').replace(/%20/g, '+');
-      } else {
-        // required for edge
-        partiallyEncodedQuery = partiallyEncodedQuery.replace(/\+/g, '%2B').replace(/ /g, '%20');
-      }
+  _getUrl: function() {
+    var url = this.path;
+    var query = this.query;
+    if (query) {
+      url += '?' + query;
     }
-
-    var partiallyEncodedHash = '';
-
     if (this.hash) {
-      partiallyEncodedHash = '#' + window.encodeURI(this.hash);
+      url += '#' + this.hash;
     }
-
-    return partiallyEncodedPath + partiallyEncodedQuery + partiallyEncodedHash;
+    return url;
   },
-  _updateUrl: function () {
+  _updateUrl: function() {
     if (this._dontUpdateUrl || !this._initialized) {
       return;
     }
-
-    if (this.path === window.decodeURIComponent(this.__location.pathname) && this.query === this.__location.search.substring(1) && this.hash === window.decodeURIComponent(this.__location.hash.substring(1))) {
-      // Nothing to do, the current URL is a representation of our properties.
+    var newUrl = this._getUrl();
+    var currentUrl = (
+        window.location.pathname +
+        window.location.search +
+        window.location.hash
+    );
+    if (newUrl == currentUrl) {
+      // nothing to do, the URL didn't change
       return;
     }
-
-    var newUrl = this._getUrl(); // Need to use a full URL in case the containing page has a base URI.
-
-
-    var fullNewUrl = new URL(newUrl, this.__location.protocol + '//' + this.__location.host).href;
-    var now = window.performance.now();
-    var shouldReplace = this._lastChangedAt + this.dwellTime > now;
+    // Need to use a full URL in case the containing page has a base URI.
+    var fullNewUrl = new URL(
+        newUrl, window.location.protocol + '//' + window.location.host).href;
+    var now = this._now();
+    var shouldReplace =
+        this._lastChangedAt + this.dwellTime > now;
     this._lastChangedAt = now;
-
     if (shouldReplace) {
       window.history.replaceState({}, '', fullNewUrl);
     } else {
       window.history.pushState({}, '', fullNewUrl);
     }
-
-    this.fire('location-changed', {}, {
-      node: window
-    });
+    this.fire('location-changed', {}, {node: window});
   },
-
   /**
    * A necessary evil so that links work as expected. Does its best to
    * bail out early if possible.
    *
    * @param {MouseEvent} event .
    */
-  _globalOnClick: function (event) {
-    // If another event handler has stopped this event then there's nothing
-    // for us to do. This can happen e.g. when there are multiple
-    // iron-location elements in a page.
-    if (event.defaultPrevented) {
-      return;
-    }
-
+  _globalOnClick: function(event) {
     var href = this._getSameOriginLinkHref(event);
-
     if (!href) {
       return;
     }
-
-    event.preventDefault(); // If the navigation is to the current page we shouldn't add a history
-    // entry or fire a change event.
-
-    if (href === this.__location.href) {
-      return;
-    }
-
     window.history.pushState({}, '', href);
-    this.fire('location-changed', {}, {
-      node: window
-    });
+    this.fire('location-changed', {}, {node: window});
+    event.preventDefault();
   },
-
   /**
    * Returns the absolute URL of the link (if any) that this click event
    * is clicking on, if we can and should override the resulting full
    * page navigation. Returns null otherwise.
    *
+   * This method is separated away from _globalOnClick for testability,
+   * as we can't test that a clicked link should have resulted in navigating
+   * away from the test page.
+   *
    * @param {MouseEvent} event .
    * @return {string?} .
    */
-  _getSameOriginLinkHref: function (event) {
+  _getSameOriginLinkHref: function(event) {
     // We only care about left-clicks.
     if (event.button !== 0) {
       return null;
-    } // We don't want modified clicks, where the intent is to open the page
+    }
+    // We don't want modified clicks, where the intent is to open the page
     // in a new tab.
-
-
     if (event.metaKey || event.ctrlKey) {
       return null;
     }
-
     var eventPath = dom(event).path;
-    var anchor = null;
-
+    var href = null;
     for (var i = 0; i < eventPath.length; i++) {
       var element = eventPath[i];
-
       if (element.tagName === 'A' && element.href) {
-        anchor = element;
+        href = element.href;
         break;
       }
-    } // If there's no link there's nothing to do.
-
-
-    if (!anchor) {
-      return null;
-    } // Target blank is a new tab, don't intercept.
-
-
-    if (anchor.target === '_blank') {
-      return null;
-    } // If the link is for an existing parent frame, don't intercept.
-
-
-    if ((anchor.target === '_top' || anchor.target === '_parent') && window.top !== window) {
-      return null;
-    } // If the link is a download, don't intercept.
-
-
-    if (anchor.download) {
+    }
+    // If there's no link there's nothing to do.
+    if (!href) {
       return null;
     }
-
-    var href = anchor.href; // It only makes sense for us to intercept same-origin navigations.
+    // It only makes sense for us to intercept same-origin navigations.
     // pushState/replaceState don't work with cross-origin links.
-
     var url;
-
     if (document.baseURI != null) {
-      url = new URL(href,
-      /** @type {string} */
-      document.baseURI);
+      url = new URL(href, /** @type {string} */(document.baseURI));
     } else {
       url = new URL(href);
     }
-
-    var origin; // IE Polyfill
-
-    if (this.__location.origin) {
-      origin = this.__location.origin;
+    var origin;
+    // IE Polyfill
+    if (window.location.origin) {
+      origin = window.location.origin;
     } else {
-      origin = this.__location.protocol + '//' + this.__location.host;
-    }
-
-    var urlOrigin;
-
-    if (url.origin) {
-      urlOrigin = url.origin;
-    } else {
-      // IE always adds port number on HTTP and HTTPS on <a>.host but not on
-      // window.location.host
-      var urlHost = url.host;
-      var urlPort = url.port;
-      var urlProtocol = url.protocol;
-      var isExtraneousHTTPS = urlProtocol === 'https:' && urlPort === '443';
-      var isExtraneousHTTP = urlProtocol === 'http:' && urlPort === '80';
-
-      if (isExtraneousHTTPS || isExtraneousHTTP) {
-        urlHost = url.hostname;
+      origin = window.location.protocol + '//' + window.location.hostname;
+      if (window.location.port) {
+        origin += ':' + window.location.port;
       }
-
-      urlOrigin = urlProtocol + '//' + urlHost;
     }
-
-    if (urlOrigin !== origin) {
+    if (url.origin !== origin) {
       return null;
     }
-
-    var normalizedHref = url.pathname + url.search + url.hash; // pathname should start with '/', but may not if `new URL` is not supported
-
-    if (normalizedHref[0] !== '/') {
-      normalizedHref = '/' + normalizedHref;
-    } // If we've been configured not to handle this url... don't handle it!
-
-
-    if (this._urlSpaceRegExp && !this._urlSpaceRegExp.test(normalizedHref)) {
+    var normalizedHref = url.pathname + url.search + url.hash;
+    // If we've been configured not to handle this url... don't handle it!
+    if (this._urlSpaceRegExp &&
+        !this._urlSpaceRegExp.test(normalizedHref)) {
       return null;
-    } // Need to use a full URL in case the containing page has a base URI.
-
-
-    var fullNormalizedHref = new URL(normalizedHref, this.__location.href).href;
+    }
+    // Need to use a full URL in case the containing page has a base URI.
+    var fullNormalizedHref = new URL(
+        normalizedHref, window.location.href).href;
     return fullNormalizedHref;
   },
-  _makeRegExp: function (urlSpaceRegex) {
+  _makeRegExp: function(urlSpaceRegex) {
     return RegExp(urlSpaceRegex);
   }
 });
