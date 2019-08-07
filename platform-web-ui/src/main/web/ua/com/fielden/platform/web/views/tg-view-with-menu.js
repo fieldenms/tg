072f4feb15fd1ae53a4ed89d7f773da448989a28
jhou-pro <jhou.pro@gmail.com>
2019-08-07 11:46:43 +0300

    #1387 I.1.c Removed checkbox from under paper-items in menu.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/views/tg-view-with-menu.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/views/tg-view-with-menu.js
@@ -179,7 +179,7 @@ const template = html`
                         <paper-item tooltip-text$="[[firstLevelItem.desc]]" slot="trigger">
                             <!-- <iron-icon class="menu-icon" icon="[[firstLevelItem.icon]]" has-no-any-icon$="[[!_hasSomeIcon]]" has-no-icon$="[[_calcHasNoIcon(firstLevelItem.icon)]]"></iron-icon> -->
                             <span class="flex menu-item-title">[[firstLevelItem.key]]</span>
-                            <paper-checkbox class$="[[_calcGroupStyle(firstLevelItem)]]" group-item$="[[groupIndex]]" hidden$="[[!canEdit]]" checked="[[firstLevelItem.visible]]" on-change="_changeGroupVisibility" on-tap="_tapCheckbox" tooltip-text$="[[_calcCheckboxTooltip(firstLevelItem.menu, firstLevelItem.visible)]]"></paper-checkbox>
+                            <!-- <paper-checkbox class$="[[_calcGroupStyle(firstLevelItem)]]" group-item$="[[groupIndex]]" hidden$="[[!canEdit]]" checked="[[firstLevelItem.visible]]" on-change="_changeGroupVisibility" on-tap="_tapCheckbox" tooltip-text$="[[_calcCheckboxTooltip(firstLevelItem.menu, firstLevelItem.visible)]]"></paper-checkbox> -->
                             <!-- <iron-icon class="submenu-trigger-icon" icon="[[_calcExpandCollapseIcon(firstLevelItem.opened)]]" opened$="[[firstLevelItem.opened]]" has-no-any-icon$="[[!_hasSomeMenu]]" without-menu$="[[!_isMenuPresent(firstLevelItem.menu)]]"></iron-icon> -->
                         </paper-item>
                         <template is="dom-if" if="[[_isMenuPresent(firstLevelItem.menu)]]">
@@ -188,7 +188,7 @@ const template = html`
                                     <paper-item class="submenu-item" name$="[[_calcItemPath(firstLevelItem, item, groupIndex)]]" tooltip-text$="[[item.desc]]">
                                         <!-- <iron-icon class="menu-icon" icon="[[item.icon]]" has-no-icon$="[[_calcHasNoIcon(item.icon)]]"></iron-icon> -->
                                         <span class="flex menu-item-title">[[item.key]]</span>
-                                        <paper-checkbox hidden$="[[!canEdit]]" checked="[[item.visible]]" on-change="_changeVisibility" on-tap="_tapCheckbox" tooltip-text$="[[_calcCheckboxTooltip(item.menu, item.visible)]]"></paper-checkbox>
+                                        <!-- <paper-checkbox hidden$="[[!canEdit]]" checked="[[item.visible]]" on-change="_changeVisibility" on-tap="_tapCheckbox" tooltip-text$="[[_calcCheckboxTooltip(item.menu, item.visible)]]"></paper-checkbox> -->
                                         <!-- <iron-icon class="submenu-trigger-icon" without-menu></iron-icon> -->
                                     </paper-item>
                                 </template>
