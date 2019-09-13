bf6263eb4a8b651ecd799976ecb342b0aa8d3d6c
Oleh <oleh@fielden.com.au>
2019-09-13 17:09:30 +0300

    #1401 Enhanced the template of responsive toolbar, replaced egi's toolbar with responsive one, Added properties to manage elements of toolbar. Implemented the toolbar element retrieving logic.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/egi/tg-responsive-toolbar.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/egi/tg-responsive-toolbar.js
@@ -1,6 +1,7 @@
 import '/resources/polymer/@polymer/iron-flex-layout/iron-flex-layout.js';
 
 import {PolymerElement, html} from '/resources/polymer/@polymer/polymer/polymer-element.js';
+import {mixinBehaviors} from '/resources/polymer/@polymer/polymer/lib/legacy/class.js';
 import { IronResizableBehavior } from '/resources/polymer/@polymer/iron-resizable-behavior/iron-resizable-behavior.js';
 
 const template = html`
@@ -24,16 +25,22 @@ const template = html`
         .grid-toolbar-content ::slotted(.group) {
             margin-left: 30px;
         }
+        #expandToolbarButton.invisible {
+            position: absolute;
+            right:0;
+            visibility: hidden;
+        }
     </style>
-    <div class="grid-toolbar">
-            <div class="grid-toolbar-content">
-                <slot id="top_action_selctor" name="entity-specific-action"></slot>
-            </div>
-            <div class="grid-toolbar-content" style="margin-left:auto">
-                <slot name="standart-action"></slot>
-            </div>
+    <div id="toolbar" class="grid-toolbar">
+        <slot></slot>
+        <div class="grid-toolbar-content">
+            <slot id="top_action_selctor" name="entity-specific-action"></slot>
+        </div>
+        <div class="grid-toolbar-content" style="margin-left:auto">
+            <slot id="standard_action_selector" name="standart-action"></slot>
+            <paper-icon-button id="expandToolbarButton" icon="more-vert" on-tap="_showMoreActions" class="invisible"></paper-icon-button>
+        </div>
     </div>
-    <paper-icon-button icon="more-vert" on-tap="_showMoreActions"></paper-icon-button>
     <iron-dropdown id="dropdown" horizontal-align="right" open-animation-config="[[openAnimationConfig]]" close-animation-config="[[closeAnimationconfig]]">
         <span class="dropdown-content" slot="dropdown-content">This is overflow menu</span>
     </iron-dropdown>`;
@@ -46,8 +53,38 @@ export class TgResponsiveToolbar extends mixinBehaviors([IronResizableBehavior],
 
     static get properties () {
         return {
-            
+            specificActions: Array,
+            standartActions: Array
         };
     }
+
+    ready () {
+        super.ready();
+        this.specificActions = [];
+        this.$.top_action_selctor.assignedNodes({ flatten: true }).array.forEach(node => {
+            const specificActionSelector = ".entity-specific-action:not(.group):not(.first-group)";
+            if (node.matches(specificActionSelector)) {
+                specificActions.push([node]);
+            } else {
+                specificActions.push([...node.querySelectorAll(specificActionSelector)]);
+            }
+        });
+        this.standartActions = [];
+        this.$.standard_action_selector.assignedNodes({ flatten: true }).array.forEach(node => {
+            const standartActionSelector = ".standart-action";
+            if (node.matches(standartActionSelector)) {
+                standartActions.push(node);
+            }
+        });
+
+        //Initialising resize event listeners.
+        this.addEventListener("iron-resize", this._resizeEventListener.bind(this));
+    }
+
+    _resizeEventListener () {
+
+    }
 }
+
+customElements.define('tg-responsive-toolbar', TgResponsiveToolbar);
 
