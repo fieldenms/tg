940e130c212f638fced983a45b76a6b08708d912
01es <oles.hodych@gmail.com>
2019-09-13 19:01:18 +1000

    #1419 Reduced class visibility. Provided documentation. Removed references to externally referenced mutable structures. Used Collection instead of Set as a simpler abstraction when accessing dynamic columns.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/file_reports/DataForWorkbookSheet.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/file_reports/DataForWorkbookSheet.java
@@ -1,33 +1,41 @@
 package ua.com.fielden.platform.file_reports;
 
+import static ua.com.fielden.platform.utils.EntityUtils.equalsEx;
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Stream;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity_centre.review.criteria.DynamicPropForExport;
-import ua.com.fielden.platform.utils.EntityUtils;
 import ua.com.fielden.platform.utils.Pair;
 
-public class DataForWorkbookSheet<E extends AbstractEntity<?>> {
+/**
+ * This is a package-private helper class to represent data that is used for building an Excel workbook sheet.
+ * 
+ * @author TG Team
+ *
+ * @param <E>
+ */
+class DataForWorkbookSheet<E extends AbstractEntity<?>> {
     private final String sheetTitle;
     private final Stream<E> entities;
     private final List<String> propNames = new ArrayList<>();
     private final List<String> propTitles = new ArrayList<>();
-    private final Map<String, DynamicPropForExport> collectionalProperties;
+    private final Map<String, DynamicPropForExport> collectionalProperties = new LinkedHashMap<>();
 
     public DataForWorkbookSheet(final String sheetTitle, final Stream<E> entities, final List<Pair<String, String>> propertyNamesAndTitles, final Map<String, DynamicPropForExport> collectionalProperties) {
-        super();
         this.sheetTitle = sheetTitle;
         this.entities = entities;
         for (final Pair<String, String> pair : propertyNamesAndTitles) {
             propNames.add(pair.getKey());
             propTitles.add(pair.getValue());
         }
-        this.collectionalProperties = collectionalProperties;
+        this.collectionalProperties.putAll(collectionalProperties);
     }
 
     public String getSheetTitle() {
@@ -46,15 +54,23 @@ public class DataForWorkbookSheet<E extends AbstractEntity<?>> {
         return propTitles;
     }
 
-    public <M extends AbstractEntity<?>> Object getValue(final M entity, final String propertyName) {
-        if (collectionalProperties.containsKey(propertyName)) {
-            final DynamicPropForExport collectionalProperty = collectionalProperties.get(propertyName);
-            final Set<? extends AbstractEntity<?>> collection = entity.get(collectionalProperty.getCollectionalPropertyName());
+    /**
+     * An abstraction to correctly obtain the value of property by {@code propName} from {@code entity}.
+     * This is needed to correctly handle cases of exporting entities that have their collectional properties in-lined and represented as dynamic properties.
+     *
+     * @param entity
+     * @param propName
+     * @return
+     */
+    public <M extends AbstractEntity<?>> Object getValue(final M entity, final String propName) {
+        if (collectionalProperties.containsKey(propName)) {
+            final DynamicPropForExport collectionalProperty = collectionalProperties.get(propName);
+            final Collection<? extends AbstractEntity<?>> collection = entity.get(collectionalProperty.getCollectionalPropertyName());
             final Optional<? extends AbstractEntity<?>> subEntity = collection.stream()
-                    .filter(e -> EntityUtils.equalsEx(e.get(collectionalProperty.getKeyProp()), collectionalProperty.getKeyPropValue()))
+                    .filter(e -> equalsEx(e.get(collectionalProperty.getKeyProp()), collectionalProperty.getKeyPropValue()))
                     .findFirst();
             return subEntity.map(e -> e.get(collectionalProperty.getValueProp())).orElse(null);
         }
-        return entity.get(propertyName);
+        return entity.get(propName);
     }
 }
