37099702ab94afa509c04f5bcf6fb74e11db7b42
jhou-pro <jhou.pro@gmail.com>
2019-09-17 15:52:56 +0300

    #1427 Added accordion for orGroup selection under missing / not mnemonics. Added auto opening if orGroup selected.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/centre/criterion/multi/tg-multi-criterion-config.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/centre/criterion/multi/tg-multi-criterion-config.js
@@ -6,20 +6,49 @@ import { Polymer } from '/resources/polymer/@polymer/polymer/lib/legacy/polymer-
 import { html } from '/resources/polymer/@polymer/polymer/lib/utils/html-tag.js';
 
 import '/resources/polymer/@polymer/paper-checkbox/paper-checkbox.js';
-import '/resources/polymer/@polymer/paper-input/paper-input.js';
+import '/resources/polymer/@polymer/iron-flex-layout/iron-flex-layout-classes.js';
+import '/resources/polymer/@polymer/polymer/lib/elements/dom-repeat.js';
+import '/resources/polymer/@polymer/paper-radio-button/paper-radio-button.js';
+
+import '/resources/components/tg-accordion.js';
 
 const template = html`
-    <style>
+    <style include="iron-flex iron-flex-reverse iron-flex-alignment iron-flex-factors iron-positioning">
         paper-checkbox {
             margin-bottom: 20px;
             font-family: 'Roboto', 'Noto', sans-serif;
             --paper-checkbox-checked-color: #0288D1;
             --paper-checkbox-checked-ink-color: #0288D1;
         }
+        tg-accordion {
+            margin-bottom: 20px;
+            --tg-accordion-selected-heading-background-color: var(--paper-light-blue-700);
+            --tg-accordion-selected-heading-color: white;
+            --tg-accordion-selected-label-color: white;
+        }
+        paper-radio-button {
+            margin: 10px;
+            --paper-radio-button-checked-color: var(--paper-light-blue-700);
+            --paper-radio-button-checked-ink-color: var(--paper-light-blue-700);
+            font-family: 'Roboto', 'Noto', sans-serif;
+        }
+        paper-radio-button {
+            --calculated-paper-radio-button-ink-size: 36px;
+        }
     </style>
     <paper-checkbox checked="{{_orNull}}" hidden$="[[_excludeMissing]]">Missing</paper-checkbox>
     <paper-checkbox checked="{{_not}}">Not</paper-checkbox>
-    <paper-input value="{{_orGroupStr}}" label="Group" hidden$="[[_excludeOrGroup]]"></paper-input>
+    <tg-accordion id="orGroupAccordion" heading="OR grouping" hidden$="[[_excludeOrGroup]]" selected="[[_calcSelected(_orGroup)]]">
+        <div class="layout horizontal wrap">
+            <template is="dom-repeat" items="{{_columns}}" as="column">
+                <div class="layout vertical">
+                    <template is="dom-repeat" items="{{_rows}}" as="row">
+                        <paper-radio-button multirow="[[row]]" multicolumn="[[column]]" toggles on-change="_multiMetaValueChanged" checked="[[_checked(_orGroup, row, column)]]">[[_radioTitle(row, column)]]</paper-radio-button>
+                    </template>
+                </div>
+            </template>
+        </div>
+    </tg-accordion>
 `;
 
 Polymer({
@@ -42,15 +71,7 @@ Polymer({
          */
         _orGroup: {
             type: Number,
-            notify: true,
-            observer: '_orGroupChanged'
-        },
-        /**
-         * String representation of '_orGroup'.
-         */
-        _orGroupStr: {
-            type: String,
-            observer: '_orGroupStrChanged'
+            notify: true
         },
 
         _excludeMissing: {
@@ -61,15 +82,43 @@ Polymer({
         }
     },
 
-    _orGroupStrChanged: function (newValue) {
-        if (newValue && newValue !== '') {
-            this._orGroup = +newValue;
+    ready: function () {
+        this._columns = [0, 1, 2]; // zero-based
+        this._rows = [1, 2, 3]; // one-based
+    },
+
+    attached: function () {
+        this.async(function () {
+            // Let's make sure that accordion with 'orGroup' mnemonic is open after dialog opens.
+            // It surely can be closed if user wants to [after that].
+            // Otherwise, if 'orGroup' mnemonic is not assigned, then accordion to be deliberately closed as a minor item for user action.
+            const accordion = this.shadowRoot.querySelector('#orGroupAccordion'); // by default the fist accordion should be open
+            accordion.opened = accordion.selected;
+        }.bind(this), 1);
+    },
+
+    _multiMetaValueChanged: function (e, detail) {
+        const source = e.target || e.srcElement;
+        if (source.checked) {
+            this._orGroup = this._groupNumber(source.multirow, source.multicolumn);
         } else {
             this._orGroup = null;
         }
     },
 
-    _orGroupChanged: function (newValue) {
-        this._orGroupStr = newValue === null ? '' : (newValue + '');
+    _groupNumber: function (row, column) {
+        return row + column * 3;
+    },
+
+    _radioTitle: function (row, column) {
+        return 'Group ' + this._groupNumber(row, column);
+    },
+
+    _checked: function (_orGroup, row, column) {
+        return this._groupNumber(row, column) === _orGroup; // _orGroup can be 'null' here
+    },
+
+    _calcSelected: function (_orGroup) {
+        return _orGroup !== null;
     }
 });
