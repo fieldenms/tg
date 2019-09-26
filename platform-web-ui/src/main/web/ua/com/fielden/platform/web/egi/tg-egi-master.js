676256b30a2aa50845cb33a8eb3892d81630a720
oleh-maikovych <oleh@fielden.com.au>
2019-09-26 16:50:36 +0300

    #938 Introduced new tg-egi-master for inline egi editing that should replace egi's row with it's content.

---
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/egi/tg-egi-master.js
@@ -0,0 +1,93 @@
+import '/resources/polymer/@polymer/iron-flex-layout/iron-flex-layout.js';
+
+import { TgEntityMasterBehavior } from '/resources/master/tg-entity-master-behavior.js';
+
+import {PolymerElement, html} from '/resources/polymer/@polymer/polymer/polymer-element.js';
+import {mixinBehaviors} from '/resources/polymer/@polymer/polymer/lib/legacy/class.js';
+
+const template = html`
+    <tg-entity-master
+        id="masterDom"
+        entity-type="[[entityType]]"
+        entity-id="[[entityId]]"
+        _post-validated-default="[[_postValidatedDefault]]"
+        _post-validated-default-error="[[_postValidatedDefaultError]]"
+        _process-response="[[_processResponse]]"
+        _process-error="[[_processError]]"
+        _process-retriever-response="[[_processRetrieverResponse]]"
+        _process-retriever-error="[[_processRetrieverError]]"
+        _process-saver-response="[[_processSaverResponse]]"
+        _process-saver-error="[[_processSaverError]]"
+        _saver-loading="{{_saverLoading}}">
+            <slot name="property-editors"></slot>
+    </tg-entity-master>`;
+
+export class TgEgiMaster extends mixinBehaviors([TgEntityMasterBehavior], PolymerElement) {
+
+    static get template() { 
+        return template;
+    }
+
+    static get properties () {
+        return {
+            
+        };
+    }
+
+    constructor () {
+        this.noUI = false;
+        this.saveOnActivation = false;
+    }
+
+    ready () {
+        super.ready();
+    }
+
+    _masterDom () {
+        return this.$.masterDom;
+    }
+
+    /**
+     * The core-ajax component for entity retrieval.
+     */
+    _ajaxRetriever () {
+        return this._masterDom()._ajaxRetriever();
+    }
+
+    /**
+     * The core-ajax component for entity saving.
+     */
+    _ajaxSaver () {
+        return this._masterDom()._ajaxSaver();
+    }
+
+    /**
+     * The validator component.
+     */
+    _validator () {
+        return this._masterDom()._validator();
+    }
+
+    /**
+     * The component for entity serialisation.
+     */
+    _serialiser () {
+        return this._masterDom()._serialiser();
+    }
+
+    /**
+     * The reflector component.
+     */
+    _reflector () {
+        return this._masterDom()._reflector();
+    }
+
+    /**
+     * The toast component.
+     */
+    _toastGreeting () {
+        return this._masterDom()._toastGreeting();
+    }
+}
+
+customElements.define('tg-egi-master', TgEgiMaster);
