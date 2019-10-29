cd49d1a3b693b2cd116a2c335a7141ff27b006c1
01es <oles.hodych@gmail.com>
2019-10-30 02:15:26 +1100

    #1432 Miniature cleanup.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/crit_gen/StubSerialiser.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/crit_gen/StubSerialiser.java
@@ -13,55 +13,46 @@ public class StubSerialiser implements ISerialiser {
 
     @Override
     public <T> T deserialise(final byte[] content, final Class<T> type, final SerialiserEngines serialiserEngine) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public <T> T deserialise(final InputStream content, final Class<T> type, final SerialiserEngines serialiserEngine) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public byte[] serialise(final Object obj, final SerialiserEngines serialiserEngine) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public byte[] serialise(final Object obj) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public <T> T deserialise(final byte[] content, final Class<T> type) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public <T> T deserialise(final InputStream content, final Class<T> type) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public EntityFactory factory() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public ISerialiserEngine getEngine(final SerialiserEngines serialiserEngine) {
-        // TODO Auto-generated method stub
         return null;
     }
     
     @Override
     public ISerialiser initJacksonEngine(final ISerialisationTypeEncoder serialisationTypeEncoder, final IIdOnlyProxiedEntityTypeCache idOnlyProxiedEntityTypeCache) {
-        // TODO Auto-generated method stub
         return this;
     }
 }
