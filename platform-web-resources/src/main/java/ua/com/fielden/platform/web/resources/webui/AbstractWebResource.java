47da8a4e085c6e1c7cf697fb992d41c978362a44
jhou-pro <jhou.pro@gmail.com>
2019-11-20 16:09:39 +0200

    #1454 Extended AbstractWebResource with client-side time-zone setting (into IUniversalConstants) to provide time-zone context for Now-derivative calculations.

--- platform-web-resources/src/main/java/ua/com/fielden/platform/web/resources/webui/AbstractWebResource.java
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/resources/webui/AbstractWebResource.java
@@ -8,6 +8,7 @@ import org.restlet.Request;
 import org.restlet.Response;
 import org.restlet.resource.ServerResource;
 
+import ua.com.fielden.platform.utils.IUniversalConstants;
 import ua.com.fielden.platform.web.interfaces.DeviceProfile;
 import ua.com.fielden.platform.web.interfaces.IDeviceProvider;
 
@@ -21,9 +22,10 @@ import ua.com.fielden.platform.web.interfaces.IDeviceProvider;
 public abstract class AbstractWebResource extends ServerResource {
     private final DeviceProfile deviceProfile;
 
-    public AbstractWebResource(final Context context, final Request request, final Response response, final IDeviceProvider deviceProvider) {
+    public AbstractWebResource(final Context context, final Request request, final Response response, final IDeviceProvider deviceProvider, final IUniversalConstants universalConstants) {
         init(context, request, response);
         deviceProvider.setDeviceProfile(deviceProfile = calculateDeviceProfile(request));
+        universalConstants.setTimeZone(request.getHeaders().getValues("Time-Zone"));
     }
 
     /**
