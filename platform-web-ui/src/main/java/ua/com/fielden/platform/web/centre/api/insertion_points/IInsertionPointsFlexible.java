dec81321724b0a8fd629ece2d10235ff556cdab1
Oleh <oleh@fielden.com.au>
2020-03-13 10:17:27 +0200

    #1503 Added java doc to flexible contract of insertion point

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/insertion_points/IInsertionPointsFlexible.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/insertion_points/IInsertionPointsFlexible.java
@@ -2,7 +2,19 @@ package ua.com.fielden.platform.web.centre.api.insertion_points;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
 
+/**
+ * A contract to make insertion point resizable.
+ *
+ * @author TG Team
+ *
+ * @param <T>
+ */
 public interface IInsertionPointsFlexible <T extends AbstractEntity<?>> extends IInsertionPoints<T>{
 
+    /**
+     * Makes insertion point flexible (resizable)
+     *
+     * @return
+     */
     IInsertionPoints<T> flex();
 }
