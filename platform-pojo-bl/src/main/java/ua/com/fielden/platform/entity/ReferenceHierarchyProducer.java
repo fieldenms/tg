d55bcab5c738c4499216cab1ea75ac69be244cc2
01es <oles.hodych@gmail.com>
2020-04-27 00:02:31 +1000

    #1426 Provided support for Synthetic entities based on Persistent for building the Reference Hierarchy view.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/ReferenceHierarchyProducer.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/ReferenceHierarchyProducer.java
@@ -1,11 +1,16 @@
 package ua.com.fielden.platform.entity;
 
+import static java.lang.String.format;
+import static ua.com.fielden.platform.utils.EntityUtils.isPersistedEntityType;
+import static ua.com.fielden.platform.utils.EntityUtils.isSyntheticBasedOnPersistentEntityType;
+
 import org.apache.commons.lang.StringUtils;
 
 import com.google.inject.Inject;
 
 import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
+import ua.com.fielden.platform.reflection.exceptions.ReflectionException;
 
 public class ReferenceHierarchyProducer extends DefaultEntityProducerWithContext<ReferenceHierarchy> {
 
@@ -19,7 +24,16 @@ public class ReferenceHierarchyProducer extends DefaultEntityProducerWithContext
         if (selectedEntitiesNotEmpty()) {
             final AbstractEntity<?> selectedEntity = selectedEntities().get(0);
             entity.setRefEntityId(selectedEntity.getId());
-            entity.setRefEntityType(selectedEntity.getType().getName());
+            // we need to be smart about getting the type as it may be a synthetic entity that represents a persistent entity
+            // so we really need to handle this case here
+            final Class<? extends AbstractEntity<?>> entityType = selectedEntity.getType();
+            if (isPersistedEntityType(entityType)) {
+                entity.setRefEntityType(entityType.getName());
+            } else if (isSyntheticBasedOnPersistentEntityType(entityType)) {
+                entity.setRefEntityType(entityType.getSuperclass().getName());
+            } else {
+                throw new ReflectionException(format("Unsupported entity type [%s] for Reference Hiearchy.", entityType.getSimpleName()));
+            }
             entity.setTitle(selectedEntity.getKey() + (StringUtils.isEmpty(selectedEntity.getDesc()) ? "" : ": " + selectedEntity.getDesc()));
         }
         return entity;
