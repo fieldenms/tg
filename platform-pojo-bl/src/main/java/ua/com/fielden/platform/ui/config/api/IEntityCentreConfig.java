aa89ebda93eb4eb1b52a8f25d8aa2339e51e6069
01es <oles.hodych@gmail.com>
2020-04-28 23:38:08 +1000

    #1522 Implemented issue requirements.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/ui/config/api/IEntityCentreConfig.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/ui/config/api/IEntityCentreConfig.java
@@ -1,6 +1,10 @@
 package ua.com.fielden.platform.ui.config.api;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 import ua.com.fielden.platform.dao.IEntityDao;
+import ua.com.fielden.platform.entity.query.DbVersion;
 import ua.com.fielden.platform.ui.config.EntityCentreConfig;
 
 /**
@@ -32,4 +36,11 @@ public interface IEntityCentreConfig extends IEntityDao<EntityCentreConfig> {
      */
     Long saveWithConflicts(final EntityCentreConfig entity);
     
+    /**
+     * Runs function {@code fun} with the {@link DbVersion} as the argument.
+     * @param fun
+     * @return
+     */
+    <T> T withDbVersion(final Function<DbVersion, T> fun);
+
 }
