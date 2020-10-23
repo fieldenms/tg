cc28aa6db816ef99903b768e9acf8799c4db10d4
Maikovych Oleh <oleh@fielden.com.au>
2020-10-23 17:33:00 +0300

    #1607 Introduced multi-action API. Also introduced action selector that should be used during data retrieval process of the entity centre

---
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/actions/multi/EntityMultiActionConfigBuilder.java
@@ -0,0 +1,49 @@
+package ua.com.fielden.platform.web.centre.api.actions.multi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ua.com.fielden.platform.web.centre.api.actions.EntityActionConfig;
+
+/**
+ * Implements the multi-action construction API.
+ *
+ * @author TG Team
+ *
+ */
+public class EntityMultiActionConfigBuilder implements IEntityMultiActionConfigAddAction, IEntityMultiActionConfigBuild {
+
+    private final Class<? extends IEntityMultiActionSelector> actionSelectorClass;
+    private final List<EntityActionConfig> actions = new ArrayList<>();
+
+    /**
+     * Creates the multi-action configuration with entry point that allows one to add first action configuration.
+     *
+     * @param actionSelectorClass
+     * @return
+     */
+    public static IEntityMultiActionConfigAddAction multiAction(final Class<? extends IEntityMultiActionSelector> actionSelectorClass) {
+        return new EntityMultiActionConfigBuilder(actionSelectorClass);
+    }
+
+    /**
+     * Creates the multi-action configuration builder for specified action selector class.
+     *
+     * @param actionSelectorClass
+     */
+    private EntityMultiActionConfigBuilder(final Class<? extends IEntityMultiActionSelector> actionSelectorClass) {
+        this.actionSelectorClass = actionSelectorClass;
+    }
+
+    @Override
+    public EntityMultileActionConfig build() {
+        return new EntityMultileActionConfig(actionSelectorClass, actions);
+    }
+
+    @Override
+    public IEntityMultiActionConfigBuild addAction(final EntityActionConfig action) {
+        actions.add(action);
+        return this;
+    }
+
+}
---
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/actions/multi/EntityMultileActionConfig.java
@@ -0,0 +1,42 @@
+package ua.com.fielden.platform.web.centre.api.actions.multi;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import ua.com.fielden.platform.web.centre.api.actions.EntityActionConfig;
+
+/**
+ * Multi-action configuration object.
+ *
+ * @author TG Team
+ *
+ */
+public class EntityMultileActionConfig {
+
+    private final Class<? extends IEntityMultiActionSelector> actionSelectorClass;
+    private final List<EntityActionConfig> actions = new ArrayList<>();
+
+    public EntityMultileActionConfig (final Class<? extends IEntityMultiActionSelector> actionSelectorClass, final List<EntityActionConfig> actions) {
+        this.actionSelectorClass = actionSelectorClass;
+        this.actions.addAll(actions);
+    }
+
+    /**
+     * Returns the action selector class.
+     *
+     * @return
+     */
+    public Class<? extends IEntityMultiActionSelector> actionSelectorClass() {
+        return actionSelectorClass;
+    }
+
+    /**
+     * Returns the list of actions from which each action will be selected for specific entity.
+     *
+     * @return
+     */
+    public List<EntityActionConfig> actions() {
+        return Collections.unmodifiableList(actions);
+    }
+}
