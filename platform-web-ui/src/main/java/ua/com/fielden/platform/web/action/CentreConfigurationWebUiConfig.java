0d1132ce6812e4d2b10dec19e0b65da3140cb4f7
jhou-pro <jhou.pro@gmail.com>
2020-12-01 12:39:56 +0200

    #1534 Fixed height of SAVE / Edit master not to show internal shadow.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/action/CentreConfigurationWebUiConfig.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/action/CentreConfigurationWebUiConfig.java
@@ -183,7 +183,7 @@ public class CentreConfigurationWebUiConfig {
             .setLayoutFor(DESKTOP, empty(), layout)
             .setLayoutFor(TABLET, empty(), layout)
             .setLayoutFor(MOBILE, empty(), layout)
-            .withDimensions(mkDim(400, 260))
+            .withDimensions(mkDim(400, 261))
             .done();
     }
     /**
