4ed8ecb3bbbd9d15c78074a8346d48de01e4578e
Nazar Chaykivskyy <nazar@fielden.com.au>
2021-03-23 02:10:33 +0200

    #1332 Enhanced with meaningful exception; corrected hashCode implementation.

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/stage3/operands/EntProp3.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/stage3/operands/EntProp3.java
@@ -3,11 +3,12 @@ package ua.com.fielden.platform.eql.stage3.operands;
 import java.util.Objects;
 
 import ua.com.fielden.platform.entity.query.DbVersion;
+import ua.com.fielden.platform.eql.exceptions.EqlStage3ProcessingException;
 import ua.com.fielden.platform.eql.stage3.sources.IQrySource3;
 
 public class EntProp3 implements ISingleOperand3 {
     public final String name;
-    public final IQrySource3 source;
+    public final IQrySource3 source; // can be null for prop headers
     public final Class<?> type;
     public final Object hibType;
 
@@ -20,9 +21,14 @@ public class EntProp3 implements ISingleOperand3 {
 
     @Override
     public String sql(final DbVersion dbVersion) {
-        if (source.column(name) == null) {
-            System.out.println("CAN'T find column for " + name + " in " +  source);
+        if (source == null) {
+            throw new EqlStage3ProcessingException(String.format("There is no query source (Stage3) associated with this property [name = %s, javaType = %s, hibType = %s].", name, type.getName(), hibType.getClass().getName())); 
         }
+        
+        if (source.column(name) == null) {
+            throw new EqlStage3ProcessingException(String.format("There is no column in the query source (Stage3) associated with this property [name = %s, javaType = %s, hibType = %s].", name, type.getName(), hibType.getClass().getName()));
+        }
+        
         return source.sqlAlias() + "." + source.column(name);
     }
     
@@ -41,7 +47,7 @@ public class EntProp3 implements ISingleOperand3 {
         final int prime = 31;
         int result = 1;
         result = prime * result + name.hashCode();
-        result = prime * result + source.hashCode();
+        result = prime * result + (source == null ? 0 : source.hashCode());
         result = prime * result + hibType.getClass().getName().hashCode();
         result = prime * result + (type == null ? 0 : type.hashCode());
         return result;
