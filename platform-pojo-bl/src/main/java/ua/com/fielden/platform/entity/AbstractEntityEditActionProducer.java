af0460f7db51c9440f2285d3856f4570fe8e2250
jhou-pro <jhou.pro@gmail.com>
2021-04-23 18:33:55 +0300

    #1628 Added support for empty IDs to show 'Theres is nothing to edit.' message for property actions. Simplified producing logic to be based on two easy-to-understand edge-cases and one traversePropPath call.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/AbstractEntityEditActionProducer.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/AbstractEntityEditActionProducer.java
@@ -3,15 +3,14 @@ package ua.com.fielden.platform.entity;
 import static java.util.Optional.ofNullable;
 import static ua.com.fielden.platform.reflection.PropertyTypeDeterminator.determinePropertyType;
 import static ua.com.fielden.platform.reflection.asm.impl.DynamicEntityClassLoader.getOriginalType;
+import static ua.com.fielden.platform.utils.EntityUtils.traversePropPath;
 import static ua.com.fielden.platform.web.centre.WebApiUtils.dslName;
 
-import java.util.Optional;
 import java.util.function.Supplier;
 
 import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
 import ua.com.fielden.platform.types.tuples.T2;
-import ua.com.fielden.platform.utils.EntityUtils;
 import ua.com.fielden.platform.web.centre.CentreContext;
 
 /**
@@ -43,23 +42,22 @@ public class AbstractEntityEditActionProducer<T extends EntityEditAction> extend
                     .orElseGet(() -> {
                         final AbstractEntity<?> currEntity = currentEntity();
                         final Class<? extends AbstractEntity<?>> entityTypeForMaster = editedEntity.getEntityTypeAsClass();
-                        // current entity is the entity to be represented by master
-                        if (currentEntityInstanceOf(entityTypeForMaster) && (chosenPropertyRepresentsThisColumn() || chosenPropertyEmpty())) {
-                            return ofNullable(currEntity.getId()).orElseThrow(NOTHING_TO_EDIT_EXCEPTION_SUPPLIER);
-                        } else if (currentEntityNotEmpty() && chosenPropertyNotEmpty()) {
-                            // there are two possible legitimate cases here:
-                            // 1. either currentEntity().get(chosenProperty()) is of type for Entity Master and all is good, or
-                            // 2. chosenProperty is a sub property of a property of type for Entity MAster, where that "parent" property belongs to the current entity, or
-                            // 3. we have a genuine bug and need to throw an appropriate error
-                            final Optional<? extends AbstractEntity<?>> optClickedEntity = EntityUtils.traversePropPath(currEntity, chosenProperty())
-                                    .filter(t2 -> entityTypeForMaster.isAssignableFrom(determineBaseEntityType(getOriginalType(determinePropertyType(currEntity.getType(), dslName(t2._1)))))) // find only type-compatible values on path
-                                    .findFirst().flatMap(t2 -> t2._2.map(v -> entityTypeForMaster.cast(v)));
-                            return optClickedEntity.orElseThrow(NOTHING_TO_EDIT_EXCEPTION_SUPPLIER).getId();
-                        } else if (currEntity != null && currEntity.getId() != null) { // "last resort" situation where we assume that ID is for the current entity
-                            return currEntity.getId();
-                        } else {
+                        // there are two possible legitimate cases here:
+                        // 1. either currentEntity().get(chosenProperty()) is of type for Entity Master and all is good, or
+                        // 2. chosenProperty is a sub property of a property of type for Entity MAster, where that "parent" property belongs to the current entity, or
+                        // 3. we have a genuine bug and need to throw an appropriate error
+                        if (currEntity == null) {
                             throw NOTHING_TO_EDIT_EXCEPTION_SUPPLIER.get();
+                        } else if (!currentEntityInstanceOf(entityTypeForMaster) && chosenPropertyEmpty()) {
+                            return ofNullable(currEntity.getId()) // "last resort" situation where we assume that ID is for the current entity
+                                    .orElseThrow(NOTHING_TO_EDIT_EXCEPTION_SUPPLIER);
                         }
+                        return traversePropPath(currEntity, chosenProperty())
+                                .filter(t2 -> entityTypeForMaster.isAssignableFrom(determineBaseEntityType(getOriginalType(determinePropertyType(currEntity.getType(), dslName(t2._1)))))) // find only type-compatible values on path
+                                .findFirst()
+                                .flatMap(t2 -> t2._2) // get clicked entity, if any
+                                .map(AbstractEntity::getId)
+                                .orElseThrow(NOTHING_TO_EDIT_EXCEPTION_SUPPLIER);
                     });
             editedEntity.setEntityId(id.toString());
         }
