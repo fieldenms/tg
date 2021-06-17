c4034253ed4b596acc717d586e6552484e1b0158
jhou-pro <jhou.pro@gmail.com>
2021-06-17 20:54:21 +0300

    #1677 7. Fixed the test that check mapping of EntityCentreConfig entity.

--- platform-dao/src/test/java/ua/com/fielden/platform/test/mapping/MappingGenerationTest.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/test/mapping/MappingGenerationTest.java
@@ -28,11 +28,15 @@ public class MappingGenerationTest {
                 + "		<column name=\"_VERSION\" default=\"0\" />\n" + "	</version>\n"
                 + "	<property name=\"configBody\" column=\"BODY\" type=\"org.hibernate.type.BinaryType\" length=\"1073741824\"/>\n"
                 + "	<property name=\"configUuid\" column=\"CONFIGUUID_\" type=\"org.hibernate.type.StringType\"/>\n"
+                + "	<many-to-one name=\"dashboardRefreshFrequency\" class=\"ua.com.fielden.platform.entity.Duration\" column=\"DASHBOARDREFRESHFREQUENCY_\"/>\n"
+                + "	<property name=\"dashboardable\" column=\"DASHBOARDABLE_\" type=\"org.hibernate.type.YesNoType\"/>\n"
+                + "	<property name=\"dashboardableDate\" column=\"DASHBOARDABLEDATE_\" type=\"org.hibernate.type.TimestampType\"/>\n"
                 + "	<property name=\"desc\" column=\"DESC_\" type=\"org.hibernate.type.StringType\"/>\n"
                 + "	<many-to-one name=\"menuItem\" class=\"ua.com.fielden.platform.ui.config.MainMenuItem\" column=\"ID_MAIN_MENU\"/>\n"
                 + "	<many-to-one name=\"owner\" class=\"ua.com.fielden.platform.security.user.User\" column=\"ID_CRAFT\"/>\n"
                 + "	<property name=\"preferred\" column=\"PREFERRED_\" type=\"org.hibernate.type.YesNoType\"/>\n"
                 + "	<property name=\"principal\" column=\"IS_PRINCIPAL\" type=\"org.hibernate.type.YesNoType\"/>\n"
+                + "	<property name=\"runAutomatically\" column=\"RUNAUTOMATICALLY_\" type=\"org.hibernate.type.YesNoType\"/>\n"
                 + "	<property name=\"title\" column=\"TITLE\" type=\"org.hibernate.type.StringType\"/>\n" + "</class>\n\n" + "</hibernate-mapping>";
         assertEquals("Incorrect mapping.", expectedMapping, tgModelMapping);
     }
