7da204e204da01bea7b407a20ac4afb867cfe990
01es <oles.hodych@gmail.com>
2021-11-17 14:07:31 +1100

    #1761 Used the newer library.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/meta/MetaPropertyFull.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/meta/MetaPropertyFull.java
@@ -2,7 +2,7 @@ package ua.com.fielden.platform.entity.meta;
 
 import static java.lang.String.format;
 import static java.util.Optional.ofNullable;
-import static org.apache.commons.lang.StringUtils.isEmpty;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 import static ua.com.fielden.platform.error.Result.failure;
 import static ua.com.fielden.platform.error.Result.successful;
 import static ua.com.fielden.platform.reflection.PropertyTypeDeterminator.isRequiredByDefinition;
