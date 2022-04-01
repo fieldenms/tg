c3e653b22c96b7ec3f12e589006e63595726106e
homedirectory <dalvkylib@gmail.com>
2022-04-01 20:58:51 +0300

    #849 implemented PropertyElement class as a wrapper for VariableElement to represent a property of a TG entity

---
+++ platform-annotation-processor/src/main/java/ua/com/fielden/platform/processors/meta_model/PropertyElement.java
@@ -0,0 +1,43 @@
+package ua.com.fielden.platform.processors.meta_model;
+
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+
+public class PropertyElement {
+    private VariableElement varElement;
+    
+    PropertyElement(VariableElement varElement) {
+        this.varElement = varElement;
+    }
+    
+    public VariableElement toVariableElement() {
+        return varElement;
+    }
+    
+    public String getName() {
+        return varElement.getSimpleName().toString();
+    }
+    
+    public boolean isTypeDeclared() {
+        return varElement.asType().getKind() == TypeKind.DECLARED;
+    }
+    
+    public TypeMirror getType() {
+        return varElement.asType();
+    }
+    
+    public TypeElement getTypeAsTypeElement() throws Exception {
+        if (!isTypeDeclared()) {
+            String message = String.format("Type of property %s is not a declared type (%s)", getName(), getType().toString());
+            throw new Exception(message, new ClassCastException());
+        }
+        return getTypeAsTypeElementOrThrow();
+    }
+
+    public TypeElement getTypeAsTypeElementOrThrow() {
+        return (TypeElement) ((DeclaredType) varElement.asType()).asElement();
+    }
+}
