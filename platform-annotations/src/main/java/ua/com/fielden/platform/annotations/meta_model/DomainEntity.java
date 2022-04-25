956ecf2cffe9acdc98c2dc9d4f008d944c8a3426
homedirectory <dalvkylib@gmail.com>
2022-04-25 14:53:19 +0300

    #849 Changed the @DomainEntity retention policy to CLASS which records the annotation in the .class files. This is necessary for the annotation processor to see the @DomainEntity annotation on platform-level entities, which get included as a dependency, thus are fed as .class files into the annotation processor.

--- platform-annotations/src/main/java/ua/com/fielden/platform/annotations/meta_model/DomainEntity.java
+++ platform-annotations/src/main/java/ua/com/fielden/platform/annotations/meta_model/DomainEntity.java
@@ -1,12 +1,12 @@
 package ua.com.fielden.platform.annotations.meta_model;
 
 import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.SOURCE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Retention(SOURCE)
+@Retention(CLASS)
 @Target(TYPE)
 public @interface DomainEntity {
 
