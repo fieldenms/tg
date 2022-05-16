8ce2c2c3d8496680b67483d8b44a9ab903bde629
homedirectory <dalvkylib@gmail.com>
2022-05-16 15:29:20 +0300

    #849 Implemented a guard against a null argument in EntityMetaModel constructor and modified its toPath() method to return "this" in absence of the dot-notation context. Also implemented test that cover these changes.

---
+++ platform-annotation-processor/platform-annotation-processor-metamodel/src/main/java/ua/com/fielden/platform/processors/metamodel/exceptions/EntityMetaModelException.java
@@ -0,0 +1,9 @@
+package ua.com.fielden.platform.processors.metamodel.exceptions;
+
+public class EntityMetaModelException extends RuntimeException {
+
+    public EntityMetaModelException(String message) {
+        super(message);
+    }
+
+}
--- platform-annotation-processor/platform-annotation-processor-metamodel/src/main/java/ua/com/fielden/platform/processors/metamodel/models/EntityMetaModel.java
+++ platform-annotation-processor/platform-annotation-processor-metamodel/src/main/java/ua/com/fielden/platform/processors/metamodel/models/EntityMetaModel.java
@@ -2,11 +2,14 @@ package ua.com.fielden.platform.processors.metamodel.models;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.processors.metamodel.IConvertableToPath;
+import ua.com.fielden.platform.processors.metamodel.exceptions.EntityMetaModelException;
 
 public abstract class EntityMetaModel implements IConvertableToPath {
-    private String path;
+    private final String path;
     
     public EntityMetaModel(String path) {
+        if (path == null)
+            throw new EntityMetaModelException(String.format("%s constructor received null as an argument.", this.getClass().getSimpleName()));
         this.path = path;
     }
     
@@ -15,14 +18,34 @@ public abstract class EntityMetaModel implements IConvertableToPath {
     }
     
     protected final String joinPath(String propName) {
-        if (this.path.length() > 0) {
-            return this.path + "." + propName;
-        }
+        if (!this.path.isEmpty())
+            return String.format("%s.%s", this.path, propName);
+
         return propName;
     }
     
+    /**
+     * Returns the dot-notated path captured in the context. If this entity meta-model has no surrounding context, then this method returns {@code "this"} string constant.
+     * <p> 
+     * Example:
+     * 
+     * <pre>
+     * public class PersonMetaModel extends EntityMetaModel {
+     *      ...
+     * }
+     * 
+     * PersonMetaModel personBase = new PersonMetaModel();
+     * personBase.toPath(); // "this"
+     * 
+     * PersonMetaModel personWithContext = new PersonMetaModel("owner");
+     * personWithContext.toPath(); // "owner"
+     * </pre>
+     */
     @Override
     public final String toPath() {
+        if (this.path.isEmpty())
+            return "this";
+
         return this.path;
     }
 
@@ -31,5 +54,9 @@ public abstract class EntityMetaModel implements IConvertableToPath {
         return toPath();
     }
 
+    /**
+     * Returns the underlying entity class of this meta-model.
+     * @return
+     */
     public abstract Class<? extends AbstractEntity> getEntityClass();
 }
