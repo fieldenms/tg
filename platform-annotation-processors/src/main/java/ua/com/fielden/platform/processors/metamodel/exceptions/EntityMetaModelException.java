f73ca1a4b47fb4fe99fbdc0d15ea9a7a1037dc90
01es <oles.hodych@gmail.com>
2022-05-26 21:34:39 +1000

    #849 Moved meta-model related classes to the pojo-bl module to remove the need for dependency on 'platform-annotation-processors' by TG-based applications.
    Also minor refactoring to make classes less mutable + javadoc.

--- platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/metamodel/exceptions/EntityMetaModelException.java
+++
@@ -1,9 +0,0 @@
-package ua.com.fielden.platform.processors.metamodel.exceptions;
-
-public class EntityMetaModelException extends RuntimeException {
-
-    public EntityMetaModelException(String message) {
-        super(message);
-    }
-
-}
---
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/processors/metamodel/models/PropertyMetaModel.java
@@ -0,0 +1,30 @@
+package ua.com.fielden.platform.processors.metamodel.models;
+
+import ua.com.fielden.platform.processors.metamodel.IConvertableToPath;
+
+/**
+ * A class representing a meta-model for an entity property, accessible at design time.
+ * At the moment it only provides a way to convert context dependent property reference to a dot-noted string path.
+ * However, it has a potential to be enhanced in future to provide access to more property-related information at both design- and runtime.
+ *
+ * @author TG Team
+ *
+ */
+public class PropertyMetaModel implements IConvertableToPath {
+    private final String path;
+    
+    public PropertyMetaModel(final String path) {
+        this.path = path;
+    }
+    
+    @Override
+    public String toPath() {
+        return this.path;
+    }
+    
+    @Override
+    public String toString() {
+        return toPath();
+    }
+
+}
