5b7ed7f5ba1f30ac087c5375788a2624d4081126
01es <oles.hodych@gmail.com>
2022-06-16 15:10:41 +1000

    #489 Improved javadoc.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/processors/metamodel/models/PropertyMetaModel.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/processors/metamodel/models/PropertyMetaModel.java
@@ -3,7 +3,8 @@ package ua.com.fielden.platform.processors.metamodel.models;
 import ua.com.fielden.platform.processors.metamodel.IConvertableToPath;
 
 /**
- * A class representing a meta-model for an entity property, accessible at design time.
+ * A class representing a meta-model for an entity property with a type, which does not have its own meta-model.
+ * <p>
  * At the moment it only provides a way to convert context dependent property reference to a dot-noted string path.
  * However, it has a potential to be enhanced in future to provide access to more property-related information at both design- and runtime.
  *
