53aaea20deecd501c7d6a06c4f3dab3bebfe0821
homedirectory <dalvkylib@gmail.com>
2022-07-12 14:15:28 +0300

    #1832 Storing java sources generated at runtime of the tests in memory instead of on disk. Implemented InMemoryJavaFileManager and InMemoryJavaFileObjects to support in-memory storage of java sources. Note that it is no longer necessary to have "target/generated-test-sources" in the build path.

--- platform-annotation-processors/src/test/java/ua/com/fielden/platform/processors/metamodel/MetaModelStructureTest.java
+++ platform-annotation-processors/src/test/java/ua/com/fielden/platform/processors/metamodel/MetaModelStructureTest.java
@@ -3,30 +3,45 @@ package ua.com.fielden.platform.processors.metamodel;
 import static java.lang.String.format;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.stream.Collectors.joining;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.URI;
 import java.net.URL;
-import java.nio.file.Paths;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 import javax.annotation.processing.Processor;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
+import javax.tools.FileObject;
 import javax.tools.JavaCompiler;
 import javax.tools.JavaCompiler.CompilationTask;
 import javax.tools.JavaFileManager;
 import javax.tools.JavaFileObject;
+import javax.tools.JavaFileObject.Kind;
+import javax.tools.SimpleJavaFileObject;
 import javax.tools.StandardJavaFileManager;
-import javax.tools.StandardLocation;
 import javax.tools.ToolProvider;
 
 import org.apache.commons.lang3.StringUtils;
@@ -36,14 +51,19 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.io.ByteSource;
 import com.google.testing.compile.CompilationRule;
+import com.google.testing.compile.ForwardingStandardJavaFileManager;
 import com.google.testing.compile.JavaFileObjects;
 
 import ua.com.fielden.platform.processors.metamodel.concepts.MetaModelConcept;
 import ua.com.fielden.platform.processors.metamodel.elements.EntityElement;
 import ua.com.fielden.platform.processors.metamodel.elements.MetaModelElement;
+import ua.com.fielden.platform.processors.metamodel.models.PropertyMetaModel;
+import ua.com.fielden.platform.processors.metamodel.test_entities.TestEntitySinkNodesOnly;
 import ua.com.fielden.platform.processors.metamodel.test_entities.TestEntityWithDescTitle;
 import ua.com.fielden.platform.processors.metamodel.test_entities.TestEntityWithoutDescTitle;
+import ua.com.fielden.platform.processors.metamodel.utils.ElementFinder;
 import ua.com.fielden.platform.processors.metamodel.utils.MetaModelFinder;
 
 
@@ -61,6 +81,7 @@ public class MetaModelStructureTest {
     private final static String TEST_ENTITIES_PKG_NAME = "ua.com.fielden.platform.processors.metamodel.test_entities";
     
     private final static List<Class<?>> testEntities = new ArrayList<>();
+    private final static List<JavaFileObject> generatedSources = new ArrayList<>();
 
     public @Rule CompilationRule rule = new CompilationRule();
     private Elements elements;
@@ -70,6 +91,7 @@ public class MetaModelStructureTest {
     public static void compileTestEntitiesAndGenerateMetaModels() {
         // reset the state
         testEntities.clear();
+        generatedSources.clear();
 
         final List<Class<?>> entityClasses = getTestEntitiesClasses();
         // stop if there are no test entities
@@ -80,11 +102,13 @@ public class MetaModelStructureTest {
         System.out.println("Test entities: " + entityClasses.stream().map(Class::getSimpleName).collect(joining(",")));
 
         // generate meta-models by compiling test entities
-        final JavaFileManager jfm = compileClassesWithProcessors(entityClasses, new MetaModelProcessor());
-        // stop if compilation failed
-        if (jfm == null) {
-            throw new AssertionError("Compilation of test entities failed.");
+        try {
+            generatedSources.addAll(compileClassesWithProcessors(entityClasses, new MetaModelProcessor()));
+        } catch (Exception e) {
+            // stop if compilation failed
+            throw new AssertionError("Compilation of test entities failed.", e);
         }
+        System.out.println("Generated: " + generatedSources.stream().map(jfo -> StringUtils.substringAfterLast(jfo.getName(), "/")).collect(joining(",")));
     }
 
     @Before
@@ -152,42 +176,41 @@ public class MetaModelStructureTest {
      * The generated sources are created in {@code target/generated-test-sources}.
      * @param entityClasses classes to compile
      * @param processors annotation processors to enable during compilation
-     * @return JavaFileManager instance which was used to create generated sources in case of successful compilation, otherwise or if {@code entityClasses} is empty returns null
+     * @return A collection of generated sources or null if compilation failed.
      */
-    private static JavaFileManager compileClassesWithProcessors(final Collection<Class<?>> entityClasses, final Processor... processors) {
+    private static List<JavaFileObject> compileClassesWithProcessors(final Collection<Class<?>> entityClasses, final Processor... processors) throws IOException {
         if (entityClasses.isEmpty()) {
-            return null;
+            return List.of();
         }
 
+        // collect java sources of entity classes in the form of JavaFileObject to compile them
         final List<JavaFileObject> entityJFOs = entityClasses.stream()
                 .map(clazz -> JavaFileObjects.forResource(String.format("%s.java", StringUtils.replaceChars(clazz.getCanonicalName(), '.', '/'))))
                 .toList();
 
+        /*
+         * A manual approach to compilation using javax.tools.* API is employed instead of using com.google.testing.compile.Compiler.
+         * This is because com.google.testing.compile.InMemoryJavaFileManager class is final and provides only package-private access to getGeneratedSources().
+         * The chosen approach allows us to both store generated sources in memory and retrieve them from the file manager for further analysis.
+         */
         final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        final StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, Locale.getDefault(), UTF_8);
-        try {
-            // set specific output location for generated sources
-            // don't forget to add this location to the build path of the project
-            final String sourceOutputPath = Paths.get("target", "generated-test-sources").toAbsolutePath().toString();
-            fileManager.setLocation(StandardLocation.SOURCE_OUTPUT, List.of(new File(sourceOutputPath)));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+        final InMemoryJavaFileManager fileManager = new InMemoryJavaFileManager(compiler.getStandardFileManager(null, Locale.getDefault(), UTF_8));
+
         final CompilationTask task = compiler
                 .getTask(
                         null, // Writer for additional output from the compiler (null => System.err)
                         fileManager,
                         null, // diagnostic listener
                         null, // options
-                        ImmutableSet.<String>of(), // names of classes to be processed by annotation processing
+                        ImmutableSet.<String>of(), // names of classes to be processed by annotation processing (?)
                         entityJFOs);
+        // explicitly set annotation processors
         task.setProcessors(List.of(processors));
         boolean succeeded = task.call();
         if (!succeeded) {
             return null;
         }
-        return fileManager;
+        return fileManager.getGeneratedSources();
     }
 
     private Optional<TypeElement> getMetaModelForEntity(final Class<?> entityClass) {
@@ -195,6 +218,7 @@ public class MetaModelStructureTest {
         final MetaModelConcept mmc = new MetaModelConcept(entityElement);
         return Optional.ofNullable(elements.getTypeElement(mmc.getQualifiedName()));
     }
+
 
     /**
      * Optionally finds a class by its {@code fullyQualifiedName}.
@@ -207,6 +231,133 @@ public class MetaModelStructureTest {
         } catch (ClassNotFoundException e) {
             System.out.println(String.format("Couldn't load class %s.", fullyQualifiedName));
             return Optional.empty();
+        }
+    }
+
+    /**
+     * Implementation of {@link JavaFileManager} that stores generated java sources in memory and provides access to retrieve them.
+     * <p>
+     * Note: this file manager provides access to java sources exclusively (i.e. {@link JavaFileObject} instances with {@code kind ==} {@link Kind.SOURCE})
+     * 
+     * @author TG Team
+     *
+     */
+    private static final class InMemoryJavaFileManager extends ForwardingStandardJavaFileManager {
+        private final Map<URI, JavaFileObject> generatedJavaSources = new HashMap<>();
+
+        private static URI uriForJavaFileObject(final Location location, final String className, final Kind kind) {
+            return URI.create(format("memory:///%s/%s", location.getName(), className.replace('.', '/') + kind.extension));
+        }
+
+        InMemoryJavaFileManager(StandardJavaFileManager fileManager) {
+            super(fileManager);
+        }
+
+        @Override
+        public JavaFileObject getJavaFileForOutput(Location location, String className, final Kind kind, FileObject sibling) throws IOException {
+            final URI uri = uriForJavaFileObject(location, className, kind);
+            final InMemoryJavaFileObject jfo = new InMemoryJavaFileObject(uri, kind);
+            if (kind == Kind.SOURCE) {
+                generatedJavaSources.put(uri, jfo);
+            }
+            return jfo;
+        }
+        
+        @Override
+        public JavaFileObject getJavaFileForInput(Location location, String className, Kind kind) throws IOException {
+            if (location.isOutputLocation() && kind == Kind.SOURCE) {
+                return generatedJavaSources.get(uriForJavaFileObject(location, className, kind));
+            }
+            return super.getJavaFileForInput(location, className, kind);
+        }
+
+        public List<JavaFileObject> getGeneratedSources() {
+            return List.copyOf(generatedJavaSources.values());
+        }
+
+        /**
+         * Implementation of {@link JavaFileObject} that is stored in memory.
+         * <p>
+         * Based on {@link com.google.testing.compile.InMemoryJavaFileManager.InMemoryJavaFileObject}.
+         * 
+         * @author TG Team
+         *
+         */
+        private static final class InMemoryJavaFileObject extends SimpleJavaFileObject {
+            private long lastModified = 0L;
+            private Optional<ByteSource> data = Optional.empty();
+
+            InMemoryJavaFileObject(final URI uri, final Kind kind) {
+                super(uri, kind);
+            }
+
+            @Override
+            public InputStream openInputStream() throws IOException {
+                if (data.isPresent()) {
+                    return data.get().openStream();
+                } else {
+                    throw new FileNotFoundException();
+                }
+            }
+
+            @Override
+            public OutputStream openOutputStream() throws IOException {
+                return new ByteArrayOutputStream() {
+                    @Override
+                    public void close() throws IOException {
+                        super.close();
+                        data = Optional.of(ByteSource.wrap(toByteArray()));
+                        lastModified = System.currentTimeMillis();
+                    }
+                };
+            }
+
+            @Override
+            public Reader openReader(boolean ignoreEncodingErrors) throws IOException {
+                if (data.isPresent()) {
+                    return data.get().asCharSource(Charset.defaultCharset()).openStream();
+                } else {
+                    throw new FileNotFoundException();
+                }
+            }
+
+            @Override
+            public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
+                if (data.isPresent()) {
+                    return data.get().asCharSource(Charset.defaultCharset()).read();
+                } else {
+                    throw new FileNotFoundException();
+                }
+            }
+
+            @Override
+            public Writer openWriter() throws IOException {
+                return new StringWriter() {
+                    @Override
+                    public void close() throws IOException {
+                        super.close();
+                        data = Optional.of(ByteSource.wrap(toString().getBytes(Charset.defaultCharset())));
+                        lastModified = System.currentTimeMillis();
+                    }
+                };
+            }
+
+            @Override
+            public long getLastModified() {
+                return lastModified;
+            }
+
+            @Override
+            public boolean delete() {
+                this.data = Optional.empty();
+                this.lastModified = 0L;
+                return true;
+            }
+
+            @Override
+            public String toString() {
+                return format("%s{uri=%s, kind=%s}", this.getClass().getSimpleName(), toUri(), kind);
+            }
         }
     }
 }
