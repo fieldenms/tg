49ada5ac5c34dfea0a769618739b3b37e80abe3e
01es <oles.hodych@gmail.com>
2022-07-18 13:24:02 +1000

    #1032 Refactored and extended tests for EntityBatchUnsertOperation.

--- platform-dao/src/test/java/ua/com/fielden/platform/entity/EntityBatchInsertOperationTest.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/entity/EntityBatchInsertOperationTest.java
@@ -3,6 +3,7 @@ package ua.com.fielden.platform.entity;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.fetchAll;
 import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.from;
 import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.select;
@@ -11,6 +12,7 @@ import static ua.com.fielden.platform.types.Colour.BLACK;
 import java.math.BigDecimal;
 import java.util.Currency;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
@@ -37,41 +39,72 @@ public class EntityBatchInsertOperationTest extends AbstractDaoTestCase {
     
     @Test
     public void batch_insert_operation_works_for_single_enity() {
-        testBatchInsertOfEntities(createEntitiesForBatchInsert("Ent1"));
+        final List<TgEntityWithManyPropTypes> entities = createEntitiesForBatchInsert("Ent1");
+        assertEquals(1, batchInserEntities(entities, 2));
+        assertEqualityForInsertedEntities(entities);
     }
     
     @Test
     public void batch_insert_operation_works_for_single_full_batch() {
-        testBatchInsertOfEntities(createEntitiesForBatchInsert("Ent1", "Ent2"));
+        final List<TgEntityWithManyPropTypes> entities = createEntitiesForBatchInsert("Ent1", "Ent2");
+        assertEquals(2, batchInserEntities(entities, 2));
+        assertEqualityForInsertedEntities(entities);
     }
 
     @Test
     public void batch_insert_operation_works_for_two_full_batches() {
-        testBatchInsertOfEntities(createEntitiesForBatchInsert("Ent1", "Ent2", "Ent3", "Ent4"));
+        final List<TgEntityWithManyPropTypes> entities = createEntitiesForBatchInsert("Ent1", "Ent2", "Ent3", "Ent4");
+        assertEquals(4, batchInserEntities(entities, 2));
+        assertEqualityForInsertedEntities(entities);
     }
 
     @Test
     public void batch_insert_operation_works_for_two_full_batches_with_last_batch_not_full() {
-        testBatchInsertOfEntities(createEntitiesForBatchInsert("Ent1", "Ent2", "Ent3", "Ent4", "Ent5"));
+        final List<TgEntityWithManyPropTypes> entities = createEntitiesForBatchInsert("Ent1", "Ent2", "Ent3", "Ent4", "Ent5");
+        assertEquals(5, batchInserEntities(entities, 2));
+        assertEqualityForInsertedEntities(entities);
     }
     
-    @Test(expected = EntityAlreadyExists.class)
+    @Test
     public void batch_insert_operation_fails_while_trying_to_insert_persisted_entities() {
-        testBatchInsertOfEntities(createEntitiesForBatchInsert("Ent1", "Ent2", "Ent3"));
-        testBatchInsertOfEntities(getInstance(TgEntityWithManyPropTypesCo.class).getAllEntities(from(select(TgEntityWithManyPropTypes.class).model()).with(fetchAll(TgEntityWithManyPropTypes.class)).model()));
+        batchInserEntities(createEntitiesForBatchInsert("Ent1", "Ent2", "Ent3"), 2);
+        try {
+            batchInserEntities(getInstance(TgEntityWithManyPropTypesCo.class).getAllEntities(from(select(TgEntityWithManyPropTypes.class).model()).with(fetchAll(TgEntityWithManyPropTypes.class)).model()), 2);
+            fail("Expected an exception due to an attempt to insert already persisten entity.");
+        } catch (final EntityAlreadyExists ex) {
+        }
+    }
+    
+    @Test
+    public void batch_insert_a_stream_ignores_persistent_entities() {
+        final List<TgEntityWithManyPropTypes> fistBatch = createEntitiesForBatchInsert("Ent1", "Ent2", "Ent3");
+        batchInserEntitiesAsStream(fistBatch.stream(), 2);
+        final List<TgEntityWithManyPropTypes> persistedFirstBatch = getInstance(TgEntityWithManyPropTypesCo.class).getAllEntities(from(select(TgEntityWithManyPropTypes.class).model()).with(fetchAll(TgEntityWithManyPropTypes.class)).model());
+        final List<TgEntityWithManyPropTypes> secondBatch = createEntitiesForBatchInsert("Ent4", "Ent5");
+        final List<TgEntityWithManyPropTypes> firstAndSecondBatches = Stream.concat(persistedFirstBatch.stream(), secondBatch.stream()).collect(toList());
+        assertEquals(2, batchInserEntitiesAsStream(firstAndSecondBatches.stream(), 2));
+        assertEqualityForInsertedEntities(firstAndSecondBatches);
     }
 
-    private void testBatchInsertOfEntities(final List<TgEntityWithManyPropTypes> entities) {
+    private int batchInserEntities(final List<TgEntityWithManyPropTypes> entities, final int batchSize) {
         final EntityBatchInsertOperation insertOp = new EntityBatchInsertOperation(getInstance(DomainMetadata.class), () -> getInstance(TransactionalExecution.class));
-        assertEquals(entities.size(), insertOp.batchInsert(entities, 2));
+        return insertOp.batchInsert(entities, batchSize);
+    }
+
+    private int batchInserEntitiesAsStream(final Stream<TgEntityWithManyPropTypes> entities, final int batchSize) {
+        final EntityBatchInsertOperation insertOp = new EntityBatchInsertOperation(getInstance(DomainMetadata.class), () -> getInstance(TransactionalExecution.class));
+        return insertOp.batchInsert(entities, batchSize);
+    }
+
+    private void assertEqualityForInsertedEntities(final List<TgEntityWithManyPropTypes> entities) {
         final List<TgEntityWithManyPropTypes> savedEntities = getInstance(TgEntityWithManyPropTypesCo.class).getAllEntities(from(select(TgEntityWithManyPropTypes.class).model()).with(fetchAll(TgEntityWithManyPropTypes.class)).model());
         assertEquals(entities, savedEntities);
     }
-    
+
     private List<TgEntityWithManyPropTypes> createEntitiesForBatchInsert(final String ...entitiesKeys) {
         return asList(entitiesKeys).stream().map(e -> createEntityForBatchInsert(e)).collect(toList());
     }
-    
+
     private TgEntityWithManyPropTypes createEntityForBatchInsert(final String entityKey) {
         final UnionEntity unionEntityValue = co$(UnionEntity.class).new_();
         unionEntityValue.setPropertyTwo(getInstance(IEntityTwo.class).findByKey(ENTITY_TWO_KEY));
