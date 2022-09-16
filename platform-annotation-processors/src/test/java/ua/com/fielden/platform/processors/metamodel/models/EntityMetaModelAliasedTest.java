40b50662ea4ae25bb99044cf5bf43fb01236a409
01es <oles.hodych@gmail.com>
2022-09-16 14:18:45 +1000

    #1855 Adjusted equality for EntityMetaModel to include the actual type. Disabled caching for aliased meta-model instance. Adjusted unit tests.

--- platform-annotation-processors/src/test/java/ua/com/fielden/platform/processors/metamodel/models/EntityMetaModelAliasedTest.java
+++ platform-annotation-processors/src/test/java/ua/com/fielden/platform/processors/metamodel/models/EntityMetaModelAliasedTest.java
@@ -1,8 +1,9 @@
 package ua.com.fielden.platform.processors.metamodel.models;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -21,7 +22,7 @@ import ua.com.fielden.platform.processors.test_entities.meta.ExampleEntityMetaMo
  *
  */
 public class EntityMetaModelAliasedTest {
-    
+
     /**
      * Normally a meta-model node can be directly converted to a path that reads "this". 
      * However, if a meta-model is aliased it should convert to its alias if the target of conversion is the immediate meta-model node.
@@ -37,7 +38,7 @@ public class EntityMetaModelAliasedTest {
         assertEquals("this", MetaModels.ExampleEntity_.toPath());
         assertEquals("this", MetaModels.ExampleEntity_.toString());
     }
-    
+
     @Test
     public void traversing_aliased_meta_model_yields_path_starting_with_alias() {
         var alias = "a";
@@ -48,21 +49,29 @@ public class EntityMetaModelAliasedTest {
         final EntityWithEntityTypedAndOrdinaryPropsMetaModel metaModel = MetaModels.EntityWithEntityTypedAndOrdinaryProps_;
         assertEquals("entity2.prop1", metaModel.entity2().prop1().toPath());
     }
-    
+
     @Test
     public void blank_aliases_are_not_permitted() {
         for (final String alias:  new String[]{"", " ", "  "} ) {
             assertThrows(EntityMetaModelAliasedException.class, () -> MetaModels.ExampleEntity_(alias) /* should an throw exception */);
         }
     }
-    
+
     @Test
-    public void same_type_meta_models_with_equal_aliases_are_cached() {
+    public void same_type_meta_models_with_equal_aliases_are_not_cached_at_the_moment() {
         final ExampleEntityMetaModelAliased exampleAliased1 = MetaModels.ExampleEntity_("e");
         final ExampleEntityMetaModelAliased exampleAliased2 = MetaModels.ExampleEntity_("e");
-        assertTrue(exampleAliased1 == exampleAliased2);
+        assertNotSame(exampleAliased1, exampleAliased2);
+        assertEquals(exampleAliased1, exampleAliased2);
         
         final ExampleEntityMetaModelAliased exampleAliased3 = MetaModels.ExampleEntity_("ex");
-        assertTrue(exampleAliased3 != exampleAliased1);
+        assertNotSame(exampleAliased3, exampleAliased1);
+        assertNotEquals(exampleAliased3, exampleAliased1);
     }
+
+    @Test
+    public void meta_models_for_different_entities_with_the_same_alias_are_not_equal() {
+        assertNotEquals(MetaModels.ExampleEntity_("m"), MetaModels.SubEntity_("m"));
+    }
+
 }
