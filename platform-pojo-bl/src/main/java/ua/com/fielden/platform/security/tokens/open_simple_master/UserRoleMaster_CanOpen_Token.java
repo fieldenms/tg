8d97d253cba22a33603593adf4a736dbe0a3e1ba
01es <oles.hodych@gmail.com>
2022-09-30 10:57:55 +1000

    #1860 Corrected the token title.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/tokens/open_simple_master/UserRoleMaster_CanOpen_Token.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/tokens/open_simple_master/UserRoleMaster_CanOpen_Token.java
@@ -10,6 +10,6 @@ import ua.com.fielden.platform.security.user.UserRole;
  * @author TG Team
  */
 public class UserRoleMaster_CanOpen_Token implements ISecurityToken {
-    public final static String TITLE = String.format(Template.MASTER_OPEN.forTitle(), UserRole.ENTITY_TITLE);
+    public final static String TITLE = String.format(Template.MASTER_OPEN.forTitle(), UserRole.ENTITY_TITLE + " Master");
     public final static String DESC = String.format(Template.MASTER_OPEN.forDesc(), UserRole.ENTITY_TITLE);
 }
