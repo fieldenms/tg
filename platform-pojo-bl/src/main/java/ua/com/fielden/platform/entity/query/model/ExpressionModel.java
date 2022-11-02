572e98240ebce38f7e86e26999d1d8a07e9ed086
Nazar Chaykivskyy <nazar@fielden.com.au>
2022-11-02 22:24:11 +0200

    #1332 Improved the 'ExpressionModel.containsAggregations()' function.
    Added detection of aggregate functions embedded into another expression models at any level of nesting. Covered by unit tests.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/query/model/ExpressionModel.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/query/model/ExpressionModel.java
@@ -2,6 +2,7 @@ package ua.com.fielden.platform.entity.query.model;
 
 import static ua.com.fielden.platform.entity.query.fluent.enums.Functions.COUNT_ALL;
 import static ua.com.fielden.platform.entity.query.fluent.enums.TokenCategory.COLLECTIONAL_FUNCTION;
+import static ua.com.fielden.platform.entity.query.fluent.enums.TokenCategory.EXPR_TOKENS;
 
 import java.util.List;
 
@@ -27,7 +28,14 @@ public class ExpressionModel extends AbstractModel {
         return getTokens().size() == 1 && getTokens().get(0).getKey() == TokenCategory.VAL;
     }
     
+    /**
+     * Returns true if any of the aggregate functions ({@code maxOf()}, {@code minOf()}, {@code sumOf()}, {@code countOf()}, {@code avgOf()}, {@code countAll()}, {@code sumOfDistinct()}, {@code countOfDistinct()}, {@code avgOfDistinct()}) is explicitly or implicitly present in the given expression.
+     * 
+     * By implicit presence here it is meant that it may be part of another expression model included into this one at any level of nesting.
+     * 
+     * @return
+     */
     public boolean containsAggregations() {
-        return getTokens().stream().anyMatch(t -> t.getKey() == COLLECTIONAL_FUNCTION || t.getValue() == COUNT_ALL);
+        return getTokens().stream().anyMatch(t -> t.getKey() == COLLECTIONAL_FUNCTION || t.getValue() == COUNT_ALL || t.getKey() == EXPR_TOKENS && ((ExpressionModel) t.getValue()).containsAggregations());
     }
 }
