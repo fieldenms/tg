35765305b18163a26dca8aa08767d76022f1adf0
01es <oles.hodych@gmail.com>
2022-11-23 16:46:19 +1100

    #1692 Improved concurrency of DynamicEntityClassLoader.
    The updated concurrent cache of class loader instances has weak keys.Aslo introduced method 'clearCaches', which is required for the cleanup service used in most production systems.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/reflection/asm/impl/DynamicEntityClassLoader.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/reflection/asm/impl/DynamicEntityClassLoader.java
@@ -1,24 +1,29 @@
 package ua.com.fielden.platform.reflection.asm.impl;
 
+import static org.apache.logging.log4j.LogManager.getLogger;
 import static ua.com.fielden.platform.reflection.asm.impl.TypeMaker.GET_ORIG_TYPE_METHOD_NAME;
+import static ua.com.fielden.platform.types.tuples.T2.t2;
 import static ua.com.fielden.platform.utils.Pair.pair;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.Logger;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 
 import net.bytebuddy.dynamic.loading.InjectionClassLoader;
-import ua.com.fielden.platform.classloader.TgSystemClassLoader;
 import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.reflection.asm.exceptions.DynamicEntityClassLoaderException;
+import ua.com.fielden.platform.types.tuples.T2;
 import ua.com.fielden.platform.utils.Pair;
 
 /**
@@ -27,35 +32,42 @@ import ua.com.fielden.platform.utils.Pair;
  * All created types should be loaded by the same instance as used for type modification.
  * The role of {@link InjectionClassLoader} as a parent type is to allow this class loader to be used by ByteBuddy for injecting new types.
  * <p>
- * <b><i>Note:</i></b> This class is NOT thread safe!!! Nor should it be!
- * <p>
- * <b>2022-08-23</b> {@link TgSystemClassLoader} is no longer used as a parent class loader. Caching is implemented directly by this class.
+ * <b>2022-08-23</b> {@code TgSystemClassLoader} is no longer used as a parent class loader. Caching is implemented directly by this class.
+ * <b>2022-11-23</b> {@code DynamicEntityClassLoader} is thread safe.
  *
  * @author TG Team
  */
 public class DynamicEntityClassLoader extends InjectionClassLoader {
+
+    private static final String ERR_COULD_NOT_CREAT_CLASS_LOADER = "Could not create a new %s".formatted(DynamicEntityClassLoader.class.getSimpleName());    
+    private static final Logger LOGGER = getLogger(DynamicEntityClassLoader.class);
     /**
-     * Permanent cache for instances of this type of the form: {@code parentClassLoader -> thisInstance}.
+     * A cache of instances of this type of the form: {@code parentClassLoader -> thisInstance}.
      */
-    private static final Map<ClassLoader, DynamicEntityClassLoader> instances = new HashMap<>();
+    private static final Cache<ClassLoader, DynamicEntityClassLoader> instances = CacheBuilder.newBuilder().weakKeys().initialCapacity(100).concurrencyLevel(50).build();
 
     private final Cache<Class<?>, byte[]> cache = CacheBuilder.newBuilder().weakKeys().initialCapacity(1000).concurrencyLevel(50).build();
 
     /**
      * Returns an instance of this class loader type with the specified parent class loader. 
-     * The returned instance might be retrieved from cache, which uses {@code parent} as a lookup key. 
+     * The returned instance might be retrieved from cache, which uses {@code parent} as a lookup key.
+     *
      * @param parent
      * @return
      */
     public static DynamicEntityClassLoader getInstance(final ClassLoader parent) {
-        final var newInstance = new DynamicEntityClassLoader(parent);
-        final var current = instances.putIfAbsent(parent, newInstance);
-        return current == null ? newInstance : current;
+        try {
+            return instances.get(parent, () -> new DynamicEntityClassLoader(parent));
+        } catch (final ExecutionException ex) {
+            LOGGER.error(ERR_COULD_NOT_CREAT_CLASS_LOADER, ex);
+            throw new DynamicEntityClassLoaderException(ERR_COULD_NOT_CREAT_CLASS_LOADER, ex);
+        }
     }
     
     /**
-     * Forcefully creates and returns a new insatnce of this class loader type with the specified parent class loader regardless of any cached instances.
+     * Forcefully creates and returns a new instance of this class loader type with the specified parent class loader regardless of any cached instances.
      * Primarily used in testing environments.
+     *
      * @param parent
      * @return
      */
@@ -66,7 +78,20 @@ public class DynamicEntityClassLoader extends InjectionClassLoader {
     private DynamicEntityClassLoader(final ClassLoader parent) {
         super(parent, /*sealed*/ false);
     }
-    
+
+    /**
+     * Clears class caches for every registered dynamic class loader.
+     *
+     * @return a tuple of sizes – the first one for the cache of dynamic class loaders, the second one is a total number of generated classes across all dynamic class loaders.
+     */
+    public static T2<Long, Long> clearCaches() {
+        final AtomicLong count = new AtomicLong(0L);
+        instances.asMap().values().forEach(decl -> {decl.clearCache(); count.addAndGet(decl.cache.size());});
+        instances.cleanUp();
+        instances.invalidateAll();
+        return t2(instances.size(), count.get());
+    }
+
     /**
      * Clears class cache.
      */
@@ -77,10 +102,11 @@ public class DynamicEntityClassLoader extends InjectionClassLoader {
 
     /**
      * Tries to retrieve a type with <code>typeName</code> from cache.
-     * @param typeName - fully-qualified name in binary form (i.e. the result of {@link Class#getName()}.
+     *
+     * @param typeName - fully-qualified name in binary form (i.e., the result of {@link Class#getName()}.
      * @return
      */
-    public Optional<Pair<Class<?>, byte[]>> lookupCache(final String typeName) {
+    private Optional<Pair<Class<?>, byte[]>> lookupCache(final String typeName) {
         return cache.asMap().entrySet().stream()
                 .filter(entry -> entry.getKey().getName().equals(typeName))
                 .findFirst()
@@ -90,6 +116,7 @@ public class DynamicEntityClassLoader extends InjectionClassLoader {
     /**
      * Creates a new cache entry that maps {@link Class} to its byte representation.
      * If an entry for the given {@link Class} already exists, it will be overwritten.
+     *
      * @param typePair
      * @return
      */
@@ -100,6 +127,7 @@ public class DynamicEntityClassLoader extends InjectionClassLoader {
 
     /**
      * Returns an array of bytes for a cached {@link Class} with binary name equal to <code>name</code>.
+     *
      * @param name binary name of a class
      * @return byte array representing a cached class or <code>null</code> if the class wasn't found
      */
@@ -183,10 +211,12 @@ public class DynamicEntityClassLoader extends InjectionClassLoader {
 
     /**
      * Converts a resource name of a java class to its binary form.
+     *
      * @param resourceName
      * @return binary name of a class or null if <code>resourceName</code> doesn't represent a java class.
      */
     private static String toBinaryName(final String resourceName) {
         return resourceName.endsWith(".class") ? StringUtils.substringBeforeLast(resourceName, ".class").replace('/', '.') : null;
     }
+
 }
