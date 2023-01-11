1625f7473a888f30694496d5de38348a7bc57823
homedirectory <nullsink@posteo.eu>
2023-01-11 10:29:14 +0200

    #1910 Using java sources generated with javapoet for tests instead of local classes.
    This improves individiual tests isolation.

--- platform-annotation-processors/src/test/java/ua/com/fielden/platform/processors/metamodel/utils/ElementFinderTest.java
+++ platform-annotation-processors/src/test/java/ua/com/fielden/platform/processors/metamodel/utils/ElementFinderTest.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static ua.com.fielden.platform.processors.test_utils.Compilation.OPTION_PROC_ONLY;
+import static ua.com.fielden.platform.reflection.asm.impl.DynamicTypeNamingService.nextTypeName;
 import static ua.com.fielden.platform.utils.CollectionUtil.isEqualContents;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -18,13 +21,24 @@ import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.ToolProvider;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import com.google.testing.compile.CompilationRule;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
 
+import ua.com.fielden.platform.processors.test_utils.Compilation;
+import ua.com.fielden.platform.processors.test_utils.InMemoryJavaFileManager;
+import ua.com.fielden.platform.reflection.asm.impl.DynamicTypeNamingService;
 import ua.com.fielden.platform.utils.CollectionUtil;
 
 /**
@@ -100,6 +114,74 @@ public class ElementFinderTest {
         final Predicate<VariableElement> isFinal = (f -> f.getModifiers().contains(Modifier.FINAL));
         assertEqualContents(List.of("STRING", "i"),
                 streamSimpleNames(finder.findDeclaredFields(finder.getTypeElement(Sub.class), isFinal)).toList());
+    }
+
+    @Test
+    public void findInheritedFields_returns_all_inherited_fields() {
+        final TypeSpec sup2 = TypeSpec.classBuilder(nextTypeName("Sup2"))
+                // protected String s
+                .addField(String.class, "s", Modifier.PROTECTED)
+                // private Integer i
+                .addField(Integer.class, "i", Modifier.PRIVATE)
+                // double d
+                .addField(double.class, "d")
+                .build();
+        final TypeSpec sup1 = TypeSpec.classBuilder(nextTypeName("Sup1"))
+                .superclass(ClassName.get("", sup2.name))
+                // public String s
+                .addField(String.class, "s", Modifier.PUBLIC)
+                // private Integer i
+                .addField(Integer.class, "i", Modifier.PRIVATE)
+                // boolean b
+                .addField(boolean.class, "b")
+                .build();
+        final TypeSpec sub = TypeSpec.classBuilder(nextTypeName("Sub"))
+                .superclass(ClassName.get("", sup1.name))
+                // these should not be included
+                .addField(String.class, "s")
+                .addField(String.class, "name")
+                .build();
+
+        processAndEvaluate(List.of(sub, sup1, sup2), finder -> {
+            assertEqualContents(
+                    Stream.concat(sup1.fieldSpecs.stream(), sup2.fieldSpecs.stream()).toList(),
+                    finder.findInheritedFields(finder.elements.getTypeElement(sub.name)).stream().map(f -> toFieldSpec(f)).toList());
+        });
+    }
+
+    /**
+     * A convenient method to convert a {@link VariableElement} to a {@link FieldSpec} for further comparison.
+     * Ignores annotations.
+     */
+    private FieldSpec toFieldSpec(final VariableElement el) {
+        return FieldSpec.builder(TypeName.get(el.asType()), el.getSimpleName().toString(), el.getModifiers().toArray(Modifier[]::new))
+                .build();
+    }
+
+    /**
+     * Simulates annotation processing of sources represented by {@code typeSpecs} and provides an instance of {@link ElementFinder}
+     * to {@code consumer}, returning after it's done.
+     * <p>
+     * {@code typeSpecs} are assumed to reside in an unnamed package.
+     *
+     * @throws RuntimeException if an exception was thrown during annotation processing
+     */
+    private void processAndEvaluate(final Collection<TypeSpec> typeSpecs, final Consumer<ElementFinder> consumer) {
+        final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        final InMemoryJavaFileManager fileManager = new InMemoryJavaFileManager(compiler.getStandardFileManager(null, null, null));
+        final List<JavaFileObject> compilationTargets = typeSpecs.stream()
+                .map(ts -> JavaFile.builder(/*packageName*/ "", ts).build().toJavaFileObject())
+                .toList();
+
+        try {
+            new Compilation(compilationTargets)
+                    .setCompiler(compiler)
+                    .setFileManager(fileManager)
+                    .setOptions(OPTION_PROC_ONLY)
+                    .compileAndEvaluate(procEnv -> consumer.accept(new ElementFinder(procEnv.getElementUtils(), procEnv.getTypeUtils())));
+        } catch (final Throwable t) {
+            throw new RuntimeException(t);
+        }
     }
 
     private static interface ISuper {}
