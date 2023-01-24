b5f3bc3a6d9ec28897c2e157b9a213e2f8e7d41a
homedirectory <nullsink@posteo.eu>
2023-01-24 14:24:53 +0200

    #1910 Created a test class for MetaModelFinder utilities

---
+++ platform-annotation-processors/src/test/java/ua/com/fielden/platform/processors/metamodel/utils/MetaModelFinderTest.java
@@ -0,0 +1,47 @@
+package ua.com.fielden.platform.processors.metamodel.utils;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import ua.com.fielden.platform.processors.metamodel.MetaModelProcessor;
+import ua.com.fielden.platform.processors.metamodel.elements.EntityElement;
+import ua.com.fielden.platform.processors.test_utils.ProcessingRule;
+
+/**
+ * A test case for utilities provided by {@link MetaModelFinder}.
+ * <p>
+ * Meta-models for test entities have to be generated prior to running these tests in order for them to be recognised by the compiler.
+ * One implication of this approach is that any modifications to the processing logic should be repackaged followed by the recompilation of 
+ * test entities to regenerate their meta-models.
+ *
+ * @author TG Team
+ */
+public class MetaModelFinderTest {
+
+    @ClassRule
+    public static ProcessingRule rule = new ProcessingRule(List.of(), new MetaModelProcessor());
+    private static ElementFinder finder;
+    private static EntityFinder entityFinder;
+    private static MetaModelFinder metaModelFinder;
+    
+    @BeforeClass
+    public static void setupOnce() {
+        final Elements elements = rule.getElements();
+        final Types types = rule.getTypes();
+        finder = new ElementFinder(elements, types);
+        entityFinder = new EntityFinder(elements, types);
+        metaModelFinder = new MetaModelFinder(elements, types);
+    }
+
+}
