abbcdbf195b2538dae4a8cb362d71e7afd74476c
homedirectory <nullsink@posteo.eu>
2023-01-26 12:39:11 +0200

    #1910 Test for findDeclaredPropertyMethods()

--- platform-annotation-processors/src/test/java/ua/com/fielden/platform/processors/metamodel/utils/MetaModelFinderTest.java
+++ platform-annotation-processors/src/test/java/ua/com/fielden/platform/processors/metamodel/utils/MetaModelFinderTest.java
@@ -1,9 +1,11 @@
 package ua.com.fielden.platform.processors.metamodel.utils;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.BiConsumer;
@@ -21,16 +23,16 @@ import ua.com.fielden.platform.processors.metamodel.elements.EntityElement;
 import ua.com.fielden.platform.processors.metamodel.elements.MetaModelElement;
 import ua.com.fielden.platform.processors.metamodel.models.EntityMetaModel;
 import ua.com.fielden.platform.processors.test_entities.EntityWithDescTitleWithoutMetaModel;
-import ua.com.fielden.platform.processors.test_entities.EntityWithOrdinaryProps;
 import ua.com.fielden.platform.processors.test_entities.MetamodeledEntity;
 import ua.com.fielden.platform.processors.test_entities.meta.EntityWithEntityTypedAndOrdinaryPropsMetaModel;
-import ua.com.fielden.platform.processors.test_entities.meta.EntityWithOrdinaryPropsMetaModel;
+import ua.com.fielden.platform.processors.test_entities.meta.EntityWithEntityTypedAndOrdinaryPropsMetaModelAliased;
 import ua.com.fielden.platform.processors.test_entities.meta.MetamodeledEntityMetaModel;
 import ua.com.fielden.platform.processors.test_entities.meta.MetamodeledEntityMetaModelAliased;
 import ua.com.fielden.platform.processors.test_entities.meta.SubEntityMetaModel;
 import ua.com.fielden.platform.processors.test_entities.meta.SubEntityMetaModelAliased;
 import ua.com.fielden.platform.processors.test_entities.meta.SuperEntityMetaModel;
 import ua.com.fielden.platform.processors.test_utils.ProcessingRule;
+import ua.com.fielden.platform.utils.CollectionUtil;
 
 /**
  * A test case for utilities provided by {@link MetaModelFinder}.
@@ -98,6 +100,20 @@ public class MetaModelFinderTest {
     }
 
     @Test
+    public void findDeclaredPropertyMethods_finds_only_declared_methods_that_model_entity_properties() {
+        final BiConsumer<Collection<String>, Class<? extends EntityMetaModel>> assertor = 
+                (expectedProperties, metaModel) -> {
+                    final MetaModelElement mme = metaModelFinder.findMetaModel(metaModel);
+                    assertEqualContents(expectedProperties,
+                            metaModelFinder.findDeclaredPropertyMethods(mme).stream().map(elt -> elt.getSimpleName().toString()).toList());
+                };
+
+        assertor.accept(List.of("desc", "entity1", "entity2", "id", "key", "prop1"), EntityWithEntityTypedAndOrdinaryPropsMetaModel.class);
+        assertor.accept(List.of(), EntityWithEntityTypedAndOrdinaryPropsMetaModelAliased.class); // alised meta-model
+        assertor.accept(List.of("desc", "id", "key", "parent", "prop1"), SubEntityMetaModel.class);
+    }
+
+    @Test
     public void findMetaModelForEntity_finds_a_meta_model_for_a_metamodeled_entity() {
         final EntityElement metamodeledEntityElt = entityFinder.findEntity(MetamodeledEntity.class);
         metaModelFinder.findMetaModelForEntity(metamodeledEntityElt).ifPresentOrElse(metaModelElt -> {
@@ -111,6 +127,13 @@ public class MetaModelFinderTest {
     public void findMetaModelForEntity_does_not_find_a_meta_model_for_a_non_metamodeled_entity() {
         final EntityElement nonMetamodeledEntityElt = entityFinder.findEntity(EntityWithDescTitleWithoutMetaModel.class);
         assertTrue(metaModelFinder.findMetaModelForEntity(nonMetamodeledEntityElt).isEmpty());
+    }
+
+    private static void assertEqualContents(final Collection<?> c1, final Collection<?> c2) {
+        if (CollectionUtil.isEqualContents(c1, c2)) {}
+        else {
+            fail("expected:<%s> but was:<%s>".formatted(CollectionUtil.toString(c1, ", "), CollectionUtil.toString(c2, ", ")));
+        }
     }
 
 }
