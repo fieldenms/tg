8dba677285f934900055520484b1b92770530041
Nazar Chaykivskyy <nazar@fielden.com.au>
2023-02-08 21:11:05 +0200

    #1332 Provided missing parameterisation.

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/stage0/YieldBuilder.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/stage0/YieldBuilder.java
@@ -3,6 +3,7 @@ package ua.com.fielden.platform.eql.stage0;
 import ua.com.fielden.platform.entity.query.fluent.enums.TokenCategory;
 import ua.com.fielden.platform.eql.stage1.etc.Yield1;
 import ua.com.fielden.platform.eql.stage1.operands.ISingleOperand1;
+import ua.com.fielden.platform.eql.stage2.operands.ISingleOperand2;
 import ua.com.fielden.platform.utils.Pair;
 
 public class YieldBuilder extends AbstractTokensBuilder {
@@ -18,7 +19,7 @@ public class YieldBuilder extends AbstractTokensBuilder {
 
     @Override
     public Pair<TokenCategory, Object> getResult() {
-        final ISingleOperand1 operand = getModelForSingleOperand(firstCat(), firstValue());
+        final ISingleOperand1<? extends ISingleOperand2<?>> operand = getModelForSingleOperand(firstCat(), firstValue());
         final String alias = (String) secondValue();
         final boolean requiredHint = (secondCat() == TokenCategory.AS_ALIAS_REQUIRED);
         return new Pair<TokenCategory, Object>(TokenCategory.QRY_YIELD, new Yield1(operand, alias == null ? "" : alias, requiredHint));
