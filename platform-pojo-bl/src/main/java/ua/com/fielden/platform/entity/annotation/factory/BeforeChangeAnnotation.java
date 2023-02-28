a6e8b9dd0abb45338887ebeca9d0782f575bb8a2
homedirectory <nullsink@posteo.eu>
2023-02-28 13:08:19 +0200

    #1931 Implement merging of multiple BeforeChange annotations into a single one

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/annotation/factory/BeforeChangeAnnotation.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/annotation/factory/BeforeChangeAnnotation.java
@@ -1,13 +1,15 @@
 package ua.com.fielden.platform.entity.annotation.factory;
 
+import java.util.Arrays;
+
 import ua.com.fielden.platform.entity.annotation.mutator.BeforeChange;
 import ua.com.fielden.platform.entity.annotation.mutator.Handler;
 
 /**
  * A factory for convenient instantiation of {@link BeforeChange} annotations, which mainly should be used for dynamic property creation.
- * 
+ *
  * @author TG Team
- * 
+ *
  */
 public class BeforeChangeAnnotation {
 
@@ -18,6 +20,21 @@ public class BeforeChangeAnnotation {
     }
 
     public static BeforeChange from(final Handler... handlers) {
+        return new BeforeChangeAnnotation(handlers).newInstance();
+    }
+
+    /**
+     * Returns the result of merging {@link BeforeChange} annotation instances.
+     * Handlers of the resulting annotation are ordered in the same way as given annotations. No deduplication takes place.
+     * <p>
+     * If a single annotation is given, then it is simply returned back.
+     *
+     * @param annotations {@link BeforeChange} instances to be merged
+     * @return merged instance of the {@link BeforeChange} annotation
+     */
+    public static BeforeChange merged(final BeforeChange... annotations) {
+        if (annotations.length == 1) return annotations[0];
+        final Handler[] handlers = Arrays.stream(annotations).flatMap(bch -> Arrays.stream(bch.value())).toArray(Handler[]::new);
         return new BeforeChangeAnnotation(handlers).newInstance();
     }
 
@@ -50,4 +67,5 @@ public class BeforeChangeAnnotation {
             }
         };
     }
+
 }
