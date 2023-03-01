eaca3facf3f90501b6bac20219c5938a298b6eb0
01es <oles.hodych@gmail.com>
2023-03-01 13:09:19 +1100

    #1937 Refactored DefaultAuthenticationModel to be thread-safe.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/user/impl/DefaultAuthenticationModel.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/user/impl/DefaultAuthenticationModel.java
@@ -12,6 +12,7 @@ import ua.com.fielden.platform.basic.config.IApplicationSettings;
 import ua.com.fielden.platform.basic.config.IApplicationSettings.AuthMode;
 import ua.com.fielden.platform.dao.QueryExecutionModel;
 import ua.com.fielden.platform.entity.query.fluent.EntityQueryProgressiveInterfaces.ICompoundCondition0;
+import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
 import ua.com.fielden.platform.entity.query.fluent.fetch;
 import ua.com.fielden.platform.entity.query.model.EntityResultQueryModel;
 import ua.com.fielden.platform.error.Result;
@@ -21,7 +22,7 @@ import ua.com.fielden.platform.security.user.UserSecret;
 import ua.com.fielden.platform.security.user.UserSecretCo;
 
 /**
- * An authentication model for explicit user logins.
+ * An authentication model for explicit user logins (thread-safe).
  * It supports both RSO and SSO modes, whereby in the SSO mode users are checked if they are permitted to login with RSO.
  *
  * @author TG Team
@@ -30,15 +31,13 @@ import ua.com.fielden.platform.security.user.UserSecretCo;
 public class DefaultAuthenticationModel implements IAuthenticationModel {
 
     private static final Result failedAuthResult = failure("The presented login credentials are not recognized.");
-    private final UserSecretCo coUserSecret;
-    private final String protectiveSalt;
+    private final ICompanionObjectFinder coFinder;
     private final AuthMode authMode;
 
     @Inject
-    public DefaultAuthenticationModel(final UserSecretCo coUserSecret, final IApplicationSettings settings) {
+    public DefaultAuthenticationModel(final ICompanionObjectFinder coFinder, final IApplicationSettings settings) {
+        this.coFinder = coFinder;
         this.authMode = settings.authMode();
-        this.coUserSecret = coUserSecret;
-        this.protectiveSalt = coUserSecret.newSalt();
     }
 
     @Override
@@ -48,7 +47,7 @@ public class DefaultAuthenticationModel implements IAuthenticationModel {
             if (User.system_users.UNIT_TEST_USER.matches(username)) {
                 return failedAuthResult;
             }
-
+            final UserSecretCo coUserSecret = coFinder.find(UserSecret.class);
             // in the SSO mode, users should only be able to login with RSO if they are permitted (i.e., their ssOnly property is false).
             final ICompoundCondition0<UserSecret> rsoCondition = select(UserSecret.class).where().lowerCase().prop("key.key").eq().lowerCase().val(username).and().prop("key.active").eq().val(true);
             final EntityResultQueryModel<UserSecret> query = (authMode == AuthMode.RSO ? rsoCondition : rsoCondition.and().prop("key.ssoOnly").eq().val(false)).model();
@@ -59,7 +58,7 @@ public class DefaultAuthenticationModel implements IAuthenticationModel {
                     .map(secret -> equalsEx(secret.getPassword(), coUserSecret.hashPasswd(password, secret.getSalt())) ? successful(secret.getKey()) : failedAuthResult)
                     .orElseGet(() -> {
                         // let's mimic password hashing for unrecognised users to protected from timing-based attacks that would allow enumerating valid user names
-                        coUserSecret.hashPasswd(password, protectiveSalt);
+                        coUserSecret.hashPasswd(password, coUserSecret.newSalt());
                         // the original result needs to be returned.
                         return failedAuthResult;
                     });
