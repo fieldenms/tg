65be6eefb07280979d90d393c29d64b89f798c52
homedirectory <nullsink@posteo.eu>
2023-03-03 09:03:12 +0200

    #1915 Introduce UnionEntityVerifier. Verify that at least one entity-typed property is declared

---
+++ platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/verify/verifiers/entity/UnionEntityVerifier.java
@@ -0,0 +1,69 @@
+package ua.com.fielden.platform.processors.verify.verifiers.entity;
+
+import static javax.tools.Diagnostic.Kind.ERROR;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.processing.ProcessingEnvironment;
+
+import ua.com.fielden.platform.processors.metamodel.elements.EntityElement;
+import ua.com.fielden.platform.processors.metamodel.utils.EntityFinder;
+import ua.com.fielden.platform.processors.verify.ViolatingElement;
+
+/**
+ * Composable verifier for union entities. Verification rules include:
+ * <ol>
+ *  <li>There should be at least 1 entity-typed property.</li>
+ *  <li>Only entity-typed properties are permitted. However, those should not be union entities themselves
+ *  (i.e., nesting of union entities is not supported).</li>
+ *  <li>There should be at most one property of a particular entity type (i.e., multiple properties of the same entity type are disallowed).</li>
+ * </ol>
+ *
+ * @author homedirectory
+ */
+public class UnionEntityVerifier extends AbstractComposableEntityVerifier {
+
+    public UnionEntityVerifier(final ProcessingEnvironment processingEnv) {
+        super(processingEnv);
+    }
+
+    @Override
+    protected List<AbstractEntityVerifier> createComponents(final ProcessingEnvironment procEnv) {
+        return List.of(new EntityTypedPropertyPresenceVerifier(procEnv));
+    }
+
+    /**
+     * There should be at least 1 entity-typed property.
+     */
+    static class EntityTypedPropertyPresenceVerifier extends AbstractEntityVerifier {
+
+        public static final String errNoEntityTypedProperties() {
+            return "A union entity must declare at least 1 entity-typed property";
+        }
+
+        protected EntityTypedPropertyPresenceVerifier(final ProcessingEnvironment processingEnv) {
+            super(processingEnv);
+        }
+
+        @Override
+        protected List<ViolatingElement> verify(final EntityRoundEnvironment roundEnv) {
+            return roundEnv.acceptUnionEntityVisitor(new EntityVisitor(entityFinder));
+        }
+
+        private class EntityVisitor extends AbstractEntityVerifyingVisitor {
+            public EntityVisitor(final EntityFinder entityFinder) {
+                super(entityFinder);
+            }
+
+            @Override
+            public Optional<ViolatingElement> visitEntity(final EntityElement entity) {
+                if (entityFinder.streamDeclaredProperties(entity).anyMatch(prop -> entityFinder.isEntityType(prop.getType()))) {
+                    return Optional.empty();
+                }
+                return Optional.of(new ViolatingElement(entity.element(), ERROR, errNoEntityTypedProperties()));
+            }
+        }
+    }
+
+}
