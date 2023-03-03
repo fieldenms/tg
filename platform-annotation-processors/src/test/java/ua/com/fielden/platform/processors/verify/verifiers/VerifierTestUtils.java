ef163439d2e1baff714a9bd738bdb0bb2187a9d1
homedirectory <nullsink@posteo.eu>
2023-03-03 12:11:08 +0200

    #1915 Add utility method to quickly build property setters with JavaPoet

--- platform-annotation-processors/src/test/java/ua/com/fielden/platform/processors/verify/verifiers/VerifierTestUtils.java
+++ platform-annotation-processors/src/test/java/ua/com/fielden/platform/processors/verify/verifiers/VerifierTestUtils.java
@@ -13,9 +13,11 @@ import javax.tools.JavaFileObject;
 
 import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeName;
 
 import ua.com.fielden.platform.entity.annotation.IsProperty;
+import ua.com.fielden.platform.entity.annotation.Observable;
 import ua.com.fielden.platform.processors.test_utils.Compilation;
 
 public class VerifierTestUtils {
@@ -58,6 +60,21 @@ public class VerifierTestUtils {
     public static FieldSpec.Builder propertyBuilder(final TypeName typeName, final String name) {
         return FieldSpec.builder(typeName, name, Modifier.PRIVATE)
                 .addAnnotation(AnnotationSpec.builder(IsProperty.class).build());
+    }
+
+    /**
+     * Returns a method builder for a property setter having the following form:
+     * <pre>
+     * {@literal @Observable}
+     * public $entityType $name($propType x) {}
+     * </pre>
+     */
+    public static MethodSpec.Builder setterBuilder(final String name, final TypeName propType, final TypeName entityType) {
+        return MethodSpec.methodBuilder(name)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(entityType)
+                .addParameter(propType, "x")
+                .addAnnotation(Observable.class);
     }
 
 }
