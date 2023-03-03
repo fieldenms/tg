975668f1e02d81309688e5f65eae95bc83548768
homedirectory <nullsink@posteo.eu>
2023-03-03 10:13:59 +0200

    #1915 Implement verification of union entity properties as a whole.
    Prohibit multiple properties of the same entity type

--- platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/verify/verifiers/entity/UnionEntityVerifier.java
+++ platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/verify/verifiers/entity/UnionEntityVerifier.java
@@ -1,10 +1,13 @@
 package ua.com.fielden.platform.processors.verify.verifiers.entity;
 
 import static javax.tools.Diagnostic.Kind.ERROR;
+import static ua.com.fielden.platform.processors.metamodel.utils.ElementFinder.asTypeElementOfTypeMirror;
 import static ua.com.fielden.platform.processors.metamodel.utils.ElementFinder.getSimpleName;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.type.TypeMirror;
@@ -110,6 +113,65 @@ public class UnionEntityVerifier extends AbstractComposableEntityVerifier {
                 }
 
                 return Optional.empty();
+            }
+        }
+    }
+
+    /**
+     * There should be at most one property of a particular entity type (i.e., multiple properties of the same entity type are disallowed).
+     */
+    static class DistinctPropertyEntityTypesVerifier extends AbstractEntityVerifier {
+
+        public static String errMultiplePropertiesOfSameType(final String entity) {
+            return "Union entity [%s] can't have multiple properties of the same entity type.".formatted(entity);
+        }
+
+        public static String errPropertyHasNonUniqueType(final String property) {
+            return "Property [%s] has the same type as some other property of this union entity.".formatted(property);
+        }
+
+        protected DistinctPropertyEntityTypesVerifier(final ProcessingEnvironment processingEnv) {
+            super(processingEnv);
+        }
+
+        @Override
+        protected List<ViolatingElement> verify(final EntityRoundEnvironment roundEnv) {
+            return roundEnv.accept(new EntityVisitor(entityFinder));
+        }
+
+        private class EntityVisitor extends AbstractEntityVerifyingVisitor {
+            public EntityVisitor(final EntityFinder entityFinder) {
+                super(entityFinder);
+            }
+
+            @Override
+            public Optional<ViolatingElement> visitEntity(final EntityElement entity) {
+                // key is an entity type and value - properties having that type
+                final Map<EntityElement, List<PropertyElement>> map = entityFinder.streamDeclaredProperties(entity)
+                        .filter(prop -> entityFinder.isEntityType(prop.asType()))
+                        .collect(Collectors.groupingBy(this::entityElementOfPropertyType));
+
+                final List<ViolatingElement> violatingProperties = map.entrySet().stream()
+                        // we are interested in those entity types that are used more than once as property types
+                        .filter(entry -> entry.getValue().size() > 1)
+                        .flatMap(entry -> {
+                            final List<PropertyElement> properties = entry.getValue();
+                            return properties.stream().map(prop -> new ViolatingElement(
+                                    prop.element(), ERROR, errPropertyHasNonUniqueType(getSimpleName(prop.element()))));
+                        }).toList();
+
+                if (!violatingProperties.isEmpty()) {
+                    return Optional.of(
+                            new ViolatingElement(entity.element(), ERROR, errMultiplePropertiesOfSameType(getSimpleName(entity.element())))
+                            .addSubElements(violatingProperties));
+                }
+
+                return Optional.empty();
+            }
+
+            /** Extracts the property type as an {@link EntityElement} */
+            private EntityElement entityElementOfPropertyType(final PropertyElement property) {
+                return entityFinder.newEntityElement(asTypeElementOfTypeMirror(property.getType()));
             }
         }
     }
