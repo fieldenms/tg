e9de0d9dc9805af514f8cde966fb4013d3309ff4
homedirectory <nullsink@posteo.eu>
2023-03-07 09:48:45 +0200

    #1913 Verify return type of property accessors

--- platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/verify/verifiers/entity/EssentialPropertyVerifier.java
+++ platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/verify/verifiers/entity/EssentialPropertyVerifier.java
@@ -45,7 +45,7 @@ public class EssentialPropertyVerifier extends AbstractComposableEntityVerifier 
     @Override
     protected List<AbstractEntityVerifier> createComponents(final ProcessingEnvironment procEnv) {
         return List.of(
-                new PropertyAccessorVerifier(procEnv), 
+                new PropertyAccessorVerifier(procEnv),
                 new PropertySetterVerifier(procEnv),
                 new CollectionalPropertyVerifier(procEnv),
                 new PropertyTypeVerifier(procEnv));
@@ -58,6 +58,10 @@ public class EssentialPropertyVerifier extends AbstractComposableEntityVerifier 
 
         public static final String errMissingAccessor(final String propName) {
             return "Missing accessor for property [%s].".formatted(propName);
+        }
+
+        public static final String errIncorrectReturnType(final String accessorName, final String propertyType) {
+            return "Accessor [%s] must have return type %s".formatted(accessorName, propertyType);
         }
 
         protected PropertyAccessorVerifier(final ProcessingEnvironment processingEnv) {
@@ -76,10 +80,21 @@ public class EssentialPropertyVerifier extends AbstractComposableEntityVerifier 
 
             @Override
             public Optional<ViolatingElement> visitProperty(final EntityElement entity, final PropertyElement property) {
-                if (entityFinder.findDeclaredPropertyAccessor(entity, getSimpleName(property.element())).isEmpty()) {
+                // accessor must be declared
+                final Optional<ExecutableElement> maybeAccessor = entityFinder.findDeclaredPropertyAccessor(entity, getSimpleName(property.element()));
+                if (maybeAccessor.isEmpty()) {
                     return Optional.of(new ViolatingElement(
                             property.element(), Kind.ERROR, errMissingAccessor(getSimpleName(property.element()))));
                 }
+
+                final ExecutableElement accessor = maybeAccessor.get();
+
+                // acessor's return type must match the property type
+                if (!elementFinder.types.isSameType(property.getType(), accessor.getReturnType())) {
+                    return Optional.of(new ViolatingElement(
+                            accessor, Kind.ERROR, errIncorrectReturnType(getSimpleName(accessor), property.getType().toString())));
+                }
+
                 return Optional.empty();
             }
         }
@@ -198,9 +213,9 @@ public class EssentialPropertyVerifier extends AbstractComposableEntityVerifier 
      * <ol>
      *   <li>Ordinary (aka primitive) types: {@link Long}, {@link Integer}, {@link BigDecimal}, {@link Date}, {@link String}, {@link boolean}.</li>
      *   <li>Custom platform types: {@link Money}, {@link Colour}, {@link Hyperlink}.</li>
-     *   <li>Entity types: any registered domain entity 
+     *   <li>Entity types: any registered domain entity
      *   (at the time of writing, this means an entity, registered in an application specific class {@link ApplicationDomain}).</li>
-     *   <li>Collectional types: any type assignable to {@link java.util.Collection}, but parameterised with any of the permitted types 
+     *   <li>Collectional types: any type assignable to {@link java.util.Collection}, but parameterised with any of the permitted types
      *   in items 1-3.</li>
      *   <li>Binary: {@code byte[]}.</li>
      *   <li>Special case collectional: {@link Map} (key and value type verification will need to be covered by another verifier).</li>
@@ -243,7 +258,7 @@ public class EssentialPropertyVerifier extends AbstractComposableEntityVerifier 
         }
 
         private boolean isEntityTypeRegistered(final TypeMirror entityType) {
-            // TODO Implement when ApplicationDomain becomes analysable by annotation processors or 
+            // TODO Implement when ApplicationDomain becomes analysable by annotation processors or
             // some other suitable entity registration mechanism is used.
             // Currently, entity types are registered in the static initialiser block, which is unreachable to annotation processors.
             return true;
@@ -310,7 +325,7 @@ public class EssentialPropertyVerifier extends AbstractComposableEntityVerifier 
                 // all allowed types were exhausted
                 return Optional.of(new ViolatingElement(property.element(), Kind.ERROR, errInvalidType(getSimpleName(property.element()))));
             }
-            
+
         }
     }
 
