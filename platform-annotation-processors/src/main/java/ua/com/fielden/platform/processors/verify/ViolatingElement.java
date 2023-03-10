97be1ad11509fd179863ebf177a0a87b7732d966
homedirectory <nullsink@posteo.eu>
2023-03-10 11:09:17 +0200

    #1935 Refactored ViolatingElement back into class to implicilty calculate the relaxed message kind

--- platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/verify/ViolatingElement.java
+++ platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/verify/ViolatingElement.java
@@ -1,7 +1,6 @@
 package ua.com.fielden.platform.processors.verify;
 
-import static java.util.Optional.empty;
-import static java.util.Optional.of;
+import static java.util.Optional.ofNullable;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -16,6 +15,7 @@ import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
+import javax.tools.Diagnostic;
 import javax.tools.Diagnostic.Kind;
 
 import ua.com.fielden.platform.processors.verify.annotation.RelaxVerification;
@@ -30,30 +30,34 @@ import ua.com.fielden.platform.processors.verify.verifiers.IVerifier;
  *
  * @author TG Team
  */
-public record ViolatingElement(
-        Element element, Kind kind, String message,
-        Optional<AnnotationMirror> annotationMirror, Optional<AnnotationValue> annotationValue,
-        List<ViolatingElement> subElements) {
+public class ViolatingElement {
+    private final Element element;
+    private final Kind kind;
+    private final Kind relaxedKind;
+    private final String message;
+    private final Optional<AnnotationMirror> annotationMirror;
+    private final Optional<AnnotationValue> annotationValue;
+    private final List<ViolatingElement> subElements = new LinkedList<>();
 
-    public ViolatingElement {
+    public ViolatingElement(final Element element, final Kind kind, final String message, final AnnotationMirror annotationMirror, final AnnotationValue annotationValue) {
         Objects.requireNonNull(element, "Argument [element] cannot be null.");
         Objects.requireNonNull(kind, "Argument [kind] cannot be null.");
         Objects.requireNonNull(message, "Argument [message] cannot be null.");
-        Objects.requireNonNull(annotationMirror, "Argument [annotationMirror] cannot be null.");
-        Objects.requireNonNull(annotationValue, "Argument [annotationValue] cannot be null.");
-        Objects.requireNonNull(subElements, "Argument [subElements] cannot be null.");
-    }
 
-    public ViolatingElement(final Element element, final Kind kind, final String message) {
-        this(element, kind, message, empty(), empty(), new LinkedList<>());
+        this.element = element;
+        this.kind = kind;
+        this.relaxedKind = RelaxVerification.Factory.policyFor(element).map(pol -> pol.relaxedKind(kind)).orElse(kind);
+        this.message = message;
+        this.annotationMirror = ofNullable(annotationMirror);
+        this.annotationValue = ofNullable(annotationValue);
     }
 
     public ViolatingElement(final Element element, final Kind kind, final String message, final AnnotationMirror annotationMirror) {
-        this(element, kind, message, of(annotationMirror), empty(), new LinkedList<>());
+        this(element, kind, message, annotationMirror, null);
     }
 
-    public ViolatingElement(final Element element, final Kind kind, final String message, final AnnotationMirror annotationMirror, final AnnotationValue annotationValue) {
-        this(element, kind, message, of(annotationMirror), of(annotationValue), new LinkedList<>());
+    public ViolatingElement(final Element element, final Kind kind, final String message) {
+        this(element, kind, message, null, null);
     }
 
     public ViolatingElement addSubElements(final Collection<ViolatingElement> elements) {
@@ -69,14 +73,29 @@ public record ViolatingElement(
         return Collections.unmodifiableList(this.subElements);
     }
 
+    public Element element() { return element; }
+    public Kind kind() { return kind; }
+    public Kind relaxedKind() { return relaxedKind; }
+    public String message() { return message; }
+    public Optional<AnnotationMirror> annotationMirror() { return annotationMirror; }
+    public Optional<AnnotationValue> annotationValue() { return annotationValue; }
+
+    /**
+     * Indicates whether this violating element indicates an error (is associated with {@link Diagnostic.Kind#ERROR}).
+     * Note that the relaxed kind is considered instead of the original one.
+     *
+     * @see RelaxVerification
+     */
+    public boolean hasError() {
+        return Kind.ERROR.equals(relaxedKind);
+    }
+
     /**
      * Prints a message using the information stored by this instance.
      * @param messager
      */
     public void printMessage(final Messager messager) {
-        // consider @RelaxVerification and relax the diagnostic kind if needed
-        final Kind relaxedKind = RelaxVerification.Factory.policyFor(element).map(pol -> pol.relaxedKind(kind)).orElse(kind);
-
+        // use the relaxed kind for the message
         if (annotationMirror.isEmpty()) { /* simplest form of message that is present directly on the element */
             messager.printMessage(relaxedKind, message, element);
         } else if (annotationValue.isEmpty()) { /* message present on the element's annotation */
