4b471537e037f1d5fb66a5ad4ebdfaa606d08cc2
homedirectory <nullsink@posteo.eu>
2023-03-14 09:19:34 +0200

    #1935 Fixed the javadoc of IVerifier

--- platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/verify/verifiers/IVerifier.java
+++ platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/verify/verifiers/IVerifier.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import javax.annotation.processing.RoundEnvironment;
 
-import ua.com.fielden.platform.processors.verify.ViolatingElement;
 import ua.com.fielden.platform.processors.verify.VerifyingProcessor;
+import ua.com.fielden.platform.processors.verify.ViolatingElement;
 
 /**
  * Interface for implementation of domain model verifiers to be used with {@link VerifyingProcessor}.
- * 
+ *
  * @author TG Team
  */
 public interface IVerifier {
@@ -18,13 +18,8 @@ public interface IVerifier {
      * Performs verification in the current round according to implementation-specific rules.
      *
      * @param roundEnv
-     * @return {@code true} if verification was passed.
+     * @return elements that did not pass verification
      */
     public List<ViolatingElement> verify(final RoundEnvironment roundEnv);
-
-    /**
-     * Returns those elements that did not pass verification.
-     * @return
-     */
 
 }
