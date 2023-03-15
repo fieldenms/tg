1a44e3b9ef23ed376a44d9cc207c7fd97aebb169
01es <oles.hodych@gmail.com>
2023-03-15 18:30:09 +1100

    #1931 Streamlined implementation of BeforeChangeAnnotation.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/annotation/factory/BeforeChangeAnnotation.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/annotation/factory/BeforeChangeAnnotation.java
@@ -1,9 +1,11 @@
 package ua.com.fielden.platform.entity.annotation.factory;
 
-import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 import ua.com.fielden.platform.entity.annotation.mutator.BeforeChange;
 import ua.com.fielden.platform.entity.annotation.mutator.Handler;
+import ua.com.fielden.platform.entity.exceptions.InvalidArgumentException;
 
 /**
  * A factory for convenient instantiation of {@link BeforeChange} annotations, which mainly should be used for dynamic property creation.
@@ -19,25 +21,46 @@ public class BeforeChangeAnnotation {
         this.value = value;
     }
 
-    public static BeforeChange from(final Handler... handlers) {
+    /**
+     * A factory method instantiating {@link BeforeChange} with {@code handlers}.
+     *
+     * @param handlers
+     * @return
+     */
+    public static BeforeChange newInstance(final Handler... handlers) {
         return new BeforeChangeAnnotation(handlers).newInstance();
     }
 
     /**
-     * Returns the result of merging {@link BeforeChange} annotation instances.
-     * Handlers of the resulting annotation are ordered in the same way as given annotations. No deduplication takes place.
-     * <p>
-     * If a single annotation is given, then it is simply returned back.
+     * Returns an instance of {@link BeforeChange} with {@code value} containing all handlers from {@code annotations}.
+     * Handlers of the resulting annotation are ordered in the same way as in {@code annotations}, but no deduplication of handlers takes place.
      *
      * @param annotations {@link BeforeChange} instances to be merged
      * @return merged instance of the {@link BeforeChange} annotation
      */
-    public static BeforeChange merged(final BeforeChange... annotations) {
-        if (annotations.length == 1) return annotations[0];
-        final Handler[] handlers = Arrays.stream(annotations).flatMap(bch -> Arrays.stream(bch.value())).toArray(Handler[]::new);
-        return new BeforeChangeAnnotation(handlers).newInstance();
+    public static BeforeChange merge(final BeforeChange... annotations) {
+        if (annotations == null || annotations.length == 0) {
+            throw new InvalidArgumentException("There are no BeforeChange annotations to merge.");
+        }
+        if (Stream.of(annotations).anyMatch(Objects::isNull)) {
+            throw new InvalidArgumentException("No annotation can be null.");
+        }
+        if (annotations == null || annotations.length == 0) {
+            throw new InvalidArgumentException("There are no BeforeChange annotations to merge.");
+        }
+
+        if (annotations.length == 1) {
+            return annotations[0];
+        }
+        final Handler[] mergedHandlers = Stream.of(annotations).flatMap(bch -> Stream.of(bch.value())).toArray(Handler[]::new);
+        return new BeforeChangeAnnotation(mergedHandlers).newInstance();
     }
 
+    /**
+     * Creates a new instance of {@link BeforeChange}.
+     *
+     * @return
+     */
     public BeforeChange newInstance() {
         return new BeforeChange() {
 
@@ -49,21 +72,6 @@ public class BeforeChangeAnnotation {
             @Override
             public Handler[] value() {
                 return value;
-            }
-        };
-    }
-
-    public BeforeChange copyFrom(final BeforeChange bch) {
-        return new BeforeChange() {
-
-            @Override
-            public Class<BeforeChange> annotationType() {
-                return BeforeChange.class;
-            }
-
-            @Override
-            public Handler[] value() {
-                return bch.value();
             }
         };
     }
