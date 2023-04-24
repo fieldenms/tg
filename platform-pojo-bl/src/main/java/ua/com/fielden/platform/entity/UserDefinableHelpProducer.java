93bced8b111325163318b90a975dfaf324e581ab
01es <oles.hodych@gmail.com>
2023-04-24 20:34:57 +1000

    #1955 Adjusted tooltip and error messages + restricted nested help invocations.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/UserDefinableHelpProducer.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/UserDefinableHelpProducer.java
@@ -1,5 +1,6 @@
 package ua.com.fielden.platform.entity;
 
+import static ua.com.fielden.platform.error.Result.failureEx;
 import static ua.com.fielden.platform.reflection.asm.impl.DynamicEntityClassLoader.getOriginalType;
 
 import com.google.inject.Inject;
@@ -7,6 +8,8 @@ import com.google.inject.Inject;
 import ua.com.fielden.platform.entity.exceptions.EntityException;
 import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
+import ua.com.fielden.platform.error.Result;
+import ua.com.fielden.platform.utils.EntityUtils;
 
 /**
  * Producer for {@link UserDefinableHelp} entity
@@ -16,7 +19,7 @@ import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
  */
 public class UserDefinableHelpProducer extends DefaultEntityProducerWithContext<UserDefinableHelp> {
 
-    public static final String ERR_HELP_MISSING = "Help doesn't exist";
+    public static final String ERR_HELP_MISSING = "There is no help to open.";
 
     @Inject
     public UserDefinableHelpProducer(final EntityFactory factory, final ICompanionObjectFinder companionFinder) {
@@ -28,6 +31,12 @@ public class UserDefinableHelpProducer extends DefaultEntityProducerWithContext<
         if (currentEntityInstanceOf(AbstractEntity.class)) {
             final AbstractEntity<?> currEntity = currentEntity(AbstractEntity.class);
             final Class<AbstractEntity<?>> entityType = getOriginalType(currEntity.getType());
+
+            // let's restrict nested calls to UserDefinableHelp Master invocations related to UserDefinableHelp itself
+            if (UserDefinableHelp.class.isAssignableFrom(entityType) && EntityUtils.equalsEx(((UserDefinableHelp) currEntity).getReferenceElement(), UserDefinableHelp.class.getName())) {
+                throw failureEx("Nested help links are not supported.", "You are currently attempting to add a help link to a master, which represents a help link for a Help Master (i.e., this is a nested call).<br>Simply finish your changes to a help link, if any, and close the curent Help Master dialog.");
+            }
+
             final UserDefinableHelpCo entityMasterHelpCo = co$(UserDefinableHelp.class);
             final UserDefinableHelp persistedEntity = entityMasterHelpCo.findByKeyAndFetch(entityMasterHelpCo.getFetchProvider().fetchModel(), entityType.getName());
             final boolean skipUi = this.chosenPropertyEmpty();
@@ -50,4 +59,5 @@ public class UserDefinableHelpProducer extends DefaultEntityProducerWithContext<
         }
         return entity;
     }
+
 }
