b5d8260fd474d08918a174120412396e91a98dbe
01es <oles.hodych@gmail.com>
2023-05-01 10:28:04 +1000

    #1960 Implemented the logic for processing default help URI at the model level and simplified the producer.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/UserDefinableHelpProducer.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/UserDefinableHelpProducer.java
@@ -2,20 +2,15 @@ package ua.com.fielden.platform.entity;
 
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
-import static org.apache.commons.lang3.StringUtils.isEmpty;
 import static ua.com.fielden.platform.error.Result.failureEx;
 import static ua.com.fielden.platform.reflection.asm.impl.DynamicEntityClassLoader.getOriginalType;
 
 import java.util.Optional;
 
-import org.apache.commons.lang3.StringUtils;
-
 import com.google.inject.Inject;
-import com.google.inject.name.Named;
 
 import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
-import ua.com.fielden.platform.types.Hyperlink;
 import ua.com.fielden.platform.utils.EntityUtils;
 
 /**
@@ -26,46 +21,27 @@ import ua.com.fielden.platform.utils.EntityUtils;
  */
 public class UserDefinableHelpProducer extends DefaultEntityProducerWithContext<UserDefinableHelp> {
 
-    private final String defaultHelpUri;
-
     @Inject
-    public UserDefinableHelpProducer(final @Named("help.defaultUri") String defaultHelpUri, final EntityFactory factory, final ICompanionObjectFinder companionFinder) {
+    public UserDefinableHelpProducer(final EntityFactory factory, final ICompanionObjectFinder companionFinder) {
         super(factory, UserDefinableHelp.class, companionFinder);
-        this.defaultHelpUri = defaultHelpUri;
     }
 
     @Override
     protected UserDefinableHelp provideDefaultValues(final UserDefinableHelp entity) {
        return getReferenceElement().map(refElement -> {
-
             // let's restrict nested calls to UserDefinableHelp Master invocations related to UserDefinableHelp itself
             if (currentEntityNotEmpty() && UserDefinableHelp.class.isAssignableFrom(getOriginalType(currentEntity(AbstractEntity.class).getType())) && EntityUtils.equalsEx(currentEntity(UserDefinableHelp.class).getReferenceElement(), UserDefinableHelp.class.getName())) {
                 throw failureEx("Nested help links are not supported.", "You are currently attempting to add a help link to a master, which represents a help link for a Help Master (i.e., this is a nested call).<br>Simply finish your changes to a help link, if any, and close the curent Help Master dialog.");
             }
 
-           final UserDefinableHelpCo coEntityMasterHelp = co$(UserDefinableHelp.class);
-           final UserDefinableHelp persistedEntity = coEntityMasterHelp.findByKeyAndFetch(coEntityMasterHelp.getFetchProvider().fetchModel(), refElement);
+           final UserDefinableHelpCo coUserDefinableHelp = co$(UserDefinableHelp.class);
+           final UserDefinableHelp help = coUserDefinableHelp.findOrNewWithDefaultHelp(refElement);
            final boolean skipUi = this.chosenPropertyEmpty();
-           if (persistedEntity != null) {
-               if (skipUi) {
-                   entity.setReferenceElement(persistedEntity.getReferenceElement());
-                   entity.setHelp(persistedEntity.getHelp());
-                   entity.setSkipUi(true);
-                   return entity;
+               if (skipUi && help.getHelp() == null) {
+                   throw failureEx("There is no help to open.", "Users with the right privileges can add a help hyperlink.<br>Alt+Tap or long press the help action to invoke Help Master.");
                }
-               return persistedEntity;
-           } else {
-               if (skipUi && isEmpty(defaultHelpUri)) {
-                  throw failureEx("There is no help to open.", "Users with the right privileges can add a help hyperlink.<br>Alt+Tap or long press the help action to invoke Help Master.");
-               } else {
-                   if (!isEmpty(defaultHelpUri)) {
-                       entity.setHelp(new Hyperlink(defaultHelpUri));
-                   }
-                   entity.setReferenceElement(refElement);
-                   entity.setSkipUi(skipUi);
-                   return entity; 
-               }
-           }
+               help.setSkipUi(skipUi);
+               return help;
        }).orElse(entity);
     }
 
