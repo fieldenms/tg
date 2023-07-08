028df48463d68beb59a684d00f31df1073a78c5a
Nazar Chaykivskyy <nazar@fielden.com.au>
2023-07-09 02:34:57 +0300

    #1332 Modified logic to be based on newer (EQL3) metadata.

--- platform-dao/src/main/java/ua/com/fielden/platform/entity/query/EntityBatchDeleteByQueryModelOperation.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/entity/query/EntityBatchDeleteByQueryModelOperation.java
@@ -10,12 +10,12 @@ import java.util.Map;
 import org.hibernate.Query;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
-import ua.com.fielden.platform.entity.query.metadata.DomainMetadataAnalyser;
-import ua.com.fielden.platform.entity.query.metadata.PersistedEntityMetadata;
 import ua.com.fielden.platform.entity.query.model.AggregatedResultQueryModel;
 import ua.com.fielden.platform.entity.query.model.EntityResultQueryModel;
+import ua.com.fielden.platform.eql.meta.EqlDomainMetadata;
 import ua.com.fielden.platform.eql.stage2.TransformationResult2;
 import ua.com.fielden.platform.eql.stage3.operands.queries.ResultQuery3;
+import ua.com.fielden.platform.reflection.asm.impl.DynamicEntityClassLoader;
 
 public class EntityBatchDeleteByQueryModelOperation {
     private final QueryExecutionContext executionContext;
@@ -25,21 +25,20 @@ public class EntityBatchDeleteByQueryModelOperation {
     }
 
     public <E extends AbstractEntity<?>> int deleteEntities(final EntityResultQueryModel<E> model, final Map<String, Object> paramValues) {
-        final DomainMetadataAnalyser domainMetadataAnalyser = new DomainMetadataAnalyser(executionContext.getDomainMetadata());
-        final DeletionModel deletionModel = getModelSql(model, paramValues, domainMetadataAnalyser);
+        final DeletionModel deletionModel = getModelSql(model, paramValues);
         final EntityHibernateDeletionQueryProducer entityHibernateDeletionQueryProducer = new EntityHibernateDeletionQueryProducer(deletionModel.sql, deletionModel.sqlParamValues);
         final Query sqlQuery = entityHibernateDeletionQueryProducer.produceHibernateQuery(executionContext.getSession());
         return sqlQuery.executeUpdate();
     }
 
-    private <T extends AbstractEntity<?>> DeletionModel getModelSql(final EntityResultQueryModel<T> model, final Map<String, Object> paramValues, final DomainMetadataAnalyser domainMetadataAnalyser) {
+    private <T extends AbstractEntity<?>> DeletionModel getModelSql(final EntityResultQueryModel<T> model, final Map<String, Object> paramValues) {
+        final EqlDomainMetadata eqlDomainMetadata = executionContext.getEqlDomainMetadata();
         final AggregatedResultQueryModel finalModel = select(model.getResultType()).where().prop(ID).in().model(model).yield().prop(ID).as(ID).modelAsAggregate();
-        final String tableName = ((PersistedEntityMetadata<AbstractEntity<?>>) domainMetadataAnalyser.getEntityMetadata(model.getResultType())).getTable();
-        final var eqlMetaData = executionContext.getEqlDomainMetadata();
-        final TransformationResult2<ResultQuery3> s2tr = transform(new QueryProcessingModel(finalModel, null, null, paramValues, true), null, null, executionContext.dates(), eqlMetaData); 
+        final String tableName = eqlDomainMetadata.getTables().get(DynamicEntityClassLoader.getOriginalType(model.getResultType()).getName()).name;
+        final TransformationResult2<ResultQuery3> s2tr = transform(new QueryProcessingModel(finalModel, null, null, paramValues, true), null, null, executionContext.dates(), eqlDomainMetadata); 
         final ResultQuery3 entQuery3 = s2tr.item;
-        final String selectionSql = entQuery3.sql(domainMetadataAnalyser.getDbVersion());
-        final String deletionSql = produceDeletionSql(selectionSql, tableName, eqlMetaData.dbVersion);
+        final String selectionSql = entQuery3.sql(eqlDomainMetadata.dbVersion);
+        final String deletionSql = produceDeletionSql(selectionSql, tableName, eqlDomainMetadata.dbVersion);
         return new DeletionModel(deletionSql, s2tr.updatedContext.getParamValues());
     }
 
