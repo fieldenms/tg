ef89985e0c117843c5d6e5c58f2b196c8105f293
Nazar Chaykivskyy <nazar@fielden.com.au>
2023-08-01 10:07:57 +0300

    #1332 Turned the class into generic one. Adjusted its properties accordingly.

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/meta/EqlEntityMetadata.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/meta/EqlEntityMetadata.java
@@ -7,12 +7,12 @@ import java.util.List;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.query.metadata.EntityTypeInfo;
 
-public class EqlEntityMetadata {
-    public final EntityTypeInfo<? extends AbstractEntity<?>> typeInfo;
-    public final Class<? extends AbstractEntity<?>> entityType;
+public class EqlEntityMetadata<ET extends AbstractEntity<?>> {
+    public final EntityTypeInfo<? super ET> typeInfo;
+    public final Class<ET> entityType;
     private final List<EqlPropertyMetadata> props = new ArrayList<>();  
     
-    public EqlEntityMetadata(final Class<? extends AbstractEntity<?>> entityType, EntityTypeInfo<? extends AbstractEntity<?>> typeInfo, final List<EqlPropertyMetadata> props) {
+    public EqlEntityMetadata(final Class<ET> entityType, EntityTypeInfo<? super ET> typeInfo, final List<EqlPropertyMetadata> props) {
         this.entityType = entityType;
         this.typeInfo = typeInfo;
         this.props.addAll(props);
