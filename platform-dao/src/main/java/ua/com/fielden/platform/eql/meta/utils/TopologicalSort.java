d8cb8ad34947d8130d7528f8b40f12feda3dc4b4
Nazar Chaykivskyy <nazar@fielden.com.au>
2023-08-02 15:46:06 +0300

    #1332 Made local copy of the argument (Map) as it is mutated as part of the sorting algorithm execution logic.

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/meta/utils/TopologicalSort.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/meta/utils/TopologicalSort.java
@@ -1,6 +1,8 @@
 package ua.com.fielden.platform.eql.meta.utils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -10,11 +12,12 @@ public class TopologicalSort {
 
     public static <T> List<T> sortTopologically(final Map<T, Set<T>> mapOfDependencies) {
         final List<T> sorted = new ArrayList<>();
-
-        while (!mapOfDependencies.isEmpty()) {
+        final Map<T, Set<T>> localMapOfDependencies = makeDefensiveCopy(mapOfDependencies);
+        
+        while (!localMapOfDependencies.isEmpty()) {
             T nextSorted = null;
             // let's find the first item without dependencies and regard it as "sorted"
-            for (final Entry<T, Set<T>> el : mapOfDependencies.entrySet()) {
+            for (final Entry<T, Set<T>> el : localMapOfDependencies.entrySet()) {
                 if (el.getValue().isEmpty()) {
                     nextSorted = el.getKey();
                     break;
@@ -22,14 +25,22 @@ public class TopologicalSort {
             }
 
             sorted.add(nextSorted);
-            mapOfDependencies.remove(nextSorted); // removing "sorted" item from map of remaining items
+            localMapOfDependencies.remove(nextSorted); // removing "sorted" item from map of remaining items
 
             // removing "sorted" item from dependencies of remaining items 
-            for (final Entry<T, Set<T>> el : mapOfDependencies.entrySet()) {
+            for (final Entry<T, Set<T>> el : localMapOfDependencies.entrySet()) {
                 el.getValue().remove(nextSorted);
             }
         }
 
         return sorted;
+    }
+    
+    private static <T> Map<T, Set<T>> makeDefensiveCopy(final Map<T, Set<T>> mapOfDependencies) {
+        final Map<T, Set<T>> result = new HashMap<>();
+        for (final Entry<T, Set<T>> entry : mapOfDependencies.entrySet()) {
+            result.put(entry.getKey(), new HashSet<>(entry.getValue()));
+        }
+        return result;
     }
 }
