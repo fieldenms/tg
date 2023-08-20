70aeb6c41e677d54c1666d23911e0ebc6d7fbec0
Nazar Chaykivskyy <nazar@fielden.com.au>
2023-08-21 00:32:30 +0300

    #1332 Provided unit test for establishing order of dependent calc. props.

---
+++ platform-dao/src/test/java/ua/com/fielden/platform/eql/meta/UtilsTest.java
@@ -0,0 +1,21 @@
+package ua.com.fielden.platform.eql.meta;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import ua.com.fielden.platform.eql.meta.utils.DependentCalcPropsOrder;
+import ua.com.fielden.platform.eql.retrieval.AbstractEqlShortcutTest;
+import ua.com.fielden.platform.sample.domain.TeVehicle;
+
+public class UtilsTest extends AbstractEqlShortcutTest {
+
+    @Test
+    public void order_of_dependent_calculated_properties_of_vehicle_is_correct() {
+        final List<String> actOrder = DependentCalcPropsOrder.orderDependentCalcProps(metadata(), qb(), metadata().getQuerySourceInfo(TeVehicle.class));
+        final List<String> expOrder = List.of("replacedByTwice", "theSameVehicle", "replacedByTwiceModel", "replacedByTwiceModelMake");
+        assertEquals(actOrder, expOrder);
+    }
+}
