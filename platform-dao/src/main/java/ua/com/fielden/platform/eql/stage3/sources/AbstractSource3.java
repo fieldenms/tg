0ce0b3ae260c589cf9721bfe38d21860ee0a63b9
Nazar Chaykivskyy <nazar@fielden.com.au>
2023-10-03 01:19:51 +0300

    #1332 Implemented quick support that allows not ending money types yields with .amount.

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/stage3/sources/AbstractSource3.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/stage3/sources/AbstractSource3.java
@@ -1,12 +1,18 @@
 package ua.com.fielden.platform.eql.stage3.sources;
 
+import static java.lang.String.format;
+
 import java.util.Map;
 import java.util.Objects;
+
+import ua.com.fielden.platform.eql.exceptions.EqlStage3ProcessingException;
 
 public abstract class AbstractSource3 implements ISource3 {
     public final String sqlAlias;
     private final Integer id;
     private final Map<String, String> columns;
+    private static final String MONEY_TYPE_SUBPROP_ENDING = ".amount";
+    private static final int MONEY_TYPE_SUBPROP_ENDING_LENGTH = MONEY_TYPE_SUBPROP_ENDING.length();
 
     protected AbstractSource3(final String sqlAlias, final Integer id, final Map<String, String> columns) {
         this.sqlAlias = sqlAlias;
@@ -20,8 +26,28 @@ public abstract class AbstractSource3 implements ISource3 {
     }
     
     @Override
-    public String column(final String colName) {
-         return sqlAlias + "." + columns.get(colName);
+    public String column(final String propName) {
+        final String column = searchForColumn(propName);
+        if (column != null) {
+            return sqlAlias + "." + column;
+        } else {
+            throw new EqlStage3ProcessingException(format("Query source doesn't contain column for property [%s]", propName));
+        }
+    }
+    
+    private String searchForColumn(final String propName) {
+        final String column = columns.get(propName);
+        
+        if (column != null) {
+            return column;
+        } 
+        
+        // Quick solution that allows to skip adding ".amount" for Money typed yields aliases.
+        if (propName.endsWith(MONEY_TYPE_SUBPROP_ENDING)) {
+            return columns.get(propName.substring(0, propName.length() - MONEY_TYPE_SUBPROP_ENDING_LENGTH));
+        }
+        
+        return null;
     }
     
     @Override
