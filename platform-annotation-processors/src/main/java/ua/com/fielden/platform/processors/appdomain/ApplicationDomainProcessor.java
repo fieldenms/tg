70ca9d0db4a731a3ec345537c5e1ec2c6cf061a7
homedirectory <nullsink@posteo.eu>
2023-10-27 11:17:37 +0300

    #1914 Additional post-merge adjustments

--- platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/appdomain/ApplicationDomainProcessor.java
+++ platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/appdomain/ApplicationDomainProcessor.java
@@ -19,7 +19,6 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import javax.annotation.processing.Generated;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
@@ -45,6 +44,7 @@ import ua.com.fielden.platform.basic.config.IApplicationDomainProvider;
 import ua.com.fielden.platform.domain.PlatformDomainTypes;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.processors.AbstractPlatformAnnotationProcessor;
+import ua.com.fielden.platform.processors.DateTimeUtils;
 import ua.com.fielden.platform.processors.appdomain.annotation.ExtendApplicationDomain;
 import ua.com.fielden.platform.processors.appdomain.annotation.RegisteredEntity;
 import ua.com.fielden.platform.processors.appdomain.annotation.SkipEntityRegistration;
@@ -99,8 +99,8 @@ public class ApplicationDomainProcessor extends AbstractPlatformAnnotationProces
     @Override
     public synchronized void init(final ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
-        this.elementFinder = new ElementFinder(processingEnv.getElementUtils(), processingEnv.getTypeUtils());
-        this.entityFinder = new EntityFinder(processingEnv.getElementUtils(), processingEnv.getTypeUtils());
+        this.elementFinder = new ElementFinder(processingEnv);
+        this.entityFinder = new EntityFinder(processingEnv);
         this.applicationDomainPackage = "%s.config".formatted(this.packageName);
     }
 
@@ -273,11 +273,9 @@ public class ApplicationDomainProcessor extends AbstractPlatformAnnotationProces
             return bld;
         };
 
-        // class-level @Generated annotation
-        final AnnotationSpec atGenerated = AnnotationSpec.builder(ClassName.get(Generated.class))
-                .addMember("value", "$S", this.getClass().getCanonicalName())
-                .addMember("date", "$S", initDateTime)
-                .build();
+        // @Generated annotation
+        final String dateString = DateTimeUtils.toIsoFormat(DateTimeUtils.zonedNow());
+        final AnnotationSpec atGenerated = buildAtGenerated(dateString);
 
         // We use @RegisteredEntity annotations just to enable the processor to get access to the list of registered types,
         // since we can't analyse the insides of the static initialiser block.
