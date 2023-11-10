1657e4ad3567b4f0a49b9ac4ce66ff8dc6598c03
jhou-pro <jhou.pro@gmail.com>
2023-11-10 16:38:57 +0200

    #2031 Added state resetting for masters being detached from its parent, similarly as when CANCEL is done. This helps with closing masters in error that has previously cached new+persistent entity.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/master/tg-entity-master-behavior.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/master/tg-entity-master-behavior.js
@@ -824,6 +824,7 @@ const TgEntityMasterBehaviorImpl = {
         }
         this.fire('tg-entity-master-detached', this, { node: this._cachedParentNode }); // start event bubbling on previous parent node from which this entity master has already been detached
         delete this._cachedParentNode; // remove reference on previous _cachedParentNode to facilitate possible releasing of parentNode from memory
+        this._resetState(); // existing state may cause problems for cached masters: for example previous entity was new and valid and next one invalid -- blocks closing of dialog with error
     },
 
     /**
