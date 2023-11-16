c388958a0fb5236cd7730569eb75732c23ad4fee
Oleh Maikovych <oleh@fielden.com.au>
2023-11-16 17:09:02 +0200

    #2031 Added logic to simple and compound entity producers to prevent creation of new entities annotated with with RestrictCreationByUsers annotation with top action on entity centre.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/AbstractProducerForOpenEntityMasterAction.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/AbstractProducerForOpenEntityMasterAction.java
@@ -1,13 +1,17 @@
 package ua.com.fielden.platform.entity;
 
+import static java.lang.String.format;
 import static ua.com.fielden.platform.entity.AbstractEntity.KEY;
 import static ua.com.fielden.platform.entity.AbstractEntityEditActionProducer.NOTHING_TO_OPEN_MSG;
+import static ua.com.fielden.platform.entity.EntityNewActionProducer.ERR_ENTITY_CAN_NOT_BE_CREATED;
 
 import java.util.function.Supplier;
 
+import ua.com.fielden.platform.entity.annotation.RestrictCreationByUsers;
 import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
 import ua.com.fielden.platform.entity_master.exceptions.CompoundMasterException;
+import ua.com.fielden.platform.reflection.AnnotationReflector;
 
 /**
  * A base class that should be applicable in most cases for implementing open entity master action producers.
@@ -35,6 +39,9 @@ public abstract class AbstractProducerForOpenEntityMasterAction<T extends Abstra
         if (currentEntityNotEmpty()) {
             openAction.setKey(refetch(chosenEntityId(entityType).orElseThrow(NOTHING_TO_OPEN_EXCEPTION_SUPPLIER), entityType, KEY));
         } else if (selectedEntitiesEmpty()) {
+            if (AnnotationReflector.isAnnotationPresentForClass(RestrictCreationByUsers.class, entityType)) {
+                throw new CompoundMasterException(format(ERR_ENTITY_CAN_NOT_BE_CREATED, entityType.getSimpleName(), RestrictCreationByUsers.class.getSimpleName()));
+            }
             // '+' action on entity T centre or '+' action on master autocompleter title
             openAction.setKey(co(entityType).new_());
         } else {
--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/EntityNewActionProducer.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/EntityNewActionProducer.java
@@ -1,23 +1,38 @@
 package ua.com.fielden.platform.entity;
 
+import static java.lang.String.format;
+
 import com.google.inject.Inject;
 
+import ua.com.fielden.platform.entity.annotation.RestrictCreationByUsers;
 import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
+import ua.com.fielden.platform.entity_master.exceptions.SimpleMasterException;
+import ua.com.fielden.platform.reflection.AnnotationReflector;
 import ua.com.fielden.platform.security.IAuthorisationModel;
 import ua.com.fielden.platform.security.provider.ISecurityTokenProvider;
 
 /**
  * Producer for {@link EntityNewAction}.
- * 
+ *
  * @author TG Team
  *
  */
 public class EntityNewActionProducer extends EntityManipulationActionProducer<EntityNewAction> {
-    
+
+    public static final String ERR_ENTITY_CAN_NOT_BE_CREATED = "%s entity can not be created due to presence of %s annotation.";
+
     @Inject
     public EntityNewActionProducer(final EntityFactory factory, final ICompanionObjectFinder companionFinder, final IAuthorisationModel authorisation, final ISecurityTokenProvider securityTokenProvider) {
         super(factory, EntityNewAction.class, companionFinder, authorisation, securityTokenProvider);
     }
-    
+
+    @Override
+    protected EntityNewAction provideDefaultValues(final EntityNewAction entity) {
+        final EntityNewAction updatedEntity = super.provideDefaultValues(entity);
+        if (AnnotationReflector.isAnnotationPresentForClass(RestrictCreationByUsers.class, updatedEntity.getEntityTypeAsClass())) {
+            throw new SimpleMasterException(format(ERR_ENTITY_CAN_NOT_BE_CREATED, updatedEntity.getEntityTypeAsClass().getSimpleName(), RestrictCreationByUsers.class.getSimpleName()));
+        }
+        return updatedEntity;
+    }
 }
