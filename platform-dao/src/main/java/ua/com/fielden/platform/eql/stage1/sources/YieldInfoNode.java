36908f46333678adfd29d24f4eb8a9cc46a6305e
Nazar Chaykivskyy <nazar@fielden.com.au>
2023-11-17 18:08:35 +0200

    #1332 Turned into a Record.

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/stage1/sources/YieldInfoNode.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/stage1/sources/YieldInfoNode.java
@@ -1,20 +1,13 @@
 package ua.com.fielden.platform.eql.stage1.sources;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import ua.com.fielden.platform.eql.meta.PropType;
 
-public class YieldInfoNode {
-    public final String name;
-    public final PropType propType;
-    public final boolean nonnullable;
-    private final Map<String, YieldInfoNode> items = new HashMap<>();
-
-    public YieldInfoNode(final String name, final PropType propType, final boolean nonnullable, final Map<String, YieldInfoNode> items) {
-        this.name = name;
-        this.propType = propType;
-        this.items.putAll(items);
-        this.nonnullable = nonnullable;
-    }
+/**
+ * A structure used for representing yielded properties. It is used strictly for metadata generation needs.
+ *
+ * @author TG Team
+ */
+public record YieldInfoNode(String name, PropType propType, boolean nonnullable, Map<String, YieldInfoNode> items) {
 }
