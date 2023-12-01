a6a8b7277e27715c7a0735ec61d920b89a646691
homedirectory <nullsink@posteo.eu>
2023-12-01 12:29:05 +0200

    #1946 Extract the analysis of ApplicationDomain and round inputs into a reusable component

--- platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/appdomain/ApplicationDomainProcessor.java
+++ platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/appdomain/ApplicationDomainProcessor.java
@@ -6,13 +6,11 @@ import static javax.lang.model.element.Modifier.PRIVATE;
 import static javax.lang.model.element.Modifier.PUBLIC;
 import static javax.lang.model.element.Modifier.STATIC;
 import static ua.com.fielden.platform.processors.ProcessorOptionDescriptor.parseOptionFrom;
-import static ua.com.fielden.platform.processors.appdomain.EntityRegistrationUtils.isRegisterable;
 import static ua.com.fielden.platform.processors.metamodel.utils.ElementFinder.isGeneric;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -27,11 +25,8 @@ import java.util.stream.Stream;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.ErrorType;
-import javax.tools.Diagnostic.Kind;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
@@ -58,7 +53,6 @@ import ua.com.fielden.platform.processors.exceptions.ProcessorInitializationExce
 import ua.com.fielden.platform.processors.metamodel.elements.EntityElement;
 import ua.com.fielden.platform.processors.metamodel.utils.ElementFinder;
 import ua.com.fielden.platform.processors.metamodel.utils.EntityFinder;
-import ua.com.fielden.platform.utils.CollectionUtil;
 
 /**
  * An annotation processor that generates and maintains the {@code ApplicationDomain} class, which implements {@link IApplicationDomainProvider}.
@@ -117,8 +111,18 @@ public class ApplicationDomainProcessor extends AbstractPlatformAnnotationProces
             return value;
         }
     };
-    protected String appDomainPkg;
 
+    /**
+     * Returns the fully-qualified name of the {@code ApplicationDomain} class determined by given processor options.
+     */
+    public static String getApplicationDomainFqn(Map<String, String> procOptions) {
+        return "%s.%s".formatted(parseOptionFrom(procOptions, ApplicationDomainProcessor.APP_DOMAIN_PKG_OPT_DESC), APPLICATION_DOMAIN_SIMPLE_NAME);
+    }
+
+    private String appDomainPkg;
+    private String appDomainFqn;
+
+    private RegisteredEntitiesCollector registeredEntitiesCollector;
     private ElementFinder elementFinder;
     private EntityFinder entityFinder;
 
@@ -132,12 +136,14 @@ public class ApplicationDomainProcessor extends AbstractPlatformAnnotationProces
     @Override
     protected void parseOptions(Map<String, String> options) {
         super.parseOptions(options);
-        appDomainPkg = ProcessorOptionDescriptor.parseOptionFrom(options, APP_DOMAIN_PKG_OPT_DESC);
+        appDomainPkg = parseOptionFrom(options, APP_DOMAIN_PKG_OPT_DESC);
+        appDomainFqn = getApplicationDomainFqn(options);
     }
 
     @Override
     public synchronized void init(final ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
+        this.registeredEntitiesCollector = RegisteredEntitiesCollector.getInstance(processingEnv);
         this.elementFinder = new ElementFinder(processingEnv);
         this.entityFinder = new EntityFinder(processingEnv);
     }
@@ -151,25 +157,19 @@ public class ApplicationDomainProcessor extends AbstractPlatformAnnotationProces
             return false;
         }
 
-        // Gather information sources.
-        // We look only at input elements to determine the kind of build being performed (incremental / full) with respect to ApplicationDomain
-        // (regenerate / generate from scratch).
+        final List<EntityElement> inputEntities;
+        final Optional<ExtendApplicationDomainMirror> maybeInputExtension;
+        {
+            var pair = registeredEntitiesCollector.scanRoundInputs(roundEnv);
+            inputEntities = pair.getKey();
+            maybeInputExtension = pair.getValue();
+        }
 
-        // 1. input entities (all of them, not just domain ones)
-        final Set<EntityElement> inputEntities = roundEnv.getRootElements().stream()
-            .filter(elt -> entityFinder.isEntityType(elt.asType()))
-            .map(elt -> entityFinder.newEntityElement((TypeElement) elt))
-            .collect(Collectors.toCollection(TreeSet::new));
-
-        // 2. previously generated ApplicationDomain
         // removal of a registered entity will cause recompilation of ApplicationDomain
-        final Optional<ApplicationDomainElement> maybeAppDomainRootElt = findApplicationDomainInRound(roundEnv);
-
-        // 3. input extension
-        final Optional<ExtendApplicationDomainMirror> inputExtension = findApplicationDomainExtensionInRound(roundEnv);
+        final Optional<ApplicationDomainElement> maybeAppDomainRootElt = registeredEntitiesCollector.findApplicationDomainInRound(roundEnv);
 
         // this is an incremental build, but it doesn't affect us
-        if (inputEntities.isEmpty() && maybeAppDomainRootElt.isEmpty() && inputExtension.isEmpty()) {
+        if (inputEntities.isEmpty() && maybeAppDomainRootElt.isEmpty() && maybeInputExtension.isEmpty()) {
             printNote("There is nothing to do.");
             return false;
         }
@@ -181,12 +181,19 @@ public class ApplicationDomainProcessor extends AbstractPlatformAnnotationProces
         maybeAppDomainElt.ifPresentOrElse(elt -> {
             // incremental build <=> regenerate
             printNote("Found existing %s (%s registered entities)", elt.getSimpleName(), elt.entities().size() + elt.externalEntities().size());
-            regenerate(elt, inputEntities, inputExtension);
         }, /*else*/ () -> {
-            // generate from scratch
-            printNote("%s hasn't been generated yet.", APPLICATION_DOMAIN_SIMPLE_NAME);
-            generate(inputEntities, inputExtension);
+            printNote("Generating %s from scratch.", APPLICATION_DOMAIN_SIMPLE_NAME);
         });
+
+        final var registerableEntities = new TreeSet<EntityElement>();
+        final var registerableExtEntities = new TreeSet<EntityElement>();
+        boolean t = registeredEntitiesCollector.mergeRegisteredEntities(
+                inputEntities, maybeInputExtension, maybeAppDomainElt, registerableEntities::add, registerableExtEntities::add);
+        if (t) {
+            writeApplicationDomain(registerableEntities, registerableExtEntities);
+        } else {
+            printNote("There is nothing to do.");
+        }
 
         return false;
     }
@@ -194,105 +201,6 @@ public class ApplicationDomainProcessor extends AbstractPlatformAnnotationProces
     private void warnIfGeneric(EntityElement entity) {
         if (isGeneric(entity)) {
             printWarning("Entity %s won't be registered because it's a generic type.", entity.getQualifiedName());
-        }
-    }
-
-    private void generate(final Collection<EntityElement> inputEntities, final Optional<ExtendApplicationDomainMirror> inputExtension) {
-        printNote("Generating %s from scratch", APPLICATION_DOMAIN_SIMPLE_NAME);
-
-        final List<EntityElement> toRegister = inputEntities.stream()
-                .filter(EntityRegistrationUtils::isRegisterable)
-                .peek(this::warnIfGeneric)
-                .toList();
-
-        final List<EntityElement> externalEntities = inputExtension.map(mirr -> mirr.streamEntityElements(entityFinder).toList())
-                .orElseGet(List::of);
-
-        if (!externalEntities.isEmpty()) {
-            printNote("Found %s entities from extensions.".formatted(externalEntities.size()));
-        }
-        writeApplicationDomain(toRegister, externalEntities);
-    }
-
-    private void regenerate(
-            final ApplicationDomainElement appDomainElt,
-            final Collection<EntityElement> inputEntities, final Optional<ExtendApplicationDomainMirror> inputExtension)
-    {
-        final Set<EntityElement> toUnregister = new HashSet<>();
-        final Set<EntityElement> toRegister = new HashSet<>();
-
-        // without external ones
-        final Set<EntityElement> currentRegisteredEntities = new HashSet<>(appDomainElt.entities());
-
-        // analyse input entities
-        // * input entities -- are there any new ones we need to register?
-        toRegister.addAll(inputEntities.stream()
-                .filter(ent -> isRegisterable(ent) && !currentRegisteredEntities.contains(ent))
-                .peek(this::warnIfGeneric)
-                .toList());
-        // * unregisterable entities -- were any of them registered? (we need to unregister them)
-        toUnregister.addAll(inputEntities.stream()
-                .filter(ent -> currentRegisteredEntities.contains(ent) && !isRegisterable(ent))
-                .toList());
-
-        // analyse the input extension if it exists
-        final Set<EntityElement> currentExternalRegisteredEntities = new HashSet<>(appDomainElt.externalEntities());
-
-        final Set<EntityElement> externalEntitiesFromExtension = inputExtension.map(mirr -> mirr.streamEntityElements(entityFinder).collect(toSet()))
-                .orElseGet(Set::of);
-        // NOTE: we assume that external entities are always registerable entities, thus don't perform additional checks
-        // * are there any new external entities we need to register?
-        final List<EntityElement> externalToRegister = externalEntitiesFromExtension.stream()
-                .filter(ent -> !currentExternalRegisteredEntities.contains(ent))
-                .toList();
-        // * are there any external entities that need to be unregistered?
-        // only if an extension has been modified
-        final List<EntityElement> externalToUnregister = inputExtension.isEmpty() ? List.of() :
-            currentExternalRegisteredEntities.stream()
-                .filter(ent -> !externalEntitiesFromExtension.contains(ent))
-                .toList();
-
-        // analyse ApplicationDomain
-        // are there any missing entity types (e.g., due to removal)?
-        final List<ErrorType> missingRegisteredTypes = appDomainElt.errorTypes();
-
-        if (!toRegister.isEmpty()) {
-            printNote("Registering (%s): [%s]".formatted(
-                    toRegister.size(),
-                    CollectionUtil.toString(toRegister.stream().map(EntityElement::getSimpleName).toList(), ", ")));
-        }
-        if (!externalToRegister.isEmpty()) {
-            printNote("Registering external (%s): [%s]".formatted(
-                    externalToRegister.size(),
-                    CollectionUtil.toString(externalToRegister.stream().map(EntityElement::getSimpleName).toList(), ", ")));
-        }
-        if (!toUnregister.isEmpty()) {
-            printNote("Unregistering (%s): [%s]".formatted(
-                    toUnregister.size(), CollectionUtil.toString(toUnregister.stream().map(EntityElement::getSimpleName).toList(), ", ")));
-        }
-        if (!externalToUnregister.isEmpty()) {
-            printNote("Unregistering external (%s): [%s]".formatted(
-                    externalToUnregister.size(),
-                    CollectionUtil.toString(externalToUnregister.stream().map(EntityElement::getSimpleName).toList(), ", ")));
-        }
-        if (!missingRegisteredTypes.isEmpty()) {
-            printNote("Missing (%s): [%s]".formatted(missingRegisteredTypes.size(), CollectionUtil.toString(missingRegisteredTypes, ", ")));
-        }
-
-        // consider if we actually need to regenerate
-        if (!missingRegisteredTypes.isEmpty() || !toUnregister.isEmpty() || !externalToUnregister.isEmpty() // anything to exclude?
-                || !toRegister.isEmpty() || !externalToRegister.isEmpty()) {  // anything to include?
-            printNote("Regenerating %s", appDomainElt.getSimpleName());
-
-            final Set<EntityElement> registeredEntities = new TreeSet<>(appDomainElt.entities());
-            registeredEntities.removeAll(toUnregister);
-            registeredEntities.addAll(toRegister);
-
-            final Set<EntityElement> externalRegisteredEntities = new TreeSet<>(appDomainElt.externalEntities());
-            externalRegisteredEntities.removeAll(externalToUnregister);
-            externalRegisteredEntities.addAll(externalToRegister);
-
-            writeApplicationDomain(registeredEntities, externalRegisteredEntities);
         }
     }
 
@@ -420,53 +328,25 @@ public class ApplicationDomainProcessor extends AbstractPlatformAnnotationProces
         try {
             javaFile.writeTo(filer);
         } catch (final IOException ex) {
-            printError("Failed to generate %s: %s\n%s", getApplicationDomainQualifiedName(), ex.getMessage(), ExceptionUtils.getStackTrace(ex));
+            printError("Failed to generate %s: %s\n%s", appDomainFqn, ex.getMessage(), ExceptionUtils.getStackTrace(ex));
             return;
         }
 
         final int totalRegistered = registeredEntities.size() + externalEntities.size();
-        printNote("Generated %s with %s registered entities.", getApplicationDomainQualifiedName(), totalRegistered);
-    }
-
-    protected String getApplicationDomainQualifiedName() {
-        return "%s.%s".formatted(appDomainPkg, APPLICATION_DOMAIN_SIMPLE_NAME);
+        printNote("Generated %s with %s registered entities.", appDomainFqn, totalRegistered);
     }
 
     protected Optional<ApplicationDomainElement> findApplicationDomain() {
-        return elementFinder.findTypeElement(getApplicationDomainQualifiedName()).map(elt -> new ApplicationDomainElement(elt, entityFinder));
-    }
-
-    public static Optional<ApplicationDomainElement> findApplicationDomain(ProcessingEnvironment procEnv, EntityFinder entityFinder) {
-        final String qualName = "%s.%s".formatted(parseOptionFrom(procEnv.getOptions(), APP_DOMAIN_PKG_OPT_DESC), APPLICATION_DOMAIN_SIMPLE_NAME);
-        return entityFinder.findTypeElement(qualName).map(elt -> new ApplicationDomainElement(elt, entityFinder));
+        return elementFinder.findTypeElement(appDomainFqn).map(elt -> new ApplicationDomainElement(elt, entityFinder));
     }
 
     protected Optional<ApplicationDomainElement> findApplicationDomainInRound(final RoundEnvironment roundEnv) {
         return roundEnv.getRootElements().stream()
                 .filter(elt -> elt.getKind() == ElementKind.CLASS)
                 .map(elt -> (TypeElement) elt)
-                .filter(elt -> elt.getQualifiedName().contentEquals(getApplicationDomainQualifiedName()))
+                .filter(elt -> elt.getQualifiedName().contentEquals(appDomainFqn))
                 .findFirst()
                 .map(elt -> new ApplicationDomainElement(elt, entityFinder));
-    }
-
-    /**
-     * Returns an {@link Optional} describing the first encountered {@link ExtendApplicationDomain} annotation in the given round.
-     * @param roundEnv
-     * @return
-     */
-    protected Optional<ExtendApplicationDomainMirror> findApplicationDomainExtensionInRound(final RoundEnvironment roundEnv) {
-        final List<? extends Element> extensions = roundEnv.getRootElements().stream()
-                .filter(elt -> elt.getAnnotation(ExtendApplicationDomain.class) != null)
-                .toList();
-
-        if (extensions.size() > 1) {
-            extensions.forEach(elt -> messager.printMessage(Kind.ERROR, ERR_AT_MOST_ONE_EXTENSION_POINT_IS_ALLOWED, elt));
-        }
-
-        return extensions.stream()
-                .map(elt -> ExtendApplicationDomainMirror.fromAnnotation(elt.getAnnotation(ExtendApplicationDomain.class), entityFinder))
-                .findFirst();
     }
 
 }
