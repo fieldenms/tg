5defb6408bc27a4b3f169232c2c294b12f9c40ed
homedirectory <nullsink@posteo.eu>
2024-01-19 10:50:06 +0200

    #2058 Implement a test case to cover validation of changes to key members

--- platform-dao/src/test/java/ua/com/fielden/platform/entity/validation/KeyMemberChangeValidationTest.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/entity/validation/KeyMemberChangeValidationTest.java
@@ -1,30 +1,42 @@
 package ua.com.fielden.platform.entity.validation;
 
-import com.google.inject.Injector;
 import org.junit.Test;
-import ua.com.fielden.platform.entity.factory.EntityFactory;
-import ua.com.fielden.platform.ioc.ApplicationInjectorFactory;
+import ua.com.fielden.platform.entity.meta.MetaProperty;
 import ua.com.fielden.platform.sample.domain.TgAuthoriser;
 import ua.com.fielden.platform.sample.domain.TgOriginator;
+import ua.com.fielden.platform.sample.domain.TgOriginatorDetails;
 import ua.com.fielden.platform.sample.domain.TgPerson;
-import ua.com.fielden.platform.sample.domain.crit_gen.CriteriaGeneratorTestModule;
 import ua.com.fielden.platform.test_config.AbstractDaoTestCase;
+import ua.com.fielden.platform.utils.EntityUtils;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class KeyMemberChangeValidationTest extends AbstractDaoTestCase {
-    private final CriteriaGeneratorTestModule module = new CriteriaGeneratorTestModule();
-    private final Injector injector = new ApplicationInjectorFactory().add(module).getInjector();
-    private final EntityFactory entityFactory = injector.getInstance(EntityFactory.class);
 
     @Test
     public void warning_is_issued_upon_changing_property_key_in_persisted_entity_that_is_referenced() {
         final TgPerson person1 = co$(TgPerson.class).findByKey("Joe");
 
         person1.setKey("Donald");
-        assertNotNull(person1.getProperty("key").getFirstWarning());
+        MetaProperty<String> keyMp = person1.getProperty("key");
+        assertTrue(keyMp.isValid());
+        assertNotNull(keyMp.getFirstWarning());
         assertEquals("Donald", person1.getKey());
+    }
+
+    @Test
+    public void warning_is_issued_upon_changing_key_member_in_persisted_entity_that_is_referenced() {
+        final TgPerson person1 = co$(TgPerson.class).findByKey("Joe");
+        assertNotNull(person1);
+        final TgOriginator originator1 = co$(TgOriginator.class).findByKey(person1);
+        assertNotNull(originator1);
+
+        final TgPerson person2 = save(new_(TgPerson.class, "Richard").setActive(true));
+        originator1.setPerson(person2);
+        MetaProperty<TgPerson> personMp = originator1.getProperty("person");
+        assertTrue(personMp.isValid());
+        assertNotNull(personMp.getFirstWarning());
+        assertTrue(EntityUtils.areEqual(person2, originator1.getPerson()));
     }
 
     @Override
@@ -32,8 +44,9 @@ public class KeyMemberChangeValidationTest extends AbstractDaoTestCase {
         super.populateDomain();
 
         final TgPerson person1 = save(new_(TgPerson.class, "Joe").setActive(true));
-        save(new_(TgAuthoriser.class).setActive(true).setPerson(person1));
-        save(new_(TgOriginator.class).setActive(true).setPerson(person1));
+        save(new_(TgAuthoriser.class).setActive(false).setPerson(person1));
+        final TgOriginator originator1 = save(new_(TgOriginator.class).setActive(true).setPerson(person1));
+        save(new_(TgOriginatorDetails.class).setOriginator(originator1).setActive(true));
     }
 
 }
