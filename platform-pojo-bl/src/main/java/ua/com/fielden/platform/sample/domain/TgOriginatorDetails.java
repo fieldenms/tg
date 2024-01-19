73d3f9ac16ccd720e1ebe9270e3980415187a7b9
homedirectory <nullsink@posteo.eu>
2024-01-19 10:49:27 +0200

    #2058 Create a new entity type for testing purposes

---
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/TgOriginatorDetails.java
@@ -0,0 +1,52 @@
+package ua.com.fielden.platform.sample.domain;
+
+import ua.com.fielden.platform.entity.ActivatableAbstractEntity;
+import ua.com.fielden.platform.entity.DynamicEntityKey;
+import ua.com.fielden.platform.entity.annotation.*;
+
+@KeyType(DynamicEntityKey.class)
+@KeyTitle("Originator Details")
+@CompanionObject(ITgOriginatorDetails.class)
+@MapEntityTo
+public class TgOriginatorDetails extends ActivatableAbstractEntity<DynamicEntityKey> {
+
+    @IsProperty
+    @MapTo
+    @Title(value = "Originator")
+    @CompositeKeyMember(1)
+    private TgOriginator originator;
+
+    @IsProperty(length = 255)
+    @MapTo
+    @Title(value = "Comment")
+    @CompositeKeyMember(2)
+    @Optional
+    private String comment;
+
+    @Observable
+    public TgOriginatorDetails setComment(final String comment) {
+        this.comment = comment;
+        return this;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    @Observable
+    public TgOriginatorDetails setOriginator(final TgOriginator originator) {
+        this.originator = originator;
+        return this;
+    }
+
+    public TgOriginator getOriginator() {
+        return originator;
+    }
+
+    @Observable
+    public TgOriginatorDetails setActive(final boolean active) {
+        super.setActive(active);
+        return this;
+    }
+
+}
