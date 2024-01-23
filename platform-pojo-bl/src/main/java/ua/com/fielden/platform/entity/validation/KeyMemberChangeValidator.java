2285ea3a7caecdfbd7b7378b670f7119c33b0b87
homedirectory <nullsink@posteo.eu>
2024-01-23 13:25:31 +0200

    #2058 Code style and documentation improvements

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/validation/KeyMemberChangeValidator.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/validation/KeyMemberChangeValidator.java
@@ -1,16 +1,11 @@
 package ua.com.fielden.platform.entity.validation;
 
-import com.google.inject.Inject;
-import ua.com.fielden.platform.dao.IEntityDao;
 import ua.com.fielden.platform.entity.AbstractEntity;
-import ua.com.fielden.platform.entity.factory.ICompanionObjectFinder;
+import ua.com.fielden.platform.entity.exceptions.EntityException;
 import ua.com.fielden.platform.entity.meta.MetaProperty;
 import ua.com.fielden.platform.entity.meta.impl.AbstractBeforeChangeEventHandler;
 import ua.com.fielden.platform.error.Result;
-import ua.com.fielden.platform.ref_hierarchy.ReferenceHierarchy;
-import ua.com.fielden.platform.ref_hierarchy.ReferenceHierarchyEntry;
-import ua.com.fielden.platform.ref_hierarchy.ReferenceHierarchyLevel;
-import ua.com.fielden.platform.ref_hierarchy.TypeLevelHierarchyEntry;
+import ua.com.fielden.platform.ref_hierarchy.*;
 import ua.com.fielden.platform.types.tuples.T2;
 
 import java.lang.annotation.Annotation;
@@ -29,8 +24,12 @@ import static ua.com.fielden.platform.utils.MessageUtils.singleOrPlural;
 
 
 /**
- * A validator for "key" and key-member properties of persistent entities that produces a warning upon value change.
- * Intended to be used on reference (aka master) data.
+ * A validator for key properties that produces a warning upon modification of the key's value.
+ * Applicable only to persisted entities (i.e., saved instances of persistent entity types).
+ * Intended to be used on reference (aka master) data. The rationale is to make the originator of the change aware
+ * of the consequences -- namely, that their change will affect all the data referencing the entity being changed.
+ * <p>
+ * This validator is applied by default. Therefore, it is not required to specify it in property definitions explicitly.
  *
  * @author TG Team
  */
@@ -38,28 +37,20 @@ public class KeyMemberChangeValidator extends AbstractBeforeChangeEventHandler<O
 
     public static final String KEY_MEMBER_CHANGE_MESSAGE = "Saving this change will be reflected in all the data referencing this %s.";
 
-    private final ICompanionObjectFinder coFinder;
-
-    @Inject
-    public KeyMemberChangeValidator(final ICompanionObjectFinder coFinder) {
-        this.coFinder = coFinder;
-    }
-
     @Override
     public Result handle(final MetaProperty<Object> property, final Object newValue, final Set<Annotation> mutatorAnnotations) {
         final AbstractEntity<?> entity = property.getEntity();
-        if (!entity.isPersistent() || !entity.isPersisted())
+        if (!entity.isPersistent() || !entity.isPersisted()) {
             return successful(newValue);
+        }
 
-        IEntityDao<ReferenceHierarchy> refHierarchyCo = coFinder.find(ReferenceHierarchy.class);
-        ReferenceHierarchy refChy = refHierarchyCo.new_();
-        refChy.setLoadedHierarchyLevel(ReferenceHierarchyLevel.REFERENCE_BY_INSTANCE);
-        refChy.setRefEntityId(entity.getId());
-        refChy.setRefEntityType(entity.getType().getName());
-        ReferenceHierarchy savedRefChy = refHierarchyCo.save(refChy);
-        List<AbstractEntity<?>> generatedChy = savedRefChy.getGeneratedHierarchy();
+        final IReferenceHierarchy coReferenceHierarchy = co(ReferenceHierarchy.class);
+        final var refChy = coReferenceHierarchy.save(coReferenceHierarchy.new_()
+                .setLoadedHierarchyLevel(ReferenceHierarchyLevel.REFERENCE_BY_INSTANCE)
+                .setRefEntityId(entity.getId())
+                .setRefEntityType(entity.getType().getName()));
 
-        List<TypeLevelHierarchyEntry> typeEntries = generatedChy.stream()
+        final List<TypeLevelHierarchyEntry> typeEntries = refChy.getGeneratedHierarchy().stream()
                 .map(x -> {
                     if (x instanceof ReferenceHierarchyEntry entry && ReferenceHierarchyLevel.REFERENCED_BY == entry.getHierarchyLevel())
                         return entry;
@@ -79,7 +70,7 @@ public class KeyMemberChangeValidator extends AbstractBeforeChangeEventHandler<O
         return warningEx(newValue, KEY_MEMBER_CHANGE_MESSAGE.formatted(entityTitle), makeMessageEx(entity, typeEntries));
     }
 
-    private String makeMessageEx(final AbstractEntity<?> entity, final List<TypeLevelHierarchyEntry> entries) {
+    private static String makeMessageEx(final AbstractEntity<?> entity, final List<TypeLevelHierarchyEntry> entries) {
         final T2<Integer, Integer> lengths = entries.stream()
                 .map(entry -> t2(entry.getEntityType().length(), entry.getNumberOfEntities().toString().length()))
                 .reduce(t2(0, 0), (accum, val) -> t2(max(accum._1, val._1), max(accum._2, val._2)));
@@ -89,7 +80,7 @@ public class KeyMemberChangeValidator extends AbstractBeforeChangeEventHandler<O
                     try {
                         entityType = (Class<? extends AbstractEntity<?>>) Class.forName(entry.getEntityType());
                     } catch (ClassNotFoundException|ClassCastException e) {
-                        throw failure(new IllegalStateException("Entity type [%s] could not be found.".formatted(entry.getEntityType())));
+                        throw failure(new EntityException("Entity type [%s] could not be found.".formatted(entry.getEntityType())));
                     }
                     return makeEntryMsg(getEntityTitleAndDesc(entityType).getKey(), entry.getNumberOfEntities().toString(), lengths);
                 })
@@ -103,7 +94,7 @@ public class KeyMemberChangeValidator extends AbstractBeforeChangeEventHandler<O
                 columns, rows);
     }
 
-    private static String makeEntryMsg(final String s1, String s2, final T2<Integer, Integer> lengths) {
+    private static String makeEntryMsg(final String s1, final String s2, final T2<Integer, Integer> lengths) {
         final String padStr = "\u00A0";
         return format("<tt>%s\u00A0%s</tt>",
                 rightPad(s1, lengths._1 + 2, padStr),
