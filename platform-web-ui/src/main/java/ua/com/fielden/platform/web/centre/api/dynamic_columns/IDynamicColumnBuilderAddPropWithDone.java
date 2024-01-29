cd0c60a0ce00cf791ced6b60c66de9e3f1e1ff15
Oleh Maikovych <oleh@fielden.com.au>
2024-01-29 18:04:33 +0200

    #2149 Adjusted documentation for dynamic column configuration API interface.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/dynamic_columns/IDynamicColumnBuilderAddPropWithDone.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/dynamic_columns/IDynamicColumnBuilderAddPropWithDone.java
@@ -2,6 +2,12 @@ package ua.com.fielden.platform.web.centre.api.dynamic_columns;
 
 import ua.com.fielden.platform.web.centre.api.IDynamicColumnConfig;
 
+/**
+ * A contract that adds a new dynamic column with a specific group property value or finishes the dynamic column configuration.
+ *
+ * @author TG Team
+ *
+ */
 public interface IDynamicColumnBuilderAddPropWithDone extends IDynamicColumnBuilderAddProp {
 
     IDynamicColumnConfig done();
