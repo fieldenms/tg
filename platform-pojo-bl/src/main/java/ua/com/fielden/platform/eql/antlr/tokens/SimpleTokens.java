c3c8b4c3be3716620231e6d676d1dc6e74669d7b
homedirectory <nullsink@posteo.eu>
2024-02-29 10:36:07 +0200

    #2196 Friendlier text representation of simple tokens

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/eql/antlr/tokens/SimpleTokens.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/eql/antlr/tokens/SimpleTokens.java
@@ -15,7 +15,22 @@ public final class SimpleTokens {
      * Returns a simple, non-parameterised token corresponding to the given type.
      */
     public static Token token(int type) {
-        return tokens.computeIfAbsent(type, typ -> new CommonToken(typ, EQLLexer.VOCABULARY.getDisplayName(typ)));
+        final int maxTokenType = EQLLexer.VOCABULARY.getMaxTokenType();
+        if (type > maxTokenType) {
+            throw new IllegalArgumentException(
+                    "Token type %s does not belong to the vocabulary (max. token type = %s)".formatted(
+                            type, maxTokenType));
+        }
+
+        return tokens.computeIfAbsent(type, typ -> {
+            String name = EQLLexer.VOCABULARY.getLiteralName(typ);
+            if (name != null) {
+                name = name.replace("'", "");
+            } else {
+                name = EQLLexer.VOCABULARY.getSymbolicName(typ);
+            }
+            return new CommonToken(typ, name != null ? name : Integer.toString(type));
+        });
     }
 
     private SimpleTokens() {}
