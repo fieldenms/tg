a39669a374b678abf1f8391f83de1f69c7b18b24
homedirectory <nullsink@posteo.eu>
2024-03-01 12:36:32 +0200

    #2196 Lay out a skeleton for compilation of `select` expressions

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/antlr/SelectVisitor.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/antlr/SelectVisitor.java
@@ -1,7 +1,11 @@
 package ua.com.fielden.platform.eql.antlr;
 
-
-import java.util.List;
+import ua.com.fielden.platform.eql.stage0.QueryModelToStage1Transformer;
+import ua.com.fielden.platform.eql.stage1.conditions.Conditions1;
+import ua.com.fielden.platform.eql.stage1.sources.IJoinNode1;
+import ua.com.fielden.platform.eql.stage1.sundries.GroupBys1;
+import ua.com.fielden.platform.eql.stage1.sundries.Yields1;
+import ua.com.fielden.platform.eql.stage2.sources.IJoinNode2;
 
 import static ua.com.fielden.platform.eql.antlr.EQLParser.*;
 
@@ -13,6 +17,27 @@ final class SelectVisitor extends AbstractEqlVisitor<EqlCompilationResult.Select
 
     @Override
     public EqlCompilationResult.Select visitSelect(final SelectContext ctx) {
+        return new EqlCompilationResult.Select(
+                compileJoinRoot(ctx),
+                compileWhere(ctx.where()),
+                compileYields(ctx.selectEnd()),
+                compileGroups(ctx.groupBy()),
+                false);
+    }
+
+    private GroupBys1 compileGroups(final GroupByContext groupByContext) {
+        throw new UnsupportedOperationException();
+    }
+
+    private Yields1 compileYields(final SelectEndContext selectEndContext) {
+        throw new UnsupportedOperationException();
+    }
+
+    private IJoinNode1<? extends IJoinNode2<?>> compileJoinRoot(final SelectContext ctx) {
+        throw new UnsupportedOperationException();
+    }
+
+    private Conditions1 compileWhere(final WhereContext where) {
         throw new UnsupportedOperationException();
     }
 
