a3665464522c552612d79f5e7c0f2e9514b70388
homedirectory <nullsink@posteo.eu>
2024-03-01 11:30:55 +0200

    #2196 Move common token-handling code to the abstract ANTLR visitor
    This is a part of migration from AbstractTokenBuilder.

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/antlr/AbstractEqlVisitor.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/antlr/AbstractEqlVisitor.java
@@ -2,12 +2,57 @@ package ua.com.fielden.platform.eql.antlr;
 
 import ua.com.fielden.platform.eql.stage0.QueryModelToStage1Transformer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static ua.com.fielden.platform.eql.meta.EqlEntityMetadataGenerator.N;
+import static ua.com.fielden.platform.eql.meta.EqlEntityMetadataGenerator.Y;
+
 abstract class AbstractEqlVisitor<T> extends EQLBaseVisitor<T> {
 
     protected final QueryModelToStage1Transformer transformer;
 
     AbstractEqlVisitor(QueryModelToStage1Transformer transformer) {
         this.transformer = transformer;
+    }
+
+
+    protected Object getParamValue(final String paramName) {
+        final Object paramValue = transformer.getParamValue(paramName);
+        if (paramValue != null) {
+            return preprocessValue(paramValue);
+        } else {
+            //TODO throw exception for all contexts except YIELD (as NULL operands can't be used in SQL conditions, groupings, etc).
+            return null;
+        }
+    }
+
+    protected static Object preprocessValue(final Object value) {
+        if (value != null && (value.getClass().isArray() || value instanceof Collection<?>)) {
+            final Iterable<?> iterable = value.getClass().isArray() ? Arrays.asList((Object[]) value) : (Collection<?>) value;
+            final List<Object> values = new ArrayList<>();
+            for (final Object object : iterable) {
+                final Object furtherPreprocessed = preprocessValue(object);
+                if (furtherPreprocessed instanceof List) {
+                    values.addAll((List<?>) furtherPreprocessed);
+                } else {
+                    values.add(furtherPreprocessed);
+                }
+            }
+            return values;
+        } else {
+            return convertValue(value);
+        }
+    }
+
+    /** Ensures that values of boolean types are converted properly. */
+    protected static Object convertValue(final Object value) {
+        if (value instanceof Boolean) {
+            return (boolean) value ? Y : N;
+        }
+        return value;
     }
 
 }
