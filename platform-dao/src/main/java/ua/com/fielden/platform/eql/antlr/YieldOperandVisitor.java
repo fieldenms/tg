0a043243148a6622d450c25814b5ebbe10f388a2
homedirectory <nullsink@posteo.eu>
2024-03-05 11:37:11 +0200

    #2196 Extend yield compilation to cover expressions

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/antlr/YieldOperandVisitor.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/antlr/YieldOperandVisitor.java
@@ -1,11 +1,18 @@
 package ua.com.fielden.platform.eql.antlr;
 
 import ua.com.fielden.platform.eql.stage0.QueryModelToStage1Transformer;
+import ua.com.fielden.platform.eql.stage1.operands.CompoundSingleOperand1;
+import ua.com.fielden.platform.eql.stage1.operands.Expression1;
 import ua.com.fielden.platform.eql.stage1.operands.ISingleOperand1;
 import ua.com.fielden.platform.eql.stage1.operands.functions.*;
 import ua.com.fielden.platform.eql.stage2.operands.ISingleOperand2;
+import ua.com.fielden.platform.utils.StreamUtils;
 
+import java.util.List;
 import java.util.function.Function;
+
+import static ua.com.fielden.platform.eql.antlr.EQLParser.*;
+import static ua.com.fielden.platform.eql.antlr.SingleOperandVisitor.toArithmeticalOperator;
 
 final class YieldOperandVisitor extends AbstractEqlVisitor<ISingleOperand1<? extends ISingleOperand2<?>>> {
 
@@ -14,23 +21,32 @@ final class YieldOperandVisitor extends AbstractEqlVisitor<ISingleOperand1<? ext
     }
 
     @Override
-    public ISingleOperand1<? extends ISingleOperand2<?>> visitYieldOperand_SingleOperand(final EQLParser.YieldOperand_SingleOperandContext ctx) {
+    public ISingleOperand1<? extends ISingleOperand2<?>> visitYieldOperand_SingleOperand(final YieldOperand_SingleOperandContext ctx) {
         return ctx.singleOperand().accept(new SingleOperandVisitor(transformer));
     }
 
     @Override
-    public ISingleOperand1<? extends ISingleOperand2<?>> visitYieldOperand_CountAll(final EQLParser.YieldOperand_CountAllContext ctx) {
+    public ISingleOperand1<? extends ISingleOperand2<?>> visitYieldOperand_CountAll(final YieldOperand_CountAllContext ctx) {
         return CountAll1.INSTANCE;
     }
 
     @Override
-    public ISingleOperand1<? extends ISingleOperand2<?>> visitYieldOperandFunction(final EQLParser.YieldOperandFunctionContext ctx) {
+    public ISingleOperand1<? extends ISingleOperand2<?>> visitYieldOperandFunction(final YieldOperandFunctionContext ctx) {
         final var argument = ctx.singleOperand().accept(new SingleOperandVisitor(transformer));
         return chooseFunction(ctx.yieldOperandFunctionName()).apply(argument);
     }
 
+    @Override
+    public ISingleOperand1<? extends ISingleOperand2<?>> visitYieldOperandExpr(final YieldOperandExprContext ctx) {
+        final var first = ctx.first.accept(this);
+        final List<CompoundSingleOperand1> rest = StreamUtils.zip(ctx.rest, ctx.operators, (rand, op) -> {
+            return new CompoundSingleOperand1(rand.accept(this), toArithmeticalOperator(op));
+        }).toList();
+        return new Expression1(first, rest);
+    }
+
     private static Function<ISingleOperand1<? extends ISingleOperand2<?>>, SingleOperandFunction1<?>>
-    chooseFunction(final EQLParser.YieldOperandFunctionNameContext ctx) {
+    chooseFunction(final YieldOperandFunctionNameContext ctx) {
         return switch (ctx.token.getType()) {
             case EQLLexer.MAXOF -> MaxOf1::new;
             case EQLLexer.MINOF -> MinOf1::new;
