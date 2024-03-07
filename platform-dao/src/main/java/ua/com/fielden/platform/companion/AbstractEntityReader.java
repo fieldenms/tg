88ed88fcb6fad20ff2621f69ea905daef622f6dd
homedirectory <nullsink@posteo.eu>
2024-03-07 14:04:15 +0200

    Include query text in exception message to facilitate debugging

--- platform-dao/src/main/java/ua/com/fielden/platform/companion/AbstractEntityReader.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/companion/AbstractEntityReader.java
@@ -298,26 +298,29 @@ public abstract class AbstractEntityReader<T extends AbstractEntity<?>> implemen
     }
 
     /**
-     * A private helper method.
-     * 
-     * @param filtered -- <code>true</code> to turn filtering on.
-     * @param id
-     * @param fetchModel
-     * @return
+     * @param filtered -- controls whether filtering is on
      */
     private T fetchOneEntityInstance(final boolean filtered, final Long id, final fetch<T> fetchModel) {
         if (id == null) {
             throw new EntityCompanionException(format(ERR_MISSING_ID_VALUE, getEntityType().getName()));
         }
+
+        final EntityResultQueryModel<T> query = select(getEntityType()).where().prop(ID).eq().val(id).model()
+                .setFilterable(filtered);
+        final var qem = instrumented()
+                ? from(query).with(fetchModel).model()
+                : from(query).with(fetchModel).lightweight().model();
+
         try {
-            final EntityResultQueryModel<T> query = select(getEntityType()).where().prop(ID).eq().val(id).model();
-            query.setFilterable(filtered);
-            return getEntity(instrumented() ? from(query).with(fetchModel).model(): from(query).with(fetchModel).lightweight().model());
+            return getEntity(qem);
         } catch (final Exception e) {
-            throw new EntityCompanionException(format("Could not fetch one entity of type [%s].", getEntityType().getName()), e);
+            throw new EntityCompanionException("""
+                    Could not fetch one entity of type [%s].
+                    Query: %s
+                    """.formatted(getEntityType().getName(), qem), e);
         }
     }
-    
+
     /**
      * Implements pagination based on the provided query.
      *
