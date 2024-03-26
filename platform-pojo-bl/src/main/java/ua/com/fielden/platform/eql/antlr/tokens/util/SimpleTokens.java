7e34ae7e69f316793332c1e649375c1eae1ad269
homedirectory <nullsink@posteo.eu>
2024-03-26 12:16:34 +0200

    #2196 Document `SimpleTokens` and use a more fitting exception type

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/eql/antlr/tokens/util/SimpleTokens.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/eql/antlr/tokens/util/SimpleTokens.java
@@ -3,20 +3,33 @@ package ua.com.fielden.platform.eql.antlr.tokens.util;
 import org.antlr.v4.runtime.CommonToken;
 import org.antlr.v4.runtime.Token;
 import ua.com.fielden.platform.eql.antlr.EQLLexer;
+import ua.com.fielden.platform.eql.antlr.exceptions.EqlSyntaxException;
 
+/**
+ * Provides access to all <i>simple</i> (non-parameterised) EQL tokens.
+ */
 public final class SimpleTokens {
 
-    // Use a single Token instance for all tokens of a given type (denoted by an integer).
-    // One consequence is that tokens don't have "position" information, but it is not needed anyway.
+    /**
+     * Storage of singleton instances for <i>simple</i> tokens.
+     * <p>
+     * Array is used instead of a Map for efficiency. Token types are used as array indices, taking advantage of token
+     * types being sequential integers.
+     * <p>
+     * <b>Caveat</b>: resulting token instances don't have "position" information (which is not needed anyway).
+     * <p>
+     * <b>Caveat</b>: although this array has room for all EQL tokens, it is used only for <i>simple</i> ones
+     * (parameterised tokens are created via custom ANTLR token types).
+     */
     private static final Token[] tokens = new Token[EQLLexer.VOCABULARY.getMaxTokenType() + 1];
 
     /**
      * Returns a simple, non-parameterised token corresponding to the given type.
      */
-    public static Token token(int type) {
+    public static Token token(final int type) {
         final int maxTokenType = EQLLexer.VOCABULARY.getMaxTokenType();
         if (type < 0 || type > maxTokenType) {
-            throw new IllegalArgumentException(
+            throw new EqlSyntaxException(
                     "Token type %s does not belong to the vocabulary (valid range: [0, %s])".formatted(
                             type, maxTokenType));
         }
