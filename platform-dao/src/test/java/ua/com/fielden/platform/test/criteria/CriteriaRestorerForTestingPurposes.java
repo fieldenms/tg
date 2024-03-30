4a06f430ad35ea7a0dd5b932074739c1debdb648
jhou-pro <jhou.pro@gmail.com>
2024-03-30 03:43:31 +0200

    #2212 Added jdocs and minor improvements for crit testing API.

--- platform-dao/src/test/java/ua/com/fielden/platform/test/criteria/CriteriaRestorerForTestingPurposes.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/test/criteria/CriteriaRestorerForTestingPurposes.java
@@ -2,6 +2,7 @@ package ua.com.fielden.platform.test.criteria;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.google.inject.Inject;
 
@@ -12,11 +13,28 @@ import ua.com.fielden.platform.entity.functional.centre.CentreContextHolder;
 import ua.com.fielden.platform.entity_centre.review.criteria.EnhancedCentreEntityQueryCriteria;
 import ua.com.fielden.platform.web.utils.ICriteriaEntityRestorer;
 
+/**
+ * {@link ICriteriaEntityRestorer} implementation for tests.
+ * <p>
+ * With this implementation we use standard criteria generation from {@link ICentreDomainTreeManagerAndEnhancer} instance,
+ * that was created and put into {@code centres} map by its root type.
+ * <p>
+ * All preparations for criteria entity mocking is done through {@link ICriteriaTestUtils#mockSelectionCrit(Class, java.util.function.Consumer)} method.
+ * <p>
+ * This implementation should be bound as singleton in test-related IoC module.
+ */
 public class CriteriaRestorerForTestingPurposes implements ICriteriaEntityRestorer {
-
-    static final String TYPE_KEY = "@@type";
+    protected static final String TYPE_KEY = "@@type";
+    /**
+     * Singleton {@link ICriteriaGenerator} instance for criteria generation.
+     */
     private final ICriteriaGenerator criteriaGenerator;
-    private final Map<Class<? extends AbstractEntity<?>>, ICentreDomainTreeManagerAndEnhancer> centres = new HashMap<>();
+    /**
+     * Cached centre managers by their root types.
+     * <p>
+     * No need to have {@link ConcurrentHashMap} here because every VM, that runs tests, do that sequentially. Also, {@code centreManager}s replacements are possible from test to test.
+     */
+    private final Map<Class<? extends AbstractEntity<?>>, ICentreDomainTreeManagerAndEnhancer> centreManagersByType = new HashMap<>();
 
     @Inject
     public CriteriaRestorerForTestingPurposes(final ICriteriaGenerator criteriaGenerator) {
@@ -24,15 +42,18 @@ public class CriteriaRestorerForTestingPurposes implements ICriteriaEntityRestor
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public EnhancedCentreEntityQueryCriteria<?, ?> restoreCriteriaEntity(final CentreContextHolder centreContextHolder) {
-        final Class<? extends AbstractEntity<?>> type = (Class<? extends AbstractEntity<?>>) centreContextHolder.getCustomObject().get(TYPE_KEY);
-        return criteriaGenerator.generateCentreQueryCriteria(centres.get(type));
+        return criteriaGenerator.generateCentreQueryCriteria(centreManagersByType.get(centreContextHolder.getCustomObject().get(TYPE_KEY)));
     }
 
-    public CriteriaRestorerForTestingPurposes addCentre(final Class<? extends AbstractEntity<?>> type, final ICentreDomainTreeManagerAndEnhancer centre) {
-        this.centres.put(type, centre);
-        return this;
+    /**
+     * Adds {@code centreManager} by its {@code type} to this instance. Replaces previous centre manager, if there was such.
+     * 
+     * @param type
+     * @param centreManager
+     */
+    public void addCentre(final Class<? extends AbstractEntity<?>> type, final ICentreDomainTreeManagerAndEnhancer centreManager) {
+        centreManagersByType.put(type, centreManager);
     }
 
 }
--- platform-dao/src/test/java/ua/com/fielden/platform/test/criteria/ICriteriaTestUtils.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/test/criteria/ICriteriaTestUtils.java
@@ -7,30 +7,72 @@ import static ua.com.fielden.platform.utils.CollectionUtil.setOf;
 
 import java.util.function.Consumer;
 
+import com.google.inject.Injector;
+
 import ua.com.fielden.platform.domaintree.centre.ICentreDomainTreeManager.ICentreDomainTreeManagerAndEnhancer;
 import ua.com.fielden.platform.domaintree.centre.impl.CentreDomainTreeManagerAndEnhancer;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.factory.EntityFactory;
 import ua.com.fielden.platform.entity.functional.centre.CentreContextHolder;
 import ua.com.fielden.platform.entity_centre.review.criteria.EnhancedCentreEntityQueryCriteria;
+import ua.com.fielden.platform.entity_centre.review.criteria.EntityQueryCriteria;
 import ua.com.fielden.platform.web.utils.ICriteriaEntityRestorer;
 
+/**
+ * Interface for constructing mocked selection criteria entities for tests.
+ * The main method is {@link #mockSelectionCrit(Class, Consumer)} that creates mocked criteria entity for some root type and its centre enhancements.
+ */
 public interface ICriteriaTestUtils {
 
+    /**
+     * Creates mocked criteria entity for type {@code root}.
+     * <p>
+     * It is expected that following methods will be mocked here:
+     * <ul>
+     *   <li>{@link EnhancedCentreEntityQueryCriteria#centreContextHolder()} with help of {@link #createCentreContextHolderFor(Class)}</li>
+     *   <li>{@link EnhancedCentreEntityQueryCriteria#getEntityClass()} with {@code root} instance</li>
+     * </ul>
+     */
     <T extends AbstractEntity<?>> EnhancedCentreEntityQueryCriteria mockSelectionCrit(final Class<T> root);
+    /**
+     * Binds {@code centreManager} to {@code mockSelectionCrit}.
+     * <p>
+     * It is expected that {@link EntityQueryCriteria#getCentreDomainTreeMangerAndEnhancer()} method will be mocked here with help of {@link #createCentreManagerFor(Class)}
+     */
     void mockSelectionCritBindCentreManager(final EnhancedCentreEntityQueryCriteria mockSelectionCrit, final ICentreDomainTreeManagerAndEnhancer centreManager);
+    /**
+     * Method to get instance from {@link Injector} by its type.
+     */
     <T> T getInstance(final Class<T> type);
 
+    /**
+     * Creates {@link CentreContextHolder} with {@code root} type indication. Used later for criteria restoration in {@link CriteriaRestorerForTestingPurposes}.
+     */
     default <T extends AbstractEntity<?>> CentreContextHolder createCentreContextHolderFor(final Class<T> root) {
         return new CentreContextHolder().setCustomObject(mapOf(t2(TYPE_KEY, root)));
     }
 
+    /**
+     * Creates empty centre manager and registers it by {@code root} type in {@link CriteriaRestorerForTestingPurposes}.
+     */
     default <T extends AbstractEntity<?>> ICentreDomainTreeManagerAndEnhancer createCentreManagerFor(final Class<T> root) {
         final ICentreDomainTreeManagerAndEnhancer centreManager = new CentreDomainTreeManagerAndEnhancer(getInstance(EntityFactory.class), setOf(root));
         ((CriteriaRestorerForTestingPurposes) getInstance(ICriteriaEntityRestorer.class)).addCentre(root, centreManager);
         return centreManager;
     }
 
+    /**
+     * Creates mocked selection criteria for {@code root} entity type.
+     * <p>
+     * It is expected that following methods will be mocked:
+     * <ul>
+     *   <li>{@link EnhancedCentreEntityQueryCriteria#centreContextHolder()}</li>
+     *   <li>{@link EnhancedCentreEntityQueryCriteria#getEntityClass()}</li>
+     *   <li>{@link EntityQueryCriteria#getCentreDomainTreeMangerAndEnhancer()}</li>
+     * </ul>
+     * 
+     * @param enhanceCentreManager -- function to adjust empty centre manager from which criteria entity will be created
+     */
     default <T extends AbstractEntity<?>> EnhancedCentreEntityQueryCriteria mockSelectionCrit(final Class<T> root, final Consumer<ICentreDomainTreeManagerAndEnhancer> enhanceCentreManager) {
         final var mockedSelectionCrit = mockSelectionCrit(root);
         final var centreManager = createCentreManagerFor(root);
