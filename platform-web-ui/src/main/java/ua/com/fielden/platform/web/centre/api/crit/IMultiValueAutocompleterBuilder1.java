5433347da8768e2d91a899e97b012a838e3759e1
homedirectory <nullsink@posteo.eu>
2024-04-01 09:40:37 +0300

    #2223 String to CharSequence refactoring for Pair and T2

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/crit/IMultiValueAutocompleterBuilder1.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/crit/IMultiValueAutocompleterBuilder1.java
@@ -1,7 +1,6 @@
 package ua.com.fielden.platform.web.centre.api.crit;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
-import ua.com.fielden.platform.processors.metamodel.IConvertableToPath;
 import ua.com.fielden.platform.types.tuples.T2;
 import ua.com.fielden.platform.utils.Pair;
 import ua.com.fielden.platform.web.centre.api.crit.defaults.IMultiStringDefaultValueAssigner;
@@ -31,13 +30,9 @@ public interface IMultiValueAutocompleterBuilder1<T extends AbstractEntity<?>> e
      * <p>
      * If this method is used it overrides the default inclusion of property <code>desc</code>.
      * Therefore, it is required to include <code>desc</code> in the list of properties if it should also be displayed.
-     *
-     * @param propNameAndLightOption
-     * @param morePropNameAndLightOption
-     * @return
      */
     @SuppressWarnings("unchecked")
-    IMultiStringDefaultValueAssigner<T> withProps(final Pair<String, Boolean> propNameAndLightOption, final Pair<String, Boolean>... morePropNameAndLightOption);
+    IMultiStringDefaultValueAssigner<T> withProps(final Pair<? extends CharSequence, Boolean> propNameAndLightOption, final Pair<? extends CharSequence, Boolean>... morePropNameAndLightOption);
 
     /**
      * Method to specify a complete set of properties to be displayed as part of the autocompleted list of matched values.
@@ -46,17 +41,14 @@ public interface IMultiValueAutocompleterBuilder1<T extends AbstractEntity<?>> e
      * <p>
      * If this method is used it overrides the default inclusion of property <code>desc</code>.
      * Therefore, it is required to include <code>desc</code> in the list of properties if it should also be displayed.
-     *
-     * @param propNameAndLightOption
-     * @param morePropNameAndLightOption
-     * @return
      */
     @SuppressWarnings("unchecked")
-    default IMultiStringDefaultValueAssigner<T> withProps(final T2<IConvertableToPath, Boolean> propNameAndLightOption, final T2<IConvertableToPath, Boolean>... morePropNameAndLightOption) {
+    default IMultiStringDefaultValueAssigner<T> withProps(final T2<? extends CharSequence, Boolean> propNameAndLightOption, final T2<? extends CharSequence, Boolean>... morePropNameAndLightOption) {
         return withProps(
-            pair(propNameAndLightOption._1.toPath(), propNameAndLightOption._2),
-            Stream.of(morePropNameAndLightOption).map(t2 -> pair(t2._1.toPath(), t2._2)).toArray(Pair[]::new)
+                pair(propNameAndLightOption._1, propNameAndLightOption._2),
+                Stream.of(morePropNameAndLightOption).map(t2 -> t2.map(Pair::pair)).toArray(Pair[]::new)
         );
     }
+
 
 }
