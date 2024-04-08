8b9a9208f2b293476cece24f71cd2831bc05a520
homedirectory <nullsink@posteo.eu>
2024-04-08 13:29:54 +0300

    Add a stream utility to construct enumerated streams

--- platform-pojo-bl/src/test/java/ua/com/fielden/platform/utils/StreamUtilsTest.java
+++ platform-pojo-bl/src/test/java/ua/com/fielden/platform/utils/StreamUtilsTest.java
@@ -333,4 +333,21 @@ public class StreamUtilsTest {
         assertOptEquals(false, areAllEqual(IntStream.of(m, m, n)));
     }
 
+    @Test
+    public void enumerated_constructs_a_stream_that_starts_from_the_given_number() {
+        List<String> result = enumerated(Stream.of("a", "b"), 97, (i, value) -> i + ":" + value).toList();
+        assertEquals(List.of("97:a", "98:b"), result);
+    }
+
+    @Test
+    public void enumerated_constructs_a_stream_that_starts_from_0_by_default() {
+        List<String> result = enumerated(Stream.of("a", "b"), (i, value) -> i + ":" + value).toList();
+        assertEquals(List.of("0:a", "1:b"), result);
+    }
+
+    @Test
+    public void enumerated_constructs_an_empty_stream_given_an_empty_stream() {
+        assertTrue(enumerated(Stream.of(), (i, v) -> "").toList().isEmpty());
+    }
+
 }
