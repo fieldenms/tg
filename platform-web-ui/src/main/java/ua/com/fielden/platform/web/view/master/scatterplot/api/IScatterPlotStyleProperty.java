8c35aafbc7cd15752390ed53cf8359313e35f641
01es <oles.hodych@gmail.com>
2024-04-18 00:08:57 +1000

    #2203 Bit more streamlining of the scatter plot API.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/scatterplot/api/IScatterPlotStyleProperty.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/view/master/scatterplot/api/IScatterPlotStyleProperty.java
@@ -1,7 +1,6 @@
 package ua.com.fielden.platform.web.view.master.scatterplot.api;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
-import ua.com.fielden.platform.processors.metamodel.IConvertableToPath;
 
 /**
  * A contract to specify a property name or a key that should be used to associate styles for rendering values on a scatter plot (shapes, their size, colours, etc.).
@@ -13,18 +12,11 @@ import ua.com.fielden.platform.processors.metamodel.IConvertableToPath;
 public interface IScatterPlotStyleProperty<T extends AbstractEntity<?>> {
 
     /**
-     * Assigns a property name that is used to retrieve styles for scatter plot values.
-     *
-     * @param propertyName
-     * @return
-     */
-    IScatterPlotTitle<T> setStylePropertyName(IConvertableToPath propertyName);
-
-    /**
      * Assigns a key that is used to retrieve styles for scatter plot values.
      *
      * @param key
      * @return
      */
     IScatterPlotTitle<T> setStyleKey(String key);
+
 }
