4177b6b5be5086236912d369bec9d2be6dde68ba
homedirectory <nullsink@posteo.eu>
2024-04-18 12:27:25 +0300

    #2235 Test persistence/retrieval of UTC datetime properties across different timezones

--- platform-dao/src/test/java/ua/com/fielden/platform/types/UtcDateTimePersistanceTestCase.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/types/UtcDateTimePersistanceTestCase.java
@@ -1,18 +1,24 @@
 package ua.com.fielden.platform.types;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
+import ua.com.fielden.platform.sample.domain.TgAuthor;
+import ua.com.fielden.platform.sample.domain.TgEntityWithTimeZoneDates;
+import ua.com.fielden.platform.sample.domain.TgPersonName;
+import ua.com.fielden.platform.test_config.AbstractDaoTestCase;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.TimeZone;
+import java.util.function.Supplier;
 
-import org.junit.Test;
-
-import ua.com.fielden.platform.sample.domain.TgAuthor;
-import ua.com.fielden.platform.sample.domain.TgPersonName;
-import ua.com.fielden.platform.test_config.AbstractDaoTestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.fetch;
+import static ua.com.fielden.platform.test_utils.TestUtils.assertPresent;
 
 public class UtcDateTimePersistanceTestCase extends AbstractDaoTestCase {
 
@@ -30,21 +36,18 @@ public class UtcDateTimePersistanceTestCase extends AbstractDaoTestCase {
     
     @Test
     public void utc_date_is_stored_and_retrieved_correctly_with_respect_to_daylightsaving() throws Exception {
-        TimeZone.setDefault(australia);
-        
-        final TgPersonName chris = save(new_(TgPersonName.class, "Chris", "Chris"));
-        
-        final Date localDate = localFormat.parse("2016-01-01 11:00:00");
-        final Date utcDate = utcFormat.parse("2016-01-01 00:00:00");
+        withTimeZone(australia, () -> {
+            final TgPersonName chris = save(new_(TgPersonName.class, "Chris", "Chris"));
 
-        
-        final TgAuthor date = save(new_composite(TgAuthor.class, chris, "Date", null)
-                .setDob(localDate).setUtcDob(utcDate));
-        
-        assertEquals(localDate, date.getDob());
-        assertEquals(utcDate, date.getUtcDob());
-        
-        TimeZone.setDefault(null);
+            final Date localDate = localFormat.parse("2016-01-01 11:00:00");
+            final Date utcDate = utcFormat.parse("2016-01-01 00:00:00");
+
+            final TgAuthor date = save(new_composite(TgAuthor.class, chris, "Date", null)
+                    .setDob(localDate).setUtcDob(utcDate));
+
+            assertEquals(localDate, date.getDob());
+            assertEquals(utcDate, date.getUtcDob());
+        });
     }
     
     @Test
@@ -60,6 +63,58 @@ public class UtcDateTimePersistanceTestCase extends AbstractDaoTestCase {
         final Date utcFromUtcDate = utcFormat.parse(utcFormat.format(utcDate));
         final Date localFromUtcDate = localFormat.parse(localFormat.format(utcFromUtcDate));
         assertEquals(localFromUtcDate.getTime(), utcFromUtcDate.getTime());
+    }
+
+    @Test
+    public void fetched_value_of_utc_datetime_property_is_the_same_across_different_timezones() {
+        final var co = co(TgEntityWithTimeZoneDates.class);
+        final var fetch = fetch(TgEntityWithTimeZoneDates.class).with("datePropUtc");
+        final var key = "something_unique_87321";
+        final Supplier<TgEntityWithTimeZoneDates> fetcher = () -> assertPresent(co.findByKeyAndFetchOptional(fetch, key));
+
+        withTimeZone(TimeZone.getTimeZone(ZoneId.of("+01")), () -> {
+            final Date myDate = Date.from(Instant.now());
+            save(new_(TgEntityWithTimeZoneDates.class, key).setDatePropUtc(myDate));
+            assertEquals(myDate, fetcher.get().getDatePropUtc());
+
+            withTimeZone(TimeZone.getTimeZone(ZoneId.of("+05")), () -> {
+                assertEquals(myDate, fetcher.get().getDatePropUtc());
+            });
+        });
+    }
+
+    @Test
+    public void fetched_value_of_datetime_property_is_different_across_different_timezones() {
+        final var co = co(TgEntityWithTimeZoneDates.class);
+        final var fetch = fetch(TgEntityWithTimeZoneDates.class).with("dateProp");
+        final var key = "something_unique_74329";
+        final Supplier<TgEntityWithTimeZoneDates> fetcher = () -> assertPresent(co.findByKeyAndFetchOptional(fetch, key));
+
+        withTimeZone(TimeZone.getTimeZone(ZoneId.of("+01")), () -> {
+            final Date myDate = date("2024-05-05 12:00:00");
+            save(new_(TgEntityWithTimeZoneDates.class, key).setDateProp(myDate));
+            assertEquals(myDate, fetcher.get().getDateProp());
+
+            withTimeZone(TimeZone.getTimeZone(ZoneId.of("+05")), () -> {
+                assertNotEquals(myDate, fetcher.get().getDateProp());
+            });
+        });
+    }
+
+    // ============================================================
+    // UTILITIES
+    // ============================================================
+
+    private static void withTimeZone(final TimeZone timeZone, final ThrowingRunnable runnable) {
+        final TimeZone origTz = TimeZone.getDefault();
+        TimeZone.setDefault(timeZone);
+        try {
+            runnable.run();
+        } catch (final Throwable e) {
+            throw new RuntimeException(e);
+        } finally {
+            TimeZone.setDefault(origTz);
+        }
     }
 
 }
