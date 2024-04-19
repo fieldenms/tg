e152b966aefd422efae5887610497fdd874b57b0
homedirectory <nullsink@posteo.eu>
2024-04-19 15:43:16 +0300

    #2235 Hibernate datetime type: wrap Date as LocalDateTime during parameter binding
    Required to make PostgreSQL recognise such values as SQL TIMESTAMP type.

--- platform-dao/src/main/java/ua/com/fielden/platform/persistence/types/DateTimeType.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/persistence/types/DateTimeType.java
@@ -1,59 +1,49 @@
 package ua.com.fielden.platform.persistence.types;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.type.*;
+import org.hibernate.type.descriptor.ValueBinder;
+import org.hibernate.type.descriptor.ValueExtractor;
+import org.hibernate.type.descriptor.WrapperOptions;
+import org.hibernate.type.descriptor.java.JavaTypeDescriptor;
+import org.hibernate.type.descriptor.sql.BasicBinder;
+import org.hibernate.type.descriptor.sql.BasicExtractor;
+import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
+import ua.com.fielden.platform.persistence.types.descriptor.DateTimeJavaTypeDescriptor;
+
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Date;
 
-import org.hibernate.dialect.Dialect;
-import org.hibernate.engine.spi.SharedSessionContractImplementor;
-import org.hibernate.type.AbstractStandardBasicType;
-import org.hibernate.type.IdentifierType;
-import org.hibernate.type.LiteralType;
-import org.hibernate.type.descriptor.sql.TimestampTypeDescriptor;
-
-import ua.com.fielden.platform.persistence.types.descriptor.DateTimeJavaTypeDescriptor;
+import static java.sql.Types.TIMESTAMP;
 
 /**
  * Custom Hibernate type for persisting {@link Date} instances with time portion excluding nanos.
  * <p>
- * The default Hibernate type <code>date</code> is not suitable as it does not store time, the <code>timestamp</code> stores millisecond fractions (nanos), which does not play well
- * with Date time.
- * <p>
- * For example, if a property is bound to a date picker component then nanos are lost, which is recognised as change.
+ * Default Hibernate type {@link DateType} is not suitable as it does not store time,
+ * {@link TimestampType} stores millisecond fractions (nanos), which does not play well with {@link Date} that doesn't
+ * store nanos. For example, if a property is bound to a date picker component, nanos are lost, which is recognised as change.
  * 
  * @author TG Team
- * 
  */
-public class DateTimeType extends AbstractStandardBasicType<Date> implements IdentifierType<Date>, LiteralType<Date> {
+public class DateTimeType extends AbstractSingleColumnStandardBasicType<Date> implements IdentifierType<Date>, LiteralType<Date> {
     private static final long serialVersionUID = 1L;
     public static final DateTimeType INSTANCE = new DateTimeType();
 
     public DateTimeType() {
         // SqlTypeDescriptor, JavaTypeDescriptor
-        super(TimestampTypeDescriptor.INSTANCE, DateTimeJavaTypeDescriptor.INSTANCE);
-    }
-
-    
-    @Override
-    public Object get(final ResultSet rs, final String name, final SharedSessionContractImplementor session) throws SQLException {
-        final Timestamp value = rs.getTimestamp(name);
-        return value != null ? new Date(value.getTime()) : null;
-    }
-
-    @Override
-    public void set(final PreparedStatement st, final Date value, final int index, final SharedSessionContractImplementor session) throws SQLException {
-        final long millis = value.getTime();
-        st.setTimestamp(index, new Timestamp(millis));
+        super(DateTimeSqlTypeDescriptor.INSTANCE, DateTimeJavaTypeDescriptor.INSTANCE);
     }
 
     @Override
     public String getName() {
-        return "date";
+        /* This name acts as a type mapping lookup key. It will be used during registration of the mapping.
+         * Hibernate documentation does not specify what this value should be to override the standard mapping.
+         * java.util.Date works.
+         * See Hibernate 5.4 User Guide, 2.3.1. Hibernate-provided BasicTypes, Table 1. Standard BasicTypes */
+        return java.util.Date.class.getCanonicalName();
     }
 
-    
     @Override
     public Date stringToObject(final String xml) throws Exception {
         return getJavaTypeDescriptor().fromString(xml); //  DateFormat.getDateInstance().parse(xml)
@@ -64,15 +54,67 @@ public class DateTimeType extends AbstractStandardBasicType<Date> implements Ide
         return '\'' + new Timestamp(value.getTime()).toString() + '\'';
     }
 
+    /**
+     * SQL side of the {@link DateTimeType} type binding.
+     *
+     * @see DateTimeType
+     */
+    private static final class DateTimeSqlTypeDescriptor implements SqlTypeDescriptor {
 
-    @Override
-    public void nullSafeSet(final PreparedStatement preparedStatement, final Object value, final int index, final boolean[] settable, final SharedSessionContractImplementor session) throws SQLException {
-        if (null == value) {
-            preparedStatement.setNull(index, getSqlTypeDescriptor().getSqlType());
-        } else {
-            set(preparedStatement, (Date) value, index, session);
+        static final DateTimeSqlTypeDescriptor INSTANCE = new DateTimeSqlTypeDescriptor();
+
+        private DateTimeSqlTypeDescriptor() {}
+
+        @Override
+        public int getSqlType() {
+            return TIMESTAMP;
         }
-        
+
+        @Override
+        public boolean canBeRemapped() {
+            return true;
+        }
+
+        @Override
+        public <X> ValueBinder<X> getBinder(final JavaTypeDescriptor<X> javaTypeDescriptor) {
+            // We need LocalDateTime specifically for PostgreSQL, as java.util.Date will be typed as UNSPECIFIED.
+            // setObject is required to accept LocalDateTime as TIMESTAMP (even PostgreSQL driver does so).
+            return new BasicBinder<>(javaTypeDescriptor, this) {
+                @Override
+                protected void doBind(final PreparedStatement st, final X value, final int index, final WrapperOptions options) throws
+                        SQLException {
+                    final LocalDateTime date = javaTypeDescriptor.unwrap(value, LocalDateTime.class, options);
+                    st.setObject(index, date, TIMESTAMP);
+                }
+
+                @Override
+                protected void doBind(final CallableStatement st, final X value, final String name, final WrapperOptions options) throws SQLException {
+                    final LocalDateTime date = javaTypeDescriptor.unwrap(value, LocalDateTime.class, options);
+                    st.setObject(name, date, TIMESTAMP);
+                }
+            };
+        }
+
+        @Override
+        public <X> ValueExtractor<X> getExtractor(final JavaTypeDescriptor<X> javaTypeDescriptor) {
+            return new BasicExtractor<>(javaTypeDescriptor, this) {
+                @Override
+                protected X doExtract(final ResultSet rs, final String name, final WrapperOptions options) throws SQLException {
+                    return javaTypeDescriptor.wrap(rs.getTimestamp(name), options);
+                }
+
+                @Override
+                protected X doExtract(final CallableStatement statement, final int index, final WrapperOptions options) throws SQLException {
+                    return javaTypeDescriptor.wrap(statement.getTimestamp(index), options);
+                }
+
+                @Override
+                protected X doExtract(final CallableStatement statement, final String name, final WrapperOptions options) throws SQLException {
+                    return javaTypeDescriptor.wrap(statement.getTimestamp(name), options);
+                }
+            };
+        }
+
     }
 
 }
--- platform-dao/src/main/java/ua/com/fielden/platform/persistence/types/descriptor/DateTimeJavaTypeDescriptor.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/persistence/types/descriptor/DateTimeJavaTypeDescriptor.java
@@ -3,6 +3,8 @@ package ua.com.fielden.platform.persistence.types.descriptor;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -67,41 +69,35 @@ public class DateTimeJavaTypeDescriptor extends AbstractTypeDescriptor<Date> {
 	@SuppressWarnings({ "unchecked" })
 	@Override
 	public <X> X unwrap(final Date value, final Class<X> type, final WrapperOptions options) {
-		if ( value == null ) {
+		if (value == null) {
 			return null;
 		}
-		if ( java.sql.Date.class.isAssignableFrom( type ) ) {
-			final java.sql.Date rtn = java.sql.Date.class.isInstance( value )
-					? ( java.sql.Date ) value
-					: new java.sql.Date( value.getTime() );
-			return (X) rtn;
-		}
-		if ( java.sql.Time.class.isAssignableFrom( type ) ) {
-			final java.sql.Time rtn = java.sql.Time.class.isInstance( value )
-					? ( java.sql.Time ) value
-					: new java.sql.Time( value.getTime() );
-			return (X) rtn;
-		}
-		if ( java.sql.Timestamp.class.isAssignableFrom( type ) ) {
-			final java.sql.Timestamp rtn = java.sql.Timestamp.class.isInstance( value )
-					? ( java.sql.Timestamp ) value
-					: new java.sql.Timestamp( value.getTime() );
-			return (X) rtn;
-		}
-		if ( Date.class.isAssignableFrom( type ) ) {
+		if (Date.class == type) {
 			return (X) value;
 		}
-		if ( Calendar.class.isAssignableFrom( type ) ) {
+		if (LocalDateTime.class == type) {
+			return (X) value.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+		}
+		if (java.sql.Date.class.isAssignableFrom(type)) {
+			return (X) (value instanceof java.sql.Date date ? date : new java.sql.Date(value.getTime()));
+		}
+		if (java.sql.Time.class.isAssignableFrom(type)) {
+			return (X) (value instanceof java.sql.Time time ? time : new java.sql.Time(value.getTime()));
+		}
+		if (java.sql.Timestamp.class.isAssignableFrom(type)) {
+			return (X) (value instanceof java.sql.Timestamp ts ? ts : new java.sql.Timestamp(value.getTime()));
+		}
+		if (Calendar.class.isAssignableFrom(type)) {
 			final GregorianCalendar cal = new GregorianCalendar();
-			cal.setTimeInMillis( value.getTime() );
+			cal.setTimeInMillis(value.getTime());
 			return (X) cal;
 		}
-		if ( Long.class.isAssignableFrom( type ) ) {
-			return (X) Long.valueOf( value.getTime() );
+		if (Long.class.isAssignableFrom(type) ) {
+			return (X) Long.valueOf(value.getTime());
 		}
-		throw unknownUnwrap( type );
+		throw unknownUnwrap(type);
 	}
-	
+
 	@Override
 	public <X> Date wrap(final X value, final WrapperOptions options) {
 		return switch (value) {
@@ -111,6 +107,7 @@ public class DateTimeJavaTypeDescriptor extends AbstractTypeDescriptor<Date> {
 			 * and Map<Class, ...> wouldn't work at all */
 			case Timestamp ts -> new Date(ts.getTime());
 			case Date date -> date;
+			case LocalDateTime ldt -> Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
 			case Long n -> new Date(n);
 			case Calendar cal -> new Date(cal.getTimeInMillis());
 			default -> throw unknownUnwrap(value.getClass());
