b30f7e1f4ac5b89f5ac568274aba28c4677c84e9
homedirectory <nullsink@posteo.eu>
2024-05-21 13:26:48 +0300

    #2258 Create a testing harness for entity and property metadata

--- platform-dao/src/test/java/ua/com/fielden/platform/meta/Assertions.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/meta/Assertions.java
@@ -1,6 +1,9 @@
 package ua.com.fielden.platform.meta;
 
+import ua.com.fielden.platform.entity.AbstractEntity;
+
 import java.util.Optional;
+import java.util.function.Consumer;
 
 import static org.junit.Assert.assertEquals;
 import static ua.com.fielden.platform.test_utils.TestUtils.assertInstanceOf;
@@ -47,6 +50,75 @@ interface Assertions {
             PropertyTypeA<PropertyTypeMetadata> elementType() {
                 return new PropertyTypeA<>(typeMetadata.elementType());
             }
+        }
+    }
+
+    class EntityA<E extends EntityMetadata> {
+        private final E entityMetadata;
+
+        private EntityA(final E entityMetadata) {
+            this.entityMetadata = entityMetadata;
+        }
+
+        static EntityA<EntityMetadata> of(final EntityMetadata entityMetadata) {
+            return new EntityA<>(entityMetadata);
+        }
+
+        public E get() { return entityMetadata; }
+
+        <U extends EntityMetadata> EntityA<U> assertIs(final Class<U> type) {
+            return new EntityA<>(assertInstanceOf(type, entityMetadata));
+        }
+
+        public EntityA<E> assertJavaType(final Class<? extends AbstractEntity<?>> expectedJavaType) {
+            assertEquals("Wrong Java Type", expectedJavaType, entityMetadata.javaType());
+            return this;
+        }
+
+        public EntityA<E> assertProperty(final CharSequence propName, final Consumer<PropertyA<PropertyMetadata>> assertor) {
+            final PropertyMetadata propertyMetadata = assertPresent(
+                    "Metadata for property [%s] not found in [%s]".formatted(propName, entityMetadata),
+                    entityMetadata.property(propName.toString()));
+            assertor.accept(PropertyA.of(propertyMetadata));
+            return this;
+        }
+
+        public EntityA<E> assertPropertyExists(final CharSequence propName) {
+            assertPresent("Metadata for property [%s] not found in [%s]".formatted(propName, entityMetadata),
+                          entityMetadata.property(propName.toString()));
+            return this;
+        }
+
+        public EntityA<E> assertPropertiesExist(final Iterable<? extends CharSequence> propName) {
+            propName.forEach(this::assertPropertyExists);
+            return this;
+        }
+    }
+
+    class PropertyA<P extends PropertyMetadata> {
+        private final P propertyMetadata;
+
+        private PropertyA(final P propertyMetadata) {
+            this.propertyMetadata = propertyMetadata;
+        }
+
+        static <P extends PropertyMetadata> PropertyA<P> of(final P propertyMetadata) {
+            return new PropertyA<>(propertyMetadata);
+        }
+
+        public P get() { return propertyMetadata; }
+
+        public <U extends PropertyMetadata> PropertyA<U> assertIs(final Class<U> type) {
+            return new PropertyA<>(assertInstanceOf(type, propertyMetadata));
+        }
+
+        public PropertyTypeA<PropertyTypeMetadata> type() {
+            return new PropertyTypeA<>(propertyMetadata.type());
+        }
+
+        public PropertyA<P> assertType(final Consumer<PropertyTypeA<?>> assertor) {
+            assertor.accept(type());
+            return this;
         }
     }
 
