b8b7650545b65cfaec2b873ac50284f6aff1ae97
homedirectory <nullsink@posteo.eu>
2024-05-27 13:53:22 +0300

    #2258 Implement DomainMetadataBuilder

--- platform-dao/src/main/java/ua/com/fielden/platform/meta/DomainMetadataBuilder.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/meta/DomainMetadataBuilder.java
@@ -1,26 +1,49 @@
 package ua.com.fielden.platform.meta;
 
-import com.google.common.collect.ImmutableSet;
 import com.google.inject.Injector;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.query.DbVersion;
+import ua.com.fielden.platform.eql.exceptions.EqlMetadataGenerationException;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.lang.String.format;
+import static java.util.stream.Collectors.toConcurrentMap;
+import static ua.com.fielden.platform.meta.TypeRegistry.COMPOSITE_TYPES;
+import static ua.com.fielden.platform.types.try_wrapper.TryWrapper.Try;
+import static ua.com.fielden.platform.types.tuples.T2.t2;
 
 public class DomainMetadataBuilder {
+
+    private final DomainMetadataGenerator generator;
+    private final Collection<? extends Class<? extends AbstractEntity<?>>> entityTypes;
+    private final DbVersion dbVersion;
+
     public DomainMetadataBuilder(final Map<? extends Class, ? extends Class> hibTypesDefaults,
                                  final Injector hibTypesInjector,
                                  final Collection<? extends Class<? extends AbstractEntity<?>>> entityTypes,
                                  final DbVersion dbVersion)
     {
-        throw new UnsupportedOperationException("Unimplemented");
+        this.generator = new DomainMetadataGenerator(hibTypesInjector, hibTypesDefaults, dbVersion);
+        this.entityTypes = entityTypes.stream().distinct().collect(toImmutableList());
+        this.dbVersion = dbVersion;
     }
 
     public IDomainMetadata build() {
-        throw new UnsupportedOperationException("Unimplemented");
+        final Map<Class<? extends AbstractEntity<?>>, EntityMetadata> entityMetadataMap = entityTypes.parallelStream()
+                .flatMap(type -> Try(() -> generator.forEntity(type))
+                        .orElseThrow(e -> new EqlMetadataGenerationException(
+                                format("Failed to generate metadata for entity [%s]", type.getTypeName()), e))
+                        .map(em -> t2(type, em)).stream())
+                .collect(toConcurrentMap(pair -> pair._1, pair -> pair._2));
+
+        final Map<Class<?>, TypeMetadata.Composite> compositeTypeMetadataMap = COMPOSITE_TYPES.parallelStream()
+                .flatMap(type -> generator.forComposite(type).map(ctm -> t2(type, ctm)).stream())
+                .collect(toConcurrentMap(pair -> pair._1, pair -> pair._2));
+
+        return new DomainMetadataImpl(entityMetadataMap, compositeTypeMetadataMap, entityTypes, generator, dbVersion);
     }
+
 }
