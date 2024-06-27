08b930fda4d4ceae68a891715d6baf2f0c76be01
homedirectory <nullsink@posteo.eu>
2024-06-27 16:32:28 +0300

    #2274 Tidy up javadoc

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/factory/IMetaPropertyFactory.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/factory/IMetaPropertyFactory.java
@@ -11,39 +11,25 @@ import java.lang.reflect.Field;
  * This is a preliminary interface for a meta-property factory.
  *
  * @author TG Team
- *
  */
 public interface IMetaPropertyFactory {
+
     /**
      * Takes an annotation instance and instantiates validators based on that annotation.
      * <p>
      * If the passed annotation is not recognised as the validation annotation then an empty array should be returned or an exception thrown.
-     *
-     * @param <K>
-     * @param annotation
-     * @param entity
-     * @param keyType
-     * @param propertyName
-     * @param propertyType
-     * @return
-     * @throws Exception
      */
-    IBeforeChangeEventHandler<?>[] create(//
-            final Annotation annotation, //
-            final AbstractEntity<?> entity,//
-            final String propertyName,//
+    IBeforeChangeEventHandler<?>[] create(
+            final Annotation annotation,
+            final AbstractEntity<?> entity,
+            final String propertyName,
             final Class<?> propertyType) throws Exception;
 
     /**
      * Instantiates property ACE handler based on entity type and property field.
-     *
-     * @param entity
-     * @param propertyField
-     * @return
-     * @throws Exception
      */
-    IAfterChangeEventHandler<?> create(//
-            final AbstractEntity<?> entity,//
+    IAfterChangeEventHandler<?> create(
+            final AbstractEntity<?> entity,
             final Field propertyField) throws Exception;
 
 }
