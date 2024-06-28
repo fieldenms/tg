844a856c87620962529e0419115090c09c4b09f1
homedirectory <nullsink@posteo.eu>
2024-06-28 09:08:01 +0300

    #2196 Replace Integer with int

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/eql/antlr/tokens/EndAsDecimalToken.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/eql/antlr/tokens/EndAsDecimalToken.java
@@ -2,18 +2,17 @@ package ua.com.fielden.platform.eql.antlr.tokens;
 
 import java.util.Objects;
 
-import static java.util.Objects.requireNonNull;
 import static ua.com.fielden.platform.eql.antlr.EQLLexer.ENDASDECIMAL;
 
 public final class EndAsDecimalToken extends AbstractParameterisedEqlToken {
 
-    public final Integer precision;
-    public final Integer scale;
+    public final int precision;
+    public final int scale;
 
-    public EndAsDecimalToken(final Integer precision, final Integer scale) {
+    public EndAsDecimalToken(final int precision, final int scale) {
         super(ENDASDECIMAL, "endAsDecimal");
-        this.precision = requireNonNull(precision);
-        this.scale = requireNonNull(scale);
+        this.precision = precision;
+        this.scale = scale;
     }
 
     @Override
