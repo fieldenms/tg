0fefbc9297d0c0046d8b5cd8b8839ea247af5e6b
01es <oles.hodych@gmail.com>
2024-07-04 12:49:45 +1000

    #2138 No need to wrap exceptions - tryToProcess is invoked in handleUndesiredExceptions.

--- platform-web-resources/src/main/java/ua/com/fielden/platform/web/resources/webui/FileProcessingResource.java
+++ platform-web-resources/src/main/java/ua/com/fielden/platform/web/resources/webui/FileProcessingResource.java
@@ -213,8 +213,6 @@ public class FileProcessingResource<T extends AbstractEntityWithInputStream<?>> 
 
             final T applied = saveRaw(entity);
             return restUtil.singleJsonRepresentation(applied);
-        } catch (final Exception e) {
-            throw new ResourceException(e);
         } finally {
             eventSource.disconnect();
         }
