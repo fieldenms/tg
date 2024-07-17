39ae269fb2e43595a4d6808cd5ef07b7f3e5bedd
homedirectory <nullsink@posteo.eu>
2024-07-17 13:52:37 +0300

    #348 Add test cases for Markdown removal rules

--- platform-pojo-bl/src/test/java/ua/com/fielden/platform/types/RichTextMarkdownTest.java
+++ platform-pojo-bl/src/test/java/ua/com/fielden/platform/types/RichTextMarkdownTest.java
@@ -12,110 +12,233 @@ import static org.junit.Assert.assertEquals;
 public class RichTextMarkdownTest {
 
     @Test
-    public void boldface_is_stripped_from_core_text() {
+    public void boldface_is_removed() {
         assertCoreText("the big bang",
                        "the **big** bang");
     }
 
     @Test
-    public void italics_are_stripped_from_core_text() {
+    public void italics_are_removed() {
         assertCoreText("the big bang",
                        "the *big* bang");
     }
 
     @Test
-    public void code_span_backticks_are_replaced_by_double_quotes() {
-        // TODO modify the renderer to strip backticks alltogether
-        assertCoreText("the \"big\" bang",
+    public void code_span_backticks_are_removed() {
+        assertCoreText("the big bang",
                        "the `big` bang");
     }
 
     @Test
-    public void simple_ordered_lists_are_unchanged_in_core_text() {
-        final var input = """
-        1. one
-        2. two""";
-        assertCoreText(input, input);
+    public void block_quote_markers_are_removed() {
+        assertCoreText("the big bang",
+                       "> the big bang");
+        assertCoreText("the big bang",
+                       ">the big bang");
+        assertCoreText("the big bang",
+                       ">  the big bang");
+        assertCoreText("one two three",
+                       """
+                       > one
+                       >two
+                       >  three
+                       """);
+        assertCoreText("one two three",
+                       """
+                       > one
+                       two
+                       >  three
+                       """);
     }
 
     @Test
-    public void simple_unordered_lists_are_unchanged_in_core_text() {
+    public void links_are_transformed_to_link_title_followed_by_parenthesised_uri() {
+        assertCoreText("hackernews (https://news.ycombinator.com/)",
+                       "[hackernews](https://news.ycombinator.com/)");
+        assertCoreText("hackernews (https://news.ycombinator.com/)",
+                       "[hackernews]  (https://news.ycombinator.com/)");
+        assertCoreText("hackernews (https://news.ycombinator.com/)",
+                       "[*hackernews*](https://news.ycombinator.com/)");
+        assertCoreText("(https://news.ycombinator.com/)",
+                       "[](https://news.ycombinator.com/)");
+        assertCoreText("hackernews",
+                       "[hackernews]()");
+        assertCoreText("", "[]()");
+    }
+
+    @Test
+    public void image_links_are_transformed_to_image_description_followed_by_parenthesised_uri() {
+        assertCoreText("my lovely cat (cat.jpeg)",
+                       "![my lovely cat](cat.jpeg)");
+//        assertCoreText("my lovely cat (cat.jpeg)",
+//                       "![my *lovely* cat](cat.jpeg)");
+        assertCoreText("(cat.jpeg)",
+                       "![](cat.jpeg)");
+        assertCoreText("my lovely cat",
+                       "![my lovely cat]()");
+        assertCoreText("", "![]()");
+    }
+
+    @Test
+    public void fenced_code_blocks_are_transformed_by_removing_fence_characters() {
+        assertCoreText("hello",
+                       """
+                       ```
+                       hello
+                       ```
+                       """);
+        assertCoreText("hello",
+                       """
+                       `````
+                       hello
+                       `````
+                       """);
+        assertCoreText("hello",
+                       """
+                       ~~~
+                       hello
+                       ~~~
+                       """);
+        assertCoreText("hello",
+                       """
+                       ~~~~~~
+                       hello
+                       ~~~~~~
+                       """);
+        assertCoreText("",
+                       """
+                       ```
+                       ```
+                       """);
+        assertCoreText("",
+                       """
+                       ```
+                       
+                       ```
+                       """);
+        assertCoreText("hello",
+                       """
+                         ```
+                         hello
+                       ```
+                       """);
+        assertCoreText("System.out.println(\"hello world\")",
+                       """
+                       ```java
+                       System.out.println("hello world")
+                       ```
+                       """);
+        assertCoreText("System.out.println(\"hello world\")",
+                       """
+                       ~~~java
+                       System.out.println("hello world")
+                       ~~~
+                       """);
+        assertCoreText("hello *world* <script>alert(1)</script>",
+                       """
+                       ```
+                       hello *world*
+                       <script>alert(1)</script>
+                       ```
+                       """);
+        assertCoreText("one two three",
+                       """
+                       one
+                         ```
+                         two
+                       ```
+                       three
+                       """);
+    }
+
+    @Test
+    public void atx_headings_are_removed() {
+        assertCoreText("Introduction", "# Introduction");
+        assertCoreText("Introduction", "## Introduction");
+        assertCoreText("Introduction", "### Introduction");
+        assertCoreText("Introduction", "#### Introduction");
+        assertCoreText("Introduction", "##### Introduction");
+        assertCoreText("Introduction", "###### Introduction");
+    }
+
+    @Test
+    public void thematic_breaks_are_removed() {
+        assertCoreText("one two",
+                       """
+                       one
+                       ---
+                       two
+                       """);
+        assertCoreText("one two",
+                       """
+                       one
+                       *****
+                       two
+                       """);
+        assertCoreText("one two",
+                       """
+                       one
+                       ____
+                       two
+                       """);
+    }
+
+    @Test
+    public void newline_characters_are_removed() {
+        assertCoreText("one two three four",
+                       """
+                       
+                       one two
+                       
+                       three four
+                       
+                       """);
+    }
+
+    @Test
+    public void simple_ordered_lists_are_squashed_into_a_single_line() {
+        assertCoreText("1. one 2. two",
+                       """
+                       1. one
+                       2. two
+                       """);
+    }
+
+    @Test
+    public void simple_unordered_lists_are_squashed_into_a_single_line_and_list_characters_removed() {
         for (var listChar : List.of('-', '*', '+')) {
             final var input = """
             %1$s one
             %1$s two""".formatted(listChar);
-            assertCoreText(input, input);
+            assertCoreText("one two", input);
         }
     }
 
-    // TODO the following tests need to be completed by specifying expected core text values
-
     @Test
-    public void content_under_a_list_item() {
-        final var input = """
-        1. item
-           content""";
-        assertCoreText("?", input);
+    public void list_items_with_content_are_squashed_into_a_single_line() {
+        assertCoreText("1. item content 2. more stuff",
+                       """
+                       1. item
+                          content
+                       2. more
+                       
+                          stuff
+                       """);
     }
 
     @Test
-    public void content_under_a_list_item_with_blank_lines() {
-        final var input = """
-        1. item
-        
-           content""";
-        assertCoreText("?", input);
-    }
-
-    @Test
-    public void nested_list() {
-        final var input = """
-        1. item
-           * subitem""";
-        assertCoreText("?", input);
-    }
-
-    @Test
-    public void code_block() {
-        final var input = """
-        above
-        ```
-        hello world
-        ```
-        below""";
-        assertCoreText("?", input);
-    }
-
-    @Test
-    public void quote() {
-        final var input = """
-        > thus the world was created""";
-        assertCoreText("?", input);
-    }
-
-    @Test
-    public void link() {
-        final var input = "[example](example.org)";
-        assertCoreText("?", input);
-    }
-
-    @Test
-    public void single_blank_line() {
-        final var input = """
-        first
-        
-        second""";
-        assertCoreText("?", input);
-    }
-
-    @Test
-    public void multiple_blank_lines() {
-        final var input = """
-        first
-        
-        
-        second""";
-        assertCoreText("?", input);
+    public void nested_list_items_are_squashed_into_a_single_line() {
+        assertCoreText("1. item subitem",
+                       """
+                       1. item
+                          * subitem
+                       """);
+        assertCoreText("1. item subitem 1. another item",
+                       """
+                       1. item
+                          * subitem
+                            1. another item
+                       """);
     }
 
     private static void assertCoreText(final String expected, final String input) {
