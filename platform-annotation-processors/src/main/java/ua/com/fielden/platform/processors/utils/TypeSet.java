048aba1e6498cdd9827557711e370826e0015f1d
homedirectory <nullsink@posteo.eu>
2024-07-19 13:23:19 +0300

    #2295 Introduce TypeSet: a set for Class and TypeMirror
    This provides optimisation opportunities in cases where a fast lookup is
    needed given a TypeMirror and a set of Classes.

---
+++ platform-annotation-processors/src/main/java/ua/com/fielden/platform/processors/utils/TypeSet.java
@@ -0,0 +1,236 @@
+package ua.com.fielden.platform.processors.utils;
+
+import ua.com.fielden.platform.processors.metamodel.utils.ElementFinder;
+
+import javax.annotation.Nullable;
+import javax.lang.model.type.*;
+import javax.lang.model.util.TypeKindVisitor14;
+import java.util.*;
+import java.util.stream.Stream;
+
+/**
+ * An immutable set that can store instances of {@link Class} and {@link TypeMirror}, and provides an efficient implementation
+ * of {@link Set#contains(Object)}.
+ * <p>
+ * The underlying common representation of stored instances is the canonical (qualified) name of a type.
+ * Therefore, only a subset of {@link TypeMirror} types can be stored -- those that have a canonical name. An attempt
+ * to store a type mirror without a canonical name will result in a runtime exception. However, the result of
+ * {@link #contains(Object)} on such a type mirror will simply return {@code false}.
+ * <p>
+ * Although this set type is defined as a set of {@link Object}, the element type should be treated as a sum type of
+ * {@link Class} and {@link TypeMirror}.
+ */
+public final class TypeSet implements Set<Object> {
+
+    public static final class Builder {
+        private final HashSet<String> names = new HashSet<>();
+        private final HashSet<Object> elements = new HashSet<>();
+
+        private Builder() {}
+
+        public Builder add(final TypeMirror typeMirror) {
+            final String name = getCanonicalName(typeMirror);
+            if (name == null) {
+                throw new IllegalArgumentException("TypeMirror [%s] has no canonical name, cannot be added to [%s]".formatted(
+                        typeMirror, TypeSet.class.getTypeName()));
+            }
+            final boolean added = names.add(name);
+            if (added) {
+                elements.add(typeMirror);
+            }
+            return this;
+        }
+
+        public Builder addTypeMirrors(final Iterable<? extends TypeMirror> typeMirrors) {
+            typeMirrors.forEach(this::add);
+            return this;
+        }
+
+        public Builder add(final Class<?> clazz) {
+            final boolean added = names.add(clazz.getCanonicalName());
+            if (added) {
+                elements.add(clazz);
+            }
+            return this;
+        }
+
+        public Builder addClasses(final Iterable<? extends Class<?>> classes) {
+            classes.forEach(this::add);
+            return this;
+        }
+
+        public TypeSet build() {
+            return new TypeSet(names, elements);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static TypeSet ofClasses(final Iterable<? extends Class<?>> classes) {
+        return builder().addClasses(classes).build();
+    }
+
+    public static TypeSet ofClasses(final Class<?>... classes) {
+        return builder().addClasses(Arrays.asList(classes)).build();
+    }
+
+    private final HashSet<String> names;
+    private final HashSet<Object> elements;
+
+    private TypeSet(final HashSet<String> names, final HashSet<Object> elements) {
+        this.names = names;
+        this.elements = elements;
+    }
+
+    @Override
+    public int size() {
+        return elements.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+
+    @Override
+    public boolean contains(final Object o) {
+        return switch (o) {
+            case Class<?> clazz -> contains(clazz);
+            case TypeMirror tm -> contains(tm);
+            case null, default -> false;
+        };
+    }
+
+    @Override
+    public boolean containsAll(final Collection<?> collection) {
+        return collection.stream().allMatch(this::contains);
+    }
+
+    public boolean contains(final Class<?> clazz) {
+        return names.contains(clazz.getCanonicalName());
+    }
+
+    public boolean contains(final TypeMirror typeMirror) {
+        final String name = typeMirror.accept(canonicalNameVisitor, null);
+        return name != null && names.contains(name);
+    }
+
+    public Stream<String> streamTypeNames() {
+        return names.stream();
+    }
+
+    public <T> Stream<T> stream(final Converter<T> converter) {
+        return elements.stream()
+                .map(elt -> elt instanceof TypeMirror tm ? converter.convert(tm) : converter.convert((Class<?>) elt));
+    }
+
+    public interface Converter<T> {
+        T convert(Class<?> clazz);
+        T convert(TypeMirror typeMirror);
+    }
+
+    @Override
+    public Iterator<Object> iterator() {
+        return elements.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return elements.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(final T[] a) {
+        return elements.toArray(a);
+    }
+
+    @Override
+    public boolean add(final Object object) {
+        throw new UnsupportedOperationException("Instances of [%s] are immutable.".formatted(this.getClass().getTypeName()));
+    }
+
+    public boolean add(final Class<?> clazz) {
+        throw new UnsupportedOperationException("Instances of [%s] are immutable.".formatted(this.getClass().getTypeName()));
+    }
+
+    public boolean add(final TypeMirror typeMirror) {
+        throw new UnsupportedOperationException("Instances of [%s] are immutable.".formatted(this.getClass().getTypeName()));
+    }
+
+    @Override
+    public boolean remove(final Object o) {
+        throw new UnsupportedOperationException("Instances of [%s] are immutable.".formatted(this.getClass().getTypeName()));
+    }
+
+    @Override
+    public boolean addAll(final Collection<?> c) {
+        throw new UnsupportedOperationException("Instances of [%s] are immutable.".formatted(this.getClass().getTypeName()));
+    }
+
+    @Override
+    public boolean retainAll(final Collection<?> c) {
+        throw new UnsupportedOperationException("Instances of [%s] are immutable.".formatted(this.getClass().getTypeName()));
+    }
+
+    @Override
+    public boolean removeAll(final Collection<?> c) {
+        throw new UnsupportedOperationException("Instances of [%s] are immutable.".formatted(this.getClass().getTypeName()));
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Instances of [%s] are immutable.".formatted(this.getClass().getTypeName()));
+    }
+
+    private static @Nullable String getCanonicalName(final TypeMirror typeMirror) {
+        return typeMirror.accept(canonicalNameVisitor, null);
+    }
+
+    private static final TypeKindVisitor14<String, Void> canonicalNameVisitor = new TypeKindVisitor14<>() {
+        @Override
+        protected String defaultAction(TypeMirror e, Void ignore) {
+            return null;
+        }
+
+        @Override
+        public String visitPrimitive(final PrimitiveType t, final Void ignore) {
+            return PRIMITIVE_TYPE_MAP.get(t.getKind()).getCanonicalName();
+        }
+
+        private static final Map<TypeKind, Class<?>> PRIMITIVE_TYPE_MAP;
+        static {
+            PRIMITIVE_TYPE_MAP = new EnumMap<>(TypeKind.class);
+            PRIMITIVE_TYPE_MAP.put(TypeKind.BOOLEAN, boolean.class);
+            PRIMITIVE_TYPE_MAP.put(TypeKind.BYTE,    byte.class);
+            PRIMITIVE_TYPE_MAP.put(TypeKind.SHORT,   short.class);
+            PRIMITIVE_TYPE_MAP.put(TypeKind.INT,     int.class);
+            PRIMITIVE_TYPE_MAP.put(TypeKind.LONG,    long.class);
+            PRIMITIVE_TYPE_MAP.put(TypeKind.CHAR,    char.class);
+            PRIMITIVE_TYPE_MAP.put(TypeKind.FLOAT,   float.class);
+            PRIMITIVE_TYPE_MAP.put(TypeKind.DOUBLE,  double.class);
+        }
+
+        // handle void type
+        @Override
+        public String visitNoType(final NoType t, final Void ignore) {
+            if (t.getKind() == TypeKind.VOID) {
+                return void.class.getCanonicalName();
+            }
+            return null;
+        }
+
+        @Override
+        public String visitArray(ArrayType t, Void ignore) {
+            final String componentName = t.getComponentType().accept(this, ignore);
+            return componentName == null ? null : componentName + "[]";
+        }
+
+        @Override
+        public String visitDeclared(final DeclaredType t, final Void ignore) {
+            return ElementFinder.asTypeElement(t).getQualifiedName().toString();
+        }
+    };
+
+}
---
+++ platform-annotation-processors/src/test/java/ua/com/fielden/platform/processors/utils/TypeSetTest.java
@@ -0,0 +1,84 @@
+package ua.com.fielden.platform.processors.utils;
+
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import ua.com.fielden.platform.processors.metamodel.utils.ElementFinder;
+import ua.com.fielden.platform.processors.test_utils.ProcessingRule;
+
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class TypeSetTest {
+
+    @ClassRule
+    public static ProcessingRule rule = new ProcessingRule();
+    private static Elements elements;
+    private static Types types;
+    private static ElementFinder elementFinder;
+
+    @BeforeClass
+    public static void setup() {
+        elements = rule.getElements();
+        types = rule.getTypes();
+        elementFinder = new ElementFinder(rule.getProcessingEnvironment());
+    }
+
+    @Test
+    public void size_reports_correct_size() {
+        assertEquals(2, TypeSet.ofClasses(int.class, List.class).size());
+        assertEquals(1, TypeSet.ofClasses(List.class).size());
+        assertEquals(0, TypeSet.ofClasses().size());
+    }
+
+    @Test
+    public void isEmpty_returns_true_only_when_TypeSet_is_empty() {
+        assertTrue(TypeSet.ofClasses().isEmpty());
+        assertFalse(TypeSet.ofClasses(int.class).isEmpty());
+        assertFalse(TypeSet.ofClasses(Integer.class, Double.class).isEmpty());
+    }
+
+    @Test
+    public void elements_with_the_same_canonical_names_are_deduplicated() {
+        final var set = TypeSet.builder()
+                .add(int.class).add(int.class)
+                .add(char.class).add(types.getPrimitiveType(TypeKind.CHAR))
+                .add(String.class).add(elementFinder.getTypeElement(String.class).asType())
+                .add(List.class).add(types.getDeclaredType(elementFinder.getTypeElement(List.class),
+                                                           elementFinder.getTypeElement(String.class).asType()))
+                .add(Double.class).add(Double.class)
+                .build();
+        assertEquals(5, set.size());
+        assertTrue(set.contains(int.class));
+        assertTrue(set.contains(types.getPrimitiveType(TypeKind.INT)));
+        assertTrue(set.contains(elementFinder.getTypeElement(Double.class).asType()));
+    }
+
+    @Test
+    public void TypeSet_contains_works_correctly_for_TypeMirror_arguments() {
+        final var set = TypeSet.ofClasses(int.class, Integer.class, List.class, String.class);
+        assertTrue(set.contains(types.getPrimitiveType(TypeKind.INT)));
+        assertFalse(set.contains(types.getPrimitiveType(TypeKind.CHAR)));
+        // List<String>
+        assertTrue(set.contains(types.getDeclaredType(elementFinder.getTypeElement(List.class),
+                                                      elementFinder.getTypeElement(String.class).asType())));
+        assertTrue(set.contains(elementFinder.getTypeElement(Integer.class).asType()));
+    }
+
+    @Test
+    public void TypeSet_contains_works_correctly_for_Class_arguments() {
+        final var set = TypeSet.ofClasses(int.class, Integer.class, List.class, String.class);
+        assertTrue(set.contains(int.class));
+        assertTrue(set.contains(Integer.class));
+        assertTrue(set.contains(List.class));
+        assertTrue(set.contains(String.class));
+        assertFalse(set.contains(double.class));
+        assertFalse(set.contains(Set.class));
+    }
+
+}
