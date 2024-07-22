80497ffe51fba98be073911703374854efb0c17d
homedirectory <nullsink@posteo.eu>
2024-07-22 16:33:26 +0300

    #1834 Check file format before attempting to process Exif data
    This avoids IllegalArgumentException if given a file that is not an
    image

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/utils/ExifGeoUtils.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/utils/ExifGeoUtils.java
@@ -1,5 +1,6 @@
 package ua.com.fielden.platform.utils;
 
+import org.apache.commons.imaging.ImageFormats;
 import org.apache.commons.imaging.Imaging;
 import org.apache.commons.imaging.ImagingException;
 import org.apache.commons.imaging.common.ImageMetadata;
@@ -9,7 +10,6 @@ import org.apache.commons.imaging.formats.tiff.TiffImageMetadata;
 import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Optional;
 
 import static java.util.Optional.empty;
@@ -20,14 +20,35 @@ import static java.util.Optional.ofNullable;
  */
 public final class ExifGeoUtils {
 
+    /**
+     * Extracts latitude and longitude from Exif data embedded within a file.
+     * If a file is not an image or has no geolocation metadata, empty optional is returned.
+     */
     public static Optional<Coordinates> getCoordinates(final File file) throws IOException {
+        if (ImageFormats.UNKNOWN == Imaging.guessFormat(file)) {
+            return empty();
+        }
         return getCoordinates(Imaging.getMetadata(file));
     }
 
-    public static Optional<Coordinates> getCoordinates(final InputStream inputStream) throws IOException {
-        return getCoordinates(Imaging.getMetadata(inputStream, null));
-    }
+    // Reading from an input stream can be supported but requires a clever InputStream wrapper so that we can guess the
+    // format first (like we do with a file). If we don't check the format and the stream's contents do not represent
+    // an image, we will receive an IllegalArgumentException, whereas we'd like to return empty Optional instead.
+    // If only Apache Commons Imaging used a specific exception type for such cases...
 
+//    /**
+//     * Extracts latitude and longitude from Exif data embedded within a file.
+//     * If a file is not an image or has no geolocation metadata, empty optional is returned.
+//     *
+//     * @param inputStream  contents of a file
+//     */
+//    public static Optional<Coordinates> getCoordinates(final InputStream inputStream) throws IOException {
+//        return getCoordinates(Imaging.getMetadata(inputStream, null));
+//    }
+
+    /**
+     * Extracts latitude and longitude from image metadata.
+     */
     public static Optional<Coordinates> getCoordinates(final ImageMetadata metadata) throws ImagingException {
         return switch (metadata) {
             case JpegImageMetadata it -> ofNullable(getCoordinates(it.getExif()));
