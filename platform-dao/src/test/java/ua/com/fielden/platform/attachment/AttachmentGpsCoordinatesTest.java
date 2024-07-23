60798d4327f59553efb170399fc60e479498003a
homedirectory <nullsink@posteo.eu>
2024-07-23 10:24:37 +0300

    #1834 Test validation of latitude and longitude of Attachment

--- platform-dao/src/test/java/ua/com/fielden/platform/attachment/AttachmentGpsCoordinatesTest.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/attachment/AttachmentGpsCoordinatesTest.java
@@ -14,10 +14,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Consumer;
 
 import static java.util.Objects.requireNonNull;
 import static org.apache.logging.log4j.LogManager.getLogger;
 import static org.junit.Assert.*;
+import static ua.com.fielden.platform.attachment.Attachment.pn_LATITUDE;
+import static ua.com.fielden.platform.attachment.Attachment.pn_LONGITUDE;
+import static ua.com.fielden.platform.attachment.validators.LatitudeValidator.ERR_LATITUDE_RANGE;
+import static ua.com.fielden.platform.attachment.validators.LongitudeValidator.ERR_LONGITUDE_RANGE;
 
 /**
  * Test case that covers the capturing of GPS coordinates from Exif image data..
@@ -89,7 +94,49 @@ public class AttachmentGpsCoordinatesTest extends AbstractDaoTestCase {
         assertNull(attachment.getLongitude());
     }
 
-    private Attachment uploadJpeg(final AttachmentUploaderDao coAttachmentUploader, final Path fileToUpload) throws IOException {
+    @Test
+    public void latitude_values_must_fall_in_the_expected_range() {
+        final Consumer<BigDecimal> assertFailure = latitude -> {
+            final var attachment = new_(Attachment.class);
+            attachment.setLatitude(latitude);
+            assertFalse(attachment.getProperty(pn_LATITUDE).isValid());
+            assertEquals(ERR_LATITUDE_RANGE, attachment.getProperty(pn_LATITUDE).getFirstFailure().getMessage());
+        };
+        final Consumer<BigDecimal> assertSuccess = latitude -> {
+            final var attachment = new_(Attachment.class);
+            attachment.setLatitude(latitude);
+            assertTrue(attachment.getProperty(pn_LATITUDE).isValid());
+        };
+
+        assertFailure.accept(new BigDecimal(-100));
+        assertFailure.accept(new BigDecimal(91));
+        assertSuccess.accept(new BigDecimal(90));
+        assertSuccess.accept(new BigDecimal(-90));
+        assertSuccess.accept(new BigDecimal(0));
+    }
+
+    @Test
+    public void longitude_values_must_fall_in_the_expected_range() {
+        final Consumer<BigDecimal> assertFailure = longitude -> {
+            final var attachment = new_(Attachment.class);
+            attachment.setLongitude(longitude);
+            assertFalse(attachment.getProperty(pn_LONGITUDE).isValid());
+            assertEquals(ERR_LONGITUDE_RANGE, attachment.getProperty(pn_LONGITUDE).getFirstFailure().getMessage());
+        };
+        final Consumer<BigDecimal> assertSuccess = longitude -> {
+            final var attachment = new_(Attachment.class);
+            attachment.setLongitude(longitude);
+            assertTrue(attachment.getProperty(pn_LONGITUDE).isValid());
+        };
+
+        assertFailure.accept(new BigDecimal(-400));
+        assertFailure.accept(new BigDecimal(181));
+        assertSuccess.accept(new BigDecimal(180));
+        assertSuccess.accept(new BigDecimal(-180));
+        assertSuccess.accept(new BigDecimal(0));
+    }
+
+    private Attachment uploadJpeg(final AttachmentUploaderDao coAttachmentUploader, final Path fileToUpload) {
         final Attachment attachment;
         try (final InputStream is = new FileInputStream(fileToUpload.toAbsolutePath().toString())) {
             final AttachmentUploader uploader = new_(AttachmentUploader.class);
@@ -97,6 +144,8 @@ public class AttachmentGpsCoordinatesTest extends AbstractDaoTestCase {
                     .setMime("image/jpeg")
                     .setInputStream(is);
             attachment = coAttachmentUploader.save(uploader).getKey();
+        } catch (final IOException ex) {
+            throw new RuntimeException(ex);
         }
 
         uploads.add(attachment);
