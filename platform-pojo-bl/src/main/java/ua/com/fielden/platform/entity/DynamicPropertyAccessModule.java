4a6dc2bcc9417b62044308f1894db99e45b575df
homedirectory <nullsink@posteo.eu>
2024-07-26 13:13:49 +0300

    #2061 Enhance DynamicPropertyAccessModule to allow caching to be forced
    With this option we don't have to change the whole Workflow to
    'development' just to enable caching, we can do so selectively.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/DynamicPropertyAccessModule.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/DynamicPropertyAccessModule.java
@@ -1,21 +1,42 @@
 package ua.com.fielden.platform.entity;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Module;
 import com.google.inject.Provides;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
 import ua.com.fielden.platform.basic.config.Workflows;
 
 public final class DynamicPropertyAccessModule extends AbstractModule {
+
+    public static final String FORCE_CACHING = "DynamicPropertyAcesss.forceCaching";
+
+    static class Options {
+        @Inject(optional = true)
+        @Named(FORCE_CACHING)
+        boolean forceCaching = false;
+    }
+
+    public static Module forceCaching() {
+        return binder -> binder.bindConstant().annotatedWith(Names.named(FORCE_CACHING)).to(true);
+    }
 
     @Override
     protected void configure() {
         requestStaticInjection(AbstractEntity.class);
     }
 
-    @Provides PropertyIndexer providePropertyIndexer(final PropertyIndexerImpl indexer, final Workflows workflow) {
-       return switch (workflow) {
-           case development -> indexer;
-           case deployment, vulcanizing -> PropertyIndexer.caching(indexer);
-       };
+    @Provides
+    PropertyIndexer providePropertyIndexer(final PropertyIndexerImpl indexer, final Workflows workflow, final Options options) {
+        if (options.forceCaching) {
+            return PropertyIndexer.caching(indexer);
+        }
+
+        return switch (workflow) {
+            case development -> indexer;
+            case deployment, vulcanizing -> PropertyIndexer.caching(indexer);
+        };
     }
 
 }
