122f7861e87db8ca4b20c87a31269ca54809baaf
homedirectory <nullsink@posteo.eu>
2024-07-30 10:09:47 +0300

    #2061 Replace ConcurrentHashMap by Guava Cache for property indices

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/CachingPropertyIndexerImpl.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/CachingPropertyIndexerImpl.java
@@ -1,15 +1,32 @@
 package ua.com.fielden.platform.entity;
 
-import java.util.concurrent.ConcurrentHashMap;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import java.util.concurrent.TimeUnit;
 
 final class CachingPropertyIndexerImpl extends PropertyIndexerImpl {
 
-    // TODO Guava Cache with smart eviction strategy
-    private final ConcurrentHashMap<Class<?>, StandardIndex> cache = new ConcurrentHashMap<>();
+    private final Cache<Class<? extends AbstractEntity<?>>, StandardIndex> cache;
+
+    CachingPropertyIndexerImpl(final CacheBuilder<Object, Object> cacheBuilder) {
+        this.cache = cacheBuilder.build();
+    }
+
+    CachingPropertyIndexerImpl() {
+        this(CacheBuilder.newBuilder()
+                     .weakKeys() // classes can be compared with ==
+                     .initialCapacity(512)
+                     .maximumSize(8192)
+                     .concurrencyLevel(50)
+                     .expireAfterAccess(1, TimeUnit.DAYS));
+    }
 
     @Override
     public StandardIndex indexFor(final Class<? extends AbstractEntity<?>> entityType) {
-        final var cached = cache.get(entityType);
+        // recursive updates are frown upon, so let's do it by hand
+        // see https://github.com/prestodb/presto/pull/4980
+        final var cached = cache.getIfPresent(entityType);
         if (cached != null) {
             return cached;
         }
