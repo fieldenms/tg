4cb97f46d93728cb09966cb924519e966fd6a127
homedirectory <nullsink@posteo.eu>
2024-08-02 08:34:12 +0300

    #2061 Introduce another property index cache for temporary entity types

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/CachingPropertyIndexerImpl.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/CachingPropertyIndexerImpl.java
@@ -5,12 +5,21 @@ import com.google.common.cache.CacheBuilder;
 
 import java.util.concurrent.TimeUnit;
 
+import static ua.com.fielden.platform.reflection.PropertyTypeDeterminator.isMockNotFoundType;
+import static ua.com.fielden.platform.reflection.PropertyTypeDeterminator.isProxied;
+
 final class CachingPropertyIndexerImpl extends PropertyIndexerImpl {
 
-    private final Cache<Class<? extends AbstractEntity<?>>, StandardIndex> cache;
+    /** Cache for lasting types (canonical entity types, generated entity types for centres, ...). */
+    private final Cache<Class<? extends AbstractEntity<?>>, StandardIndex> lastingTypeCache;
 
-    CachingPropertyIndexerImpl(final CacheBuilder<Object, Object> cacheBuilder) {
-        this.cache = cacheBuilder.build();
+    /** Cache for temporary types (generated for a temporary purpose). */
+    private final Cache<Class<? extends AbstractEntity<?>>, StandardIndex> tmpTypeCache;
+
+    CachingPropertyIndexerImpl(final CacheBuilder<Object, Object> lastingTypeCacheBuilder,
+                               final CacheBuilder<Object, Object> tmpTypeCacheBuilder ) {
+        this.lastingTypeCache = lastingTypeCacheBuilder.build();
+        this.tmpTypeCache = tmpTypeCacheBuilder.build();
     }
 
     CachingPropertyIndexerImpl() {
@@ -19,17 +28,38 @@ final class CachingPropertyIndexerImpl extends PropertyIndexerImpl {
                      .initialCapacity(512)
                      .maximumSize(8192)
                      .concurrencyLevel(50)
-                     .expireAfterAccess(1, TimeUnit.DAYS));
+                     .expireAfterAccess(1, TimeUnit.DAYS),
+             CacheBuilder.newBuilder()
+                     .initialCapacity(512)
+                     .maximumSize(8192)
+                     .concurrencyLevel(50)
+                     .expireAfterAccess(5, TimeUnit.MINUTES));
     }
 
     @Override
     public StandardIndex indexFor(final Class<? extends AbstractEntity<?>> entityType) {
-        // recursive updates are frown upon, so let's do it by hand
-        // see https://github.com/prestodb/presto/pull/4980
-        final var cached = cache.getIfPresent(entityType);
-        if (cached != null) {
-            return cached;
+        // performing a lookup in both caches first is faster than first determining which cache to use
+        final var cachedInLasting = lastingTypeCache.getIfPresent(entityType);
+        if (cachedInLasting != null) {
+            return cachedInLasting;
         }
+        final var cachedInTmp = tmpTypeCache.getIfPresent(entityType);
+        if (cachedInTmp != null) {
+            return cachedInTmp;
+        }
+
+        final Cache<Class<? extends AbstractEntity<?>>, StandardIndex> cache;
+        if (isProxied(entityType) || isMockNotFoundType(entityType)) {
+            cache = tmpTypeCache;
+        }
+        else {
+            cache = lastingTypeCache;
+        }
+        return storeIndex(entityType, cache);
+    }
+
+    private StandardIndex storeIndex(final Class<? extends AbstractEntity<?>> entityType,
+                                     final Cache<Class<? extends AbstractEntity<?>>, StandardIndex> cache) {
         final var newIndex = super.indexFor(entityType);
         cache.put(entityType, newIndex);
         return newIndex;
