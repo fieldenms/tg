fe6d3bc558d63b720926057af25e0d989f1029df
jhou-pro <jhou.pro@gmail.com>
2024-08-23 18:48:24 +0300

    #2271 #2275 Corrected unfortunate merge.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/centre/tg-entity-centre-insertion-point.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/centre/tg-entity-centre-insertion-point.js
@@ -149,7 +149,7 @@ const template = html`
     <style include="iron-flex iron-flex-reverse iron-flex-alignment iron-flex-factors iron-positioning tg-entity-centre-styles paper-material-styles"></style>
     <div id="insertionPointContainer" class="relative layout vertical flex">
         <div id="titleBar" draggable$="[[_titleBarDraggable]]" class="title-bar layout horizontal justified center" hidden$="[[!_hasTitleBar(shortDesc, alternativeView)]]" on-track="_moveDialog" tooltip-text$="[[_calcTitleTooltip(longDesc, saveAsName, saveAsDesc)]]">
-            <span class="title-text truncate">[[shortDesc]]</span>
+            <span class="title-text truncate">[[_calcTitle(shortDesc, saveAsName)]]</span>
             <div class="layout horizontal centre">
                 <paper-icon-button class="title-bar-button" icon="[[_detachButtonIcon(detachedView)]]" on-tap="_toggleDetach" tooltip-text$="[[_detachTooltip(detachedView)]]"></paper-icon-button>
                 <paper-icon-button class="title-bar-button" icon="[[_minimiseButtonIcon(minimised)]]" on-tap="_toggleMinimised" tooltip-text$="[[_minimisedTooltip(minimised)]]" disabled="[[maximised]]"></paper-icon-button>
