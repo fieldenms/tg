9245561dffcef79c779a6adecd6c914d96c89753
Oleh Maikovych <oleh@fielden.com.au>
2024-08-28 17:21:09 +0300

    #185 Moved resizing logic into rich text editor as it is more appropriate place for that logic.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/components/tg-rich-text-input.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/components/tg-rich-text-input.js
@@ -1,9 +1,6 @@
 
 import { html, PolymerElement } from '/resources/polymer/@polymer/polymer/polymer-element.js';
-import {GestureEventListeners} from '/resources/polymer/@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import '/resources/toastui-editor/toastui-editor-all.min.js';
-
-import { tearDownEvent, localStorageKey} from '/resources/reflection/tg-polymer-utils.js';
 
 const template = html`
     <link rel="stylesheet" href="/resources/toastui-editor/toastui-editor.min.css" />
@@ -24,37 +21,10 @@ const template = html`
         .toastui-editor-contents {
             font-size: inherit !important;
         }
-        .noselect {
-            -webkit-touch-callout: none;
-            /* iOS Safari */
-            -webkit-user-select: none;
-            /* Safari */
-            -khtml-user-select: none;
-            /* Konqueror HTML */
-            -moz-user-select: none;
-            /* Firefox */
-            -ms-user-select: none;
-            /* Internet Explorer/Edge */
-            user-select: none;
-            /* Non-prefixed version, currently
-               supported by Chrome and Opera */
-        }
-        #resizer {
-            position: absolute;
-            bottom: 0;
-            right: 0;
-            z-index: 21;
-            --iron-icon-fill-color: var(--paper-grey-600);
-        }
-        #resizer:hover {
-            cursor: ns-resize;
-        }
-
     </style>
-    <div id="editor"></div>
-    <iron-icon id="resizer" icon="tg-icons:resize-bottom-right" on-tap="_resetHeight" on-track="_resizeInput" tooltip-text="Drag to resize<br>Double tap to reset height"></iron-icon>`; 
+    <div id="editor"></div>`; 
 
-class TgRichTextInput extends GestureEventListeners(PolymerElement) {
+class TgRichTextInput extends PolymerElement {
 
     static get template() { 
         return template;
@@ -73,25 +43,13 @@ class TgRichTextInput extends GestureEventListeners(PolymerElement) {
                 value: null
             },
 
-            entityType: {
-                type: String
-            },
-
-            propertyName: {
-                type: String
-            },
-
             height: {
-                type: String
+                type: String,
+                observer: "_heightChanged"
             },
 
             minHeight: {
                 type: String
-            },
-
-            withoutResizing: {
-                type: Boolean,
-                value: false
             },
 
             _editor: Object,
@@ -103,12 +61,12 @@ class TgRichTextInput extends GestureEventListeners(PolymerElement) {
         
         this._editor = new toastui.Editor({
             el: this.$.editor,
-            height: this._readHeight() || this.height,
+            height: this.height,
             minHeight: this.minHeight,
             initialEditType: 'wysiwyg',
             events: {
                 change: this._changeValue.bind(this),
-                //blur: this.changeEventHandler.bind(this)
+                blur: this.changeEventHandler.bind(this)
             },
             useCommandShortcut: true,
             usageStatistics: false,
@@ -138,6 +96,18 @@ class TgRichTextInput extends GestureEventListeners(PolymerElement) {
         this._editor.exec('strike');
     }
 
+    makeReadOnly() {
+        this._editor.getEditorElements().wwEditor.children[0].setAttribute("contenteditable", "false");
+    }
+    
+    makeEditable() {
+        this._editor.getEditorElements().wwEditor.children[0].setAttribute("contenteditable", "true");
+    }
+
+    getHeight() {
+        return this._editor && this._editor.getHeight();
+    }
+
     _valueChanged(newValue) {
         if(this._editor && newValue !== this._editor.getMarkdown()) {
             this._editor.setMarkdown(newValue);
@@ -151,49 +121,10 @@ class TgRichTextInput extends GestureEventListeners(PolymerElement) {
         }
     }
 
-    _resizeInput (event) {
-        const target = event.target || event.srcElement;
-        if (target === this.$.resizer && !this.withoutResizing) {
-            switch (event.detail.state) {
-                case 'start':
-                    this.$.editor.classList.toggle("noselect", true);
-                    this._editor.getEditorElements().wwEditor.children[0].setAttribute("contenteditable", "false");
-                    document.styleSheets[0].insertRule('* { cursor: ns-resize !important; }', 0); // override custom cursors in all application with resizing cursor
-                    break;
-                case 'track':
-                    let newHeight = this.$.editor.offsetHeight + event.detail.ddy;
-                    if (newHeight < this.minHeight) {
-                        newHeight = this.minHeight;
-                    }
-                    this._editor.setHeight(newHeight + "px");
-                    break;
-                case 'end':
-                    this.$.editor.classList.toggle("noselect", false);
-                    this._editor.getEditorElements().wwEditor.children[0].setAttribute("contenteditable", "true");
-                    if (document.styleSheets.length > 0 && document.styleSheets[0].cssRules.length > 0) {
-                        document.styleSheets[0].deleteRule(0);
-                    }
-                    this._saveHeight(this._editor.getHeight());
-                    break;
-            }
+    _heightChanged (newHeight) {
+        if (this._editor) {
+            this._editor.setHeight(newHeight);
         }
-        tearDownEvent(event);  
-    }
-
-    _resetHeight(e) {
-        console.log("Height was reset");
-    }
-
-    _saveHeight(height) {
-        localStorage.setItem(this._generateKey(), height);
-    }
-
-    _readHeight() {
-        return localStorage.getItem(this._generateKey());
-    }
-
-    _generateKey() {
-        return localStorageKey(`${this.entityType}_${this.propertyName}_height`);
     }
 }
 
--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/editors/tg-rich-text-editor.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/editors/tg-rich-text-editor.js
@@ -2,9 +2,11 @@ import '/resources/polymer/@polymer/polymer/polymer-legacy.js';
 
 import '/resources/components/tg-rich-text-input.js';
 
-import {html} from '/resources/polymer/@polymer/polymer/polymer-element.js';
+import { html } from '/resources/polymer/@polymer/polymer/polymer-element.js';
+import {GestureEventListeners} from '/resources/polymer/@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 
-import { TgEditor, createEditorTemplate} from '/resources/editors/tg-editor.js';
+import { TgEditor, createEditorTemplate } from '/resources/editors/tg-editor.js';
+import { tearDownEvent, localStorageKey } from '/resources/reflection/tg-polymer-utils.js';
 
 const additionalTemplate = html`
     <style>
@@ -22,6 +24,31 @@ const additionalTemplate = html`
         }
         :host(:hover) .title-action,#decorator[focused] .title-action {
             display: unset;
+        }
+        #resizer {
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            z-index: 21;
+            --iron-icon-fill-color: var(--paper-grey-600);
+        }
+        #resizer:hover {
+            cursor: ns-resize;
+        }
+        .noselect {
+            -webkit-touch-callout: none;
+            /* iOS Safari */
+            -webkit-user-select: none;
+            /* Safari */
+            -khtml-user-select: none;
+            /* Konqueror HTML */
+            -moz-user-select: none;
+            /* Firefox */
+            -ms-user-select: none;
+            /* Internet Explorer/Edge */
+            user-select: none;
+            /* Non-prefixed version, currently
+               supported by Chrome and Opera */
         }
     </style>`;
 const customLabelTemplate = html`
@@ -42,14 +69,13 @@ const customInputTemplate = html`
         disabled$="[[_disabled]]" 
         value="{{_editingValue}}"
         change-event-handler="[[_onChange]]"
-        entity-type="[[entityType]]"
-        property-name="[[propertyName]]"
         min-height="[[minHeight]]"
-        height="[[height]]">
-    </tg-rich-text-input>`;
+        height="[[_calcHeight(height, entityType, propertyName)]]">
+    </tg-rich-text-input>
+    <iron-icon id="resizer" icon="tg-icons:resize-bottom-right" on-tap="_resetHeight" on-down="_makeInputUnselectable" on-up="_makeInputSelectable" on-track="_resizeInput" tooltip-text="Drag to resize<br>Double tap to reset height"></iron-icon>`;
 const propertyActionTemplate = html`<slot id="actionSlot" name="property-action"></slot>`;
 
-export class TgRichTextEditor extends TgEditor {
+export class TgRichTextEditor extends GestureEventListeners(TgEditor) {
 
     static get template() { 
         return createEditorTemplate(additionalTemplate, html``, customInputTemplate, html``, html``, propertyActionTemplate, customLabelTemplate);
@@ -70,6 +96,11 @@ export class TgRichTextEditor extends TgEditor {
             height: {
                 type: String,
                 value: "100px"
+            },
+
+            withoutResizing: {
+                type: Boolean,
+                value: false
             }
         }
     }
@@ -105,6 +136,12 @@ export class TgRichTextEditor extends TgEditor {
         this.$.input.applyStrikethough();
     }
 
+    _calcHeight(height, entityType, propertyName) {
+        if (height && entityType && propertyName) {
+            return this._readHeight() || height;
+        }
+    }
+
     /**
      * Returns tooltip for action
      */
@@ -116,6 +153,57 @@ export class TgRichTextEditor extends TgEditor {
             <div style='margin-right:10px;'>With action: </div>
             <div style='flex-grow:1;'>${actionStr.join('<br><br>')}</div>
             </div>`
+    }
+
+    _resizeInput (event) {
+        const target = event.target || event.srcElement;
+        if (target === this.$.resizer && !this.withoutResizing) {
+            switch (event.detail.state) {
+                case 'start':
+                    break;
+                case 'track':
+                    let newHeight = this.$.input.offsetHeight + event.detail.ddy;
+                    if (newHeight < parseInt(this.minHeight)) {
+                        newHeight = parseInt(this.minHeight);
+                    }
+                    this.$.input.height = newHeight + "px";
+                    break;
+                case 'end':
+                    this._saveHeight(this.$.input.getHeight());
+                    break;
+            }
+        }
+        tearDownEvent(event);  
+    }
+
+    _makeInputUnselectable () {
+        this.$.input.classList.toggle("noselect", true);
+        this.$.input.makeReadOnly();
+        document.styleSheets[0].insertRule('* { cursor: ns-resize !important; }', 0); // override custom cursors in all application with resizing cursor
+    }
+
+    _makeInputSelectable () {
+        this.$.input.classList.toggle("noselect", false);
+        this.$.input.makeEditable();
+        if (document.styleSheets.length > 0 && document.styleSheets[0].cssRules.length > 0) {
+            document.styleSheets[0].deleteRule(0);
+        }
+    }
+
+    _resetHeight(e) {
+        console.log("Height was reset");
+    }
+
+    _saveHeight(height) {
+        localStorage.setItem(this._generateKey(), height);
+    }
+
+    _readHeight() {
+        return localStorage.getItem(this._generateKey());
+    }
+
+    _generateKey() {
+        return localStorageKey(`${this.entityType}_${this.propertyName}_height`);
     }
 }
 
