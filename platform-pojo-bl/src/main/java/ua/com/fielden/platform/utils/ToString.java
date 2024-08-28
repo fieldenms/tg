019121324c2c980bf4e7ff0723ef5083785e5f88
homedirectory <nullsink@posteo.eu>
2024-08-28 11:05:38 +0300

    #2306 Improve ToString util's ability to handle nested structures

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/utils/ToString.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/utils/ToString.java
@@ -1,10 +1,9 @@
 package ua.com.fielden.platform.utils;
 
+import ua.com.fielden.platform.entity.exceptions.InvalidArgumentException;
+
 import javax.annotation.Nullable;
-import java.util.Collection;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.StringJoiner;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -49,6 +48,26 @@ class Sub extends Super {
 }
  * }
  *
+ * Another version, with {@link IFormattable}:
+ * {@snippet :
+class Super implements IFormattable {
+    private final Object x;
+
+    public String toString(final IFormat format) {
+        return format.toString(this).add("x", x).pipe(this::addToString).$();
+    }
+
+    public String toString() {
+        return toString(ToString.separateLines);
+    }
+
+    // override to include more fields
+    protected ToString addToString(final ToString toString) {
+        return toString;
+    }
+}
+ *}
+ *
  * @see Format
  */
 public final class ToString {
@@ -56,7 +75,7 @@ public final class ToString {
     /**
      * Standard format with all fields on a single line.
      */
-    public static final Format standard = formatBuilder()
+    public static final IFormat standard = formatBuilder()
             .beforeFields("(")
             .afterFields(")")
             .fieldDelimiter(" ")
@@ -64,40 +83,35 @@ public final class ToString {
             .build();
 
     /**
-     * A format that puts each field on a separate line.
+     * @see SeparateLinesFormat
      */
-    public static final Format separateLines = formatBuilder()
-            .beforeFields(" {\n  ")
-            .afterFields("\n}")
-            .fieldDelimiter("\n  ")
-            .fieldFormatter((name, value) -> name + ": " + value)
-            .build();
+    public static final SeparateLinesFormat separateLines = new SeparateLinesFormat();
 
-    private final Format format;
+    private final IFormat format;
     private final StringJoiner stringJoiner;
 
-    private ToString(final Format format, final String prefix) {
+    ToString(final IFormat format, final String prefix) {
         this.format = format;
-        this.stringJoiner = new StringJoiner(format.fieldDelimiter, prefix + format.beforeFields, format.afterFields);
+        this.stringJoiner = new StringJoiner(format.fieldDelimiter(), prefix + format.beforeFields(), format.afterFields());
     }
 
     public ToString add(final String name, final Object value) {
         if (applyValueFilter(value)) {
-            stringJoiner.add(format.fieldFormatter.apply(name, value));
+            stringJoiner.add(format.fieldFormatter().apply(name, value));
         }
         return this;
     }
 
     public <X> ToString addIf(final String name, final X value, final Predicate<? super X> test) {
         if (applyValueFilter(value) && test.test(value)) {
-            stringJoiner.add(format.fieldFormatter.apply(name, value));
+            stringJoiner.add(format.fieldFormatter().apply(name, value));
         }
         return this;
     }
 
     public <X> ToString addIfNot(final String name, final X value, final Predicate<? super X> test) {
         if (applyValueFilter(value) && !test.test(value)) {
-            stringJoiner.add(format.fieldFormatter.apply(name, value));
+            stringJoiner.add(format.fieldFormatter().apply(name, value));
         }
         return this;
     }
@@ -137,41 +151,81 @@ public final class ToString {
     }
 
     private boolean applyValueFilter(final Object value) {
-        return format.valueFilter == null || format.valueFilter.test(value);
+        return format.valueFilter() == null || format.valueFilter().test(value);
     }
 
     /**
-     * @param beforeFields  sequence of characters that is prepended before fields
-     * @param afterFields  sequence of characters that is appended after fields
-     * @param fieldDelimiter  delimier of field entries
-     * @param valueFilter  filter of field values; if a value does not satisfy the predicate, the field is not included
-     *                     in the string; {@code null} represents the absence of a filter
-     * @param fieldFormatter  formats a field entry
+     * This interface should be implemented by any class whose instances can be formatted from another class that uses
+     * such an {@link IFormat} that recommends its users to implement {@link IFormattable}.
+     * <p>
+     * For example, if class {@code B} uses {@link IFormat} for its string representation and has a field of type {@code A},
+     * then, depending on the {@link IFormat} used, if {@code A} implements {@link IFormattable}, it will know that it
+     * is being formatted in a context (in the context of {@code B} in this example), and thus can contribute to a prettier
+     * format.
+     * <p>
+     * One use case of this facility is multi-line formats that use increasing levels of indentation.
      */
-    public record Format(CharSequence beforeFields, CharSequence afterFields, CharSequence fieldDelimiter,
-                         @Nullable Predicate<Object> valueFilter,
-                         BiFunction<String, Object, String> fieldFormatter)
+    public interface IFormattable {
+        String toString(IFormat format);
+    }
+
+    public interface IFormat {
+
+        default ToString toString(final Object object) {
+            requireNonNull(object, "object");
+            return new ToString(this, object.getClass().getSimpleName());
+        }
+
+        default ToString toString(final Class<?> type) {
+            requireNonNull(type, "type");
+            return new ToString(this, type.getSimpleName());
+        }
+
+        default ToString toString(final CharSequence prefix) {
+            requireNonNull(prefix, "prefix");
+            return new ToString(this, prefix.toString());
+        }
+
+        /**
+         * Sequence of characters that is prepended before formatting fields. Typically, this is an opening delimiter.
+         */
+        CharSequence beforeFields();
+
+        /**
+         * Sequence of characters that is appended after formatting fields. Typically, this is a closing delimiter.
+         */
+        CharSequence afterFields();
+
+        /**
+         * Sequence of characters that is inserted between formatted fields. It is used only if there are 2 or more fields.
+         */
+        CharSequence fieldDelimiter();
+
+        /**
+         * A predicate that determines whether a field will be formatted. It is applied to a field's value, which may be null.
+         */
+        Predicate<Object> valueFilter();
+
+        /**
+         * A function that formats a field. It is applied to a field's name and its value, which may be null.
+         */
+        BiFunction<String, Object, String> fieldFormatter();
+
+    }
+
+    /**
+     * The most general format implementation that can be used to create arbitrary simple formats.
+     */
+    public record Format (CharSequence beforeFields, CharSequence afterFields, CharSequence fieldDelimiter,
+                          @Nullable Predicate<Object> valueFilter,
+                          BiFunction<String, Object, String> fieldFormatter)
+            implements IFormat
     {
         public Format {
             requireNonNull(beforeFields, "beforeFields");
             requireNonNull(afterFields, "afterFields");
             requireNonNull(fieldDelimiter, "fieldDelimiter");
             requireNonNull(fieldFormatter, "fieldFormatter");
-        }
-
-        public ToString toString(final Object object) {
-            requireNonNull(object, "object");
-            return new ToString(this, object.getClass().getSimpleName());
-        }
-
-        public ToString toString(final Class<?> type) {
-            requireNonNull(type, "type");
-            return new ToString(this, type.getSimpleName());
-        }
-
-        public ToString toString(final CharSequence prefix) {
-            requireNonNull(prefix, "prefix");
-            return new ToString(this, prefix.toString());
         }
 
         public Builder toBuilder() {
@@ -186,14 +240,13 @@ public final class ToString {
             private @Nullable Predicate<Object> valueFilter;
             private BiFunction<String, Object, String> fieldFormatter;
 
-            private Builder () {}
+            private Builder() {}
 
             private Builder(final CharSequence beforeFields,
                             final CharSequence afterFields,
                             final CharSequence fieldDelimiter,
                             final Predicate<Object> valueFilter,
-                            final BiFunction<String, Object, String> fieldFormatter)
-            {
+                            final BiFunction<String, Object, String> fieldFormatter) {
                 this.beforeFields = beforeFields;
                 this.afterFields = afterFields;
                 this.fieldDelimiter = fieldDelimiter;
@@ -201,7 +254,7 @@ public final class ToString {
                 this.fieldFormatter = fieldFormatter;
             }
 
-            public Format build() {
+            public IFormat build() {
                 return new Format(beforeFields, afterFields, fieldDelimiter, valueFilter, fieldFormatter);
             }
 
@@ -251,6 +304,100 @@ public final class ToString {
                 return this;
             }
         }
+    }
+
+    /**
+     * A format that puts each field on a separate line.
+     * <p>
+     * It is highly recommended to implement {@link IFormattable}, which will enable control over levels of indentation.
+     */
+    public static final class SeparateLinesFormat implements IFormat {
+
+        private final CharSequence beforeFields;
+        private final CharSequence afterFields;
+        private final CharSequence fieldDelimiter;
+        private final @Nullable Predicate<Object> valueFilter;
+        /** Track the current depth to determine the indentation correctly. Starts from 0. */
+        private final int depth;
+
+        public SeparateLinesFormat(final @Nullable Predicate<Object> valueFilter) {
+            this(0, valueFilter);
+        }
+
+        public SeparateLinesFormat() {
+            this(0, null);
+        }
+
+        private SeparateLinesFormat(final int depth, final @Nullable Predicate<Object> valueFilter) {
+            if (depth < 0) {
+                throw new InvalidArgumentException("Depth must be a non-negative integer, but was: %s".formatted(depth));
+            }
+            final String indentString = indent(depth);
+            this.beforeFields = " {\n" + indentString;
+            this.afterFields = '\n' + indent(depth - 1) + '}';
+            this.fieldDelimiter = "\n" + indentString;
+            this.valueFilter = valueFilter;
+            this.depth = depth;
+        }
+
+        private static String indent(final int depth) {
+            return "  ".repeat(depth + 1);
+        }
+
+        /**
+         * Returns a new instance with the specified depth.
+         */
+        public SeparateLinesFormat setDepth(final int newDepth) {
+            return new SeparateLinesFormat(newDepth, valueFilter);
+        }
+
+        @Override
+        public ToString toString(final Object object) {
+            return IFormat.super.toString(object);
+        }
+
+        @Override
+        public ToString toString(final Class<?> type) {
+            return IFormat.super.toString(type);
+        }
+
+        @Override
+        public ToString toString(final CharSequence prefix) {
+            return IFormat.super.toString(prefix);
+        }
+
+        @Override
+        public CharSequence beforeFields() {
+            return beforeFields;
+        }
+
+        @Override
+        public CharSequence afterFields() {
+            return afterFields;
+        }
+
+        @Override
+        public CharSequence fieldDelimiter() {
+            return fieldDelimiter;
+        }
+
+        @Override
+        public Predicate<Object> valueFilter() {
+            return valueFilter;
+        }
+
+        @Override
+        public BiFunction<String, Object, String> fieldFormatter() {
+            return (name, value) -> name + ": " + formatValue(value);
+        }
+
+        private String formatValue(final Object value) {
+            return switch (value) {
+                case IFormattable it -> it.toString(setDepth(depth + 1));
+                case null, default -> Objects.toString(value);
+            };
+        }
+
     }
 
 }
