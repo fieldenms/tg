fieldenms/tg

commit ca9d46a09e6ee055bc5e7b847a252d3fb2598f38

#348 Use PropertyInliner in HibernateMappingsGenerator and DomainMetadataModelGenerator

platform-dao/src/main/java/ua/com/fielden/platform/eql/meta/DomainMetadataModelGenerator.java
@@ -2,7 +2,11 @@ package ua.com.fielden.platform.eql.meta;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.AbstractUnionEntity;
-import ua.com.fielden.platform.meta.*;
+import ua.com.fielden.platform.eql.dbschema.PropertyInliner;
+import ua.com.fielden.platform.meta.EntityMetadata;
+import ua.com.fielden.platform.meta.IDomainMetadata;
+import ua.com.fielden.platform.meta.PropertyMetadata;
+import ua.com.fielden.platform.meta.PropertyTypeMetadata;
 import ua.com.fielden.platform.types.Money;
 import ua.com.fielden.platform.utils.Pair;
 
@@ -24,9 +28,11 @@ import static ua.com.fielden.platform.utils.EntityUtils.isUnionEntityType;
 public final class DomainMetadataModelGenerator {
 
     private final IDomainMetadata domainMetadata;
+    private final PropertyInliner propertyInliner;
 
-    public DomainMetadataModelGenerator(final IDomainMetadata domainMetadata) {
+    public DomainMetadataModelGenerator(final IDomainMetadata domainMetadata, final PropertyInliner propertyInliner) {
         this.domainMetadata = domainMetadata;
+        this.propertyInliner = propertyInliner;
     }
 
     public Map<Class<?>, DomainTypeData> generateDomainTypesData(final Set<Class<? extends AbstractEntity<?>>> entityTypes) {
@@ -102,7 +108,6 @@ public final class DomainMetadataModelGenerator {
     }
 
     public List<DomainPropertyData> generateDomainPropsData(final Map<Class<?>, DomainTypeData> typesMap) {
-        final PropertyMetadataUtils pmUtils = domainMetadata.propertyMetadataUtils();
         final List<DomainPropertyData> result = new ArrayList<>();
 
         long id = typesMap.size();
@@ -140,26 +145,31 @@ public final class DomainMetadataModelGenerator {
                                                                                       : pm),
                                                   position));
 
-                // adding subproperties of union type properties
-                if (pmUtils.isPropEntityType(pm.type(), EntityMetadata::isUnion)) {
-                    final List<PropertyMetadata> subProps = pmUtils.subProperties(pm);
-                    final long holderId = id;
-                    int subItemPosition = 0;
-                    for (final var spm : subProps.stream().flatMap(spm -> spm.asPersistent().stream()).toList()) {
-                        id = id + 1;
-                        subItemPosition = subItemPosition + 1;
-                        final Pair<String, String> titleAndDesc = getTitleAndDesc(spm.name(), propJavaType);
-                        result.add(new DomainPropertyData(id,
-                                                          spm.name(),
-                                                          null,
-                                                          holderId,
-                                                          typesMap.get((Class<?>) spm.type().javaType()).id(),
-                                                          titleAndDesc.getKey(),
-                                                          titleAndDesc.getValue(),
-                                                          null,
-                                                          false,
-                                                          spm.data().column().name,
-                                                          subItemPosition));
+                // subproperties
+                if (pm.isPersistent()) {
+                    final var ppm = pm.asPersistent().orElseThrow();
+                    final var optSubProps = propertyInliner.inline(ppm)
+                            // ignore single-component composite types, they are treated as primitive types
+                            .filter(props -> !ppm.type().isComposite() || props.size() > 1);
+                    if (optSubProps.isPresent()) {
+                        final long holderId = id;
+                        int subItemPosition = 0;
+                        for (final var subProp : optSubProps.get().stream().flatMap(spm -> spm.asPersistent().stream()).toList()) {
+                            id = id + 1;
+                            subItemPosition = subItemPosition + 1;
+                            final var titleAndDesc = getTitleAndDesc(subProp.name(), propJavaType);
+                            result.add(new DomainPropertyData(id,
+                                                              subProp.name(),
+                                                              null,
+                                                              holderId,
+                                                              typesMap.get((Class<?>) subProp.type().javaType()).id(),
+                                                              titleAndDesc.getKey(),
+                                                              titleAndDesc.getValue(),
+                                                              null,
+                                                              false,
+                                                              subProp.data().column().name,
+                                                              subItemPosition));
+                        }
                     }
                 }
             }
@@ -171,15 +181,12 @@ public final class DomainMetadataModelGenerator {
     private @Nullable String determinePropColumn(final PropertyMetadata pm) {
         return switch (pm) {
             case PropertyMetadata.CritOnly $ -> CRITERION;
-            case PropertyMetadata.Persistent ppm ->
-                    domainMetadata.propertyMetadataUtils().isPropEntityType(ppm, EntityMetadata::isUnion)
-                            ? null : ppm.data().column().name;
-            default -> {
-                final var subProps = domainMetadata.propertyMetadataUtils().subProperties(pm);
-                yield subProps.size() == 1
-                        ? subProps.getFirst().asPersistent().map(pspm -> pspm.data().column().name).orElse(null)
-                        : null;
-            }
+            case PropertyMetadata.Persistent it -> propertyInliner.inline(it)
+                    .filter(ps -> ps.size() == 1)
+                    .map(List::getFirst)
+                    .map(prop -> prop.data().column().name)
+                    .orElse(null);
+            default -> null;
         };
     }
 

platform-dao/src/main/java/ua/com/fielden/platform/eql/dbschema/HibernateMappingsGenerator.java
@@ -41,15 +41,18 @@ public class HibernateMappingsGenerator {
     private final EqlTables eqlTables;
     private final IDbVersionProvider dbVersionProvider;
     private final PropertyMetadataUtils pmUtils;
+    private final PropertyInliner propertyInliner;
 
     @Inject
     public HibernateMappingsGenerator(final IDomainMetadata domainMetadata,
                                       final IDbVersionProvider dbVersionProvider,
-                                      final EqlTables eqlTables) {
+                                      final EqlTables eqlTables,
+                                      final PropertyInliner propertyInliner) {
         this.eqlTables = eqlTables;
         this.domainMetadata = domainMetadata;
         this.pmUtils = domainMetadata.propertyMetadataUtils();
         this.dbVersionProvider = dbVersionProvider;
+        this.propertyInliner = propertyInliner;
     }
 
     public String generateMappings() {
@@ -214,29 +217,27 @@ public class HibernateMappingsGenerator {
         if (pmUtils.isPropEntityType(prop, EntityMetadata::isUnion)) {
             return generateUnionEntityPropertyMapping(prop);
         }
-        // potential multi-column mapping
-        else if (prop.type().isComposite() || pmUtils.isPropEntityType(prop, EntityMetadata::isUnion)) {
-            final List<PropColumn> subColumns = pmUtils.subProperties(prop).stream()
-                    .flatMap(subProp -> subProp.asPersistent().stream())
-                    .map(subProp -> subProp.data().column())
-                    .toList();
-
-            final Either<PropColumn, List<String>> column = subColumns.size() == 1
-                    ? left(subColumns.getFirst())
-                    : right(subColumns.stream().map(c -> c.name).toList());
-            return generatePlainPropertyMapping(prop.name(), column, prop.hibType().getClass().getName());
-        }
-        else if (pmUtils.isPropEntityType(prop, EntityMetadata::isPersistent)) {
-            final var et = prop.type().asEntity().orElseThrow();
-            if (KEY.equals(prop.name())) {
-                return generateOneToOnePropertyMapping(prop.name(), et.javaType());
-            } else {
-                return generateManyToOnePropertyMapping(prop.name(), prop.data().column().name, et.javaType());
-            }
-        }
-        else {
-            return generatePlainPropertyMapping(prop.name(), left(prop.data().column()), prop.hibType().getClass().getName());
-        }
+        return propertyInliner.inline(prop)
+                .map(props -> props.stream().map(p -> p.data().column()).toList())
+                .map(columns -> {
+                    final Either<PropColumn, List<String>> column = columns.size() == 1
+                            ? left(columns.getFirst())
+                            : right(columns.stream().map(c -> c.name).toList());
+                    return generatePlainPropertyMapping(prop.name(), column, prop.hibType().getClass().getName());
+                })
+                .orElseGet(() -> {
+                    if (pmUtils.isPropEntityType(prop, EntityMetadata::isPersistent)) {
+                        final var et = prop.type().asEntity().orElseThrow();
+                        if (KEY.equals(prop.name())) {
+                            return generateOneToOnePropertyMapping(prop.name(), et.javaType());
+                        } else {
+                            return generateManyToOnePropertyMapping(prop.name(), prop.data().column().name, et.javaType());
+                        }
+                    }
+                    else {
+                        return generatePlainPropertyMapping(prop.name(), left(prop.data().column()), prop.hibType().getClass().getName());
+                    }
+                });
     }
 
     private static IllegalArgumentException unexpectedPropNature(Object prop, Object expectedNature) {

commit 47a61955a292b1a2c43d9a65e080a726685ec3c5

#348 Address a regression that made DomainPropertyData.column null for all non-inlineable properties
ca9d46a09e6ee055bc5e7b847a252d3fb2598f38 caused the regression.

platform-dao/src/main/java/ua/com/fielden/platform/eql/meta/DomainMetadataModelGenerator.java
@@ -8,6 +8,8 @@ import ua.com.fielden.platform.meta.EntityMetadata;
 import ua.com.fielden.platform.meta.IDomainMetadata;
 import ua.com.fielden.platform.meta.PropertyMetadata;
 import ua.com.fielden.platform.meta.PropertyTypeMetadata;
+import ua.com.fielden.platform.types.either.Left;
+import ua.com.fielden.platform.types.either.Right;
 import ua.com.fielden.platform.utils.Pair;
 
 import javax.annotation.Nullable;
@@ -177,14 +179,21 @@ public final class DomainMetadataModelGenerator {
         return result;
     }
 
+    /**
+     * Determines the column name of the specified property.
+     * <p>
+     * Properties that have a component type are handles specially.
+     * If the component type has a single component, that component's column name is returned; otherwise, null is returned.
+     */
     private @Nullable String determinePropColumn(final PropertyMetadata pm) {
         return switch (pm) {
             case PropertyMetadata.CritOnly $ -> CRITERION;
-            case PropertyMetadata.Persistent it -> propertyInliner.inline(it)
-                    .filter(ps -> ps.size() == 1)
-                    .map(List::getFirst)
-                    .map(prop -> prop.data().column().name)
-                    .orElse(null);
+            case PropertyMetadata.Persistent it ->
+                    propertyInliner.inlineOrGet(it)
+                            .fold(it_ -> it_.data().column().name,
+                                  inlined -> inlined.size() == 1
+                                          ? inlined.getFirst().data().column().name
+                                          : null);
             default -> null;
         };
     }