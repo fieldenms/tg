62c14d2349de1996caa8173e608c280673c73991
01es <oles.hodych@gmail.com>
2024-10-05 16:58:02 +1000

    #348 Avoid nulls and rely on the default implementation.

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/dbschema/PropertyInlinerImpl.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/dbschema/PropertyInlinerImpl.java
@@ -1,5 +1,6 @@
 package ua.com.fielden.platform.eql.dbschema;
 
+import com.google.common.collect.ImmutableList;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import ua.com.fielden.platform.eql.dbschema.exceptions.DbSchemaException;
@@ -8,12 +9,15 @@ import ua.com.fielden.platform.meta.IDomainMetadata;
 import ua.com.fielden.platform.meta.PropertyMetadata;
 import ua.com.fielden.platform.types.either.Either;
 
-import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
 import java.util.List;
 import java.util.Optional;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
-import static java.lang.String.format;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+import static ua.com.fielden.platform.types.either.Either.left;
+import static ua.com.fielden.platform.types.either.Either.right;
 
 // TODO reduce visibility after configuring tests to use Injector
 @Singleton
@@ -28,16 +32,11 @@ public final class PropertyInlinerImpl implements PropertyInliner {
 
     @Override
     public Optional<List<PropertyMetadata.Persistent>> inline(final PropertyMetadata.Persistent property) {
-        return Optional.ofNullable(inline_(property));
-    }
-
-    @Override
-    public Either<PropertyMetadata.Persistent, List<PropertyMetadata.Persistent>> inlineOrGet(final PropertyMetadata.Persistent property) {
         final var inlined = inline_(property);
-        return inlined == null ? Either.left(property) : Either.right(inlined);
+        return inlined.isEmpty() ? empty() : of(inlined);
     }
 
-    private @Nullable List<PropertyMetadata.Persistent> inline_(final PropertyMetadata.Persistent property) {
+    private @Nonnull List<PropertyMetadata.Persistent> inline_(final PropertyMetadata.Persistent property) {
         final var pmUtils = domainMetadata.propertyMetadataUtils();
 
         final var subProps = pmUtils.subProperties(property).stream()
@@ -45,12 +44,12 @@ public final class PropertyInlinerImpl implements PropertyInliner {
                 .collect(toImmutableList());
         if (property.type().isComponent() || pmUtils.isPropEntityType(property, EntityMetadata::isUnion)) {
             if (subProps.isEmpty()) {
-                throw new DbSchemaException(format("Invalid property: sub-properties must not be empty. Property: %s", property));
+                throw new DbSchemaException("Invalid property: sub-properties must not be empty. Property: %s".formatted(property));
             }
             return subProps;
         }
         else {
-            return null;
+            return ImmutableList.of();
         }
     }
 
