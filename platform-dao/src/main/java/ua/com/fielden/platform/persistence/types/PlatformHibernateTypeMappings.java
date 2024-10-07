2314dd9818b07c68fc691f1aa2645fc56be8f9af
homedirectory <nullsink@posteo.eu>
2024-10-07 16:57:45 +0300

    #348 Modify PlatformHibernateTypeMappings to use a DB-dependent Hibernate custom RichText type

--- platform-dao/src/main/java/ua/com/fielden/platform/persistence/types/PlatformHibernateTypeMappings.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/persistence/types/PlatformHibernateTypeMappings.java
@@ -1,7 +1,9 @@
 package ua.com.fielden.platform.persistence.types;
 
+import com.google.inject.Inject;
 import org.hibernate.type.YesNoType;
 import ua.com.fielden.platform.entity.meta.PropertyDescriptor;
+import ua.com.fielden.platform.entity.query.IDbVersionProvider;
 import ua.com.fielden.platform.types.*;
 import ua.com.fielden.platform.types.markers.*;
 
@@ -10,34 +12,49 @@ import java.util.Date;
 /**
  * Mappings of primitive and component types to Hibernate user types.
  * <p>
+ * {@link Provider} should be bound in a Guice module: {@code bind(HibernateTypeMappings.class).toProvider(PlatformHibernateTypeMappings.Provider.class)},
+ * which will enable access to a {@link HibernateTypeMappings} instance via an injector.
+ * <p>
  * This information used to be provided by {@code HibernateUserTypesModule} and {@code HibernateSetup} at the level of applications.
  */
 public final class PlatformHibernateTypeMappings {
 
-    public static final HibernateTypeMappings PLATFORM_HIBERNATE_TYPE_MAPPINGS = HibernateTypeMappings.builder()
-            .put(boolean.class, YesNoType.INSTANCE)
-            .put(Date.class, DateTimeType.INSTANCE)
-            .put(PropertyDescriptor.class, PropertyDescriptorType.INSTANCE)
-            .put(Money.class, SimpleMoneyType.INSTANCE)
-            .put(Colour.class, ColourType.INSTANCE)
-            .put(Hyperlink.class, HyperlinkType.INSTANCE)
-            .put(RichText.class, RichTextType.INSTANCE)
+    public static final class Provider implements jakarta.inject.Provider<HibernateTypeMappings> {
 
-            .put(ISecurityTokenType.class, SecurityTokenType.INSTANCE)
-            .put(IPropertyDescriptorType.class, PropertyDescriptorType.INSTANCE)
-            .put(IColourType.class, ColourType.INSTANCE)
-            .put(IHyperlinkType.class, HyperlinkType.INSTANCE)
-            .put(IUtcDateTimeType.class, UtcDateTimeType.INSTANCE)
-            .put(IRichTextType.class, RichTextType.INSTANCE)
+        private final IDbVersionProvider dbVersionProvider;
 
-            .put(IMoneyType.class, MoneyType.INSTANCE)
-            .put(ISimpleMoneyType.class, SimpleMoneyType.INSTANCE)
-            .put(ISimplyMoneyWithTaxAndExTaxAmountType.class, SimplyMoneyWithTaxAndExTaxAmountType.INSTANCE)
-            .put(ISimplyMoneyWithTaxAmountType.class, SimplyMoneyWithTaxAmountType.INSTANCE)
-            .put(IMoneyWithTaxAmountType.class, MoneyWithTaxAmountType.INSTANCE)
+        @Inject
+        public Provider(final IDbVersionProvider dbVersionProvider) {
+            this.dbVersionProvider = dbVersionProvider;
+        }
 
-            .build();
+        @Override
+        public HibernateTypeMappings get() {
+            final var richTextType = RichTextType.getInstance(dbVersionProvider.dbVersion());
 
-    private PlatformHibernateTypeMappings() {}
+            return HibernateTypeMappings.builder()
+                    .put(boolean.class, YesNoType.INSTANCE)
+                    .put(Date.class, DateTimeType.INSTANCE)
+                    .put(PropertyDescriptor.class, PropertyDescriptorType.INSTANCE)
+                    .put(Money.class, SimpleMoneyType.INSTANCE)
+                    .put(Colour.class, ColourType.INSTANCE)
+                    .put(Hyperlink.class, HyperlinkType.INSTANCE)
+                    .put(RichText.class, richTextType)
+
+                    .put(ISecurityTokenType.class, SecurityTokenType.INSTANCE)
+                    .put(IPropertyDescriptorType.class, PropertyDescriptorType.INSTANCE)
+                    .put(IColourType.class, ColourType.INSTANCE)
+                    .put(IHyperlinkType.class, HyperlinkType.INSTANCE)
+                    .put(IUtcDateTimeType.class, UtcDateTimeType.INSTANCE)
+                    .put(IRichTextType.class, richTextType)
+
+                    .put(IMoneyType.class, MoneyType.INSTANCE)
+                    .put(ISimpleMoneyType.class, SimpleMoneyType.INSTANCE)
+                    .put(ISimplyMoneyWithTaxAndExTaxAmountType.class, SimplyMoneyWithTaxAndExTaxAmountType.INSTANCE)
+                    .put(ISimplyMoneyWithTaxAmountType.class, SimplyMoneyWithTaxAmountType.INSTANCE)
+                    .put(IMoneyWithTaxAmountType.class, MoneyWithTaxAmountType.INSTANCE)
+                    .build();
+        }
+    }
 
 }
