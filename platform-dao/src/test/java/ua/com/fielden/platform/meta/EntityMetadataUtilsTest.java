8c9839c11c1a3d539572f209795a18823dfb7b80
01es <oles.hodych@gmail.com>
2024-10-08 13:02:48 +1100

    #348 No need to rebuild domain metadata for each test.

--- platform-dao/src/test/java/ua/com/fielden/platform/meta/EntityMetadataUtilsTest.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/meta/EntityMetadataUtilsTest.java
@@ -2,6 +2,7 @@ package ua.com.fielden.platform.meta;
 
 import org.junit.Test;
 import ua.com.fielden.platform.entity.query.DbVersion;
+import ua.com.fielden.platform.entity.query.IDbVersionProvider;
 import ua.com.fielden.platform.meta.Assertions.EntityA;
 import ua.com.fielden.platform.persistence.types.PlatformHibernateTypeMappings;
 import ua.com.fielden.platform.sample.domain.TgAuthor;
@@ -16,16 +17,9 @@ import static ua.com.fielden.platform.test_utils.CollectionTestUtils.assertEqual
 
 public class EntityMetadataUtilsTest {
 
-    private final IDomainMetadata domainMetadata;
-    private final EntityMetadataUtils emUtils;
-
-    public EntityMetadataUtilsTest() {
-        final var dbVersionProvider = constantDbVersion(DbVersion.MSSQL);
-        domainMetadata = new DomainMetadataBuilder(
-                new PlatformHibernateTypeMappings.Provider(dbVersionProvider).get(), List.of(), dbVersionProvider)
-                .build();
-        emUtils =  domainMetadata.entityMetadataUtils();
-    }
+    private static final IDbVersionProvider dbVersionProvider = constantDbVersion(DbVersion.MSSQL);
+    private static final IDomainMetadata domainMetadata = new DomainMetadataBuilder(new PlatformHibernateTypeMappings.Provider(dbVersionProvider).get(), List.of(), dbVersionProvider).build();
+    private static final EntityMetadataUtils emUtils = domainMetadata.entityMetadataUtils();
 
     @Test
     public void unionMembers_returns_properties_that_are_union_members_of_a_given_union_entity() {
