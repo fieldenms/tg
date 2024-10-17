cbfc5fa13c3c589a833fe8984d68ea5552748fd4
jhou-pro <jhou.pro@gmail.com>
2024-10-17 12:53:18 +0300

    Revert "#2329 Auto-fix for insecure randomness."
    This reverts commit 4fe98c5f4f9154a39f12850da355511be0d696ff.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/reflection/tg-polymer-utils.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/reflection/tg-polymer-utils.js
@@ -4,11 +4,11 @@ import { TgReflector } from '/app/tg-reflector.js';
  * Generates the unique identifier.
  */
 export function generateUUID () {
-    var array = new Uint8Array(16);
-    window.crypto.getRandomValues(array);
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c, i) {
-       var r = array[i] % 16;
-       return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
     return uuid;
 };
