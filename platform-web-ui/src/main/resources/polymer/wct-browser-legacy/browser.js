9855f1bb0a1b487705f038534d8c502bddc9cdd8
jhou-pro <jhou.pro@gmail.com>
2024-10-19 01:24:49 +0300

    #2329 Lib transform: wct-browser-legacy/browser.
    9. After major upgrade for Mocha from last supported by WCT version to version 10.7.3 only one web test can be run. Others in suite and in other suites are not started due to hidden error inside WCT saying that 'runner.stats' are not initialised. We take wct-mocha.js way of initialising this object.

--- platform-web-ui/src/main/resources/polymer/wct-browser-legacy/browser.js
+++ platform-web-ui/src/main/resources/polymer/wct-browser-legacy/browser.js
@@ -1027,6 +1027,106 @@
   function logException(error) {
     log(error.stack || error.message || error + '', 'stack');
   }
+
+    /*TG #2329*/
+    // Define statsCollector exactly as in wct-mocha.js to be able to use it in Title and Console.
+    // Otherwise only one test can be run and it stops there.
+  	function createCommonjsModule(fn, module) {
+  		return module = { exports: {} }, fn(module, module.exports), module.exports;
+  	}
+
+	function unwrapExports (x) {
+		return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+	}
+
+	var statsCollector = createCommonjsModule(function (module, exports) {
+	Object.defineProperty(exports, "__esModule", { value: true });
+	/**
+	(The MIT License)
+
+	Copyright (c) 2011-2018 JS Foundation and contributors, https://js.foundation
+
+	Permission is hereby granted, free of charge, to any person obtaining
+	a copy of this software and associated documentation files (the
+	'Software'), to deal in the Software without restriction, including
+	without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to
+	permit persons to whom the Software is furnished to do so, subject to
+	the following conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+	CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	*/
+	/**
+	 * The following was extracted from
+	 * https://github.com/mochajs/mocha/blob/master/lib/stats-collector.js
+	 */
+	/**
+	 * Test statistics collector.
+	 *
+	 * @typedef {Object} StatsCollector
+	 * @property {number} suites - integer count of suites run.
+	 * @property {number} tests - integer count of tests run.
+	 * @property {number} passes - integer count of passing tests.
+	 * @property {number} pending - integer count of pending tests.
+	 * @property {number} failures - integer count of failed tests.
+	 * @property {Date} start - time when testing began.
+	 * @property {Date} end - time when testing concluded.
+	 * @property {number} duration - number of msecs that testing took.
+	 */
+	/**
+	 * Provides stats such as test duration,
+	 * number of tests passed / failed etc.
+	 *
+	 * @public
+	 * @memberof Mocha
+	 * @param {Runner} runner
+	 */
+	exports.createStatsCollector = function (runner) {
+	    var stats = { suites: 0, tests: 0, passes: 0, pending: 0, failures: 0 };
+	    if (!runner) {
+	        throw new TypeError('Missing runner argument');
+	    }
+	    runner.stats = stats;
+	    runner.once('start', function () {
+	        stats.start = new Date();
+	    });
+	    runner.on('suite', function (suite) {
+	        suite.root || stats.suites++;
+	    });
+	    runner.on('pass', function () {
+	        stats.passes++;
+	    });
+	    runner.on('fail', function () {
+	        stats.failures++;
+	    });
+	    runner.on('pending', function () {
+	        stats.pending++;
+	    });
+	    runner.on('test end', function () {
+	        stats.tests++;
+	    });
+	    runner.once('end', function () {
+	        stats.end = new Date();
+	        // To coerce to numbers and make TS compiler happy, we use unary `+` prefix
+	        // for date arithmetic.
+	        stats.duration = +stats.end - +stats.start;
+	    });
+	};
+
+	});
+
+	unwrapExports(statsCollector);
+	/*TG #2329*/
+
   /**
    * A Mocha reporter that logs results out to the web `console`.
    */
@@ -1039,6 +1139,17 @@
      * @param runner The runner that is being reported on.
      */
     function Console(runner) {
+      /*TG #2329*/
+      // Use statsCollector exactly as in wct-mocha.js to insert stats into runner.
+      // Otherwise only one test can be run and it stops there.
+      // ------------------------------------------------------------------
+      // Mocha 6 runner doesn't have stats at this point so we need to use
+      // the stats-collector from Mocha to add them before calling the base
+      // reporter.
+      if (!runner.stats) {
+          statsCollector.createStatsCollector(runner);
+      }
+
       Mocha.reporters.Base.call(this, runner);
       runner.on('suite', function (suite) {
         if (suite.root) {
@@ -1407,6 +1518,17 @@
   /** @class */
   function () {
     function Title(runner) {
+      /*TG #2329*/
+      // Use statsCollector exactly as in wct-mocha.js to insert stats into runner.
+      // Otherwise only one test can be run and it stops there.
+      // ------------------------------------------------------------------
+      // Mocha 6 runner doesn't have stats at this point so we need to use
+      // the stats-collector from Mocha to add them before calling the base
+      // reporter.
+      if (!runner.stats) {
+          statsCollector.createStatsCollector(runner);
+      }
+
       Mocha.reporters.Base.call(this, runner);
       runner.on('test end', this.report.bind(this));
     }
