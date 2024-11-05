d1791004e5ba1e27f315c46560c88c3ce5d164e9
homedirectory <nullsink@posteo.eu>
2024-11-05 09:08:15 +0200

    #2349 Javadoc adjustments

--- platform-pojo-bl/src/test/java/ua/com/fielden/platform/test/EntityTestIocModuleWithPropertyFactory.java
+++ platform-pojo-bl/src/test/java/ua/com/fielden/platform/test/EntityTestIocModuleWithPropertyFactory.java
@@ -12,9 +12,7 @@ import ua.com.fielden.platform.utils.IDates;
 /**
  * This Guice module ensures that all observable and validatable properties are handled correctly. In addition to {@link EntityIocModule}, this module binds
  * {@link IMetaPropertyFactory}.
- * 
- * IMPORTANT: This module is applicable strictly for testing purposes! Left in the main source (e.i. not test) due to the need to be visible in other projects.
- * 
+ *
  * @author TG Team
  */
 public class EntityTestIocModuleWithPropertyFactory extends EntityIocModule {
