b43c955238774bad195f61fa046cfb7f418ced9c
01es <oles.hodych@gmail.com>
2024-11-06 19:54:12 +1100

    #2061 Minor Javadoc correction.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/indexer/IPropertyIndexer.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/indexer/IPropertyIndexer.java
@@ -6,7 +6,7 @@ import javax.annotation.Nullable;
 import java.lang.invoke.MethodHandle;
 
 /**
- * A contract for indexers that build {@link PropertyIndex}es to be used to accessing (reading/writing) entity properties.
+ * A contract for indexers that build {@link PropertyIndex}es to be used for accessing (reading/writing) entity properties.
  */
 public interface IPropertyIndexer {
 
