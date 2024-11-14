2552c5166a1073ad9c64531c9eeda3c30623c32c
Oleh Maikovych <oleh@fielden.com.au>
2024-11-14 12:30:31 +0200

    #185 Adjusted line height for text and related styles to make it look the same as in multiline text editor.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/components/rich-text/tg-rich-text-input-enhanced-styles.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/components/rich-text/tg-rich-text-input-enhanced-styles.js
@@ -24,14 +24,18 @@ styleElement.innerHTML = `
                 font-family: inherit !important;
                 font-size: 16px !important; /*this font size was taken from paper-style typography. It was taken from there because there is no other way to inherit or specify a proper font-size*/
             }
-            .toastui-editor-contents li {
-                padding: 2px 0;
-            }
             .toastui-editor-contents h1, .toastui-editor-contents h2 {
                 border-bottom: none !important;
             }
             .toastui-editor-contents a {
                 cursor: pointer !important;
+            }
+            .toastui-editor-contents .task-list-item:before {
+                top: 3px !important; /* 3px = 24px(lineheight)/2 - 18px(checkbox height)/2 */
+            }
+            .toastui-editor-contents ul>li:before {
+                margin-top: 0 !important;
+                top: 9px !important; /* 10px = 24px(lineheight)/2 - 5px(point height)/2 */
             }
             .toastui-editor-contents .task-list-item {
                 cursor: pointer; /*Need to set this style to display pointer cursor on checkboxes of task lins on Safari*/
@@ -39,11 +43,8 @@ styleElement.innerHTML = `
             .toastui-editor-contents .task-list-item * {
                 cursor: auto; /*Set automatic cusrsor  on any element of task list to make pointer cursor available only on checkbox*/
             }
-            .toastui-editor-contents .task-list-item:before {
-                top: 2px !important; /*should add 2px top because of padding for li element that also has 2px on top*/
-            }
             .toastui-editor-contents :not(table){
-                line-height: normal;
+                line-height: 24px;
             }
             del a span, del a {
                 text-decoration: line-through underline !important;
