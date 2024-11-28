a9eeed5e6dc7bed8c3af0374cb2e7b970746a29d
01es <oles.hodych@gmail.com>
2024-11-28 16:57:25 +1100

    #2316 Added TODOs to better handle interruptions.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/validation/ActivePropertyValidator.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/validation/ActivePropertyValidator.java
@@ -11,17 +11,12 @@ import ua.com.fielden.platform.entity.meta.MetaProperty;
 import ua.com.fielden.platform.entity.meta.impl.AbstractBeforeChangeEventHandler;
 import ua.com.fielden.platform.entity.query.EntityAggregates;
 import ua.com.fielden.platform.entity.query.fluent.fetch;
-import ua.com.fielden.platform.entity.query.model.AggregatedResultQueryModel;
-import ua.com.fielden.platform.entity.query.model.OrderingModel;
-import ua.com.fielden.platform.entity.validation.custom.DomainEntityDependencies;
-import ua.com.fielden.platform.entity.validation.custom.DomainEntityDependencies.DomainEntityDependency;
 import ua.com.fielden.platform.error.Result;
 import ua.com.fielden.platform.types.tuples.T3;
 
 import java.lang.annotation.Annotation;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import static java.lang.Math.max;
@@ -75,26 +70,41 @@ public class ActivePropertyValidator extends AbstractBeforeChangeEventHandler<Bo
             final int count;
             if (!co.isStale(entity.getId(), entity.getVersion())) {
                 count = entity.getRefCount();
-            } else {
-                // need to retrieve the latest refCount
+            }
+            // If an entity is stale, need to retrieve the latest refCount.
+            else {
                 final fetch fetch = fetchOnly(entity.getType()).with(REF_COUNT);
                 final ActivatableAbstractEntity<?> updatedEntity = (ActivatableAbstractEntity<?>) co.findById(entity.getId(), fetch);
                 count = updatedEntity.getRefCount();
             }
 
+            // TODO: refCount excludes deactivatable dependencies.
+            //       In light of issue #2316, this condition is no longer sufficient.
+            //       Issue #1745 already touched on that, suggesting converting refCount to a function and removing all refCount-related computations.
+            // If refCount indicates 0 references, return success, permitting deactivation.
             if (count == 0) {
                 return successful();
-            } else {
-                final Map<Class<? extends AbstractEntity<?>>, DomainEntityDependencies> domainDependency = getEntityDependantsMap(applicationDomainProvider.entityTypes());
-                final Set<DomainEntityDependency> dependencies = domainDependency.get(entity.getType()).getActivatableDependencies();
+            }
+            // Otherwise, need to identify active dependencies, preventing deactivation.
+            else {
+                // TODO: Optimise by considering only activatable dependencies.
+                final var domainDependency = entityDependencyMap(applicationDomainProvider.entityTypes());
+                // TODO: At the moment deactivatable entities, associated with the current entity are excluded not to count transitive references/dependencies.
+                //       However, it is now required to count such transitive references, which in turn requires recursive analysis of dependencies.
+                //       This is to cater for rare, but possible cases of having deactivatable dependencies on top of deactivatable dependencies.
+                final var activatableDependencies = domainDependency.get(entity.getType()).getActivatableDependencies();
                 // There can be cases where the domain model evolved and entity dependencies were relaxed/removed, but refCount has not been updated.
                 // In such cases, there will be no dependencies identified in the domain model, and thus there is no reason to report any errors.
-                if (dependencies.isEmpty()) {
+                if (activatableDependencies.isEmpty()) {
                     LOGGER.warn(WARN_ENTITY_HAS_NO_ACTIVATABLE_DEPENDENCIES.formatted(entity.getType().getSimpleName()));
                     return successful();
                 } else {
-                    final AggregatedResultQueryModel query = generateQuery(dependencies, true);
-                    final OrderingModel orderBy = orderBy().yield(COUNT).desc().yield(ENTITY_TYPE_TITLE).asc().yield(DEPENDENT_PROP_TITLE).asc().model();
+                    // Exclude inactive dependencies to guarantee that only records with dependency COUNT > 0 are returned.
+                    final var query = dependencyCountQuery(activatableDependencies, true);
+                    // TODO: Ordering would need to take into account the presence of deactivatable dependencies somehow,
+                    //       so that the output would be a linearised tree structure of dependencies.
+                    //       It would potentially be useful to make deactivatable dependencies obvious in the resultant message.
+                    final var orderBy = orderBy().yield(COUNT).desc().yield(ENTITY_TYPE_TITLE).asc().yield(DEPENDENT_PROP_TITLE).asc().model();
                     return failure(count, mkErrorMsg(entity, count, co(EntityAggregates.class).getAllEntities(from(query).with(orderBy).with(mapOf(t2(PARAM, entity))).model())));
                 }
             }
@@ -105,7 +115,7 @@ public class ActivePropertyValidator extends AbstractBeforeChangeEventHandler<Bo
             // We cannot rely on the fact that all activatable are fetched.
             // Therefore, we should only perform a so-called soft validation,
             // where validation would occur strictly against fetched values.
-            // Later, during saving all activatable properties would get checked anyway.
+            // Later during saving, all activatable properties would get checked anyway.
             final var activatableProps = collectActivatableNotNullNotProxyProperties(entity);
 
             // Need to check if already referenced activatables are active and thus may be referenced by this entity, which is being activated.
@@ -124,7 +134,7 @@ public class ActivePropertyValidator extends AbstractBeforeChangeEventHandler<Bo
     }
 
     private String mkErrorMsg(final ActivatableAbstractEntity<?> entity, final int count, final List<EntityAggregates> dependencies) {
-        final T3<Integer, Integer, Integer> lengths = dependencies.stream()
+        final var lengths = dependencies.stream()
                 .map(dep -> t3(dep.get(ENTITY_TYPE_TITLE).toString().length(), dep.get(DEPENDENT_PROP_TITLE).toString().length(), dep.get(COUNT).toString().length()))
                 .reduce(t3(0, 0, 0), (accum, val) -> t3(max(accum._1, val._1), max(accum._2, val._2), max(accum._3, val._3)), (v1, v2) -> {throw failure("Should not happen");}); 
         final String deps = dependencies.stream()
