da793f4c26bc8eac1bc78b844bcc8e811235055c
01es <oles.hodych@gmail.com>
2024-11-28 16:07:14 +1100

    #2316 Further implementation imporovements to the legacy code.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/validation/custom/DomainEntitiesDependenciesUtils.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/validation/custom/DomainEntitiesDependenciesUtils.java
@@ -3,22 +3,21 @@ package ua.com.fielden.platform.entity.validation.custom;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.annotation.MapTo;
 import ua.com.fielden.platform.entity.query.model.AggregatedResultQueryModel;
-import ua.com.fielden.platform.entity.query.model.ConditionModel;
 import ua.com.fielden.platform.entity.validation.custom.DomainEntityDependencies.DomainEntityDependency;
 import ua.com.fielden.platform.utils.EntityUtils;
 
-import java.lang.reflect.Field;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import static ua.com.fielden.platform.entity.ActivatableAbstractEntity.ACTIVE;
 import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.cond;
 import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.select;
 import static ua.com.fielden.platform.reflection.AnnotationReflector.getKeyType;
-import static ua.com.fielden.platform.reflection.AnnotationReflector.isAnnotationPresent;
-import static ua.com.fielden.platform.reflection.Finder.findRealProperties;
 import static ua.com.fielden.platform.reflection.Finder.getKeyMembers;
+import static ua.com.fielden.platform.reflection.Finder.streamRealProperties;
 import static ua.com.fielden.platform.utils.EntityUtils.isOneToOne;
-import static ua.com.fielden.platform.utils.EntityUtils.isPersistedEntityType;
 
 public class DomainEntitiesDependenciesUtils {
     public static final String PARAM = "ENTITY_VALUE";
@@ -27,48 +26,63 @@ public class DomainEntitiesDependenciesUtils {
     public static final String DEPENDENT_PROP_NAME = "dependent_prop_name";
     public static final String DEPENDENT_PROP_TITLE = "dependent_prop_title";
     public static final String COUNT = "KOUNT";
-    
-    public static AggregatedResultQueryModel generateQuery(final Set<DomainEntityDependency> dependencies, final boolean deactivationOnly) {
-        
-        final List<AggregatedResultQueryModel> models = new ArrayList<>();
-        
-        for (final DomainEntityDependency dependency : dependencies) {
-            final ConditionModel firstCondition = cond().prop(dependency.propName).eq().param(PARAM).model();
-            final ConditionModel finalCondition =  deactivationOnly ? cond().condition(firstCondition).and().prop(ACTIVE).eq().val(true).model() : firstCondition;
 
-            models.add(select(dependency.entityType).
-                    where().condition(finalCondition).
-                    yield().val(dependency.entityType.getName()).as(ENTITY_TYPE_NAME).
-                    yield().val(dependency.entityTitle).as(ENTITY_TYPE_TITLE).
-                    yield().val(dependency.propName).as(DEPENDENT_PROP_NAME).
-                    yield().val(dependency.propTitle).as(DEPENDENT_PROP_TITLE).
-                    modelAsAggregate());
-        }
+    /**
+     * A factory method to generate a query for retrieving entity dependencies on either all (`deactivationOnly = false`)
+     * or active only (`deactivationOnly = true`) entity instances of types specified in `dependencies`.
+     *
+     * @param dependencies
+     *           A set of dependencies, which is determined elsewhere.
+     * @param deactivationOnly
+     *           A parameter controlling whether only active dependencies should be counted.
+     * @return
+     *           An aggregate query model to retrieve the total count of entity dependencies per dependency entity type and property.
+     *           The shape of the query result is `(entity title, property title, count)`.
+     */
+    public static AggregatedResultQueryModel dependencyCountQuery(final Set<DomainEntityDependency> dependencies, final boolean deactivationOnly) {
+        final var models = dependencies.stream().map(dependency -> {
+            final var cond = deactivationOnly
+                             ? cond().prop(dependency.propName).eq().param(PARAM).and().prop(ACTIVE).eq().val(true).model()
+                             : cond().prop(dependency.propName).eq().param(PARAM).model();
+            return select(dependency.entityType)
+                   .where().condition(cond)
+                   .yield().val(dependency.entityType.getName()).as(ENTITY_TYPE_NAME)
+                   .yield().val(dependency.entityTitle).as(ENTITY_TYPE_TITLE)
+                   .yield().val(dependency.propName).as(DEPENDENT_PROP_NAME)
+                   .yield().val(dependency.propTitle).as(DEPENDENT_PROP_TITLE)
+                  .modelAsAggregate();
+        }).toList();
 
-        return select(models.toArray(new AggregatedResultQueryModel[] {})).
-                groupBy().prop(ENTITY_TYPE_NAME).
-                groupBy().prop(DEPENDENT_PROP_NAME).
-                groupBy().prop(ENTITY_TYPE_TITLE).
-                groupBy().prop(DEPENDENT_PROP_TITLE).
-                yield().prop(ENTITY_TYPE_TITLE).as(ENTITY_TYPE_TITLE).
-                yield().prop(DEPENDENT_PROP_TITLE).as(DEPENDENT_PROP_TITLE).
-                yield().countAll().as(COUNT).
-                modelAsAggregate();
+        return select(models.toArray(new AggregatedResultQueryModel[] {}))
+               .groupBy().prop(ENTITY_TYPE_NAME)
+               .groupBy().prop(DEPENDENT_PROP_NAME)
+               .groupBy().prop(ENTITY_TYPE_TITLE)
+               .groupBy().prop(DEPENDENT_PROP_TITLE)
+               .yield().prop(ENTITY_TYPE_TITLE).as(ENTITY_TYPE_TITLE)
+               .yield().prop(DEPENDENT_PROP_TITLE).as(DEPENDENT_PROP_TITLE)
+               .yield().countAll().as(COUNT)
+               .modelAsAggregate();
     }
 
-    public static Map<Class<? extends AbstractEntity<?>>, DomainEntityDependencies> getEntityDependantsMap(final Collection<Class<? extends AbstractEntity<?>>> domainEntityTypes) {
+    /**
+     * A function that builds a dependency map for each persistent entity type.
+     * As a result, each entity type (call it a target entity) gets associated with an instance of [DomainEntityDependencies],
+     * which contains a set of [DomainEntityDependency], representing pairs of entity types and their properties of the target entity type.
+     *
+     * @param domainEntityTypes  a collection of domain entities to be analysed; usually a complete domain is expected;
+     * @return  a map between persistent entity types and their persistent dependencies.
+     */
+    public static Map<Class<? extends AbstractEntity<?>>, DomainEntityDependencies> entityDependencyMap(final Collection<Class<? extends AbstractEntity<?>>> domainEntityTypes) {
         final var map = new HashMap<Class<? extends AbstractEntity<?>>, DomainEntityDependencies>();
         domainEntityTypes.stream().filter(EntityUtils::isPersistedEntityType)
         .forEach(entType -> {
-            for (final Field field : findRealProperties(entType)) {
-                if (isAnnotationPresent(field, MapTo.class) && isPersistedEntityType(field.getType())) {
-                    if (!map.containsKey(field.getType())) {
-                        final var propType = (Class<? extends AbstractEntity<?>>) field.getType();
-                        map.put(propType, new DomainEntityDependencies(propType));
-                    }
-                    map.get(field.getType()).addDependency(entType, field);
+            streamRealProperties(entType, MapTo.class).filter(field -> EntityUtils.isPersistedEntityType(field.getType())).forEach(field -> {
+                if (!map.containsKey(field.getType())) {
+                    final var propType = (Class<? extends AbstractEntity<?>>) field.getType();
+                    map.put(propType, new DomainEntityDependencies(propType));
                 }
-            }
+                map.get(field.getType()).addDependency(entType, field);
+            });
 
             if (isOneToOne(entType)) {
                 final Class<? extends Comparable<?>> keyType = getKeyType(entType);
