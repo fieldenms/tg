6b6e82464f0fc091baa002f44aaf8f861be0d65a
jhou-pro <jhou.pro@gmail.com>
2024-12-14 22:21:57 +0200

    #2329 Added all production dependencies to polymer building.
    It is understood that all production dependencies are better to be tree-shaken and only exactly required files are needed. This will facilitate proper Code Scanning and reduce unnecessary code.
    
    Previous:
    @polymer/polymer
    @polymer/all-web-components
    @google-web-components
    +@polymer/test-fixture [testing dependency]
    
    New:
    @webcomponents/shadycss (@polymer/polymer depends on this)
    @webcomponents/webcomponentsjs (only webcomponents-bundle.js needed)
    web-animations-js (only web-animations-next-lite.min.js needed)
    
    TODO The only outstanding production dependency is antlr4 (except others, not [yet] added to npm infrastructure).

--- platform-web-ui/src/main/resources/install-polymer.bat
+++ platform-web-ui/src/main/resources/install-polymer.bat
@@ -4,9 +4,13 @@ del node_modules\stacky && ^
 polymer build && ^
 rmdir /S /Q polymer & ^
 rename node_modules polymer && ^
-rmdir /S /Q polymer\@polymer & ^
 rmdir /S /Q polymer\@google-web-components & ^
-move build\tg-custom-build\node_modules\@polymer polymer\ && ^
+rmdir /S /Q polymer\@polymer & ^
+rmdir /S /Q polymer\@webcomponents & ^
+rmdir /S /Q polymer\web-animations-js & ^
 move build\tg-custom-build\node_modules\@google-web-components polymer\ && ^
+move build\tg-custom-build\node_modules\@polymer polymer\ && ^
+move build\tg-custom-build\node_modules\@webcomponents polymer\ && ^
+move build\tg-custom-build\node_modules\web-animations-js polymer\ && ^
 rmdir /S /Q build & ^
 forfiles /p polymer /s /m package.json /c "cmd /c del @path" && ^
--- platform-web-ui/src/main/resources/install-polymer.sh
+++ platform-web-ui/src/main/resources/install-polymer.sh
@@ -4,9 +4,13 @@ rm -f node_modules/stacky
 polymer build
 rm -r -f polymer
 mv node_modules polymer
-rm -r -f polymer/@polymer
 rm -r -f polymer/@google-web-components
-mv build/tg-custom-build/node_modules/@polymer polymer/
+rm -r -f polymer/@polymer
+rm -r -f polymer/@webcomponents
+rm -r -f polymer/web-animations-js
 mv build/tg-custom-build/node_modules/@google-web-components polymer/
+mv build/tg-custom-build/node_modules/@polymer polymer/
+mv build/tg-custom-build/node_modules/@webcomponents polymer/
+mv build/tg-custom-build/node_modules/web-animations-js polymer/
 rm -r -f build
 find polymer -type f -name "package.json" -delete
