1e25aba97579adc94bcf2487c88a169ae2f25a74
jhou-pro <jhou.pro@gmail.com>
2024-12-23 23:07:57 +0200

    #2329 49. 51. Exported 'moment' from 'moment-lib'.
    Regenerated the tree.

--- platform-web-ui/src/main/resources/_virtual/moment-timezone-with-data.js
+++ platform-web-ui/src/main/resources/_virtual/moment-timezone-with-data.js
@@ -1,3 +1,7 @@
+import { getDefaultExportFromCjs } from './_commonjsHelpers.js';
 import { __require as requireMomentTimezoneWithData } from '../polymer/moment-timezone/builds/moment-timezone-with-data.js';
 
-requireMomentTimezoneWithData();
+var momentTimezoneWithDataExports = requireMomentTimezoneWithData();
+var moment = /*@__PURE__*/getDefaultExportFromCjs(momentTimezoneWithDataExports);
+
+export { moment as default };
--- platform-web-ui/src/main/resources/polymer/lib/moment-lib.js
+++ platform-web-ui/src/main/resources/polymer/lib/moment-lib.js
@@ -1,2 +1,3 @@
-import '../../_virtual/moment-timezone-with-data.js';
+import moment from '../../_virtual/moment-timezone-with-data.js';
+export { default } from '../../_virtual/moment-timezone-with-data.js';
 import '../../_virtual/locales.js';
