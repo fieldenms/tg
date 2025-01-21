1b72b7ac9a44daa23a1b8c8df88069c1b91758c1
homedirectory <nullsink@posteo.eu>
2025-01-21 11:18:29 +0200

    #2349 Always use the original entity type to generate audit companions

--- platform-dao/src/main/java/ua/com/fielden/platform/audit/AuditEntityCompanionGeneratorImpl.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/audit/AuditEntityCompanionGeneratorImpl.java
@@ -4,48 +4,60 @@ import jakarta.inject.Singleton;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.description.annotation.AnnotationDescription;
 import ua.com.fielden.platform.dao.CommonEntityDao;
+import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.annotation.EntityType;
+import ua.com.fielden.platform.reflection.PropertyTypeDeterminator;
 
+/**
+ * Generated companions should always be based on the original entity type. If an instrumented entity type is provided,
+ * its original type should be used.
+ */
 @Singleton
 final class AuditEntityCompanionGeneratorImpl implements IAuditEntityCompanionGenerator {
 
     @Override
     public Class<?> generateCompanion(final Class<? extends AbstractAuditEntity> type)
     {
+        final var baseType = PropertyTypeDeterminator.baseEntityType((Class<? extends AbstractEntity<?>>) type);
+
         return new ByteBuddy()
                 .subclass(CommonAuditEntityDao.class)
-                .name(type.getCanonicalName() + "Dao")
+                .name(baseType.getCanonicalName() + "Dao")
                 .annotateType(AnnotationDescription.Builder.ofType(EntityType.class)
-                                      .define("value", type)
+                                      .define("value", baseType)
                                       .build())
                 .make()
-                .load(type.getClassLoader())
+                .load(baseType.getClassLoader())
                 .getLoaded();
     }
 
     @Override
     public Class<?> generateCompanionForAuditProp(final Class<? extends AbstractAuditProp> type) {
+        final var baseType = PropertyTypeDeterminator.baseEntityType((Class<? extends AbstractEntity<?>>) type);
+
         return new ByteBuddy()
                 .subclass(CommonAuditPropDao.class)
-                .name(type.getCanonicalName() + "Dao")
+                .name(baseType.getCanonicalName() + "Dao")
                 .annotateType(AnnotationDescription.Builder.ofType(EntityType.class)
-                                      .define("value", type)
+                                      .define("value", baseType)
                                       .build())
                 .make()
-                .load(type.getClassLoader())
+                .load(baseType.getClassLoader())
                 .getLoaded();
     }
 
     @Override
     public Class<?> generateCompanionForSynAuditEntity(final Class<? extends AbstractSynAuditEntity> type) {
+        final var baseType = PropertyTypeDeterminator.baseEntityType((Class<? extends AbstractEntity<?>>) type);
+
         return new ByteBuddy()
                 .subclass(CommonEntityDao.class)
-                .name(type.getCanonicalName() + "Dao")
+                .name(baseType.getCanonicalName() + "Dao")
                 .annotateType(AnnotationDescription.Builder.ofType(EntityType.class)
-                                      .define("value", type)
+                                      .define("value", baseType)
                                       .build())
                 .make()
-                .load(type.getClassLoader())
+                .load(baseType.getClassLoader())
                 .getLoaded();
     }
 
