6a17f3e83185ee76c71083141e2d9fbf198ddd3f
jhou-pro <jhou.pro@gmail.com>
2025-02-03 12:35:48 +0200

    #185 Added CSS file processing for '@toast-ui/editor' using 'rollup-plugin-import-css'.
    Regenerated the tree.

--- platform-web-ui/src/main/resources/polymer/lib/toastui-editor-lib.js
+++ platform-web-ui/src/main/resources/polymer/lib/toastui-editor-lib.js
@@ -1,2 +1,10 @@
+import toastuiEditorStyleStrings from '../@toast-ui/editor/dist/toastui-editor.css.js';
+import { createStyleModule } from './tg-style-utils.js';
+import { html } from '../@polymer/polymer/lib/utils/html-tag.js';
 import { Editor as ToastUIEditor } from '../@toast-ui/editor/dist/esm/index.js';
 export { Editor as default } from '../@toast-ui/editor/dist/esm/index.js';
+
+createStyleModule('toastui-editor-styles', toastuiEditorStyleStrings);
+const toastuiEditorStyles = html`<style include='toastui-editor-styles'></style>`; // can't use 'const ... = 'toastui-editor-styles'', because of html tag function stringent security
+
+export { toastuiEditorStyles };
