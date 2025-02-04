53b02132e849a7455fe31242edbb9bbdd8acfdc9
jhou-pro <jhou.pro@gmail.com>
2025-02-04 15:49:24 +0200

    #185 Ensure 'prosemirror-view' latest styles loading.
    Even though latest 'prosemirror-view' dependency is loaded using npm (1.37.2), its styles was not used. Instead, the packaged style file was used ('toastui-editor.css'). This file contains all Prosemirror styles and Toast UI Editor styles, that were packaged as part of 3.2.2 release, -- including older Prosemirror styles.
    
    To improve this situation we load Prosemirror styles explicitly after Toast UI styles.
    
    There are also other versions of styles in Toast UI Editor package. However, they all are insufficient for our needs. That's why we are now loading full styles and apply latest Prosemirror styles on top of them.
    
    Missing change in CSS (references):
    https://github.com/ProseMirror/prosemirror-view/commit/a0b72d3bcbf9d5ac86288a6af34fa226e0e11c9b
    https://github.com/ProseMirror/prosemirror/issues/1421#issuecomment-1759320191

--- platform-web-ui/src/main/resources/lib/toastui-editor-lib.js
+++ platform-web-ui/src/main/resources/lib/toastui-editor-lib.js
@@ -1,6 +1,7 @@
 import toastuiEditorStyleStrings from '@toast-ui/editor/toastui-editor.css';
+import prosemirrorViewStyleStrings from 'prosemirror-view/style/prosemirror.css'; // ensure the latest `prosemirror-view` (1.37.2, see package-lock.json) styles are attached on top of older `@toast-ui/editor` packaged one
 import { createStyleModule } from './tg-style-utils.js';
-createStyleModule('toastui-editor-styles', toastuiEditorStyleStrings);
+createStyleModule('toastui-editor-styles', toastuiEditorStyleStrings, prosemirrorViewStyleStrings);
 
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 export const toastuiEditorStyles = html`<style include='toastui-editor-styles'></style>`; // can't use 'const ... = 'toastui-editor-styles'', because of html tag function stringent security
