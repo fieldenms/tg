8b88f6928691a3f7e246e9a61c99399602308904
jhou-pro <jhou.pro@gmail.com>
2025-02-25 08:57:50 +0200

    #2379 Made test exception type as platform one.

--- platform-pojo-bl/src/test/java/ua/com/fielden/platform/serialisation/SelfReferencingException.java
+++ platform-pojo-bl/src/test/java/ua/com/fielden/platform/serialisation/SelfReferencingException.java
@@ -1,10 +1,12 @@
 package ua.com.fielden.platform.serialisation;
 
+import ua.com.fielden.platform.exceptions.AbstractPlatformRuntimeException;
+
 /**
  * Exception type that self-references in getter ({@link #getCause()}).
  * See https://github.com/fieldenms/tg/issues/2379 for more details.
  */
-public class SelfReferencingException extends RuntimeException {
+public class SelfReferencingException extends AbstractPlatformRuntimeException {
 
     public SelfReferencingException(final String message) {
         super(message);
