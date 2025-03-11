bdbf4d7d4e71ee8311985ae77c1655f9cd3e38b4
homedirectory <nullsink@posteo.eu>
2025-03-11 16:33:04 +0200

    #2375 Query transformation to order unions by id
    The transformation is in OrderByIdForUnion.

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/stage2/queries/ResultQuery2.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/stage2/queries/ResultQuery2.java
@@ -5,8 +5,16 @@ import ua.com.fielden.platform.eql.stage2.ITransformableFromStage2To3;
 import ua.com.fielden.platform.eql.stage2.QueryComponents2;
 import ua.com.fielden.platform.eql.stage2.TransformationContextFromStage2To3;
 import ua.com.fielden.platform.eql.stage2.TransformationResultFromStage2To3;
+import ua.com.fielden.platform.eql.stage2.conditions.Conditions2;
+import ua.com.fielden.platform.eql.stage2.sources.IJoinNode2;
+import ua.com.fielden.platform.eql.stage2.sundries.GroupBys2;
+import ua.com.fielden.platform.eql.stage2.sundries.OrderBys2;
+import ua.com.fielden.platform.eql.stage2.sundries.Yields2;
 import ua.com.fielden.platform.eql.stage3.QueryComponents3;
 import ua.com.fielden.platform.eql.stage3.queries.ResultQuery3;
+import ua.com.fielden.platform.eql.stage3.sources.IJoinNode3;
+
+import java.util.Optional;
 
 public class ResultQuery2 extends AbstractQuery2 implements ITransformableFromStage2To3<ResultQuery3> {
 
@@ -14,10 +22,26 @@ public class ResultQuery2 extends AbstractQuery2 implements ITransformableFromSt
         super(queryComponents, resultType);
     }
 
+    public ResultQuery2(
+            final Optional<IJoinNode2<? extends IJoinNode3>> maybeJoinRoot,
+            final Conditions2 whereConditions,
+            final Yields2 yields,
+            final GroupBys2 groups,
+            final OrderBys2 orderings,
+            final Class<?> resultType)
+    {
+        super(maybeJoinRoot, whereConditions, yields, groups, orderings, resultType);
+    }
+
     @Override
     public TransformationResultFromStage2To3<ResultQuery3> transform(final TransformationContextFromStage2To3 context) {
         final TransformationResultFromStage2To3<QueryComponents3> queryComponentsTr = transformQueryComponents(context);
         return new TransformationResultFromStage2To3<>(new ResultQuery3(queryComponentsTr.item, resultType), queryComponentsTr.updatedContext);
+    }
+
+    @Override
+    public ResultQuery2 setOrderings(final OrderBys2 orderings) {
+        return new ResultQuery2(maybeJoinRoot, whereConditions, yields, groups, orderings, resultType);
     }
 
     @Override
