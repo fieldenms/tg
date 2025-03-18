5ebf8a0106016255fec41eb5dc961c5bbf1c6511
jhou-pro <jhou.pro@gmail.com>
2025-03-18 17:58:53 +0200

    #1750 Updated fullcalendar dependency to 6.1.15 from 5.6.0.

--- platform-web-ui/src/main/resources/lib/fullcalendar-lib.js
+++ platform-web-ui/src/main/resources/lib/fullcalendar-lib.js
@@ -1,11 +1,11 @@
-import fullcalendarStyleStrings from 'fullcalendar/main.css';
-import { createStyleModule } from './tg-style-utils.js';
-createStyleModule('fullcalendar-styles', fullcalendarStyleStrings);
-
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
-export const fullcalendarStyles = html`<style include='fullcalendar-styles'></style>`; // can't use 'const ... = 'fullcalendar-styles'', because of html tag function stringent security
+//import fullcalendarStyleStrings from 'fullcalendar/main.css';
+//import { createStyleModule } from './tg-style-utils.js';
+//createStyleModule('fullcalendar-styles', fullcalendarStyleStrings);
+//
+//import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+//export const fullcalendarStyles = html`<style include='fullcalendar-styles'></style>`; // can't use 'const ... = 'fullcalendar-styles'', because of html tag function stringent security
 
 //import '/resources/fullcalendar/main.js';
 //export const FullCalendar = window.FullCalendar;
-export FullCalendar from 'fullcalendar';
-//export default FullCalendar;
+import { Calendar } from 'fullcalendar';
+export const FullCalendar = Calendar;
