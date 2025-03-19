446734606f65511e2a94b9608697e919f33397ea
jhou-pro <jhou.pro@gmail.com>
2025-03-19 18:05:37 +0200

    #1750 Removed old manual fullcalendar dependencies.

--- platform-web-ui/src/main/resources/fullcalendar/fullcalendar-lib.js
+++
@@ -1,9 +0,0 @@
-import fullcalendarStyleStrings from '/resources/fullcalendar/main.css.js';
-import { createStyleModule } from '/resources/reflection/tg-style-utils.js';
-createStyleModule('fullcalendar-styles', fullcalendarStyleStrings);
-
-import { html } from '/resources/polymer/@polymer/polymer/lib/utils/html-tag.js';
-export const fullcalendarStyles = html`<style include='fullcalendar-styles'></style>`; // can't use 'const ... = 'fullcalendar-styles'', because of html tag function stringent security
-
-import '/resources/fullcalendar/main.js';
-export const FullCalendar = window.FullCalendar;
--- platform-web-ui/src/main/resources/fullcalendar/moment-timezone/fullcalendar-with-timezones-lib.js
+++
@@ -1,6 +0,0 @@
-export { fullcalendarStyles, FullCalendar } from '/resources/fullcalendar/fullcalendar-lib.js';
-
-import '/resources/components/moment-lib.js';
-
-import '/resources/fullcalendar/moment-timezone/main.global.js';
-export const momentTimezonePlugin = FullCalendar.momentTimezonePlugin;
