9a7155da34882257dd6912cc5f3df30bbb8cc39f
Oleh Maikovych <oleh@fielden.com.au>
2025-03-19 12:51:19 +0200

    #1750 Adjusted the logic that switches between months, weeks, or days to ensure the calendar resizes correctly when a scrollbar appears.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/components/fullcalendar/tg-fullcalendar.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/components/fullcalendar/tg-fullcalendar.js
@@ -268,12 +268,14 @@ export class TgFullcalendar extends mixinBehaviors([IronResizableBehavior], Poly
     _prev() {
         if (this._calendar) {
             this._calendar.prev();
+            this.notifyResize();;
         }
     }
 
     _next() {
         if (this._calendar) {
             this._calendar.next();
+            this.notifyResize();;
         }
     }
 
