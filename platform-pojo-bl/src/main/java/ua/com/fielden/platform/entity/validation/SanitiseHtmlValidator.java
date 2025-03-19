8856def606965a0a36c24d5267bc57f863f84c79
homedirectory <nullsink@posteo.eu>
2025-03-19 13:48:29 +0200

    #2391 Documentation improvements

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/validation/SanitiseHtmlValidator.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/validation/SanitiseHtmlValidator.java
@@ -13,6 +13,7 @@ import static ua.com.fielden.platform.error.Result.successful;
 /**
  * A default validator for properties of type {@link String} to prevent injection of dangerous JS code, used for XSS.
  * Every non-calculated property with `length > 0` attains this validator automatically.
+ * This validator can also be explicitly declared on a property, although the effect will be the same as in its absence.
  */
 public class SanitiseHtmlValidator implements IBeforeChangeEventHandler<String> {
 
