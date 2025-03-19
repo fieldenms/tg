d9b86275b3b266ed5feeebd9816311dea2a564b5
jhou-pro <jhou.pro@gmail.com>
2025-03-19 17:57:29 +0200

    #1750 Added @fullcalendar/moment-timezone dependency to npm.

--- platform-web-ui/src/main/resources/lib/fullcalendar-lib.js
+++ platform-web-ui/src/main/resources/lib/fullcalendar-lib.js
@@ -1,11 +1,3 @@
-//import fullcalendarStyleStrings from 'fullcalendar/main.css';
-//import { createStyleModule } from './tg-style-utils.js';
-//createStyleModule('fullcalendar-styles', fullcalendarStyleStrings);
-//
-//import { html } from '@polymer/polymer/lib/utils/html-tag.js';
-//export const fullcalendarStyles = html`<style include='fullcalendar-styles'></style>`; // can't use 'const ... = 'fullcalendar-styles'', because of html tag function stringent security
-
-//import '/resources/fullcalendar/main.js';
-//export const FullCalendar = window.FullCalendar;
-import { Calendar } from 'fullcalendar';
-export const FullCalendar = Calendar;
+export { Calendar as FullCalendar } from 'fullcalendar';
+import './moment-lib.js';
+export { default as momentTimezonePlugin } from '@fullcalendar/moment-timezone';
