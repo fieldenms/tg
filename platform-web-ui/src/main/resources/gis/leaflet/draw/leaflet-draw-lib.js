002af2d8392961f7a013efd7bbcf42b1d02b276d
jhou-pro <jhou.pro@gmail.com>
2025-03-20 17:19:33 +0200

    #1750 Removed incorrect TODO.
    We should always use unminified version of libs. Vulcaniser still minifies them using terser for deployment mode.

--- platform-web-ui/src/main/resources/gis/leaflet/draw/leaflet-draw-lib.js
+++ platform-web-ui/src/main/resources/gis/leaflet/draw/leaflet-draw-lib.js
@@ -5,5 +5,5 @@ import { createStyleModule } from '/resources/polymer/lib/tg-style-utils.js';
 export const leafletDrawStylesName = 'leaflet-draw-styles';
 createStyleModule(leafletDrawStylesName, leafletDrawStyles);
 
-import '/resources/gis/leaflet/draw/leaflet.draw-src.js'; // TODO use leaflet.draw.js, which is minimised version of the plugin
+import '/resources/gis/leaflet/draw/leaflet.draw-src.js';
 export const LeafletDraw = L.Control.Draw;
