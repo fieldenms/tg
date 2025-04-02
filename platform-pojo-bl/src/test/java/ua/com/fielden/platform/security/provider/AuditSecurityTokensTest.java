6362ba34c7138022cf7ad6e9c85170cbfbe16b4f
homedirectory <nullsink@posteo.eu>
2025-04-02 11:21:19 +0300

    #2349 Disable auditing by default for most pojo-bl tests
    For dao tests, however, auditing must remain enabled, as there is no
    granular control over the IoC configuration yet -- a single
    configuration is used for all tests.

---
+++ platform-pojo-bl/src/test/java/ua/com/fielden/platform/security/provider/AuditSecurityTokensTest.java
@@ -0,0 +1,56 @@
+package ua.com.fielden.platform.security.provider;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
+import com.google.inject.name.Names;
+import org.junit.Test;
+import ua.com.fielden.platform.audit.AuditingMode;
+import ua.com.fielden.platform.audit.IAuditTypeFinder;
+import ua.com.fielden.platform.ioc.ApplicationInjectorFactory;
+import ua.com.fielden.platform.sample.domain.AuditedEntity;
+import ua.com.fielden.platform.sample.domain.TgVehicle;
+import ua.com.fielden.platform.security.tokens.Template;
+import ua.com.fielden.platform.test.CommonEntityTestIocModuleWithPropertyFactory;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * A test case to ensure correct generation of security tokens for audit types.
+ *
+ * @author TG Team
+ */
+public class AuditSecurityTokensTest {
+
+    private final Injector injector = new ApplicationInjectorFactory()
+            .add(new CommonEntityTestIocModuleWithPropertyFactory() {
+                @Override protected AuditingMode auditingMode() {
+                    return AuditingMode.ENABLED;
+                }
+            })
+            .add(new AbstractModule() {
+                @Override protected void configure() {
+                    bindConstant().annotatedWith(Names.named("tokens.path")).to("target/test-classes");
+                    bindConstant().annotatedWith(Names.named("tokens.package")).to("ua.com.fielden.platform.security.provider");
+                }
+            })
+            .getInjector();
+
+    @Test
+    public void synthetic_audit_entity_type_has_READ_and_READ_MODEL_tokens() {
+        final var provider = injector.getInstance(ISecurityTokenProvider.class);
+
+        final var auditTypeFinder = injector.getInstance(IAuditTypeFinder.class);
+
+        assertThat(List.of(TgVehicle.class, AuditedEntity.class))
+                .allSatisfy(ty -> {
+                    final var synAuditType = auditTypeFinder.navigate(TgVehicle.class).synAuditEntityType();
+                    assertThat(provider.getTokenByName(Template.READ.forClassName().formatted(synAuditType.getSimpleName())))
+                            .isPresent();
+                    assertThat(provider.getTokenByName(Template.READ_MODEL.forClassName().formatted(synAuditType.getSimpleName())))
+                            .isPresent();
+                });
+    }
+
+}
--- platform-pojo-bl/src/test/java/ua/com/fielden/platform/test/CommonEntityTestIocModuleWithPropertyFactory.java
+++ platform-pojo-bl/src/test/java/ua/com/fielden/platform/test/CommonEntityTestIocModuleWithPropertyFactory.java
@@ -1,6 +1,8 @@
 package ua.com.fielden.platform.test;
 
 import com.google.inject.name.Names;
+import ua.com.fielden.platform.audit.AuditingIocModule;
+import ua.com.fielden.platform.audit.AuditingMode;
 import ua.com.fielden.platform.basic.config.IApplicationDomainProvider;
 import ua.com.fielden.platform.entity.factory.IMetaPropertyFactory;
 import ua.com.fielden.platform.ioc.EntityIocModule;
@@ -15,14 +17,15 @@ import ua.com.fielden.platform.web.test.config.ApplicationDomain;
 import java.util.Properties;
 
 /**
- * This Guice module ensures that all observable and validatable properties are handled correctly. In addition to {@link EntityIocModule}, this module binds
- * {@link IMetaPropertyFactory}.
+ * This Guice module ensures that all observable and validatable properties are handled correctly.
+ * In addition to {@link EntityIocModule}, this module binds {@link IMetaPropertyFactory}.
+ * This modules disables auditing, but this can be changed by overriding {@link #auditingMode()}.
  * <p>
  * <b>IMPORTANT</b>: This module is strictly for testing purposes!
  * 
  * @author TG Team
  */
-public final class CommonEntityTestIocModuleWithPropertyFactory extends EntityTestIocModuleWithPropertyFactory {
+public class CommonEntityTestIocModuleWithPropertyFactory extends EntityTestIocModuleWithPropertyFactory {
 
     public CommonEntityTestIocModuleWithPropertyFactory() {
         super();
@@ -30,6 +33,10 @@ public final class CommonEntityTestIocModuleWithPropertyFactory extends EntityTe
 
     public CommonEntityTestIocModuleWithPropertyFactory(final Properties properties) {
         super(properties);
+    }
+
+    protected AuditingMode auditingMode() {
+        return AuditingMode.DISABLED;
     }
 
     @Override
@@ -44,6 +51,8 @@ public final class CommonEntityTestIocModuleWithPropertyFactory extends EntityTe
         bind(IDates.class).to(DatesForTesting.class);
         bind(IUniversalConstants.class).to(UniversalConstantsForTesting.class);
         bind(ITgPersistentEntityWithProperties.class).to(TgPersistentEntityWithPropertiesDaoStub.class);
+
+        install(AuditingIocModule.withAuditingMode(auditingMode()));
     }
     
 }
