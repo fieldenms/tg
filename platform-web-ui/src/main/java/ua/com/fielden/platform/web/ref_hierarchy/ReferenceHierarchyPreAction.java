85a77240b2b129a7fe889d82b00769e573470506
jhou-pro <jhou.pro@gmail.com>
2025-04-18 19:50:43 +0300

    #2407 Make ref-hierarchy action more open for extension.
    Generally it is a good idea to have all platform generic pre/postActions public.

---
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/ref_hierarchy/ReferenceHierarchyPreAction.java
@@ -0,0 +1,49 @@
+package ua.com.fielden.platform.web.ref_hierarchy;
+
+import ua.com.fielden.platform.web.minijs.JsCode;
+import ua.com.fielden.platform.web.minijs.JsImport;
+import ua.com.fielden.platform.web.view.master.api.actions.pre.IPreAction;
+
+import java.util.Set;
+
+import static java.util.Set.of;
+import static ua.com.fielden.platform.web.minijs.JsCode.jsCode;
+import static ua.com.fielden.platform.web.minijs.JsImport.namedImport;
+
+/**
+ * Common {@link IPreAction} for reference hierarchy action.
+ *
+ * @author TG Team
+ */
+public record ReferenceHierarchyPreAction(boolean useMasterEntity) implements IPreAction {
+
+    @Override
+    public Set<JsImport> importStatements() {
+        return of(namedImport("TgReflector", "/app/tg-reflector"));
+    }
+
+    @Override
+    public JsCode build() {
+        return jsCode("""
+            const reflector = new TgReflector();
+            let entity = null;
+            if (action.requireSelectedEntities === 'ONE') {
+                entity = action.currentEntity();
+            } else if (action.requireSelectedEntities === 'ALL' && self.$.egi.getSelectedEntities().length > 0) {
+                entity = self.$.egi.getSelectedEntities()[0];
+            } else if (action.requireMasterEntity === "true") {
+                if(%s) {
+                    entity = action.parentElement.entity['@@origin'];
+                } else {
+                    const value = reflector.tg_getFullValue(action.parentElement.entity, action.parentElement.propertyName);
+                    entity = reflector.isEntity(value) ? value : action.parentElement.entity['@@origin'];
+                }
+            }
+            if (entity) {
+                action.shortDesc = reflector.getType(entity.constructor.prototype.type.call(entity).notEnhancedFullClassName()).entityTitle();
+            }
+            """.formatted(this.useMasterEntity)
+        );
+    }
+
+}
--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/ref_hierarchy/ReferenceHierarchyWebUiConfig.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/ref_hierarchy/ReferenceHierarchyWebUiConfig.java
@@ -5,21 +5,13 @@ import ua.com.fielden.platform.entity.ActivatableAbstractEntity;
 import ua.com.fielden.platform.ref_hierarchy.ReferenceHierarchy;
 import ua.com.fielden.platform.web.centre.api.actions.EntityActionConfig;
 import ua.com.fielden.platform.web.centre.api.context.CentreContextConfig;
-import ua.com.fielden.platform.web.minijs.JsCode;
-import ua.com.fielden.platform.web.minijs.JsImport;
 import ua.com.fielden.platform.web.view.master.EntityMaster;
-import ua.com.fielden.platform.web.view.master.api.actions.pre.IPreAction;
 import ua.com.fielden.platform.web.view.master.hierarchy.ReferenceHierarchyMaster;
 
-import java.util.Set;
-
-import static java.util.Set.of;
 import static ua.com.fielden.platform.error.Result.failuref;
 import static ua.com.fielden.platform.reflection.PropertyTypeDeterminator.determinePropertyType;
 import static ua.com.fielden.platform.web.centre.api.actions.impl.EntityActionBuilder.action;
 import static ua.com.fielden.platform.web.centre.api.context.impl.EntityCentreContextSelector.context;
-import static ua.com.fielden.platform.web.minijs.JsCode.jsCode;
-import static ua.com.fielden.platform.web.minijs.JsImport.namedImport;
 
 /**
  * Web UI configuration for reference hierarchy master and action needed to call reference hierarchy.
@@ -116,43 +108,6 @@ public class ReferenceHierarchyWebUiConfig {
             .longDesc("Opens Reference Hierarchy")
             .withNoParentCentreRefresh()
             .build();
-    }
-
-    /**
-     * Common {@link IPreAction} for reference hierarchy action.
-     *
-     * @author TG Team
-     */
-    private record ReferenceHierarchyPreAction(boolean useMasterEntity) implements IPreAction {
-
-        @Override
-        public Set<JsImport> importStatements() {
-            return of(namedImport("TgReflector", "/app/tg-reflector"));
-        }
-
-        @Override
-        public JsCode build() {
-            return jsCode("""
-                const reflector = new TgReflector();
-                let entity = null;
-                if (action.requireSelectedEntities === 'ONE') {
-                    entity = action.currentEntity();
-                } else if (action.requireSelectedEntities === 'ALL' && self.$.egi.getSelectedEntities().length > 0) {
-                    entity = self.$.egi.getSelectedEntities()[0];
-                } else if (action.requireMasterEntity === "true") {
-                    if(%s) {
-                        entity = action.parentElement.entity['@@origin'];
-                    } else {
-                        const value = reflector.tg_getFullValue(action.parentElement.entity, action.parentElement.propertyName);
-                        entity = reflector.isEntity(value) ? value : action.parentElement.entity['@@origin'];
-                    }
-                }
-                if (entity) {
-                    action.shortDesc = reflector.getType(entity.constructor.prototype.type.call(entity).notEnhancedFullClassName()).entityTitle();
-                }
-                """.formatted(this.useMasterEntity));
-
-        }
     }
 
 }
