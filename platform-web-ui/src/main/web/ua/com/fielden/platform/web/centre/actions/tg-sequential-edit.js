405ee89203dcc789bc548a341a94097ecb547092
jhou-pro <jhou.pro@gmail.com>
2025-04-26 17:49:32 +0300

    #2407 Reduce gen Seq Edit actions code by moving it to separate JS file.
    This also allows easier JavaScript editing through normal means and better future maintenance.
    
     * Added missing Postal import, that worked only accidentally because of global 'postal' variable.

---
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/centre/actions/tg-sequential-edit.js
@@ -0,0 +1,91 @@
+import '/resources/components/postal-lib.js';
+
+/**
+ * A function to initialise sequential editing support for 'self' centre and 'action'.
+ * This is intended to be invoked in 'preAction' of the corresponding 'action'.
+ */
+export function sequentialEdit(action, self) {
+    if (!self.seqEditEntities && !self.$.egi.isEditing()) {
+        if (self.$.egi.getSelectedEntities().length === 0) {
+            self.$.egi.selectAll(true);
+        }
+        self.seqEditEntities = self.$.egi.getSelectedEntities();
+        const firstEntity = self.seqEditEntities.shift();
+        action.currentEntity = () => firstEntity;
+        action._oldRestoreActionState = action.restoreActionState;
+        action.restoreActionState = function () {
+            action._oldRestoreActionState();
+            cancelEditing();
+        }.bind(self);
+        const cancelEditing = (function (data) {
+            delete this.seqEditEntities;
+            this.seqEditSuccessPostal.unsubscribe();
+            this.seqEditCancelPostal.unsubscribe();
+            const master = action._masterReferenceForTesting;
+            if (master) {
+                master.publishCloseForcibly();
+            }
+            action.currentEntity = () => null;
+            action.restoreActionState = action._oldRestoreActionState;
+        }).bind(self);
+        const updateCacheAndContinueSeqSaving = (function (shouldUnselect) {
+            const nextEntity = this.seqEditEntities && this.seqEditEntities.shift();
+            if (shouldUnselect !== false) {
+                self.$.egi.selectEntity(action.currentEntity(), false);
+            }
+            if (nextEntity) {
+                setEntityAndReload(nextEntity, shouldUnselect ? null : 'skipNext');
+            } else {
+                cancelEditing();
+            }
+        }).bind(self);
+        const setEntityAndReload = function (entity, spinnerInvoked) {
+            if (entity) {
+                action.currentEntity = () => entity;
+                const master = action._masterReferenceForTesting;
+                if (master) {
+                    master.fire('tg-action-navigation-invoked', {spinner: spinnerInvoked});
+                    master.savingContext = action._createContextHolderForAction();
+                    master.retrieve(master.savingContext).then(function(ironRequest) {
+                        if (action.modifyFunctionalEntity) {
+                            action.modifyFunctionalEntity(master._currBindingEntity, master, action);
+                        }
+                        master.addEventListener('binding-entity-loaded-and-focused', restoreNavigationButtonState);
+                        master.save().then(function(value) {}, function (error) {
+                            fireNavigationChangeEvent(true);
+                        }.bind(self));
+                    }.bind(self), function (error) {
+                        fireNavigationChangeEvent(true);
+                    }.bind(self));
+                }
+             }
+        }.bind(self),
+        fireNavigationChangeEvent = function (shouldResetSpinner) {
+            const master = action._masterReferenceForTesting;
+            if (master) {
+                master.fire('tg-action-navigation-changed', {
+                    shouldResetSpinner: shouldResetSpinner
+                });
+            }
+        }.bind(self),
+        restoreNavigationButtonState = function (e) {
+            fireNavigationChangeEvent(false);
+            const master = action._masterReferenceForTesting;
+            master.removeEventListener('binding-entity-loaded-and-focused', restoreNavigationButtonState);
+        }.bind(self);
+        action.continuous = true;
+        action.skipNext = function() {
+            updateCacheAndContinueSeqSaving(false);
+        };
+        self.seqEditSuccessPostal = postal.subscribe({
+            channel: self.uuid,
+            topic: 'save.post.success',
+            callback: updateCacheAndContinueSeqSaving
+        }).defer();
+        self.seqEditCancelPostal = postal.subscribe({
+            channel: self.uuid,
+            topic: 'refresh.post.success',
+            callback: cancelEditing
+        }).defer();
+    }
+}
