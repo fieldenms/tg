9a72ecdef1e6f52fe2a0fda10968229546aa7432
homedirectory <nullsink@posteo.eu>
2025-05-12 15:10:02 +0300

    #2421 The identity transformation should be a singleton

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/provider/SecurityTokenNodeIdentityTransformation.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/security/provider/SecurityTokenNodeIdentityTransformation.java
@@ -1,9 +1,12 @@
 package ua.com.fielden.platform.security.provider;
 
+import jakarta.inject.Singleton;
+
 import java.util.SortedSet;
 
 /// The default implementation -- the identity function.
 ///
+@Singleton
 final class SecurityTokenNodeIdentityTransformation implements ISecurityTokenNodeTransformation {
 
     @Override
