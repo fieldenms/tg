762112d422775d2490b9420d706490afffec2ebe
homedirectory <nullsink@posteo.eu>
2025-05-15 12:07:38 +0300

    #2394 Optimise SQL for ifNull: subquery instead of COALESCE

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/stage3/operands/functions/IfNull3.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/stage3/operands/functions/IfNull3.java
@@ -3,6 +3,7 @@ package ua.com.fielden.platform.eql.stage3.operands.functions;
 import ua.com.fielden.platform.entity.query.DbVersion;
 import ua.com.fielden.platform.eql.meta.PropType;
 import ua.com.fielden.platform.eql.stage3.operands.ISingleOperand3;
+import ua.com.fielden.platform.eql.stage3.queries.SubQuery3;
 import ua.com.fielden.platform.meta.IDomainMetadata;
 
 import static java.lang.String.format;
@@ -15,7 +16,23 @@ public class IfNull3 extends TwoOperandsFunction3 {
 
     @Override
     public String sql(final IDomainMetadata metadata, final DbVersion dbVersion) {
-        return format("COALESCE(%s, %s)", operand1.sql(metadata, dbVersion), operand2.sql(metadata, dbVersion));
+        if (operand1 instanceof SubQuery3) {
+            // This optimisation ensures that the first argument to COALESCE is computed only once.
+            // See Issue #2394 for more details.
+            // The aliases are chosen so as to be unique.
+            // The only place where a name conflict could occur is in the second argument to COALESCE,
+            // if that expression happens to use one of the chosen aliases.
+            // Although it is unlikely for a conflict to occur, a stronger guarantee would be to use a generated id,
+            // such as TransformationContextFromStage2To3.sqlId.
+            // However, that would require changing method IGenerateSql.sql to introduce another parameter,
+            // which would demand significant refactoring effort.
+            interface $ { String QUERY_ALIAS ="EQL_Q12778210642", COLUMN_ALIAS = "EQL_C51037967375"; }
+            return format("(SELECT COALESCE(%1$s.%2$s, %3$s) FROM (SELECT (%4$s) AS %2$s) AS %1$s)",
+                          $.QUERY_ALIAS, $.COLUMN_ALIAS, operand2.sql(metadata, dbVersion), operand1.sql(metadata, dbVersion));
+        }
+        else {
+            return format("COALESCE(%s, %s)", operand1.sql(metadata, dbVersion), operand2.sql(metadata, dbVersion));
+        }
     }
 
     @Override
