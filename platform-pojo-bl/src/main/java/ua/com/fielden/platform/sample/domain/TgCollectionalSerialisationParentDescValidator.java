a1907625f152bad7efc805bd091e48f9cd97bd10
jhou-pro <jhou.pro@gmail.com>
2025-05-28 23:59:26 +0300

    #2432 Minor improvement to web testing validator.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/TgCollectionalSerialisationParentDescValidator.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/TgCollectionalSerialisationParentDescValidator.java
@@ -11,6 +11,9 @@ import java.util.Set;
 
 import static ua.com.fielden.platform.error.Result.successful;
 
+/// Validator for some property to imitate concurrent changes for `tg-entity-centre-entity-editing-with-collections` web test.
+///
+/// @author TG Team
 public class TgCollectionalSerialisationParentDescValidator implements IBeforeChangeEventHandler<String> {
     private static final String VALUE_FOR_CONFLICT = "validate + conflict";
     private static final String VALUE_FOR_CONFLICT_AND_COLLECTION_CHANGE = "validate + conflict + collection";
@@ -25,23 +28,27 @@ public class TgCollectionalSerialisationParentDescValidator implements IBeforeCh
         final var entity = property.<TgCollectionalSerialisationParent>getEntity();
         if (VALUE_FOR_CONFLICT.equals(newValue)) {
             // Imitate concurrently edited and saved situation.
-            companion.save(companion
-                .findByEntityAndFetch(companion.getFetchProvider().fetchModel(), entity)
-                .setDesc(new Date().getTime() + "")
-            );
+            saveParentEntity(entity);
         } else if (VALUE_FOR_CONFLICT_AND_COLLECTION_CHANGE.equals(newValue)) {
             // Imitate concurrently edited and saved situation with collection update.
-            final var savedParent = companion.save(companion
-                    .findByEntityAndFetch(companion.getFetchProvider().fetchModel(), entity)
-                    .setDesc(new Date().getTime() + "")
-            );
+            final var savedParent = saveParentEntity(entity);
             childCompanion.save(
                 childCompanion.new_()
                     .setKey1(savedParent)
+                    // Ensure new value every time to be unique in multiple web test runs.
                     .setKey2(new Date().getTime() + "")
             );
         }
         return successful();
+    }
+
+    /// Saves parent entity, that holds collection.
+    private TgCollectionalSerialisationParent saveParentEntity(TgCollectionalSerialisationParent entity) {
+        return companion.save(companion
+            .findByEntityAndFetch(companion.getFetchProvider().fetchModel(), entity)
+            // Ensure new value every time to be unique in multiple web test runs.
+            .setDesc(new Date().getTime() + "")
+        );
     }
 
 }
