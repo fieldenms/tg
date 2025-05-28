c21d4a8bc920d77eae9b57f55172297f6da5bd50
Oleh Maikovych <oleh@fielden.com.au>
2025-05-28 16:25:51 +0300

    #2438 Changed color for link in confirmation dialog.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/reflection/tg-polymer-utils.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/reflection/tg-polymer-utils.js
@@ -459,7 +459,7 @@ export const checkLinkAndOpen = function (url, target, windowFeatures) {
     
     if (!isAllowedSite() && !wasAcceptedByUser()) {
         const text = `The link is taking you to another site.<br>Are you sure you would like to continue?<br>
-                        <pre style="line-break:anywhere;max-width:500px;white-space:normal;color:-webkit-link;text-decoration:underline;">${url}</pre>`
+                        <pre style="line-break:anywhere;max-width:500px;white-space:normal;color:var(--paper-light-blue-500);text-decoration:underline;">${url}</pre>`
         const options = ["Don't show this again for this link", "Don't show this again for this site"];
         const buttons = [{ name: 'Cancel' }, { name: 'Continue', confirm: true, autofocus: true, style: "color:var(--google-red-500);" }];
         confirmationDialog.showConfirmationDialog(text, buttons, {single: true, options}, "Double-check this link").then(opt => {
