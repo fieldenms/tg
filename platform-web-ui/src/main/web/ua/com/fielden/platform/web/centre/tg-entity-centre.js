05ed1784f632b988f551301f56f5eb0367095d67
jhou-pro <jhou.pro@gmail.com>
2025-06-10 15:00:44 +0300

    #2275 Re-factor / improve IP order restoration / resetting.
    Instead of trying to restore IP orders for empty persisted set of IPs, do nothing explicitly. This case was not anticipated during implementation of IP settings resetting originally (not obvious and thus fragile).
    
    Also
     1. Added more explicit documentation.
     2. Improv docs by placing sentences in new lines each.
     3. Reduce computations, especially DOM queries - do only if absolutely necessary.
     4. Settings resetting: more simple resetting (only persistedIps) and set equality logic (isSubsetOf one way and the other way around, meaning 'equals' exactly).

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/centre/tg-entity-centre.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/centre/tg-entity-centre.js
@@ -1004,27 +1004,41 @@ Polymer({
     },
 
     _restoreInsertionPointOrder: function() {
-        const containers = [this.$.topInsertionPointContent.assignedNodes()[0],
-                            this.$.leftInsertionPointContent.assignedNodes()[0],
-                            this.$.bottomInsertionPointContent.assignedNodes()[0],
-                            this.$.rightInsertionPointContent.assignedNodes()[0]];
         const keys = [this._generateKey(ST.TOP_INSERTION_POINT_ORDER),
                       this._generateKey(ST.LEFT_INSERTION_POINT_ORDER),
                       this._generateKey(ST.BOTTOM_INSERTION_POINT_ORDER),
                       this._generateKey(ST.RIGHT_INSERTION_POINT_ORDER)];
-        const ips = [...this.querySelectorAll("tg-entity-centre-insertion-point")];
         const ipOrders = keys.map(key => JSON.parse(localStorage.getItem(key) || "[]"));
         const persistedIps = new Set(ipOrders.flat());
-        const dslIps = new Set(ips.map(ip => ip.functionalMasterTagName));
-        const removedIps = persistedIps.difference(dslIps);
-        if (removedIps.size === 0 && (persistedIps.size === 0 || dslIps.isSubsetOf(persistedIps))) { // all previously persisted insertion points in custom layout exist in Centre DSL list of IPs and vice versa
-            keys.forEach((key, i) => {
-                this._restoreOrderForContainer(containers[i], ipOrders[i], ips);
-            });
-        } else { // some IP(s) has added / disappeared to / from Centre DSL -- fallback to the layout as per Centre DSL
-            ips.forEach(ip => resetCustomSettings(this.miType, ip.functionalMasterTagName)); // remove all settings from each and every insertion points first; ip contextRetriever() is not bound yet, so need to use this.miType explicitly
-            removedIps.forEach(tagName => resetCustomSettings(this.miType, tagName)); // remove all settings (this user only) from IP(s), that has disappeared from Centre DSL
-            this.resetCustomSettingsForInsertionPoints(); // then remove all IP orders from each container and splitter positions too
+        // There are some persisted IP orders in local storage iff user has changed the order in at least one container.
+        // In this case orders are stored for all containers and will always be restored as a whole since the first change.
+        // Only proceed with restoring in case if `persistedIps` is not empty.
+        // Otherwise, IPs are already in correct placements from generated Entity Centre source configuration.
+        if (persistedIps.size > 0) {
+            const ips = [...this.querySelectorAll("tg-entity-centre-insertion-point")];
+            const dslIps = new Set(ips.map(ip => ip.functionalMasterTagName));
+            // If all previously persisted insertion points in custom layout exist in Centre DSL list of IPs and vice versa
+            //   (effectively meaning that nothing was added / removed to / from DSL IPs, i.e. persistedIps === dslIps as sets).
+            if (persistedIps.isSubsetOf(dslIps) && dslIps.isSubsetOf(persistedIps)) {
+                // Iterate through all persisted container orders and restore.
+                const containers = [this.$.topInsertionPointContent.assignedNodes()[0],
+                                    this.$.leftInsertionPointContent.assignedNodes()[0],
+                                    this.$.bottomInsertionPointContent.assignedNodes()[0],
+                                    this.$.rightInsertionPointContent.assignedNodes()[0]];
+                keys.forEach((key, i) => {
+                    this._restoreOrderForContainer(containers[i], ipOrders[i], ips);
+                });
+            }
+            // Otherwise, some IP(s) has been added / removed to / from Centre DSL.
+            else {
+                // Fallback to the layout as per Centre DSL.
+                // For this, remove all settings from each currently persisted insertion points.
+                // These IPs may include those, removed from DSL config (garbage).
+                // And these IPs DO NOT include those, added to DSL config (no custom settings exist for them).
+                persistedIps.forEach(tagName => resetCustomSettings(this.miType, tagName));
+                // Remove all IP orders from each container and splitter positions too.
+                this.resetCustomSettingsForInsertionPoints();
+            }
         }
     },
 
