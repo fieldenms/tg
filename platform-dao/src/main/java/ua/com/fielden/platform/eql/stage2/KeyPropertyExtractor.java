6fa59bce263404ea5210e24b0649dec19766e515
homedirectory <nullsink@posteo.eu>
2025-06-19 13:44:26 +0300

    #2452 Improve code quality

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/stage2/KeyPropertyExtractor.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/stage2/KeyPropertyExtractor.java
@@ -1,49 +1,52 @@
 package ua.com.fielden.platform.eql.stage2;
 
-import static java.util.Arrays.asList;
-import static ua.com.fielden.platform.entity.AbstractEntity.KEY;
-import static ua.com.fielden.platform.utils.EntityUtils.isEntityType;
-import static ua.com.fielden.platform.utils.EntityUtils.keyPaths;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import ua.com.fielden.platform.eql.meta.query.AbstractQuerySourceItem;
 import ua.com.fielden.platform.eql.meta.query.QuerySourceItemForEntityType;
-import ua.com.fielden.platform.eql.meta.query.QuerySourceInfo;
 import ua.com.fielden.platform.eql.meta.query.QuerySourceItemForUnionType;
 import ua.com.fielden.platform.eql.stage1.PropResolutionProgress;
 import ua.com.fielden.platform.eql.stage2.operands.Prop2;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static ua.com.fielden.platform.entity.AbstractEntity.KEY;
+import static ua.com.fielden.platform.utils.CollectionUtil.concatList;
+import static ua.com.fielden.platform.utils.EntityUtils.isEntityType;
+import static ua.com.fielden.platform.utils.EntityUtils.keyPaths;
 
 public class KeyPropertyExtractor {
 
     private KeyPropertyExtractor() {}
 
-    public static List<Prop2> extract(final Prop2 original) {
-        if (needsExtraction(original.lastPart(), original.penultPart())) {
-            final int pathSize = original.getPath().size();
-            final QuerySourceInfo<?> ei = pathSize == 1 ? original.source.querySourceInfo() : ((QuerySourceItemForEntityType<?>) original.getPath().get(pathSize - 2)).querySourceInfo;
-            final List<String> keyTreeLeaves = keyPaths(ei.javaType());
-            final List<Prop2> result = new ArrayList<>();
-            for (final String keyTreeLeaf : keyTreeLeaves) {
-                final PropResolutionProgress resolution = ei.resolve(new PropResolutionProgress(keyTreeLeaf));
-                final List<AbstractQuerySourceItem<?>> enhancedPath = new ArrayList<>(original.getPath().subList(0, pathSize - 1));
-                enhancedPath.addAll(resolution.getResolved());
-                result.add(new Prop2(original.source, enhancedPath));
-            }
-            return result;
+    /// If `prop` represents a path that ends with `key`, the result is a stream of paths, each of which replaces `key` by a specific key member.
+    /// Otherwise, the result is a stream that contains only `prop`.
+    ///
+    public static Stream<Prop2> extract(final Prop2 prop) {
+        if (needsExtraction(prop.lastPart(), prop.penultPart())) {
+            final int pathSize = prop.getPath().size();
+            final var querySourceInfo = pathSize == 1
+                    ? prop.source.querySourceInfo()
+                    : ((QuerySourceItemForEntityType<?>) prop.getPath().get(pathSize - 2)).querySourceInfo;
+            return keyPaths(querySourceInfo.javaType())
+                    .stream()
+                    .map(keyPath -> {
+                        final var resolution = querySourceInfo.resolve(new PropResolutionProgress(keyPath));
+                        return new Prop2(prop.source, concatList(prop.getPath().subList(0, pathSize - 1), resolution.getResolved()));
+                    });
         }
-
-        return asList(original);
+        else {
+            return Stream.of(prop);
+        }
     }
 
-    public static boolean needsExtraction(final AbstractQuerySourceItem<?> operandPropLastMember, final Optional<AbstractQuerySourceItem<?>> maybeOperandPropPenultMember) {
-        // TODO for now we simply skip the extraction for key members of a union entity
-        //      but this has the limitation of representing the key members of union properties, in case they are composite and have more than 1 member, as a concatenated string and thus, for example, ordering would happen by the string representation
-        //      rather than structural ordering by each key member individually
-        //      this needs to be addressed
-        final var caseOfUnionEntity = maybeOperandPropPenultMember.map(part -> part instanceof QuerySourceItemForUnionType).orElse(false);
-        return !caseOfUnionEntity && KEY.equals(operandPropLastMember.name) && (operandPropLastMember.hasExpression()/*composite*/ || isEntityType(operandPropLastMember.javaType())/*1-2-1*/);
+    public static boolean needsExtraction(final AbstractQuerySourceItem<?> lastItem, final Optional<AbstractQuerySourceItem<?>> maybePenultItem) {
+        // TODO Support extraction of key members in union entities.
+        //      For now, we ignore union entities, and the effect is that `union.key` will be used -- a string representation --
+        //      instead of having access to each key member individually.
+        final var caseOfUnionEntity = maybePenultItem.filter(item -> item instanceof QuerySourceItemForUnionType).isPresent();
+        return !caseOfUnionEntity
+               && KEY.equals(lastItem.name)
+               && (lastItem.hasExpression()/*composite*/ || isEntityType(lastItem.javaType())/*1-2-1*/);
     }
+
 }
