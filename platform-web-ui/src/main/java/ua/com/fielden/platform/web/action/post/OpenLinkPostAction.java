3520567338f17203557f87ea12ddb3480f741c5d
Oleh Maikovych <oleh@fielden.com.au>
2025-07-16 23:27:15 +0300

    #2407 Adjusted link opener post action according to latest changes in 2.0.3 tg version

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/action/post/OpenLinkPostAction.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/action/post/OpenLinkPostAction.java
@@ -29,14 +29,14 @@ public class OpenLinkPostAction implements IPostAction {
 
     @Override
     public Set<JsImport> importStatements() {
-        return of(namedImport("openLink", "reflection/tg-polymer-utils"));
+        return of(namedImport("checkLinkAndOpen", "components/tg-link-opener"));
     }
 
     @Deprecated(since = WARN_DEPRECATION_DANGEROUS_CODE_CONCATENATION_WITHOUT_IMPORTS)
     @Override
     public JsCode build() {
         return jsCode("""
-            openLink(functionalEntity.get('%s'));
+            checkLinkAndOpen(functionalEntity.get('%s'));
         """.formatted(
             property.toPath()
         ));
