33ac78a7e179ef7f0a0138d218afde3e51fa395f
01es <oles.hodych@gmail.com>
2025-07-28 19:36:05 +1000

    #2452 A bit of clean up and clarifications around EQL-related exceptions.

--- platform-dao/src/main/java/ua/com/fielden/platform/entity/query/exceptions/EntityRetrievalModelException.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/entity/query/exceptions/EntityRetrievalModelException.java
@@ -1,18 +1,13 @@
 package ua.com.fielden.platform.entity.query.exceptions;
 
 import ua.com.fielden.platform.entity.query.EntityRetrievalModel;
-import ua.com.fielden.platform.exceptions.AbstractPlatformRuntimeException;
 
 /// Exception pertaining to [EntityRetrievalModel].
 ///
-public final class EntityRetrievalModelException extends AbstractPlatformRuntimeException {
+public final class EntityRetrievalModelException extends EqlException {
 
     public EntityRetrievalModelException(final String msg) {
         super(msg);
-    }
-
-    public EntityRetrievalModelException(final Throwable cause) {
-        super(cause);
     }
 
     public EntityRetrievalModelException(final String msg, final Throwable cause) {
