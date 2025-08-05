a0e4aa472e211dc4fb2a53b9d1812d49f638414a
homedirectory <nullsink@posteo.eu>
2025-08-05 14:45:33 +0300

    #1450 Minor improvements and modernisation of entity-exists tests

--- platform-dao/src/test/java/ua/com/fielden/platform/entity/validation/exists/EntityExistsValidationTest.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/entity/validation/exists/EntityExistsValidationTest.java
@@ -6,12 +6,11 @@ import ua.com.fielden.platform.criteria.generator.ICriteriaGenerator;
 import ua.com.fielden.platform.domaintree.centre.impl.CentreDomainTreeManagerAndEnhancer;
 import ua.com.fielden.platform.entity.annotation.SkipEntityExistsValidation;
 import ua.com.fielden.platform.entity.factory.EntityFactory;
-import ua.com.fielden.platform.entity.meta.MetaProperty;
-import ua.com.fielden.platform.entity.meta.PropertyDescriptor;
 import ua.com.fielden.platform.entity.validation.EntityExistsValidator;
-import ua.com.fielden.platform.error.Result;
 import ua.com.fielden.platform.ioc.ApplicationInjectorFactory;
-import ua.com.fielden.platform.sample.domain.*;
+import ua.com.fielden.platform.sample.domain.TgCategory;
+import ua.com.fielden.platform.sample.domain.TgSubSystem;
+import ua.com.fielden.platform.sample.domain.TgSystem;
 import ua.com.fielden.platform.sample.domain.crit_gen.CriteriaGeneratorTestIocModule;
 import ua.com.fielden.platform.test_config.AbstractDaoTestCase;
 
@@ -20,9 +19,10 @@ import java.util.HashSet;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
-import static ua.com.fielden.platform.entity.validation.EntityExistsValidator.ERR_DIRTY;
-import static ua.com.fielden.platform.entity.validation.EntityExistsValidator.ERR_UNION_INVALID;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static ua.com.fielden.platform.entity.meta.PropertyDescriptor.pd;
+import static ua.com.fielden.platform.entity.validation.EntityExistsValidator.*;
 import static ua.com.fielden.platform.reflection.TitlesDescsGetter.getEntityTitleAndDesc;
 
 /// This test case covers rules of the entity-exists validation.
@@ -30,8 +30,8 @@ import static ua.com.fielden.platform.reflection.TitlesDescsGetter.getEntityTitl
 /// @see EntityExistsValidator
 /// @see SkipEntityExistsValidation
 ///
-// TODO Some parts of this test class can be made generic in the fashion of AbstractEntityActivatabilityTestCase.
 public class EntityExistsValidationTest extends AbstractDaoTestCase {
+
     private final CriteriaGeneratorTestIocModule module = new CriteriaGeneratorTestIocModule();
     private final Injector injector = new ApplicationInjectorFactory().add(module).getInjector();
     private final EntityFactory entityFactory = injector.getInstance(EntityFactory.class);
@@ -46,75 +46,69 @@ public class EntityExistsValidationTest extends AbstractDaoTestCase {
     }
 
     @Test
-    public void existing_but_inactive_entity_cannot_be_assigned_to_property_of_activatable_entity_with_default_validation() {
-        final TgCategory cat2 = co$(TgCategory.class).findByKey("Cat2");
-        final TgSystem sys = new_(TgSystem.class, "Sys2").setActive(true).setFirstCategory(cat2);
+    public void existing_inactive_entity_cannot_be_assigned_to_property_of_activatable_entity_with_default_validation() {
+        final var cat2 = co$(TgCategory.class).findByKey("Cat2");
+        final var sys = new_(TgSystem.class, "Sys2").setActive(true).setFirstCategory(cat2);
 
-        final Result result = sys.isValid();
-        assertFalse(result.isSuccessful());
-        assertEquals("Tg Category [Cat2] exists, but is not active.", result.getMessage());
+        assertThat(sys.getProperty("firstCategory").getFirstFailure())
+                .hasMessage(format(ERR_ENTITY_EXISTS_BUT_NOT_ACTIVE, getEntityTitleAndDesc(cat2).getKey(), cat2));
     }
 
     @Test
-    public void existing_but_inactive_entity_cannot_be_assigned_to_property_of_non_activatable_entity_with_default_validation() {
+    public void existing_inactive_entity_cannot_be_assigned_to_property_of_non_activatable_entity_with_default_validation() {
         final TgCategory cat2 = co$(TgCategory.class).findByKey("Cat2");
         final var subSys = new_(TgSubSystem.class, "SubSys2").setFirstCategory(cat2);
 
-        final Result result = subSys.isValid();
-        assertFalse(result.isSuccessful());
-        assertEquals("Tg Category [Cat2] exists, but is not active.", result.getMessage());
+        assertThat(subSys.getProperty("firstCategory").getFirstFailure())
+                .hasMessage(format(ERR_ENTITY_EXISTS_BUT_NOT_ACTIVE, getEntityTitleAndDesc(cat2).getKey(), cat2));
     }
 
     @Test
-    public void existing_but_inactive_entity_can_be_assigned_to_property_of_inactive_entity_with_default_validation() {
+    public void existing_inactive_entity_can_be_assigned_to_property_of_inactive_entity_with_default_validation() {
         final var cat = save(new_(TgCategory.class, "CAT10").setActive(false));
         final var sys = new_(TgSystem.class, "Sys2").setActive(false).setCategory(cat);
-        assertTrue(sys.isValid().isSuccessful());
         assertTrue(sys.getProperty("category").isValid());
     }
 
     @Test
-    public void existing_but_inactive_entity_can_be_assigned_to_property_of_active_entity_with_skipActiveOnly() {
+    public void existing_inactive_entity_can_be_assigned_to_property_of_active_entity_with_skipActiveOnly() {
         final var cat = save(new_(TgCategory.class, "CAT10").setActive(false));
         final var sys = new_(TgSystem.class, "Sys2").setActive(true).setThirdCategory(cat);
         assertTrue(sys.getProperty("thirdCategory").isValid());
     }
 
     @Test
-    public void existing_but_inactive_entity_can_be_assigned_to_property_of_criteria_entity_with_default_validation() {
+    public void existing_inactive_entity_can_be_assigned_to_property_of_criteria_entity_with_default_validation() {
         cdtm.getFirstTick().check(TgSystem.class, "critOnlySingleCategory", true);
         final var cg = injector.getInstance(ICriteriaGenerator.class);
         final var criteriaEntity = cg.generateCentreQueryCriteria(cdtm);
         
         final TgCategory cat2 = co$(TgCategory.class).findByKey("Cat2");
         criteriaEntity.set("tgSystem_critOnlySingleCategory", cat2);
-        
-        final Result result = criteriaEntity.isValid();
-        assertTrue(result.isSuccessful());
+
+        assertTrue(criteriaEntity.getProperty("tgSystem_critOnlySingleCategory").isValid());
     }
     
     @Test
     public void non_existing_entity_cannot_be_assigned_to_property_with_default_validation() {
-        final TgCategory cat2 = co$(TgCategory.class).findByKey("Cat2");
+        final var cat2 = co$(TgCategory.class).findByKey("Cat2");
 
         // let's delete cat2 to make it non-existing
         co$(TgCategory.class).delete(cat2);
 
-        final TgSystem sys = new_(TgSystem.class, "Sys2").setActive(true).setFirstCategory(cat2);
+        final var sys = new_(TgSystem.class, "Sys2").setActive(true).setFirstCategory(cat2);
 
-        final Result result = sys.isValid();
-        assertFalse(result.isSuccessful());
-        assertEquals("Tg Category [Cat2] was not found.", result.getMessage());
+        assertThat(sys.getProperty("firstCategory").getFirstFailure())
+                .hasMessage(format(ERR_ENTITY_WAS_NOT_FOUND, getEntityTitleAndDesc(cat2).getKey(), cat2));
     }
 
     @Test
     public void new_entity_cannot_be_assigned_to_property_with_default_validation() {
-        final TgCategory newCat = co(TgCategory.class).new_().setKey("NEW CAT");
-        final TgSystem sys = new_(TgSystem.class, "Sys2").setActive(true).setFirstCategory(newCat);
+        final var newCat = co(TgCategory.class).new_().setKey("NEW CAT");
+        final var sys = new_(TgSystem.class, "Sys2").setActive(true).setFirstCategory(newCat);
 
-        final Result result = sys.isValid();
-        assertFalse(result.isSuccessful());
-        assertEquals(format(EntityExistsValidator.ERR_WAS_NOT_FOUND, TgCategory.ENTITY_TITLE), result.getMessage());
+        assertThat(sys.getProperty("firstCategory").getFirstFailure())
+                .hasMessage(format(ERR_WAS_NOT_FOUND, getEntityTitleAndDesc(newCat).getKey()));
     }
 
     
@@ -130,22 +124,21 @@ public class EntityExistsValidationTest extends AbstractDaoTestCase {
         co$(TgCategory.class).delete(cat2);
         
         criteriaEntity.set("tgSystem_critOnlySingleCategory", cat2);
-        
-        final Result result = criteriaEntity.isValid();
-        assertTrue(result.isSuccessful());
+
+        assertTrue(criteriaEntity.getProperty("tgSystem_critOnlySingleCategory").isValid());
     }
     
     @Test
-    public void existing_entity_can_be_assigned_to_property_with_skipped_exists_validation() {
-        final TgCategory cat2 = co$(TgCategory.class).findByKey("Cat2");
-        final TgSystem sys = new_(TgSystem.class, "Sys2").setActive(true).setSecondCategory(cat2);
+    public void existing_entity_can_be_assigned_to_property_with_default_SkipEntityExistsValidation() {
+        final var cat2 = co$(TgCategory.class).findByKey("Cat2");
+        final var sys = new_(TgSystem.class, "Sys2").setActive(true).setSecondCategory(cat2);
 
         assertTrue(sys.isValid().isSuccessful());
         assertNotNull(sys.getSecondCategory());
     }
 
     @Test
-    public void non_existing_entity_can_be_assigned_to_property_with_skipped_exists_validation() {
+    public void non_existing_entity_can_be_assigned_to_property_with_default_SkipEntityExistsValidation() {
         final TgSystem sys = new_(TgSystem.class, "Sys2").setActive(true).setSecondCategory(new_(TgCategory.class, "Cat3"));
 
         assertTrue(sys.isValid().isSuccessful());
@@ -153,109 +146,96 @@ public class EntityExistsValidationTest extends AbstractDaoTestCase {
     }
 
     @Test
-    public void non_existing_entity_cannot_be_assigned_to_property_with_only_active_check_skipped_validation() {
-        final TgCategory cat2 = co$(TgCategory.class).findByKey("Cat2");
+    public void non_existing_entity_cannot_be_assigned_to_property_with_skipActiveOnly() {
+        final var cat2 = co$(TgCategory.class).findByKey("Cat2");
 
         // let's delete cat2 to make it non-existing
         co$(TgCategory.class).delete(cat2);
 
-        final TgSystem sys = new_(TgSystem.class, "Sys2").setActive(true).setThirdCategory(cat2);
+        final var sys = new_(TgSystem.class, "Sys2").setActive(true).setThirdCategory(cat2);
 
-        final Result result = sys.isValid();
-        assertFalse(result.isSuccessful());
-        assertEquals("Tg Category [Cat2] was not found.", result.getMessage());
+        assertThat(sys.getProperty("thirdCategory").getFirstFailure())
+                .hasMessage(format(ERR_ENTITY_WAS_NOT_FOUND, getEntityTitleAndDesc(cat2).getKey(), cat2));
     }
 
     @Test
-    public void existing_but_inactive_entity_can_be_assigned_to_property_with_only_active_check_skipped_validation() {
-        final TgCategory cat2 = co$(TgCategory.class).findByKey("Cat2");
-        final TgSystem sys = new_(TgSystem.class, "Sys2").setActive(true).setThirdCategory(cat2);
+    public void existing_inactive_entity_can_be_assigned_to_property_with_skipActiveOnly() {
+        final var cat2 = co$(TgCategory.class).findByKey("Cat2");
+        final var sys = new_(TgSystem.class, "Sys2").setActive(true).setThirdCategory(cat2);
 
-        assertTrue(sys.isValid().isSuccessful());
-        assertNotNull(sys.getThirdCategory());
+        assertTrue(sys.getProperty("thirdCategory").isValid());
     }
 
     @Test
-    public void existing_and_active_entity_can_be_assigned_to_property_with_only_active_check_skipped_validation() {
-        final TgCategory cat1 = co$(TgCategory.class).findByKey("Cat1");
-        final TgSystem sys = new_(TgSystem.class, "Sys2").setActive(true).setThirdCategory(cat1);
+    public void existing_active_entity_can_be_assigned_to_property_with_skipActiveOnly() {
+        final var cat1 = co$(TgCategory.class).findByKey("Cat1");
+        final var sys = new_(TgSystem.class, "Sys2").setActive(true).setThirdCategory(cat1);
 
-        assertTrue(sys.isValid().isSuccessful());
-        assertNotNull(sys.getThirdCategory());
+        assertTrue(sys.getProperty("thirdCategory").isValid());
     }
 
     @Test
-    public void entity_exists_validation_does_not_permit_dirty_entities() {
-        final TgCategory cat1 = co$(TgCategory.class).findByKey("Cat1");
+    public void dirty_entity_cannot_be_assigned_to_property_with_default_validation() {
+        final var cat1 = co$(TgCategory.class).findByKey("Cat1");
         cat1.setDesc(cat1.getDesc() + "some change");
-        final TgSystem sys = new_(TgSystem.class, "Sys2").setActive(true).setFirstCategory(cat1);
+        final var sys = new_(TgSystem.class, "Sys2").setActive(true).setFirstCategory(cat1);
 
-        final String entityTitle = getEntityTitleAndDesc(cat1.getType()).getKey();
-        final Result result = sys.isValid();
-        assertFalse(result.isSuccessful());
-        assertEquals(format(ERR_DIRTY, cat1, entityTitle), result.getMessage());
+        assertThat(sys.getProperty("firstCategory").getFirstFailure())
+                .hasMessage(format(ERR_DIRTY, cat1, getEntityTitleAndDesc(cat1).getKey()));
     }
 
     @Test
-    public void dirty_values_are_allowed_for_property_annotated_with_SkipEntityExistsValidation() {
-        final TgCategory cat1 = co$(TgCategory.class).findByKey("Cat1");
+    public void dirty_entity_can_be_assigned_to_property_with_default_SkipEntityExistsValidation() {
+        final var cat1 = co$(TgCategory.class).findByKey("Cat1");
         cat1.setDesc(cat1.getDesc() + "some change");
-        final TgSystem sys = new_(TgSystem.class, "Sys2").setActive(true).setSecondCategory(cat1);
+        final var sys = new_(TgSystem.class, "Sys2").setActive(true).setSecondCategory(cat1);
 
-        assertTrue(sys.isValid().isSuccessful());
-        assertNotNull(sys.getSecondCategory());
+        assertTrue(sys.getProperty("firstCategory").isValid());
     }
 
     @Test
-    public void dirty_values_are_not_allowed_for_property_annotated_with_SkipEntityExistsValidation_if_skipActiveOnly_is_true() {
+    public void dirty_entity_cannot_be_assigned_to_property_with_skipActiveOnly() {
         final var cat1 = co$(TgCategory.class).findByKey("Cat1");
         cat1.setDesc(cat1.getDesc() + "some change");
         final var sys = new_(TgSystem.class, "Sys2").setActive(true).setThirdCategory(cat1);
 
-        assertFalse(sys.getProperty("thirdCategory").isValid());
-        assertEquals(format(ERR_DIRTY, cat1, getEntityTitleAndDesc(cat1).getKey()),
-                     sys.getProperty("thirdCategory").getFirstFailure().getMessage());
+        assertThat(sys.getProperty("thirdCategory").getFirstFailure())
+                .hasMessage(format(ERR_DIRTY, cat1, getEntityTitleAndDesc(cat1).getKey()));
     }
 
     @Test
-    public void dirty_values_are_not_allowed_for_property_annotated_with_SkipEntityExistsValidation_if_skipNew_is_true() {
+    public void dirty_entity_cannot_be_assigned_to_property_with_skipNew() {
         final var cat1 = co$(TgCategory.class).findByKey("Cat1");
         cat1.setDesc(cat1.getDesc() + "some change");
         final var sys = new_(TgSystem.class, "Sys2").setActive(true).setPermitNewCategory(cat1);
 
-        assertFalse(sys.getProperty("permitNewCategory").isValid());
-        assertEquals(format(ERR_DIRTY, cat1, getEntityTitleAndDesc(cat1).getKey()),
-                     sys.getProperty("permitNewCategory").getFirstFailure().getMessage());
+        assertThat(sys.getProperty("permitNewCategory").getFirstFailure())
+                .hasMessage(format(ERR_DIRTY, cat1, getEntityTitleAndDesc(cat1).getKey()));
     }
 
     @Test
-    public void dirty_values_are_not_allowed_for_property_annotated_with_SkipEntityExistsValidation_if_skipNew_and_skipActiveOnly_are_true() {
+    public void dirty_entity_cannot_be_assigned_to_property_with_skipNew_and_skipActiveOnly() {
         final var cat1 = co$(TgCategory.class).findByKey("Cat1");
         cat1.setDesc(cat1.getDesc() + "some change");
         final var sys = new_(TgSystem.class, "Sys2").setActive(true).setPermitNewAndSkipActiveOnlyCategory(cat1);
 
-        assertFalse(sys.getProperty("permitNewAndSkipActiveOnlyCategory").isValid());
-        assertEquals(format(ERR_DIRTY, cat1, getEntityTitleAndDesc(cat1).getKey()),
-                     sys.getProperty("permitNewAndSkipActiveOnlyCategory").getFirstFailure().getMessage());
+        assertThat(sys.getProperty("permitNewAndSkipActiveOnlyCategory").getFirstFailure())
+                .hasMessage(format(ERR_DIRTY, cat1, getEntityTitleAndDesc(cat1).getKey()));
     }
 
     @Test
-    public void non_persisted_entity_value_is_allowed_for_property_annotated_with_SkipEntityExistsValidation_if_skipNew_is_true() {
+    public void new_entity_can_be_assigned_to_property_with_skipNew() {
         final var cat42 = co$(TgCategory.class).new_().setKey("Cat42");
         final var sys = new_(TgSystem.class, "Sys2").setActive(true).setPermitNewCategory(cat42);
 
         assertTrue(sys.getProperty("permitNewCategory").isValid());
-        assertNotNull(sys.getPermitNewCategory());
     }
 
     @Test
     public void values_of_type_PropertyDescriptor_are_recognised_as_existent() {
-        final PropertyDescriptor<TgCategory> pd = new PropertyDescriptor<>(TgCategory.class, "parent");
-        final TgSystem sys = new_(TgSystem.class, "Sys2");
-        sys.setPropDescriptor(pd);
-        final MetaProperty<?> mp = sys.getProperty("propDescriptor");
-        assertTrue(mp.isValid());
-        assertEquals(pd, mp.getValue());
+        final var pd = pd(TgCategory.class, "parent");
+        final var sys = new_(TgSystem.class, "Sys2").setPropDescriptor(pd);
+        assertTrue(sys.getProperty("propDescriptor").isValid());
     }
 
     @Override
