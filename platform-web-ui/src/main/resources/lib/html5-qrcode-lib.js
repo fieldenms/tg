0d1f94497fc1e1b717dc327efae3fcae0e4380c2
Oleh Maikovych <oleh@fielden.com.au>
2025-09-02 14:51:41 +0300

    #2477 Added html5-qrcode dependency.

---
+++ platform-web-ui/src/main/resources/lib/html5-qrcode-lib.js
@@ -0,0 +1,2 @@
+export {Html5Qrcode as Html5Qrcode} from "html5-qrcode";
+export {Html5QrcodeScanner as Html5QrcodeScanner} from "html5-qrcode";
---
+++ platform-web-ui/src/main/resources/polymer/lib/html5-qrcode-lib.js
@@ -0,0 +1,4 @@
+export { Html5Qrcode } from '../html5-qrcode/esm/html5-qrcode.js';
+export { Html5QrcodeScanner } from '../html5-qrcode/esm/html5-qrcode-scanner.js';
+import '../html5-qrcode/esm/core.js';
+import '../html5-qrcode/esm/state-manager.js';
