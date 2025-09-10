6d8b478bb559a04a28c247e0a77574589e52cf48
01es <oles.hodych@gmail.com>
2025-09-10 21:49:41 +1000

    #2497 Introduced 'batchInsert' and 'defaultBatchInsert'.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/companion/IPersistentEntityMutator.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/companion/IPersistentEntityMutator.java
@@ -2,12 +2,16 @@ package ua.com.fielden.platform.companion;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
 
+import java.util.stream.Stream;
+
 /// A contract that combines [IPersistentEntityDeleter] and [IEntityActuator] as a convenience in cases where
 /// both sets of mutating methods (save and delete) are required.
 ///
 /// In addition, this contract adds method [#quickSave(AbstractEntity)], which could only be applicable to persistent entities.
 ///
 public interface IPersistentEntityMutator<T extends AbstractEntity<?>> extends IPersistentEntityDeleter<T>, IEntityActuator<T> {
+
+    String ERR_BATCH_INSERTION_IS_UNSUPPORTED = "By default batch insertion is not supported. Carefully consider if it is appropriate in your case.";
 
     /// Similar to method [#save(AbstractEntity)], but applicable only to *persistent* entities.
     /// It returns an `id` of the saved entity.
@@ -20,6 +24,34 @@ public interface IPersistentEntityMutator<T extends AbstractEntity<?>> extends I
     ///
     default long quickSave(final T entity) {
         throw new UnsupportedOperationException(); 
+    }
+
+    /// Performs batch insertion of new entities for optimised database performance.
+    ///
+    /// This method inserts multiple new entities in batches rather than individually,
+    /// which can significantly improve performance when dealing with large volumes of data.
+    /// The entities are processed in chunks according to the specified batch size.
+    ///
+    /// **Important:**
+    /// - This method should only be used for inserting **NEW** entities.
+    ///   For updating existing entities, use the standard save operations.
+    /// - When overriding consider carefully if the method should be annotated with `@SessionRequired`.
+    ///   It is not applicable if batch insertion should happen **only** in the scope of an open session.
+    ///
+    ///
+    /// **Implementation note:** By default, this operation is not supported and will throw
+    /// an [UnsupportedOperationException].
+    /// Implementers should carefully consider whether batch insertion is appropriate for their specific entity type before
+    /// providing an implementation.
+    ///
+    /// @param newEntities a stream of new entities to be inserted into the database
+    /// @param batchSize   the number of entities to process in each batch;
+    ///                    typical values range from 100 to 1000 depending on entity complexity
+    ///
+    /// @return the total number of entities successfully inserted
+    ///
+    default int batchInsert(final Stream<T> newEntities, final int batchSize) {
+        throw new UnsupportedOperationException(ERR_BATCH_INSERTION_IS_UNSUPPORTED);
     }
 
 }
