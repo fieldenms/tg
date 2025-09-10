cf9061e873af5cd823eadfa6fa72df16c144c4be
01es <oles.hodych@gmail.com>
2025-09-10 19:17:43 +1000

    #2497 Documentation improvements.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/companion/IPersistentEntityMutator.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/companion/IPersistentEntityMutator.java
@@ -2,30 +2,24 @@ package ua.com.fielden.platform.companion;
 
 import ua.com.fielden.platform.entity.AbstractEntity;
 
-/**
- * A contract that combines {@link IPersistentEntityDeleter} and {@link IPersistentEntitySaver} as a convenience in cases where both sets of mutating methods (save and delete) are required.
- * <p>
- * In addition, this contract adds method {@link #quickSave(AbstractEntity)}, which could only be applicable to persistent entities.
- *  
- * @author TG Team
- *
- * @param <T>
- */
+/// A contract that combines [IPersistentEntityDeleter] and [IEntityActuator] as a convenience in cases where
+/// both sets of mutating methods (save and delete) are required.
+///
+/// In addition, this contract adds method [#quickSave(AbstractEntity)], which could only be applicable to persistent entities.
+///
 public interface IPersistentEntityMutator<T extends AbstractEntity<?>> extends IPersistentEntityDeleter<T>, IEntityActuator<T> {
-    
-    /**
-     * Similar to method {@link IEntityActuator#save(AbstractEntity)}, but applicable only to persistent entities. 
-     * It returns an <code>id</code> of the saved entity.
-     * The implication is that this method should execute faster by skipping the steps required to re-fetch the resultant entity.
-     * <p>
-     * This method is relevant only for simple cases where method <code>save</code> is not overridden to provide an application specific logic.
-     * 
-     * @param entity
-     * @return
-     */
+
+    /// Similar to method [#save(AbstractEntity)], but applicable only to *persistent* entities.
+    /// It returns an `id` of the saved entity.
+    ///
+    /// The implication is that this method should execute faster by skipping the steps required to re-fetch the resultant entity.
+    /// This method is relevant only for simple cases where method `save` is not overridden to provide an application specific logic.
+    ///
+    /// **Important:** A more modern and better alternative is method `save(T entity, Optional<fetch<T>> maybeFetch)` in class `CommonEntityDao`.
+    /// Prefer that method over [#quickSave(AbstractEntity)].
+    ///
     default long quickSave(final T entity) {
         throw new UnsupportedOperationException(); 
     }
-
 
 }
