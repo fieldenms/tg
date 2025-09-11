62545035907fed78b8f9c3867869acd71e381d39
01es <oles.hodych@gmail.com>
2025-09-11 15:48:12 +1000

    #2500 Improved type checking and error reporting.

--- platform-pojo-bl/src/test/java/ua/com/fielden/platform/expression/ExpressionBooleanComparisonTest.java
+++ platform-pojo-bl/src/test/java/ua/com/fielden/platform/expression/ExpressionBooleanComparisonTest.java
@@ -5,8 +5,10 @@ import ua.com.fielden.platform.expression.ast.AstNode;
 import ua.com.fielden.platform.expression.ast.visitor.entities.EntityLevel1;
 import ua.com.fielden.platform.expression.exception.RecognitionException;
 import ua.com.fielden.platform.expression.exception.semantic.SemanticException;
+import ua.com.fielden.platform.expression.exception.semantic.UnsupportedTypeException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ExpressionBooleanComparisonTest {
 
@@ -23,5 +25,44 @@ public class ExpressionBooleanComparisonTest {
         final AstNode root = ev.convert();
         assertEquals("Should be boolean type", boolean.class, root.getType());
     }
+
+    @Test
+    public void boolean_property_cannot_be_equal_to_NULL() throws RecognitionException, SemanticException {
+        try {
+            final ExpressionText2ModelConverter ev = new ExpressionText2ModelConverter(EntityLevel1.class, "boolProperty = NULL");
+            final AstNode root = ev.convert();
+            fail();
+        } catch (final UnsupportedTypeException ex) {
+            assertEquals("Operands for operation EQ should have compatible types [boolean, class ua.com.fielden.platform.expression.type.Null].", ex.getMessage());
+        }
+
+        try {
+            final ExpressionText2ModelConverter ev = new ExpressionText2ModelConverter(EntityLevel1.class, "NULL = boolProperty");
+            final AstNode root = ev.convert();
+            fail();
+        } catch (final UnsupportedTypeException ex) {
+            assertEquals("Operands for operation EQ should have compatible types [class ua.com.fielden.platform.expression.type.Null, boolean].", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void boolean_property_cannot_be_not_equal_to_NULL() throws RecognitionException, SemanticException {
+        try {
+            final ExpressionText2ModelConverter ev = new ExpressionText2ModelConverter(EntityLevel1.class, "boolProperty <> NULL");
+            final AstNode root = ev.convert();
+            fail();
+        } catch (final UnsupportedTypeException ex) {
+            assertEquals("Operands for operation NE should have compatible types [boolean, class ua.com.fielden.platform.expression.type.Null].", ex.getMessage());
+        }
+
+        try {
+            final ExpressionText2ModelConverter ev = new ExpressionText2ModelConverter(EntityLevel1.class, "NULL <> boolProperty");
+            final AstNode root = ev.convert();
+            fail();
+        } catch (final UnsupportedTypeException ex) {
+            assertEquals("Operands for operation NE should have compatible types [class ua.com.fielden.platform.expression.type.Null, boolean].", ex.getMessage());
+        }
+    }
+
 
 }
