85476223def1e1b744b5c9ebcd686c665336c104
01es <oles.hodych@gmail.com>
2025-09-25 18:19:30 +1000

    #2476 Enhanced CO finder documentation to cover the case of dynamically generated compaions.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/annotation/CompanionIsGenerated.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/annotation/CompanionIsGenerated.java
@@ -5,12 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Annotates an entity type to indicate that its companion object is dynamically generated instead of existing in source code.
- *
- * @see CompanionObject
- * @author TG Team
- */
+/// Annotates an entity type to indicate that its companion object is dynamically generated instead of existing in source code.
+///
+/// @see CompanionObject
+///
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE })
 public @interface CompanionIsGenerated {}
