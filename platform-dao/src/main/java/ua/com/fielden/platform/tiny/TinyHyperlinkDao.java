5981706a03c91e1792cf0f564a9dec4c5e5e36c7
homedirectory <nullsink@posteo.eu>
2025-10-24 19:37:55 +0300

    Implement entity retrieval from a TinyHyperlink

--- platform-dao/src/main/java/ua/com/fielden/platform/tiny/TinyHyperlinkDao.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/tiny/TinyHyperlinkDao.java
@@ -1,11 +1,19 @@
 package ua.com.fielden.platform.tiny;
 
 import ua.com.fielden.platform.dao.CommonEntityDao;
+import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.annotation.EntityType;
 import ua.com.fielden.platform.entity.fetch.IFetchProvider;
+import ua.com.fielden.platform.reflection.ClassesRetriever;
 
 @EntityType(TinyHyperlink.class)
 public class TinyHyperlinkDao extends CommonEntityDao<TinyHyperlink> implements TinyHyperlinkCo {
+
+    @Override
+    public <E extends AbstractEntity<?>> E entityFromLink(final TinyHyperlink link) {
+        final var entityType = (Class<E>) ClassesRetriever.findClass(link.getEntityTypeName());
+        return co(entityType).findById(link.getEntityId());
+    }
 
     @Override
     protected IFetchProvider<TinyHyperlink> createFetchProvider() {
--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/tiny/TinyHyperlinkCo.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/tiny/TinyHyperlinkCo.java
@@ -1,6 +1,7 @@
 package ua.com.fielden.platform.tiny;
 
 import ua.com.fielden.platform.dao.IEntityDao;
+import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.fetch.IFetchProvider;
 
 import static ua.com.fielden.platform.utils.EntityUtils.fetch;
@@ -10,4 +11,7 @@ public interface TinyHyperlinkCo extends IEntityDao<TinyHyperlink> {
     IFetchProvider<TinyHyperlink> FETCH_PROVIDER = fetch(TinyHyperlink.class).with(
             "entityTypeName",
             "entityId");
+
+    <E extends AbstractEntity<?>> E entityFromLink(TinyHyperlink link);
+
 }
