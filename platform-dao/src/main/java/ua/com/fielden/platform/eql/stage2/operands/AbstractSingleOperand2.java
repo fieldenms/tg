f32352780750c490a5a6fc33baf6305eb28a9329
homedirectory <nullsink@posteo.eu>
2025-10-28 10:17:21 +0200

    #2553 Ignore null types during type resolution

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/stage2/operands/AbstractSingleOperand2.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/stage2/operands/AbstractSingleOperand2.java
@@ -1,15 +1,19 @@
 package ua.com.fielden.platform.eql.stage2.operands;
 
 import ua.com.fielden.platform.entity.query.exceptions.EqlException;
+import ua.com.fielden.platform.eql.exceptions.EqlStage2ProcessingException;
 import ua.com.fielden.platform.eql.meta.PropType;
 import ua.com.fielden.platform.utils.CollectionUtil;
 import ua.com.fielden.platform.utils.ToString;
 
 import java.util.Set;
 
+import static java.util.function.Predicate.not;
+import static java.util.stream.Collectors.toSet;
 import static ua.com.fielden.platform.eql.meta.PropType.*;
 
 public abstract class AbstractSingleOperand2 implements ToString.IFormattable {
+
     public final PropType type;
 
     public AbstractSingleOperand2(final PropType type) {
@@ -21,28 +25,38 @@ public abstract class AbstractSingleOperand2 implements ToString.IFormattable {
     }
 
     public static PropType getTypeHighestPrecedence(final Set<PropType> types) {
-        if (types.size() == 1) {
-            return types.iterator().next();
-        } else if (types.contains(UTCDATETIME_PROP_TYPE)) {
+        if (types.isEmpty()) {
+            throw new EqlStage2ProcessingException("Argument [types] must not be empty.");
+        }
+
+        final var nonNullTypes = types.contains(NULL_TYPE)
+                ? types.stream().filter(not(PropType::isNull)).collect(toSet())
+                : types;
+
+        if (nonNullTypes.isEmpty()) {
+            return NULL_TYPE;
+        } else if (nonNullTypes.size() == 1) {
+            return nonNullTypes.iterator().next();
+        } else if (nonNullTypes.contains(UTCDATETIME_PROP_TYPE)) {
             return UTCDATETIME_PROP_TYPE;
-        } else if (types.contains(DATETIME_PROP_TYPE)) {
+        } else if (nonNullTypes.contains(DATETIME_PROP_TYPE)) {
             return DATETIME_PROP_TYPE;
-        } else if (types.contains(DATE_PROP_TYPE)) {
+        } else if (nonNullTypes.contains(DATE_PROP_TYPE)) {
             return DATE_PROP_TYPE;
-        } else if (types.contains(BIGDECIMAL_PROP_TYPE)) {
+        } else if (nonNullTypes.contains(BIGDECIMAL_PROP_TYPE)) {
             return BIGDECIMAL_PROP_TYPE;
-        } else if (types.contains(LONG_PROP_TYPE)) {
+        } else if (nonNullTypes.contains(LONG_PROP_TYPE)) {
             return LONG_PROP_TYPE;
-        } else if (types.contains(INTEGER_PROP_TYPE)){
+        } else if (nonNullTypes.contains(INTEGER_PROP_TYPE)){
             return INTEGER_PROP_TYPE;
-        } else if (types.contains(INT_PROP_TYPE)){
+        } else if (nonNullTypes.contains(INT_PROP_TYPE)){
             return INT_PROP_TYPE;
-        } else if (types.contains(NSTRING_PROP_TYPE)) {
+        } else if (nonNullTypes.contains(NSTRING_PROP_TYPE)) {
             return NSTRING_PROP_TYPE;
-        } else if (types.contains(STRING_PROP_TYPE)) {
+        } else if (nonNullTypes.contains(STRING_PROP_TYPE)) {
             return STRING_PROP_TYPE;
         } else {
-            throw new EqlException("Can't determine type with highest precedence among {%s}".formatted(CollectionUtil.toString(types, ", ")));
+            throw new EqlException("Can't determine type with highest precedence among {%s}".formatted(CollectionUtil.toString(nonNullTypes, ", ")));
         }
     }
 
