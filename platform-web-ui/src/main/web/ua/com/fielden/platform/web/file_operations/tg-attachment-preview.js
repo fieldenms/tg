7da2b7086a2506f176c99ba930528881c8c7cbb6
Oleh Maikovych <oleh@fielden.com.au>
2025-10-31 15:19:01 +0200

    #2545 Adjusted tooltip-text attribute so that it becomes binded as attribute instead of like property.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/file_operations/tg-attachment-preview.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/file_operations/tg-attachment-preview.js
@@ -66,7 +66,7 @@ const template = html`
     <img id="imageLoader" src$="[[_getImageUri(_linkCheckRes, _wasConfirmed, _attachmentUri)]]" hidden$="[[!_isImageVisible(_loadingError, _attachmentUri)]]" on-load="_imageLoaded" on-error="_imageLoadeError"/>
     <div id="altImage" hidden$="[[_isImageVisible(_loadingError, _attachmentUri)]]">
         <span id="message">[[_getAltImageText(_linkCheckRes, _wasConfirmed)]]</span>
-        <paper-button raised on-tap="_downloadOrOpenAttachment" tooltip-text="[[_getButtonTooltip(_linkCheckRes)]]" disabled$="[[_working]]">
+        <paper-button raised on-tap="_downloadOrOpenAttachment" tooltip-text$="[[_getButtonTooltip(_linkCheckRes)]]" disabled$="[[_working]]">
             <span>[[_getButtonText(_linkCheckRes)]]</span>
             <paper-spinner id="spinner" active="[[_working]]" class="blue" style="display: none;" alt="in progress"></paper-spinner>
         </paper-button>
