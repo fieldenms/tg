db695071ab953b9975b99a1295b2581a6bb42e71
homedirectory <nullsink@posteo.eu>
2025-10-31 16:38:02 +0200

    #2422 Implement instantiation and saving of tiny hyperlinks

--- platform-dao/src/main/java/ua/com/fielden/platform/tiny/TinyHyperlinkDao.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/tiny/TinyHyperlinkDao.java
@@ -1,15 +1,62 @@
 package ua.com.fielden.platform.tiny;
 
+import jakarta.inject.Inject;
 import ua.com.fielden.platform.dao.CommonEntityDao;
+import ua.com.fielden.platform.dao.annotations.SessionRequired;
+import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.annotation.EntityType;
 import ua.com.fielden.platform.entity.fetch.IFetchProvider;
+import ua.com.fielden.platform.entity.functional.centre.CentreContextHolder;
+import ua.com.fielden.platform.entity.functional.centre.SavingInfoHolder;
+import ua.com.fielden.platform.serialisation.api.ISerialiser;
+import ua.com.fielden.platform.serialisation.api.SerialiserEngines;
+
+import java.util.Map;
+
+import static ua.com.fielden.platform.reflection.PropertyTypeDeterminator.baseEntityType;
 
 @EntityType(TinyHyperlink.class)
 public class TinyHyperlinkDao extends CommonEntityDao<TinyHyperlink> implements TinyHyperlinkCo {
 
+    private final ISerialiser serialiser;
+
+    @Inject
+    TinyHyperlinkDao(final ISerialiser serialiser) {
+        this.serialiser = serialiser;
+    }
+
     @Override
     protected IFetchProvider<TinyHyperlink> createFetchProvider() {
         return FETCH_PROVIDER;
+    }
+
+    @Override
+    @SessionRequired
+    public TinyHyperlink save(
+            final Class<? extends AbstractEntity<?>> entityType,
+            final Map<String, Object> modifiedProperties,
+            final CentreContextHolder centreContextHolder)
+    {
+        // TODO Perform a transformation.
+        //      `modifiedProperties` contains conventional property values, while `modifHolder` requires a special structure used in marshalling.
+        final var modifHolder = modifiedProperties;
+        final var savingInfoHolder = getEntityFactory().newEntity(SavingInfoHolder.class)
+                .setModifHolder(modifHolder)
+                .setCentreContextHolder(centreContextHolder);
+
+        return save(entityType, savingInfoHolder);
+    }
+
+    @Override
+    @SessionRequired
+    public TinyHyperlink save(final Class<? extends AbstractEntity<?>> entityType, final SavingInfoHolder savingInfoHolder) {
+        final var serialisedSavingInfoHolder = serialiser.serialise(savingInfoHolder, SerialiserEngines.JACKSON);
+        final var link = new_()
+                .setEntityTypeName(baseEntityType(entityType).getCanonicalName())
+                .setSavingInfoHolder(serialisedSavingInfoHolder)
+                .setUser(getUser())
+                .setCreatedDate(now().toDate());
+        return save(link);
     }
 
 }
--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/tiny/TinyHyperlinkCo.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/tiny/TinyHyperlinkCo.java
@@ -1,7 +1,12 @@
 package ua.com.fielden.platform.tiny;
 
 import ua.com.fielden.platform.dao.IEntityDao;
+import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.entity.fetch.IFetchProvider;
+import ua.com.fielden.platform.entity.functional.centre.CentreContextHolder;
+import ua.com.fielden.platform.entity.functional.centre.SavingInfoHolder;
+
+import java.util.Map;
 
 import static ua.com.fielden.platform.utils.EntityUtils.fetch;
 
@@ -12,5 +17,11 @@ public interface TinyHyperlinkCo extends IEntityDao<TinyHyperlink> {
             TinyHyperlink.CREATED_DATE,
             TinyHyperlink.ENTITY_TYPE_NAME,
             TinyHyperlink.SAVING_INFO_HOLDER);
+
+    TinyHyperlink save(Class<? extends AbstractEntity<?>> entityType,
+                       Map<String, Object> modifiedProperties,
+                       CentreContextHolder centreContextHolder);
+
+    TinyHyperlink save(Class<? extends AbstractEntity<?>> entityType, SavingInfoHolder savingInfoHolder);
 
 }
