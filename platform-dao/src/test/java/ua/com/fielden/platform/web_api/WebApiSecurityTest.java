78d37c5706ac0328cd84c7da2300b40f9aafbcf0
01es <oles.hodych@gmail.com>
2025-11-10 23:09:53 +1100

    #2517 Refactored WebApiSecurityTest to use 'populateDomain'.

--- platform-dao/src/test/java/ua/com/fielden/platform/web_api/WebApiSecurityTest.java
+++ platform-dao/src/test/java/ua/com/fielden/platform/web_api/WebApiSecurityTest.java
@@ -1,6 +1,10 @@
 package ua.com.fielden.platform.web_api;
 
 import org.junit.Test;
+import ua.com.fielden.platform.dao.QueryExecutionModel;
+import ua.com.fielden.platform.entity.query.fluent.fetch;
+import ua.com.fielden.platform.entity.query.model.EntityResultQueryModel;
+import ua.com.fielden.platform.entity.query.model.OrderingModel;
 import ua.com.fielden.platform.sample.domain.TgVehicleMake;
 import ua.com.fielden.platform.sample.domain.TgVehicleModel;
 import ua.com.fielden.platform.sample.domain.TgWebApiEntity;
@@ -14,10 +18,13 @@ import ua.com.fielden.platform.security.user.UserRole;
 import ua.com.fielden.platform.test_config.AbstractDaoTestCase;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
+import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.*;
 import static ua.com.fielden.platform.types.tuples.T2.t2;
 import static ua.com.fielden.platform.utils.CollectionUtil.*;
+import static ua.com.fielden.platform.utils.EntityUtils.fetch;
 import static ua.com.fielden.platform.web_api.WebApiUtils.*;
 
 /// Test case for GraphQL Web API reading / reading model security.
@@ -25,24 +32,8 @@ import static ua.com.fielden.platform.web_api.WebApiUtils.*;
 public class WebApiSecurityTest extends AbstractDaoTestCase {
     private final IWebApi webApi = getInstance(IWebApi.class);
 
-    private void createEntities() {
-        final TgVehicleMake merc = save(new_(TgVehicleMake.class, "MERC", "Mercedes"));
-        final TgVehicleModel model = save(new_(TgVehicleModel.class, "316", "316").setMake(merc));
-        save(new_(TgWebApiEntity.class, "VEH1", "veh1 desc").setModel(model));
-    }
-
-    private void removeAccessTo(final Class<? extends ISecurityToken> token) {
-        final SecurityRoleAssociationCo securityRoleAssociationCo = co(SecurityRoleAssociation.class);
-        securityRoleAssociationCo.removeAssociations(setOf(
-            securityRoleAssociationCo.new_()
-            .setRole(co(UserRole.class).findByKey(UNIT_TEST_ROLE))
-            .setSecurityToken(token)
-        ));
-    }
-
     @Test
     public void first_level_data_is_not_fetched_if_CanRead_access_is_prohibited() {
-        createEntities();
         removeAccessTo(TgVehicleModel_CanRead_Token.class);
 
         final Map<String, Object> result = webApi.execute(input("{tgVehicleModel{key}}"));
@@ -58,7 +49,6 @@ public class WebApiSecurityTest extends AbstractDaoTestCase {
 
     @Test
     public void data_on_other_levels_is_fetched_if_CanRead_access_is_prohibited_but_root_type_CanRead_access_is_allowed() {
-        createEntities();
         removeAccessTo(TgVehicleModel_CanRead_Token.class);
 
         final Map<String, Object> result = webApi.execute(input("{tgWebApiEntity{model{key}}}"));
@@ -77,7 +67,6 @@ public class WebApiSecurityTest extends AbstractDaoTestCase {
 
     @Test
     public void first_level_data_is_not_fetched_if_CanReadModel_access_is_prohibited() {
-        createEntities();
         removeAccessTo(TgVehicleModel_CanReadModel_Token.class);
 
         final Map<String, Object> result = webApi.execute(input("{tgVehicleModel{key}}"));
@@ -92,7 +81,6 @@ public class WebApiSecurityTest extends AbstractDaoTestCase {
 
     @Test
     public void data_on_other_levels_is_not_fetched_if_CanReadModel_access_is_prohibited_but_root_type_CanRead_access_is_allowed() {
-        createEntities();
         removeAccessTo(TgVehicleModel_CanReadModel_Token.class);
 
         final Map<String, Object> result = webApi.execute(input("{tgWebApiEntity{model{key}}}"));
@@ -107,11 +95,9 @@ public class WebApiSecurityTest extends AbstractDaoTestCase {
 
     @Test
     public void first_level_data_is_not_fetched_if_CanRead_property_access_is_prohibited() {
-        createEntities();
         removeAccessTo(TgVehicleModel_CanRead_make_Token.class);
 
         final Map<String, Object> result = webApi.execute(input("{tgVehicleModel{make{key}}}"));
-
         assertFalse(errors(result).isEmpty());
         assertEquals(listOf(linkedMapOf(
             t2("message", "Validation error (FieldUndefined@[tgVehicleModel/make]) : Field 'make' in type 'TgVehicleModel' is undefined"),
@@ -122,11 +108,9 @@ public class WebApiSecurityTest extends AbstractDaoTestCase {
 
     @Test
     public void data_on_other_levels_is_not_fetched_if_CanRead_property_access_is_prohibited_but_root_type_CanRead_access_is_allowed() {
-        createEntities();
         removeAccessTo(TgVehicleModel_CanRead_make_Token.class);
 
         final Map<String, Object> result = webApi.execute(input("{tgWebApiEntity{model{make{key}}}}"));
-
         assertFalse(errors(result).isEmpty());
         assertEquals(listOf(linkedMapOf(
             t2("message", "Validation error (FieldUndefined@[tgWebApiEntity/model/make]) : Field 'make' in type 'TgVehicleModel' is undefined"),
@@ -143,6 +127,30 @@ public class WebApiSecurityTest extends AbstractDaoTestCase {
     @Override
     public boolean useSavedDataPopulationScript() {
         return false;
+    }
+
+    @Override
+    protected void populateDomain() {
+        super.populateDomain();
+
+        if (useSavedDataPopulationScript()) {
+            return;
+        }
+
+        final TgVehicleMake merc = save(new_(TgVehicleMake.class, "MERC", "Mercedes"));
+        final TgVehicleModel model = save(new_(TgVehicleModel.class, "316", "316").setMake(merc));
+        save(new_(TgWebApiEntity.class, "VEH1", "veh1 desc").setModel(model));
+    }
+
+    /// This is more like just in case `token` would get associated with `UNIT_TEST_ROLE` by the base test class logic.
+    ///
+    private void removeAccessTo(final Class<? extends ISecurityToken> token) {
+        final SecurityRoleAssociationCo co = co(SecurityRoleAssociation.class);
+        co.removeAssociations(setOf(
+                co.new_()
+                        .setRole(co(UserRole.class).findByKey(UNIT_TEST_ROLE))
+                        .setSecurityToken(token)
+        ));
     }
 
 }
