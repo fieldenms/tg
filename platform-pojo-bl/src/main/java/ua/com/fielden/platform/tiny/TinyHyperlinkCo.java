0d99373eb1930733fe33247f79e2a985fda9076a
homedirectory <nullsink@posteo.eu>
2025-11-14 11:15:30 +0200

    #2422 Improve documentation of TinyHyperlinkCo

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/tiny/TinyHyperlinkCo.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/tiny/TinyHyperlinkCo.java
@@ -19,12 +19,34 @@ public interface TinyHyperlinkCo extends IEntityDao<TinyHyperlink> {
 
     IFetchProvider<TinyHyperlink> FETCH_PROVIDER = fetch(TinyHyperlink.class).with(HASH);
 
+    /// Creates and saves a tiny hyperlink that executes a shared action entity.
+    ///
+    /// @param entityType
+    ///        The entity type associated with the action’s master.
+    ///        For standard "open new" actions, this must be a persistent domain entity type (e.g., `Person`).
+    ///        For actions that open a master for a functional entity, this must be the functional entity type itself
+    ///        (e.g., `CopyWorkOrderAction`).
+    /// @param modifiedProperties
+    ///        A mapping of property names to values that will be assigned to the constructed instance of `entityType`.
+    ///        These assignments take effect as though the user had entered them manually.
+    /// @param centreContextHolder
+    ///        The context in which the action will be executed.
+    /// @param actionIdentifier
+    ///        The identifier of the `EntityActionConfig` to use when executing the action.
+    ///        The caller must ensure that this identifier corresponds to an existing action configuration.
+    ///
+    /// @return The saved tiny hyperlink refetched with a default fetch model.
+    ///
     TinyHyperlink save(
             Class<? extends AbstractEntity<?>> entityType,
             Map<String, Object> modifiedProperties,
             CentreContextHolder centreContextHolder,
             String actionIdentifier);
 
+    /// Overloads [#save(Class, Map, CentreContextHolder, String, Optional)].
+    ///
+    /// @param maybeFetch The fetch model for the returned tiny hyperlink.
+    ///
     Either<Long, TinyHyperlink> save(
             Class<? extends AbstractEntity<?>> entityType,
             Map<String, Object> modifiedProperties,
@@ -32,27 +54,37 @@ public interface TinyHyperlinkCo extends IEntityDao<TinyHyperlink> {
             String actionIdentifier,
             Optional<fetch<TinyHyperlink>> maybeFetch);
 
+    /// Creates and saves a tiny hyperlink that executes a shared action entity.
+    ///
+    /// This method is primarily for platform use.
+    ///
     TinyHyperlink save(
             Class<? extends AbstractEntity<?>> entityType,
             SavingInfoHolder savingInfoHolder,
             String actionIdentifier);
 
+    /// Overloads [#save(Class, SavingInfoHolder, String)].
+    ///
+    /// @param maybeFetch The fetch model for the returned tiny hyperlink.
+    ///
     Either<Long, TinyHyperlink> save(
             Class<? extends AbstractEntity<?>> entityType,
             SavingInfoHolder savingInfoHolder,
             String actionIdentifier,
             Optional<fetch<TinyHyperlink>> maybeFetch);
 
-    /// Saves a tiny hyperlink whose [TinyHyperlink#target] is `hyperlink`.
+    /// Creates and saves a tiny hyperlink whose [TinyHyperlink#target] is `hyperlink`.
     ///
     /// The returned tiny hyperlink is refetched with a default fetch model.
     ///
     TinyHyperlink saveWithTarget(Hyperlink hyperlink);
 
-    /// Saves a tiny hyperlink whose [TinyHyperlink#target] is `hyperlink`.
+    /// Creates and saves a tiny hyperlink whose [TinyHyperlink#target] is `hyperlink`.
     ///
     Either<Long, TinyHyperlink> saveWithTarget(Hyperlink hyperlink, Optional<fetch<TinyHyperlink>> maybeFetch);
 
+    /// Converts `tinyHyperlink` into a URL for the current application.
+    ///
     String toURL(TinyHyperlink tinyHyperlink);
 
     /// Computes a hash for `tinyHyperlink`.
