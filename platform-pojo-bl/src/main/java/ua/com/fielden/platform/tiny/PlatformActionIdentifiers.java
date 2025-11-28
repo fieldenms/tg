18db82c8e3d328c9aeb3ac15b05d982014d118c5
homedirectory <nullsink@posteo.eu>
2025-11-28 12:08:13 +0200

    #2109 Introduce an action identifiers enum

---
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/tiny/PlatformActionIdentifiers.java
@@ -0,0 +1,9 @@
+package ua.com.fielden.platform.tiny;
+
+/// Identifiers for actions provided by the platform.
+///
+/// To reduce the likelihood of name clashes with applications, all identifiers defined here start with `PLATFORM_`.
+///
+public enum PlatformActionIdentifiers implements IActionIdentifier {
+
+}
