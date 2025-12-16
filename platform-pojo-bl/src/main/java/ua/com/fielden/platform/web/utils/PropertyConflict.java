01d073133b20ec2713c476f460f9cbecd304edc8
homedirectory <nullsink@posteo.eu>
2025-12-16 13:56:16 +0200

    #2473 Disable stack traces for a few more Result subclasses

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/web/utils/PropertyConflict.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/web/utils/PropertyConflict.java
@@ -2,16 +2,14 @@ package ua.com.fielden.platform.web.utils;
 
 import ua.com.fielden.platform.error.Result;
 
-/**
- * A special kind of {@link Result} that represent property conflict, where another user has been changed the value and it is conflicting with currently edited value.
- * 
- * @author TG Team
- *
- */
+/// A special kind of [Result] that represent a property conflict, where a change to a property's value by another user
+/// has caused it to conflict with the value edited by the current user.
+///
 public class PropertyConflict extends Result {
     private static final long serialVersionUID = 1L;
     
     public PropertyConflict(final Object instance, final String reason) {
-        super(instance, new Exception(reason));
+        // The created Exception will capture a stack trace, hence no need to do the same for Result.
+        super(instance, new Exception(reason), false);
     }
 }
