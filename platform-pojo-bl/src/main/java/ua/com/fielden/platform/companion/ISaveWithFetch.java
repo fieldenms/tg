907cb06ab7ca3a3c89ea7f785f370bf03c7583c0
homedirectory <nullsink@posteo.eu>
2025-12-23 19:56:50 +0200

    #2612 Introduce ISaveWithFetch

---
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/companion/ISaveWithFetch.java
@@ -0,0 +1,28 @@
+package ua.com.fielden.platform.companion;
+
+import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.entity.query.fluent.fetch;
+import ua.com.fielden.platform.types.either.Either;
+
+import java.util.Optional;
+
+/// A contract to be implemented by companions that support _save-with-fetch_, which is a save operation with an explicit refetching strategy.
+///
+/// The platform will recognise companions that implement this interface, and may use that information for certain optimisations.
+///
+/// In the future, it is likely that the operations in this interface will be integrated into [IEntityActuator].
+///
+public interface ISaveWithFetch<T extends AbstractEntity<?>> {
+
+    /// Saves the entity and returns either its ID (left, iff `maybeFetch` is an empty optional)
+    /// or a refetched instance of the saved entity (right, `maybeFetch` is used for refetching).
+    ///
+    /// This method is the single extension point for custom saving logic.
+    /// Implementations must put all custom saving logic here and must not implement [IEntityActuator#save(AbstractEntity)],
+    /// to avoid multiple execution paths.
+    ///
+    /// This method should be used as an alternative to [IPersistentEntityMutator#quickSave(AbstractEntity)] by specifying an empty optional for `maybeFetch`.
+    ///
+    Either<Long, T> save(T entity, Optional<fetch<T>> maybeFetch);
+
+}
