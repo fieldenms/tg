e8f3d424cb91a1253ef5590c4b62eea7dd53603b
homedirectory <nullsink@posteo.eu>
2026-01-09 19:54:25 +0200

    #2617 EQL tests for concatOf

---
+++ platform-dao/src/test/java/ua/com/fielden/platform/eql/execution/functions/ConcatOfEqlFunctionTest.java
@@ -0,0 +1,60 @@
+package ua.com.fielden.platform.eql.execution.functions;
+
+import org.junit.Test;
+import ua.com.fielden.platform.eql.execution.AbstractEqlExecutionTestCase;
+import ua.com.fielden.platform.sample.domain.TgPersonName;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.select;
+
+public class ConcatOfEqlFunctionTest extends AbstractEqlExecutionTestCase {
+
+    @Test
+    public void concatOf_prop() {
+        final var qry = select(TgPersonName.class)
+                .yield().concatOf().prop("key").separator().val(", ").as(RESULT)
+                .modelAsAggregate();
+        // Cannot use orderBy because PostgreSQL doesn't like it.
+        assertThat(retrieveResult(qry))
+                .isIn("Alan Turing, John Conway", "John Conway, Alan Turing");
+    }
+
+    @Test
+    public void concatOf_prop_inside_a_function() {
+        final var qry = select(TgPersonName.class)
+                .yield().concatOf().upperCase().prop("key").separator().val(", ").as(RESULT)
+                .modelAsAggregate();
+        // Cannot use orderBy because PostgreSQL doesn't like it.
+        assertThat(retrieveResult(qry))
+                .isIn("ALAN TURING, JOHN CONWAY", "JOHN CONWAY, ALAN TURING");
+    }
+
+    @Test
+    public void concatOf_val() {
+        final var qry = select(TgPersonName.class)
+                .yield().concatOf().val("word").separator().val(", ").as(RESULT)
+                .modelAsAggregate();
+        assertThat(retrieveResult(qry))
+                .isEqualTo("word, word");
+    }
+
+    @Test
+    public void concatOf_separated_by_param() {
+        final var qry = select(TgPersonName.class)
+                .yield().concatOf().val("word").separator().param("sep").as(RESULT)
+                .modelAsAggregate();
+        assertThat(retrieveResult(qry, Map.of("sep", ":")))
+                .isEqualTo("word:word");
+    }
+
+    @Override
+    protected void populateDomain() {
+        super.populateDomain();
+
+        save(new_(TgPersonName.class, "John Conway"));
+        save(new_(TgPersonName.class, "Alan Turing"));
+    }
+
+}
