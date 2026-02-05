5d838d5d8e73bb4c869936a8310b2f1d7740995a
01es <oles.hodych@gmail.com>
2026-02-06 10:53:04 +1100

    #2349 JavaDoc improvements.

--- platform-annotations/src/main/java/ua/com/fielden/platform/annotations/appdomain/SkipEntityRegistration.java
+++ platform-annotations/src/main/java/ua/com/fielden/platform/annotations/appdomain/SkipEntityRegistration.java
@@ -6,7 +6,9 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
-/// Annotates an entity type to indicate that it should not be registered, i.e., should not be included in `ApplictionDomain` by the annotation processor.
+/// Annotates an entity type to indicate that it should not be registered as a domain entity.
+/// Such entities are not included in `ApplictionDomain` by the annotation processor.
+///
 @Retention(CLASS)
 @Target(TYPE)
 public @interface SkipEntityRegistration {
--- platform-annotations/src/main/java/ua/com/fielden/platform/annotations/metamodel/WithoutMetaModel.java
+++ platform-annotations/src/main/java/ua/com/fielden/platform/annotations/metamodel/WithoutMetaModel.java
@@ -5,11 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Annotates an entity type to indicate that its meta-model should <b>not</b> be generated.
- *
- * @author TG Team
- */
+/// Annotates an entity type to indicate that its meta-model should **not** be generated.
+///
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface WithoutMetaModel {}
