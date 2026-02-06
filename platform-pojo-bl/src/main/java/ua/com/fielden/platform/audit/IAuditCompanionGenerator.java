fcac0687a8be2174a630d7ed97230cc233d65f61
01es <oles.hodych@gmail.com>
2026-02-06 11:59:11 +1100

    #2349 Rationalised the companion generator implementation.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/audit/IAuditCompanionGenerator.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/audit/IAuditCompanionGenerator.java
@@ -2,33 +2,28 @@ package ua.com.fielden.platform.audit;
 
 import ua.com.fielden.platform.audit.exceptions.AuditingModeException;
 
-/**
- * This interface defines the generator of audit-entity companion object types.
- * <p>
- * Cannot be used if auditing is disabled, all methods will throw {@link AuditingModeException}.
- */
+/// This interface defines the generator of audit-entity companion object types.
+///
+/// Cannot be used if auditing is disabled, all methods will throw [AuditingModeException].
+///
 public interface IAuditCompanionGenerator {
 
-    /**
-     * Generates a companion object implementation for the specified audit-entity type.
-     */
-    Class<?> generateCompanion(Class<? extends AbstractAuditEntity> type);
+    /// Generates a companion object implementation for the specified audit-entity type.
+    ///
+    Class<?> generateCompanion(Class<? extends AbstractAuditEntity<?>> type);
 
-    /**
-     * Generates a companion object implementation for the specified audit-prop entity type.
-     */
-    Class<?> generateCompanionForAuditProp(Class<? extends AbstractAuditProp> type);
+    /// Generates a companion object implementation for the specified audit-prop entity type.
+    ///
+    Class<?> generateCompanionForAuditProp(Class<? extends AbstractAuditProp<?>> type);
 
-    /**
-     * Generates a companion object implementation for the specified synthetic audit-entity type.
-     * <p>
-     * The generated type will implement {@link ISynAuditEntityDao}.
-     */
-    Class<?> generateCompanionForSynAuditEntity(Class<? extends AbstractSynAuditEntity> type);
+    /// Generates a companion object implementation for the specified synthetic audit-entity type.
+    ///
+    /// The generated type will implement [ISynAuditEntityDao].
+    ///
+    Class<?> generateCompanionForSynAuditEntity(Class<? extends AbstractSynAuditEntity<?>> type);
 
-    /**
-     * Generates a companion object implementation for the specified synthetic audit-prop entity type.
-     */
-    Class<?> generateCompanionForSynAuditProp(Class<? extends AbstractSynAuditProp> type);
+    /// Generates a companion object implementation for the specified synthetic audit-prop entity type.
+    ///
+    Class<?> generateCompanionForSynAuditProp(Class<? extends AbstractSynAuditProp<?>> type);
 
 }
--- platform-pojo-bl/src/test/java/ua/com/fielden/platform/test/CompanionGeneratorStub.java
+++ platform-pojo-bl/src/test/java/ua/com/fielden/platform/test/CompanionGeneratorStub.java
@@ -4,20 +4,17 @@ import jakarta.inject.Singleton;
 import ua.com.fielden.platform.companion.ICompanionGenerator;
 import ua.com.fielden.platform.entity.AbstractEntity;
 
-/**
- * Stub implementation of entity companion generation for tests in the {@code platform-pojo-bl} module.
- * It is needed to satisfy the dependent components, such as entity companion finder implementation.
- * <p>
- * This implementation always throws an exception, as the generation of companions can only be implemented in the
- * {@code platform-dao} module.
- * Therefore, if this facility is required in tests, the tests should be put in the {@code platform-dao} module and the
- * IoC module that binds the real implementation should be used.
- */
+/// Stub implementation of entity companion generation for tests in the `platform-pojo-bl` module.
+/// It is needed to satisfy the dependent components, such as entity companion finder implementation.
+///
+/// This implementation always throws an exception, as the generation of companions can only be implemented in the `platform-dao` module.
+/// Therefore, if this facility is required in tests, the tests should be put in the `platform-dao` module and the IoC module that binds the real implementation should be used.
+///
 @Singleton
 public final class CompanionGeneratorStub implements ICompanionGenerator {
 
     @Override
-    public Class<?> generateCompanion(final Class<? extends AbstractEntity> type) {
+    public Class<?> generateCompanion(final Class<? extends AbstractEntity<?>> type) {
         throw new UnsupportedOperationException();
     }
 
