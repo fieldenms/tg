b454215c7ba0edca62932257eef9ecf420a030a2
01es <oles.hodych@gmail.com>
2026-02-11 22:04:50 +1100

    #2349 Documentation improvements.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/audit/AbstractSynAuditEntity.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/audit/AbstractSynAuditEntity.java
@@ -10,14 +10,13 @@ import ua.com.fielden.platform.security.user.User;
 import java.util.Date;
 import java.util.Set;
 
-/**
- * Base type for synthetic audit-entity types.
- * <p>
- * It is expected that an audit-entity type declares key-member property {@link #AUDITED_ENTITY} that represents a reference to the audited entity.
- * This property cannot be declared in this base type due to a limitation on using type variables in property types.
- *
- * @param <E>  type of the audited entity
- */
+/// Base type for synthetic audit-entity types.
+///
+/// It is expected that an audit-entity type declares key-member property [#AUDITED_ENTITY] that represents a reference to the audited entity.
+/// This property cannot be declared in this base type due to a limitation on using type variables in property types.
+///
+/// @param <E>  type of the audited entity
+///
 @KeyType(DynamicEntityKey.class)
 public abstract class AbstractSynAuditEntity<E extends AbstractEntity<?>> extends AbstractEntity<DynamicEntityKey> {
 
@@ -28,49 +27,41 @@ public abstract class AbstractSynAuditEntity<E extends AbstractEntity<?>> extend
             AUDIT_USER = "auditUser",
             AUDITED_TRANSACTION_GUID = "auditedTransactionGuid";
 
-    /**
-     * Name of a property declared by subtypes.
-     * This property is a key-member representing a reference to the audited entity.
-     * Its type is the type of the audited entity.
-     * <p>
-     * For example, audit-entity {@code ReVehicle_a3t} is expected to declare property {@code auditedEntity} of type {@code Vehicle}.
-     */
+    /// Name of a property declared by subtypes.
+    /// This property is a key-member representing a reference to the audited entity.
+    /// Its type is the type of the audited entity.
+    ///
+    /// For example, audit-entity `ReVehicle_a3t` is expected to declare property `auditedEntity` of type `Vehicle`.
+    ///
     public static final String AUDITED_ENTITY = "auditedEntity";
 
-    /**
-     * The composite key order that must be specified by the {@link #AUDITED_ENTITY} property.
-     */
-    // Placing this property first ensures that its column comes first in the multi-column unique index that is always
-    // generated for composite keys, which improves performance because the first column in such an index is searchable.
+    /// The composite key order that must be specified by the [#AUDITED_ENTITY] property.
+    ///
+    /// Placing this property first ensures that its column comes first in the multi-column unique index that is always generated for composite keys
+    /// This improves performance because the first column in such an index is searchable.
+    ///
     static final int AUDITED_ENTITY_KEY_MEMBER_ORDER = 1;
 
-    /**
-     * Name of a property declared by subtypes.
-     * This property models the one-to-many relationship between this synthetic audit-entity and a {@linkplain AbstractSynAuditProp synthetic audit-prop} entity.
-     * Its type is a {@link Set} parameterised with a corresponding synthetic audit-prop type.
-     * <p>
-     * For example, synthetic audit-entity {@code ReVehicle_a3t} is expected to declare property {@code changedProps} of type {@code Set<ReVehicle_a3t_Prop>}.
-     */
+    /// Name of a property declared by subtypes.
+    /// This property models the one-2-many relationship between this synthetic audit-entity and an [AbstractSynAuditProp] entity.
+    /// Its type is a [Set] parameterised with a corresponding synthetic audit-prop type.
+    ///
+    /// For example, synthetic audit-entity `ReVehicle_a3t` is expected to declare property `changedProps` of type `Set<ReVehicle_a3t_Prop>`.
+    ///
     public static final String CHANGED_PROPS = "changedProps";
 
-    /**
-     * Name of a property declared by subtypes.
-     * This property is a criterion for searching by changed properties (i.e., by property {@link #CHANGED_PROPS}).
-     * Its type is a {@link PropertyDescriptor} parameterised with the synthetic audit-entity type itself.
-     * <p>
-     * For example, synthetic audit-entity {@code ReVehicle_a3t} is expected to declare property {@code changedPropsCrit}
-     * of type {@code PropertyDescriptor<ReVehicle_a3t>}.
-     */
+    /// Name of a property declared by subtypes.
+    /// This property is a criterion for searching by changed properties (i.e., by property [#CHANGED_PROPS]).
+    /// Its type is a [PropertyDescriptor] parameterised with the synthetic audit-entity type itself.
+    ///
+    /// For example, synthetic audit-entity `ReVehicle_a3t` is expected to declare property `changedPropsCrit`
+    /// of type `PropertyDescriptor<ReVehicle_a3t>`.
+    ///
     public static final String CHANGED_PROPS_CRIT = "changedPropsCrit";
 
     public static final Set<String> BASE_PROPERTIES = ImmutableSet.of(
             AUDITED_ENTITY, AUDITED_VERSION, AUDIT_DATE, AUDIT_USER, AUDITED_TRANSACTION_GUID
     );
-
-    /**
-     * Getter for property {@link #AUDITED_ENTITY}.
-     */
-    public abstract E getAuditedEntity();
 
     @IsProperty
     @Title(value = "Version", desc = "Version of the entity for which this audit record was created.")
@@ -93,18 +84,20 @@ public abstract class AbstractSynAuditEntity<E extends AbstractEntity<?>> extend
     @DenyIntrospection
     private String auditedTransactionGuid;
 
-    /**
-     * Getter for property {@link #CHANGED_PROPS}.
-     */
+    /// Getter for property [#AUDITED_ENTITY].
+    ///
+    public abstract E getAuditedEntity();
+
+    /// Getter for property [#CHANGED_PROPS].
+    ///
     public abstract Set<? extends AbstractSynAuditProp<E>> getChangedProps();
 
-    /**
-     * Dynamic getter for accessing values of audited properties.
-     * Given the name of an audited property as declared in the audited entity, this method accesses the value of a corresponding property in this audit-entity.
-     * It is an error if the specified property is not audited by this audit-entity.
-     *
-     * @param property  simple property name
-     */
+    /// Dynamic getter for accessing values of audited properties.
+    /// Given the name of an audited property as declared in the audited entity, this method accesses the value of a corresponding property in this audit-entity.
+    /// It is an error if the specified property is not audited by this audit-entity.
+    ///
+    /// @param property  simple property name
+    ///
     public final <T> T getA3t(final CharSequence property) {
         return get(AuditUtils.auditPropertyName(property));
     }
