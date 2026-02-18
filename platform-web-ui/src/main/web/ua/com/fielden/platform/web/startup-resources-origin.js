a2fbf88756f95cc927fd75b52ed579224c5a0d6a
01es <oles.hodych@gmail.com>
2026-02-18 21:22:11 +1100

    #2567 Rearranged imports for clarity of the documentation.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/startup-resources-origin.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/startup-resources-origin.js
@@ -1,11 +1,12 @@
 /* main-app related resources */
-// this import is required to ensure the SSE initialisation upon loading of a web client
+// This import is required to ensure the SSE initialisation upon loading of a web client
 import '/resources/components/tg-event-source.js';
+// Load styles.
 import '/resources/polymer/@polymer/paper-styles/paper-styles.js';
-// IMPORTANT: The following import for rich-text styles must always occur
-// before any other component import. This prevents early style processing
-// and caching by the `dom-bind` element.
 import '/resources/polymer/@polymer/iron-flex-layout/iron-flex-layout-classes.js';
+// Load components.
+// IMPORTANT: The following import for rich-text styles must always occur before any other component import.
+//            This prevents early style processing and caching by the `dom-bind` element.
 import '/resources/components/rich-text/tg-rich-text-styles.js';
 import '/resources/images/tg-reference-hierarchy.js';
 import '/resources/components/tg-qr-code-scanner.js';
