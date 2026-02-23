c8cf993272487e59c37d0d9100831fad23c7bab1
Oleh Maikovych <oleh@fielden.com.au>
2026-02-23 18:45:48 +0200

    #2647 Added example to test custom can leave for master wwith master.

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/compound/master/menu/actions/TgCompoundEntityMaster_OpenTgCompoundEntityDetail_MenuItem.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/compound/master/menu/actions/TgCompoundEntityMaster_OpenTgCompoundEntityDetail_MenuItem.java
@@ -1,9 +1,9 @@
 package ua.com.fielden.platform.sample.domain.compound.master.menu.actions;
 
 import ua.com.fielden.platform.entity.AbstractFunctionalEntityForCompoundMenuItem;
-import ua.com.fielden.platform.entity.annotation.CompanionObject;
-import ua.com.fielden.platform.entity.annotation.EntityTitle;
-import ua.com.fielden.platform.entity.annotation.KeyType;
+import ua.com.fielden.platform.entity.CanLeaveOptions;
+import ua.com.fielden.platform.entity.ICustomisableCanLeave;
+import ua.com.fielden.platform.entity.annotation.*;
 import ua.com.fielden.platform.reflection.TitlesDescsGetter;
 import ua.com.fielden.platform.sample.domain.compound.TgCompoundEntity;
 import ua.com.fielden.platform.utils.Pair;
@@ -17,10 +17,85 @@ import ua.com.fielden.platform.utils.Pair;
 @KeyType(TgCompoundEntity.class)
 @CompanionObject(ITgCompoundEntityMaster_OpenTgCompoundEntityDetail_MenuItem.class)
 @EntityTitle("Tg Compound Entity Master Tg Compound Entity Detail Menu Item")
-public class TgCompoundEntityMaster_OpenTgCompoundEntityDetail_MenuItem extends AbstractFunctionalEntityForCompoundMenuItem<TgCompoundEntity> {
+public class TgCompoundEntityMaster_OpenTgCompoundEntityDetail_MenuItem extends AbstractFunctionalEntityForCompoundMenuItem<TgCompoundEntity> implements ICustomisableCanLeave {
 
     private static final Pair<String, String> entityTitleAndDesc = TitlesDescsGetter.getEntityTitleAndDesc(TgCompoundEntityMaster_OpenTgCompoundEntityDetail_MenuItem.class);
     public static final String ENTITY_TITLE = entityTitleAndDesc.getKey();
     public static final String ENTITY_DESC = entityTitleAndDesc.getValue();
+
+    @IsProperty
+    @Title("Can Leave")
+    private boolean canLeave;
+
+    @IsProperty
+    @Title("Can not Leave Reason")
+    private String cannotLeaveReason;
+
+    @IsProperty
+    @Title("Can Leave Options")
+    private CanLeaveOptions canLeaveOptions;
+
+    @IsProperty
+    @Title(value = "Is Closing?", desc = "Indicates whether this menu item is closing")
+    private boolean closing;
+
+    @IsProperty
+    @Title("Close Instructions")
+    private String closeInstructions;
+
+    @Override
+    public String getCloseInstructions() {
+        return closeInstructions;
+    }
+
+    @Observable
+    public TgCompoundEntityMaster_OpenTgCompoundEntityDetail_MenuItem setCloseInstructions(final String closeInstructions) {
+        this.closeInstructions = closeInstructions;
+        return this;
+    }
+
+    @Override
+    public boolean isClosing() {
+        return closing;
+    }
+
+    @Observable
+    public TgCompoundEntityMaster_OpenTgCompoundEntityDetail_MenuItem setClosing(final boolean closing) {
+        this.closing = closing;
+        return this;
+    }
+
+    @Override
+    public CanLeaveOptions getCanLeaveOptions() {
+        return canLeaveOptions;
+    }
+
+    @Observable
+    public TgCompoundEntityMaster_OpenTgCompoundEntityDetail_MenuItem setCanLeaveOptions(final CanLeaveOptions canLeaveOptions) {
+        this.canLeaveOptions = canLeaveOptions;
+        return this;
+    }
+
+    @Override
+    public String getCannotLeaveReason() {
+        return cannotLeaveReason;
+    }
+
+    @Observable
+    public TgCompoundEntityMaster_OpenTgCompoundEntityDetail_MenuItem setCannotLeaveReason(final String cannotLeaveReason) {
+        this.cannotLeaveReason = cannotLeaveReason;
+        return this;
+    }
+
+    @Override
+    public boolean isCanLeave() {
+        return canLeave;
+    }
+
+    @Observable
+    public TgCompoundEntityMaster_OpenTgCompoundEntityDetail_MenuItem setCanLeave(final boolean canLeave) {
+        this.canLeave = canLeave;
+        return this;
+    }
 
 }
