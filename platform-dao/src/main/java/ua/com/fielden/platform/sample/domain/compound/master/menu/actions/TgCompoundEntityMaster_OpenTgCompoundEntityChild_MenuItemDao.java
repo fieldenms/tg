8663dbf06664994bcabe04e489c2133974a56ecd
Oleh Maikovych <oleh@fielden.com.au>
2026-02-24 19:43:19 +0200

    #2647 Adjusted compound master menu item can leave logic to make web unit test work and at the same time to test remote canLeave logic.

--- platform-dao/src/main/java/ua/com/fielden/platform/sample/domain/compound/master/menu/actions/TgCompoundEntityMaster_OpenTgCompoundEntityChild_MenuItemDao.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/sample/domain/compound/master/menu/actions/TgCompoundEntityMaster_OpenTgCompoundEntityChild_MenuItemDao.java
@@ -3,15 +3,10 @@ package ua.com.fielden.platform.sample.domain.compound.master.menu.actions;
 import com.google.inject.Inject;
 import ua.com.fielden.platform.dao.CommonEntityDao;
 import ua.com.fielden.platform.dao.annotations.SessionRequired;
-import ua.com.fielden.platform.entity.CanLeaveOptions;
 import ua.com.fielden.platform.entity.annotation.EntityType;
-import ua.com.fielden.platform.entity.query.model.EntityResultQueryModel;
 import ua.com.fielden.platform.error.Result;
-import ua.com.fielden.platform.sample.domain.compound.TgCompoundEntityChild;
 import ua.com.fielden.platform.security.IAuthorisationModel;
 import ua.com.fielden.platform.security.tokens.compound_master_menu.TgCompoundEntityMaster_OpenTgCompoundEntityChild_MenuItem_CanAccess_Token;
-
-import static ua.com.fielden.platform.entity.query.fluent.EntityQueryUtils.select;
 
 /** 
  * DAO implementation for companion object {@link ITgCompoundEntityMaster_OpenTgCompoundEntityChild_MenuItem}.
@@ -32,24 +27,8 @@ public class TgCompoundEntityMaster_OpenTgCompoundEntityChild_MenuItemDao extend
     @Override
     @SessionRequired
     public TgCompoundEntityMaster_OpenTgCompoundEntityChild_MenuItem save(final TgCompoundEntityMaster_OpenTgCompoundEntityChild_MenuItem entity) {
-        final Result authorisationResult = authModel.authorise(TgCompoundEntityMaster_OpenTgCompoundEntityChild_MenuItem_CanAccess_Token.class);
-        if (entity.isClosing()) {
-            if (!authorisationResult.isSuccessful()) {
-                entity.setCanLeave(true);
-            } else {
-                final EntityResultQueryModel<TgCompoundEntityChild> query = select(TgCompoundEntityChild.class)
-                        .where().prop("tgCompoundEntity").eq().val(entity.getKey()).model();
-                boolean childExist = co(TgCompoundEntityChild.class).exists(query);
-                entity.setCanLeave(!childExist);
-                if (childExist) {
-                    entity.setCannotLeaveReason("There are not completed items. Would you like to close this master?");
-                    entity.setCloseInstructions("Please complete all tasks.");
-                    entity.setCanLeaveOptions(CanLeaveOptions.Options.YES_NO.getCanLeaveOptions());
-                }
-            }
-        } else if (!authorisationResult.isSuccessful()) {
-            throw authorisationResult;
-        }
+        authModel.authorise(TgCompoundEntityMaster_OpenTgCompoundEntityChild_MenuItem_CanAccess_Token.class).ifFailure(Result::throwRuntime);
+        entity.setCanLeave(true);
         return super.save(entity);
     }
 
