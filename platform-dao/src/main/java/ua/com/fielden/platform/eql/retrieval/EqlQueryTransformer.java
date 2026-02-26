1506547da61abaf7366fb72b46b52885271995d2
homedirectory <nullsink@posteo.eu>
2026-02-26 19:49:19 +0200

    #2651 Remove ListTokenSource

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/retrieval/EqlQueryTransformer.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/retrieval/EqlQueryTransformer.java
@@ -38,7 +38,7 @@ import static ua.com.fielden.platform.utils.EntityUtils.isPersistentEntityType;
 /// The transformation of EQL into SQL happens in 4 stages:
 ///
 /// 1. **Stage 0: parsing**.
-///    A [sequence of EQL tokens][ua.com.fielden.platform.eql.antlr.tokens.util.ListTokenSource] is transformed into a [stage 1 AST][ResultQuery1].
+///    A sequence of EQL tokens is transformed into a [stage 1 AST][ResultQuery1].
 ///    See [ua.com.fielden.platform.eql.antlr.EqlCompiler].
 /// 2. **Stage 1: property resolution**.
 ///    Properties are resolved to their respective sources.
