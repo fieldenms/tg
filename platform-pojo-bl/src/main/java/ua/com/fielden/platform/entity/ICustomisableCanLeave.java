4c3a19cc240c83dad5ba2c125da7a75f37836f9a
homedirectory <nullsink@posteo.eu>
2026-03-02 15:59:51 +0200

    #2647 Improve documentation of ICustomisableCanLeave

--- platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/ICustomisableCanLeave.java
+++ platform-pojo-bl/src/main/java/ua/com/fielden/platform/entity/ICustomisableCanLeave.java
@@ -2,38 +2,77 @@ package ua.com.fielden.platform.entity;
 
 import java.util.Optional;
 
-/// A contract to be implemented by an entity.
+/// A contract that enables an entity to define custom leave behaviour for an entity master.
 ///
-/// This contract indicates that the `canLeave` method, although invoked on the
-/// client side, is executed on the server. The corresponding business logic
-/// must be implemented in the `save` method of the associated companion object.
+/// ### Compound master
+///
+/// For compound masters, custom leave behaviour is defined at the level of menu items.
+/// This interface should be implemented by a [menu item][AbstractFunctionalEntityForCompoundMenuItem] entity type directly
+/// or by extending [AbstractFunctionalEntityForCompoundMenuItemWithCustomCanLeave].
+///
+/// The actual leave behaviour should be implemented in the `save` method of the companion object associated with the menu item entity.
+/// The system will save a menu item entity when the menu item is opened and when it is left (see [LeaveReason]).
+/// To differentiate between these cases, method [#leaveReason()] should be used.
+///
+/// ### Simple master
+///
+/// For simple masters, custom leave behaviour is not supported at present.
+///
+/// ### Implementation notes
+///
+/// If this interface is implemented directly, the entity type must declare the necessary properties, as documented
+/// in the methods of this interface.
 ///
 public interface ICustomisableCanLeave {
 
-    /// Indicates whether the master view for this entity can be exited.
+    /// Indicates whether the leave action that initiated the saving of the entity can be performed.
+    ///
+    /// This method must be defined as an accessor for property `canLeave: boolean`.
+    ///
+    /// If this method returns false, it is required that these methods also return a value:
+    /// * [#getCannotLeaveReason()]
+    /// * [#canLeaveOptions()]
+    /// * [#getCloseInstructions()]
     ///
     boolean isCanLeave();
 
-    /// Provides the reason why the master view for this entity cannot be exited.
+    /// Returns the reason the leave action cannot be performed.
+    ///
+    /// This method must be defined as an accessor for property `cannotLeaveReason: String`.
     ///
     String getCannotLeaveReason();
 
-    /// Returns the set of buttons to be displayed in the confirmation dialog
-    /// during the execution of the `canLeave` method.
+    /// Returns a configuration for a confirmation dialog that will displayed if the leave action cannot be performed.
     ///
-    /// @return An Optional containing the CanLeaveOptions if confirmation is required;
-    ///         otherwise, an empty Optional.
+    /// This method must be implemented as an extra accessor for property `canLeaveOptions: String` that must store
+    /// the [name][CanLeaveOptions#name()] of the enum instance.
+    /// This extra effort is required because the platform does not support enums as property types at present.
+    ///
+    /// @return An optional containing the options if confirmation is required.
+    ///         Otherwise, an empty optional.
     ///
     Optional<CanLeaveOptions> canLeaveOptions();
 
-    /// Returns the reason explaining why the master view for this entity is about to be exited.
+    /// Returns the reason the leave action was initiated.
+    /// This method should be used in application business logic to define custom leave behaviour.
+    /// The returned optional will be present iff a leave action was initiated.
     ///
-    /// @return An Optional containing the leave reason if one exists; otherwise, an empty Optional.
+    /// This method must be implemented as an extra accessor for property `leaveReason: String` that must store
+    /// the [name][LeaveReason#name()] of the enum instance.
+    /// This extra effort is required because the platform does not support enums as property types at present.
+    ///
+    /// The underlying property is read-only from the application's perspective.
+    /// The platform handles its assignment when a leave action is initiated.
+    ///
+    /// @return An optional containing the leave reason if a leave action was initiated.
+    ///         Otherwise, an empty optional.
     ///
     Optional<LeaveReason> leaveReason();
 
-    /// Returns the message displayed after the user rejects leaving the master view for this entity.
-    /// The message explains what the user can do to close the master view without confirmation.
+    /// Returns a message to be displayed after the user rejects leaving in the confirmation dialog.
+    /// This message should explain what the user can do to leave without confirmation.
+    ///
+    /// This method must be defined as an accessor for property `closeInstructions: String`.
     ///
     String getCloseInstructions();
 }
