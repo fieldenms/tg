f98a040dddf068fba59eb0f439ec4977a42c32bd
homedirectory <nullsink@posteo.eu>
2026-03-06 20:28:45 +0200

    #2675 A test for an explicitly declared expression for calculated Money.currency

--- platform-dao/src/main/java/ua/com/fielden/platform/sample/domain/TgFuelUsageDao.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/sample/domain/TgFuelUsageDao.java
@@ -2,24 +2,14 @@ package ua.com.fielden.platform.sample.domain;
 
 import ua.com.fielden.platform.dao.CommonEntityDao;
 import ua.com.fielden.platform.entity.annotation.EntityType;
-import ua.com.fielden.platform.entity.query.IFilter;
-import ua.com.fielden.platform.sample.domain.ITgFuelUsage;
-import ua.com.fielden.platform.sample.domain.TgFuelUsage;
+import ua.com.fielden.platform.entity.fetch.IFetchProvider;
 
-import com.google.inject.Inject;
-
-/**
- * DAO for retrieving TgTimesheets.
- * 
- * @author TG Team
- * 
- */
 @EntityType(TgFuelUsage.class)
 public class TgFuelUsageDao extends CommonEntityDao<TgFuelUsage> implements ITgFuelUsage {
 
-    @Inject
-    protected TgFuelUsageDao(final IFilter filter) {
-        super(filter);
+    @Override
+    protected IFetchProvider<TgFuelUsage> createFetchProvider() {
+        return FETCH_PROVIDER;
     }
 
 }
