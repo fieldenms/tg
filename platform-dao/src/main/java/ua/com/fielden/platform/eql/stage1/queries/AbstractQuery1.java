809636d3a11d224f00ce48bc5d866e6bb7a2f6b8
homedirectory <nullsink@posteo.eu>
2026-03-13 16:29:51 +0200

    #2675 Expand Money.currency if present in a key member for groupBy and orderBy

--- platform-dao/src/main/java/ua/com/fielden/platform/eql/stage1/queries/AbstractQuery1.java
+++ platform-dao/src/main/java/ua/com/fielden/platform/eql/stage1/queries/AbstractQuery1.java
@@ -20,6 +20,7 @@ import ua.com.fielden.platform.eql.stage2.sources.ISource2;
 import ua.com.fielden.platform.eql.stage2.sundries.*;
 import ua.com.fielden.platform.eql.stage3.sources.IJoinNode3;
 import ua.com.fielden.platform.eql.stage3.sources.ISource3;
+import ua.com.fielden.platform.meta.IDomainMetadata;
 import ua.com.fielden.platform.utils.ToString;
 
 import java.util.*;
@@ -117,8 +118,8 @@ public abstract class AbstractQuery1 implements ToString.IFormattable {
         final IJoinNode2<? extends IJoinNode3> joinRoot2 = joinRootTr.item;
         final Conditions2 whereConditions2 = enhanceWithUserDataFilterConditions(joinRoot2.mainSource(), context, whereConditions.transform(enhancedContext));
         final Yields2 yields2 = yields.transform(enhancedContext, this);
-        final GroupBys2 groups2 = enhanceGroupBys(groups.transform(enhancedContext));
-        final OrderBys2 orderings2 = enhanceOrderBys(orderings.transform(enhancedContext), yields2, joinRoot2.mainSource());
+        final GroupBys2 groups2 = enhanceGroupBys(groups.transform(enhancedContext), context.domainMetadata);
+        final OrderBys2 orderings2 = enhanceOrderBys(orderings.transform(enhancedContext), yields2, joinRoot2.mainSource(), context.domainMetadata);
         // it is important to enhance yields after orderings to enable functioning of 'orderBy().yield(..)' in application to properties rather than true yields
         final Yields2 enhancedYields2 = enhanceYields(yields2, joinRoot2.mainSource()).yields;
         return new QueryComponents2(Optional.of(joinRoot2), whereConditions2, enhancedYields2, groups2, orderings2);
@@ -165,15 +166,20 @@ public abstract class AbstractQuery1 implements ToString.IFormattable {
         }
     }
 
-    protected static GroupBys2 enhanceGroupBys(final GroupBys2 groupBys) {
+    protected static GroupBys2 enhanceGroupBys(final GroupBys2 groupBys, final IDomainMetadata domainMetadata) {
         if (groupBys.equals(EMPTY_GROUP_BYS)) {
             return EMPTY_GROUP_BYS;
         }
 
-        return new GroupBys2(groupBys.groups().stream().flatMap(AbstractQuery1::enhanceGroupBy).collect(toImmutableList()));
+        return new GroupBys2(groupBys.groups().stream().flatMap(groupBy -> enhanceGroupBy(groupBy, domainMetadata)).collect(toImmutableList()));
     }
 
-    protected static OrderBys2 enhanceOrderBys(final OrderBys2 orderBys, final Yields2 yields, final ISource2<? extends ISource3> mainSource) {
+    protected static OrderBys2 enhanceOrderBys(
+            final OrderBys2 orderBys,
+            final Yields2 yields,
+            final ISource2<? extends ISource3> mainSource,
+            final IDomainMetadata domainMetadata)
+    {
         if (orderBys.equals(EMPTY_ORDER_BYS)) {
             return EMPTY_ORDER_BYS;
         }
@@ -181,18 +187,23 @@ public abstract class AbstractQuery1 implements ToString.IFormattable {
         final var enhanced = orderBys.orderBys()
                 .stream()
                 .flatMap(orderBy -> orderBy.operand() != null
-                        ? transformOrderByOperand(orderBy.operand(), orderBy.isDesc())
-                        : transformOrderByYield(orderBy, yields, mainSource))
+                        ? transformOrderByOperand(orderBy.operand(), orderBy.isDesc(), domainMetadata)
+                        : transformOrderByYield(orderBy, yields, mainSource, domainMetadata))
                 .collect(toImmutableList());
         return orderBys.updateOrderBys(enhanced);
     }
 
-    private static Stream<OrderBy2> transformOrderByYield(final OrderBy2 orderBy, final Yields2 yields, final ISource2<? extends ISource3> mainSource) {
+    private static Stream<OrderBy2> transformOrderByYield(
+            final OrderBy2 orderBy,
+            final Yields2 yields,
+            final ISource2<? extends ISource3> mainSource,
+            final IDomainMetadata domainMetadata)
+    {
         final var yieldsMap = yields.getYieldsMap();
         if (yieldsMap.containsKey(orderBy.yieldName())) {
             final var yield = yieldsMap.get(orderBy.yieldName());
             if (yield.operand() instanceof Prop2 yieldedProp && isExpandable(yieldedProp.lastPart(), yieldedProp.penultPart())) {
-                return transformOrderByOperand(yieldedProp, orderBy.isDesc());
+                return transformOrderByOperand(yieldedProp, orderBy.isDesc(), domainMetadata);
             }
             else {
                 return Stream.of(orderBy);
@@ -202,21 +213,25 @@ public abstract class AbstractQuery1 implements ToString.IFormattable {
             final PropResolution propResolution = Prop1.resolvePropAgainstSource(mainSource, new Prop1(orderBy.yieldName(), false));
             if (propResolution != null) {
                 final List<AbstractQuerySourceItem<?>> path = enhancePath(propResolution.getPath());
-                return transformOrderByOperand(new Prop2(mainSource, path), orderBy.isDesc());
+                return transformOrderByOperand(new Prop2(mainSource, path), orderBy.isDesc(), domainMetadata);
             }
         }
         throw new EqlStage1ProcessingException(ERR_CANNOT_FIND_YIELD_FOR_ORDER_BY.formatted(orderBy.yieldName()));
     }
 
-    private static Stream<OrderBy2> transformOrderByOperand(final ISingleOperand2<?> operand, final boolean isDesc) {
+    private static Stream<OrderBy2> transformOrderByOperand(
+            final ISingleOperand2<?> operand,
+            final boolean isDesc,
+            final IDomainMetadata domainMetadata)
+    {
         return operand instanceof Prop2 prop
-                ? expand(prop).map(expProp -> new OrderBy2(expProp, isDesc))
+                ? expand(prop, domainMetadata).map(expProp -> new OrderBy2(expProp, isDesc))
                 : Stream.of(new OrderBy2(operand, isDesc));
     }
 
-    private static Stream<GroupBy2> enhanceGroupBy(final GroupBy2 groupBy) {
+    private static Stream<GroupBy2> enhanceGroupBy(final GroupBy2 groupBy, final IDomainMetadata domainMetadata) {
         return groupBy.operand() instanceof Prop2 prop
-                ? expand(prop).map(GroupBy2::new)
+                ? expand(prop, domainMetadata).map(GroupBy2::new)
                 : Stream.of(groupBy);
     }
 
