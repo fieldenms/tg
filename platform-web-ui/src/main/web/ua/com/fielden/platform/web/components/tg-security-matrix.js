cb1e7e263af1954baaf3cb79643c724cae656f51
Oleh Maikovych <oleh@fielden.com.au>
2026-04-01 16:15:58 +0300

    #2689 Removed redundant code.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/components/tg-security-matrix.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/components/tg-security-matrix.js
@@ -500,12 +500,6 @@ Polymer({
         return this._masterDom()._ajaxSaver();
     },
 
-    /**
-     * The core-ajax component for custom canLeave logic.
-     */
-    _canLeaveAjax: function () {
-        return this._masterDom()._canLeaveAjax();
-    },
 
     /**
      * The validator component.
