85afe76bd1b06822668289c0bd7577776cd28a55
jhou-pro <jhou.pro@gmail.com>
2026-04-06 22:23:57 +0300

    #2701 Update dev dependencies, only used for web testing.
    All other updated dependencies and transformations were carefully ignored, including from package-lock.json (manually).

---
+++ platform-web-ui/src/main/resources/polymer/lru-cache/dist/commonjs/diagnostics-channel.d.ts
@@ -0,0 +1,7 @@
+import { tracingChannel, channel } from 'node:diagnostics_channel';
+export { tracingChannel, channel };
+import type { TracingChannel, Channel } from 'node:diagnostics_channel';
+export type { TracingChannel, Channel };
+export declare const metrics: Channel<unknown>;
+export declare const tracing: TracingChannel<unknown>;
+//# sourceMappingURL=diagnostics-channel.d.ts.map
