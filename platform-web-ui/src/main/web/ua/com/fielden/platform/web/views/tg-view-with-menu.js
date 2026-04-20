3b7ce1e2e5946723f92073f07fbfaaec85184592
01es <oles.hodych@gmail.com>
2026-04-20 20:27:00 +1000

    #2708 Used optional chaining + documentation improvement.
    In this case calls to his._centreConfigInfo() are cheap, but otherwise there would be 3 identical, potentially expensive calls.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/views/tg-view-with-menu.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/views/tg-view-with-menu.js
@@ -654,10 +654,10 @@ Polymer({
         if (menuItem.key === decodeURIComponent(this.selectedModule)) {
             const parts = selectedSubmodule.substring(1).split('?');
             const selectedSubmodulePart = parts[0];
-            // In the case where `selectedSubmodule` where changed from some external source, then `configUuid` can be missing.
-            // This happens, e.g., when Entity Centre transition is made through F3 global search.
-            // Then `selectedSubmodule` === '/Work%20Orders', not '/Work%20Orders/8469d323-e3e5-4eec-8984-f848c466ddc7'.
-            const uuid = this._centreConfigInfo() && this._centreConfigInfo()[selectedSubmodulePart] && this._centreConfigInfo()[selectedSubmodulePart].configUuid;
+            // In the case where `selectedSubmodule` was changed from some external source, the `configUuid` can be missing.
+            // This happens, for example, when Entity Centre transition is made through F3 global search.
+            // In such cases `selectedSubmodule` === '/Work%20Orders', not '/Work%20Orders/8469d323-e3e5-4eec-8984-f848c466ddc7'.
+            const uuid = this._centreConfigInfo()?.[selectedSubmodulePart]?.configUuid;
             // If `uuid` is missing, but Entity Centre named configuration was loaded, let's adjust `selectedSubmodule` accordingly.
             // To narrow down the possible impact, don't adjust in the case where `?...` part is present.
             if (!parts[1] && uuid && !selectedSubmodulePart.includes('/' + uuid)) {
