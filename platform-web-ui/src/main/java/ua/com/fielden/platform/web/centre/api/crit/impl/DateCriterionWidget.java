4aa4eda51dbf3604610913b0a6bda7abac39d708
jhou-pro <jhou.pro@gmail.com>
2026-04-28 01:43:21 +0300

    #2716 Simplify date widgets generation by removing time-zone. The time-zone are now taken from entity type property on the client.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/crit/impl/DateCriterionWidget.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/crit/impl/DateCriterionWidget.java
@@ -1,39 +1,29 @@
 package ua.com.fielden.platform.web.centre.api.crit.impl;
 
-import java.util.Map;
-
 import ua.com.fielden.platform.entity.AbstractEntity;
 import ua.com.fielden.platform.serialisation.jackson.DefaultValueContract;
 import ua.com.fielden.platform.web.view.master.api.widgets.datetimepicker.impl.DateTimePickerWidget;
 
-/**
- * An implementation for date double-editor criterion.
- *
- * @author TG Team
- *
- */
+import java.util.Map;
+
+/// An implementation for date double-editor criterion.
+///
 public class DateCriterionWidget extends AbstractRangeCriterionWidget {
 
-    /**
-     * Creates an instance of {@link DateCriterionWidget} for specified entity type and property name.
-     *
-     * @param criteriaType
-     * @param propertyName
-     */
+    /// Creates an instance of [DateCriterionWidget] for specified entity type and property name.
+    ///
     public DateCriterionWidget(final Class<? extends AbstractEntity<?>> root, final Class<?> managedType, final String propertyName) {
         super(root, "centre/criterion/multi/range/tg-date-range-criterion", propertyName,
                 new DateTimePickerWidget(
                         AbstractCriterionWidget.generateTitleDesc(root, managedType, propertyName).getKey(),
                         AbstractCriterionWidget.generateNames(root, managedType, propertyName).getKey(),
                         false,
-                        DefaultValueContract.getTimeZone(managedType, propertyName),
                         DefaultValueContract.getTimePortionToDisplay(managedType, propertyName)
                 ),
                 new DateTimePickerWidget(
                         AbstractCriterionWidget.generateTitleDesc(root, managedType, propertyName).getValue(),
                         AbstractCriterionWidget.generateNames(root, managedType, propertyName).getValue(),
                         true,
-                        DefaultValueContract.getTimeZone(managedType, propertyName),
                         DefaultValueContract.getTimePortionToDisplay(managedType, propertyName)
                 ));
     }
