a4190c78ca81c5a4755943447d9ecbe88a2c8c45
jhou-pro <jhou.pro@gmail.com>
2026-04-30 14:27:36 +0300

    #2716 Unify date value conversions in chart deckers.
    Date values convert to String representation. See end-application Lead Time Analysis reports, where horizontal axis has date values.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/components/chart-decker/tg-chart-decker-template-behavior.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/components/chart-decker/tg-chart-decker-template-behavior.js
@@ -75,13 +75,12 @@ const TgChartDeckerTemplateBehaviorImpl = {
         tearDownEvent(event);
     },
     
-    _datePropAccessor: function (propertyName, dateType) {
-        return function (entity, value) {
+    _datePropAccessor: function (propertyName) {
+        return (entity, value) => {
             if (!value) {
-                const splitedType = dateType.split(':');
-                return _millisDateRepresentation(entity.get(propertyName), entity.type().prop(propertyName), splitedType[1] || null);
+                return this._reflector().tg_toString(entity.get(propertyName), entity.type(), propertyName, { display: true });
             }
-        }
+        };
     },
 
     _moneyPropAccessor: function (propertyName) {
