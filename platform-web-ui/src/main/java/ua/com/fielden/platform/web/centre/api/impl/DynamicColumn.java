df97cdc2e8b2ec0da7dffe6de0896cdedf013a80
jhou-pro <jhou.pro@gmail.com>
2026-05-29 13:12:36 +0300

    #2023 Preliminary change that fixes minWidth functionality for dynamic columns.

--- platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/impl/DynamicColumn.java
+++ platform-web-ui/src/main/java/ua/com/fielden/platform/web/centre/api/impl/DynamicColumn.java
@@ -6,6 +6,7 @@ import ua.com.fielden.platform.web.centre.api.dynamic_columns.*;
 
 import java.util.Optional;
 
+import static java.lang.Math.min;
 import static java.util.Optional.ofNullable;
 import static ua.com.fielden.platform.web.centre.api.resultset.impl.PropertyColumnElement.DEFAULT_COLUMN_WIDTH;
 import static ua.com.fielden.platform.web.centre.api.resultset.impl.PropertyColumnElement.MIN_COLUMN_WIDTH;
@@ -38,7 +39,6 @@ public class DynamicColumn<T extends AbstractEntity<?>> implements IDynamicColum
     private String title;
     private Optional<String> desc = Optional.empty();
     private int width = DEFAULT_COLUMN_WIDTH;
-    private int minWidth = MIN_COLUMN_WIDTH;
     private int growFactor = 1;
     private boolean wordWrap;
 
@@ -68,7 +68,7 @@ public class DynamicColumn<T extends AbstractEntity<?>> implements IDynamicColum
 
     @Override
     public IDynamicColumnBuilderWordWrap minWidth(final int minWidth) {
-        this.minWidth = Math.min(minWidth, this.width);
+        this.width = minWidth;
         return this;
     }
 
@@ -100,7 +100,7 @@ public class DynamicColumn<T extends AbstractEntity<?>> implements IDynamicColum
     }
 
     public int getMinWidth() {
-        return minWidth;
+        return min(width, MIN_COLUMN_WIDTH);
     }
 
     public int getGrowFactor() {
