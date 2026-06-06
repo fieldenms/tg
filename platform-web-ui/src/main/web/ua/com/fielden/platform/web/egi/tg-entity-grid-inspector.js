ffb414f7a84d6f8b3edc08f9f645b14462d45200
jhou-pro <jhou.pro@gmail.com>
2026-06-06 15:56:50 +0300

    #2716 Correct column type condition for special tooltip logic.
    Column types are either 'Date::' or 'Date:someTimeZone:someTimePortion'.

--- platform-web-ui/src/main/web/ua/com/fielden/platform/web/egi/tg-entity-grid-inspector.js
+++ platform-web-ui/src/main/web/ua/com/fielden/platform/web/egi/tg-entity-grid-inspector.js
@@ -2232,7 +2232,7 @@ Polymer({
         } else if (column.type === 'RichText') {
             const value = this.getBindedValue(entity, column).toString();
             return value && (`<div class="toastui-editor-contents" style="overflow:hidden;padding:8px;border-radius:2px;">${value}</div>`);
-        } else if (column.type === 'Date' && this._reflector.tg_determinePropertyType(realEntity.type(), realProperty) === 'Date') {
+        } else if (column.type?.startsWith?.('Date') && this._reflector.tg_determinePropertyType(realEntity.type(), realProperty) === 'Date') {
             const valueToFormat = this.getValueFromEntity(entity, column);
             // Date columns augment the tooltip with the server time-zone representation (see @DependentTimeZoneMode).
             return this._reflector.tg_toString(valueToFormat, realEntity.type(), realProperty, { display: true, asTooltip: true });
